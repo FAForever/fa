4a89051caf562f35f42d1df737889621dc1df502
Supreme Commander@FunkOn.om.cox.net <Supreme Commander@FunkOn.om.cox.net>
2011-11-05 23:01:54 -0500

    Moritz's adding of Sorian AI tooltips

--- schook/lua/ui/help/tooltips.lua
+++ schook/lua/ui/help/tooltips.lua
@@ -127,4 +127,52 @@ do
         title = "<LOC lobui_0618>Large Preview",
         description = "<LOC lobui_0619>Click to show a large map preview showing all the mass and hydrocarbon spots",
     }
+	Tooltips.aitype_sorian = {
+        title = "<LOC sorian_0001>AI: Sorian",
+        description = "<LOC sorian_0002>A balanced AI. Good for 20x20+ maps.",
+    }
+	Tooltips.aitype_sorianrush = {
+        title = "<LOC sorian_0003>AI: Sorian Rush",
+        description = "<LOC sorian_0004>A rush AI. Good for 10x10 or smaller maps.",
+    }
+	Tooltips.aitype_sorianair = {
+        title = "<LOC sorian_0005>AI: Sorian Air",
+        description = "<LOC sorian_0006>An AI that focuses on air. Good for 20x20+ maps.",
+    }
+	Tooltips.aitype_sorianwater = {
+        title = "<LOC sorian_0007>AI: Sorian Water",
+        description = "<LOC sorian_0008>An AI that focuses on water. Good for 20x20+ water maps.",
+    }
+	Tooltips.aitype_sorianturtle = {
+        title = "<LOC sorian_0009>AI: Sorian Turtle",
+        description = "<LOC sorian_0010>A turtle AI. Good for 20x20+ maps.",
+    }
+	Tooltips.aitype_sorianadaptive = {
+        title = "<LOC sorian_0011>AI: Sorian Adaptive",
+        description = "<LOC sorian_0012>Chooses a Sorian AI based on the map. Use this if you can\'t decide on an AI.",
+    }
+	Tooltips.aitype_soriancheat = {
+        title = "<LOC sorian_0013>AIx: Sorian Cheat",
+        description = "<LOC sorian_0014>A cheating balanced AI. Good for 20x20+ maps.",
+    }
+	Tooltips.aitype_sorianrushcheat = {
+        title = "<LOC sorian_0015>AIx: Sorian Rush Cheat",
+        description = "<LOC sorian_0016>A cheating rush AI. Good for 10x10 or smaller maps.",
+    }
+	Tooltips.aitype_sorianaircheat = {
+        title = "<LOC sorian_0017>AIx: Sorian Air Cheat",
+        description = "<LOC sorian_0018>A cheating AI that focuses on air. Good for 20x20+ maps.",
+    }
+	Tooltips.aitype_sorianwatercheat = {
+        title = "<LOC sorian_0019>AIx: Sorian Water Cheat",
+        description = "<LOC sorian_0020>A cheating AI that focuses on water. Good for 20x20+ water maps.",
+    }
+	Tooltips.aitype_sorianturtlecheat = {
+        title = "<LOC sorian_0021>AIx: Sorian Turtle Cheat",
+        description = "<LOC sorian_0022>A cheating turtle AI. Good for 20x20+ maps.",
+    }
+	Tooltips.aitype_sorianadaptivecheat = {
+        title = "<LOC sorian_0023>AIx: Sorian Adaptive Cheat",
+        description = "<LOC sorian_0024>Chooses a Sorian cheating AI based on the map. Use this if you can\'t decide on an AI.",
+    }
 end
