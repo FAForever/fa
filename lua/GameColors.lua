6d56eea356a5a8b9ed0607b93a094cae0930bc40
Fernando@Phenom <Fernando@Phenom>
2012-03-29 21:55:24 -0300

    fix for infinite gamecolors.lua hooking, and changing colors for some better colors.

---
+++ lua/GameColors.lua
@@ -0,0 +1,45 @@
+GameColors = {
+    PlayerColors = {
+	"ffff3431",
+	"FF0066FF",
+	"FF009900",
+	"FFFF8800",
+ 	"ff7f807f",
+	"ff00ff00",
+	"ffff00ff",
+	"ffffff00",
+	"FFA75508",
+	"ff9161ff",
+	"FF66FFCC",
+	"FFFF88FF",
+	"FFFFFFCC",
+    },
+
+    ArmyColors = {
+	"ffff3431",
+	"FF0066FF",
+	"FF009900",
+	"FFFF8800",
+ 	"ff7f807f",
+	"ff00ff00",
+	"ffff00ff",
+	"ffffff00",
+	"FFA75508",
+	"ff9161ff",
+	"FF66FFCC",
+	"FFFF88FF",
+	"FFFFFFCC",
+    },
+
+    CivilianArmyColor = "BurlyWood",
+
+    TeamColorMode = {
+        Self = "RoyalBlue",
+        Enemy = "FFE80A0A",
+        Ally = "DarkGreen",
+        Neutral = "Goldenrod",
+    },
+
+    UnidentifiedColor = "FF808080",
+}
+
