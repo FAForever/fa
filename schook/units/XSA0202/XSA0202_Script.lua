47c7c990ae309d6194106283b13a27c6e311f98f
FunkOff@FunkOff-PC.om.cox.net <FunkOff@FunkOff-PC.om.cox.net>
2012-04-30 15:10:25 -0500

    Multi-drop fix (it is now impossible to drop a bomb from a bomber under 80% max speed) un-did rate of fire nerf for T1 bombers

--- schook/units/XSA0202/XSA0202_Script.lua
+++ schook/units/XSA0202/XSA0202_Script.lua
@@ -53,7 +53,36 @@ XSA0202 = Class(SAirUnit) {
                 SDFBombOtheWeapon.OnLostTarget(self)
                 local speedMulti = self.unit:GetSpeedModifier()   # [168]
                 self.unit:SetSpeedMult(speedMulti)
-            end,  	
+            end,  
+
+			CreateProjectileAtMuzzle = function(self, muzzle)
+				local proj = self:CreateProjectileForWeapon(muzzle)
+				proj.BombSpeedThread = proj:ForkThread(self.BombSpeedThread, self.unit:GetBlueprint().Air.MaxAirspeed, self)
+			end,
+			
+			BombSpeedThread = function(bomb, bomberMaxSpeed, bombWeapon)
+				#WARN ('BombSpeedThread started')
+				#WARN('Bombermaxspeed is ' .. repr(bomberMaxSpeed))
+				local minBombSpeed = 0.8 * (bomberMaxSpeed/10)
+				WaitTicks(1)
+				if not bomb:BeenDestroyed() then
+					local vx,vy,vz = bomb:GetVelocity()
+					local BombVelocity = {vx,vy,vz}
+					#WARN ('BombVelocity is ' .. repr(BombVelocity))
+					local BombSpeed = math.sqrt((vx*vx) + (vz*vz))
+					#WARN ('BombSpeed and minBombSpeed are ' .. repr(BombSpeed) .. ' and ' .. repr(minBombSpeed))
+					local bp = bombWeapon:GetBlueprint()
+					if BombSpeed < minBombSpeed then
+						bomb:Destroy()
+						LOG('bomb has been destroyed due to low velocity')
+					elseif bp.Audio.Fire then
+						bombWeapon:PlaySound(bp.Audio.Fire)
+					end
+				end	
+				KillThread(bomb.BombSpeedThread)
+			
+			end,
+			
         },
     },
 	
