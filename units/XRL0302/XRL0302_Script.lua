6986112b669e4d22ee9014f2e24d4d27b96c99ce
thepilot <none@none>
2013-03-19 09:22:15 +0100

    Changelog: - ASF: __Aeon ASF : HP = 1750 (from 2500), mass cost = 350 (from 400), wreck mulitplier = 0.45 (from 0.9) __Cybran ASF : HP = 1700 (from 2450),mass cost = 350 (from 400), wreck mulitplier = 0.45 (from 0.9) __UEF ASF : HP = 1850 (from 2600), mass cost = 350 (from 400),wreck mulitplier = 0.45 (from 0.9) __Sera ASF : HP = 1800 (from 2550), mass cost = 350 (from 400),wreck mulitplier = 0.45 (from 0.9)
    - SAMS (almost same changes for all): Health = 7000 HP (from 10500), DamageRadius = 1.5 (from 0); Muzzle Velocity = 45 (from 30, except Seraphim, remains at 100)
    
    - T3 artilleries = - 20% mass
    __Aeon Emissary (uab2302): 73200 (from 91500)
    __Cybran Disruptor (urb2302): 69600 (from 87000)
    __UEF Duke (ueb2302) : 72000 (from 90000)
    __Seraphim Hovatham  (xsb2302):70800 (from 88500)
    
    - Game Enders (except Sacthis) = -25% mass cost
    __Salvation (xab2307) : 202500 (from 270000)
    __Mavor (ueb2401): 224775 (from 299700)
    __Seraphim Yolona Oss (xsb2401) : 187650 (from 250200)
    
    Cybran Frigate : AA weapon damage = 4 (from 3) = 16 DPS (from 12), muzzlevelolicty = 60 (from 45)
    Cybran Destroyer AA weapon damage = 10 (from 2) = 60 DPS (from 12).
    
    --HG--
    branch : 3622

--- units/XRL0302/XRL0302_Script.lua
+++ units/XRL0302/XRL0302_Script.lua
@@ -1,42 +1,8 @@
-#****************************************************************************
-#**
-#**  File     :  /data/units/XRL0302/XRL0302_script.lua
-#**  Author(s):  Jessica St. Croix, Gordon Duclos
-#**
-#**  Summary  :  Cybran Mobile Bomb Script
-#**
-#**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
-local CWalkingLandUnit = import('/lua/cybranunits.lua').CWalkingLandUnit
-local CMobileKamikazeBombWeapon = import('/lua/cybranweapons.lua').CMobileKamikazeBombWeapon
-local CMobileKamikazeBombDeathWeapon = import('/lua/cybranweapons.lua').CMobileKamikazeBombDeathWeapon
-
-
-XRL0302 = Class(CWalkingLandUnit) {
-    Weapons = {
-
-        DeathWeapon = Class(CMobileKamikazeBombDeathWeapon) {},
-        
-        Suicide = Class(CMobileKamikazeBombWeapon) {   
-			OnFire = function(self)		
-				#disable death weapon
-				self.unit:SetDeathWeaponEnabled(false)
-				CMobileKamikazeBombWeapon.OnFire(self)
-			end,
-        },
-    },
-
-	OnProductionPaused = function(self)
+local CBP_oldXRL0302 = XRL0302
+XRL0302 = Class(CBP_oldXRL0302) {
+    OnProductionPaused = function(self)
         local wep = self:GetWeapon(1)
         wep.OnFire(wep)
     end,
-	
-	OnKilled = function(self, instigator, type, overkillRatio)
-        CWalkingLandUnit.OnKilled(self, instigator, type, overkillRatio)
-		if not instigator and self.DeathWeaponEnabled != false then
-			self:GetWeaponByLabel('Suicide'):FireWeapon()
-		end
-    end,
-	
 }
 TypeClass = XRL0302
