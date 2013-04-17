f05c0f60207ea540b86e96ea57b64e32cb48b9af
Xinnony <none@none>
2013-04-17 17:54:51 +0200

    Some change

---
+++ lua/ui/help/tooltips-country.lua
@@ -0,0 +1,29 @@
+-- The tooltip appears when you hover your mouse over the flag
+-- > Value : Flag file name (ex: us.dds)
+-- > Title : Title of Tooltip
+-- > Text : Text of Tooltip
+
+tooltip =
+{
+	{
+		value="fr",
+		title="Country",
+		text="French flag",
+	},
+}
+
+---
+
+tooltipSpecial =
+{
+	{
+		value="world",
+		title="Country",
+		text="Country not find ! (PM: Xinnony)",
+	},
+	{
+		value="xinnony",
+		title="Country",
+		text="Xinnony's flag !",
+	},
+}
