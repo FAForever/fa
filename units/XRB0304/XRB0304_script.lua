36e49646d9709acda57a5616e08c65624279109e
Ze PilOt@ZePilOt-PC <Ze PilOt@ZePilOt-PC>
2013-07-07 15:59:52 +0200

    Fix : Hive does not play deploy animation correctly.
    --HG--
    branch : 3626

---
+++ units/XRB0304/XRB0304_script.lua
@@ -0,0 +1,53 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/XRB0304/XRB0304_script.lua
+#**  Author(s):  Dru Staltman, Gordon Duclos
+#**
+#**  Summary  :  Cybran Engineering tower
+#**
+#**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+local CConstructionStructureUnit = import('/lua/cybranunits.lua').CConstructionStructureUnit
+
+XRB0304 = Class(CConstructionStructureUnit) 
+{
+    OnStartBeingBuilt = function(self, builder, layer)
+        CConstructionStructureUnit.OnStartBeingBuilt(self, builder, layer)
+        self:HideBone('xrb0304', true)
+        self:ShowBone('TurretT3', true)
+        self:ShowBone('Door3_B03', true)
+        self:ShowBone('B03', true)
+        self:ShowBone('Attachpoint03', true)
+    end,   
+    
+    OnStartBuild = function(self, unitBeingBuilt, order)
+        self:ForkThread(self.OpenState, unitBeingBuilt, order )
+    end,
+    
+    OnStopBuild = function(self, unitBeingBuilt)
+        self:ForkThread(self.CloseState, unitBeingBuilt)        
+    end,
+
+    OpenState = function(self, unitBeingBuilt, order)
+            if not self.AnimationManipulator then
+                self.AnimationManipulator = CreateAnimator(self)
+                self.Trash:Add(self.AnimationManipulator)
+            end
+            self.AnimationManipulator:PlayAnim(self:GetBlueprint().Display.AnimationOpen, false):SetRate(1)
+            WaitFor(self.AnimationManipulator)
+            CConstructionStructureUnit.OnStartBuild(self, unitBeingBuilt, order)
+        end,
+
+    CloseState = function(self, unitBeingBuilt)
+            if not self.AnimationManipulator then
+                self.AnimationManipulator = CreateAnimator(self)
+                self.Trash:Add(self.AnimationManipulator)
+            end
+            self.AnimationManipulator:SetRate(-1)
+            WaitFor(self.AnimationManipulator)
+            CConstructionStructureUnit.OnStopBuild(self, unitBeingBuilt)
+        end,
+
+}
+}
+TypeClass = XRB0304
