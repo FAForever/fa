863af75931d69d6ad5f4dec2c532e6cda9e917c4
Ze PilOt@ZePilOt-PC <Ze PilOt@ZePilOt-PC>
2014-04-12 12:48:23 +0200

    Fixing UI issue.

--- lua/ui/game/layouts/unitview_left.lua
+++ lua/ui/game/layouts/unitview_left.lua
@@ -144,22 +144,18 @@ function SetLayout()
     controls.abilityBG.BR.Top:Set(controls.abilities.Bottom)
 
     if options.gui_detailed_unitview != 0 then
-        local TV = import('/modules/tvcheck.lua').Init()
-        if TV == false then
-            local controls = import('/lua/ui/game/unitview.lua').controls
-            LayoutHelpers.AtLeftTopIn(controls.healthBar, controls.bg, 66, 25)
-            LayoutHelpers.Below(controls.shieldBar, controls.healthBar)
-            controls.shieldBar.Height:Set(14)
-            LayoutHelpers.CenteredBelow(controls.shieldText, controls.shieldBar,0)
-            controls.shieldBar.Height:Set(2)
-            LayoutHelpers.AtLeftTopIn(controls.statGroups[1].icon, controls.bg, 70, 55)
-            LayoutHelpers.RightOf(controls.statGroups[1].value, controls.statGroups[1].icon, 5)
-            LayoutHelpers.Below(controls.statGroups[2].icon, controls.statGroups[1].icon,0)
-            -- LayoutHelpers.AtRightTopIn(controls.StorageMass, controls.bg, 145, 55)
-            LayoutHelpers.RightOf(controls.statGroups[2].value, controls.statGroups[2].icon, 5)
-            -- LayoutHelpers.AtRightTopIn(controls.StorageEnergy, controls.bg, 145, 73)
-            LayoutHelpers.Below(controls.Buildrate, controls.statGroups[2].value,1)
-        end
+        LayoutHelpers.AtLeftTopIn(controls.healthBar, controls.bg, 66, 25)
+        LayoutHelpers.Below(controls.shieldBar, controls.healthBar)
+        controls.shieldBar.Height:Set(14)
+        LayoutHelpers.CenteredBelow(controls.shieldText, controls.shieldBar,0)
+        controls.shieldBar.Height:Set(2)
+        LayoutHelpers.AtLeftTopIn(controls.statGroups[1].icon, controls.bg, 70, 55)
+        LayoutHelpers.RightOf(controls.statGroups[1].value, controls.statGroups[1].icon, 5)
+        LayoutHelpers.Below(controls.statGroups[2].icon, controls.statGroups[1].icon,0)
+        -- LayoutHelpers.AtRightTopIn(controls.StorageMass, controls.bg, 145, 55)
+        LayoutHelpers.RightOf(controls.statGroups[2].value, controls.statGroups[2].icon, 5)
+        -- LayoutHelpers.AtRightTopIn(controls.StorageEnergy, controls.bg, 145, 73)
+        LayoutHelpers.Below(controls.Buildrate, controls.statGroups[2].value,1)
     end
 end
 
