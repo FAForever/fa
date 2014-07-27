016db6d56cc86cf5b25b55bda7c911892069555c
Ze PilOt@ZePilOt-PC <Ze PilOt@ZePilOt-PC>
2014-07-27 13:25:54 +0200

    Implementing the T3 Mobile AA.
    --HG--
    branch : Mobile_T3_AA

---
+++ lua/kirveseffects.lua
@@ -0,0 +1,39 @@
+EmtBpPath = '/effects/emitters/'
+CustomEmtBpPath = '/effects/emitters/'
+
+TPhalanxGunPolyTrails = {
+    EmtBpPath .. 'phalanx_munition_polytrail_01_emit.bp',
+}
+TPhalanxGunMuzzleFlash = {
+    EmtBpPath .. 'phalanx_muzzle_flash_01_emit.bp',
+    EmtBpPath .. 'phalanx_muzzle_glow_01_emit.bp',
+}
+TPhalanxGunShells = {
+    CustomEmtBpPath .. 'aaphalanx_shells_01_emit.bp',
+}
+TPhalanxGunPolyTrailsOffsets = {0.05,0.05,0.05}
+
+OrbGlowEffect = {
+    CustomEmtBpPath .. 'orbeffect_01.bp',
+    CustomEmtBpPath .. 'orbeffect_02.bp',    
+}
+
+SExperimentalUnstablePhasonLaserMuzzle01 = {
+	EmtBpPath .. 'seraphim_expirimental_laser_charge_01_emit.bp',
+    EmtBpPath .. 'seraphim_expirimental_laser_charge_01_emit.bp', 
+}
+
+OthuyElectricityStrikeBeam = {
+    CustomEmtBpPath .. 'seraphim_lightning_beam_01_emit.bp',
+}
+
+OthuyElectricityStrikeBeam2 = {
+    CustomEmtBpPath .. 'seraphim_lightning_beam_02_emit.bp',
+}
+
+OthuyElectricityStrikeHit = {
+    CustomEmtBpPath .. 'seraphim_lightning_hit_01_emit.bp',
+    CustomEmtBpPath .. 'seraphim_lightning_hit_02_emit.bp',
+    CustomEmtBpPath .. 'seraphim_lightning_hit_03_emit.bp',
+    CustomEmtBpPath .. 'seraphim_lightning_hit_04_emit.bp',
+}
---
+++ projectiles/aaphalanx01/AAPhalanx01_script.lua
@@ -0,0 +1,7 @@
+#
+# Terran AA Phalanx projectile
+#
+TDPhalanx01 = Class(import('/lua/kirvesprojectiles.lua').TShellPhalanxProjectile) { }
+
+TypeClass = TDPhalanx01
+
