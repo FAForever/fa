d5650d9ee8225a0c69842bebc68be3ee0f23821b
Crotalus <->
2014-09-27 17:54:40 +0200

    No longer add resources from active consumption into acu/scu upgrades, fixes #9

--- lua/sim/tasks/EnhanceTask.lua
+++ lua/sim/tasks/EnhanceTask.lua
@@ -1,7 +1,7 @@
 #*****************************************************************************
 #* File: lua/modules/EnhanceTask.lua
 #*
-#* Copyright © 2008 Gas Powered Games, Inc.  All rights reserved.
+#* Copyright Å  2008 Gas Powered Games, Inc.  All rights reserved.
 #*****************************************************************************
 local ScriptTask = import('/lua/sim/ScriptTask.lua').ScriptTask
 local TASKSTATUS = import('/lua/sim/ScriptTask.lua').TASKSTATUS
@@ -55,17 +55,19 @@ EnhanceTask = Class(ScriptTask) {
             
             local current = unit.WorkProgress
             
-            local obtained = unit:GetResourceConsumed()
-            if obtained > 0 then
-                local frac = ( 1 / ( unit.WorkItemBuildTime / unit:GetBuildRate()) ) * obtained * SecondsPerTick()
-                current = current + frac
-                unit.WorkProgress = current
+            if not unit:IsPaused() then
+                local obtained = unit:GetResourceConsumed()
+                if obtained > 0 then
+                    local frac = ( 1 / ( unit.WorkItemBuildTime / unit:GetBuildRate()) ) * obtained * SecondsPerTick()
+                    current = current + frac
+                    unit.WorkProgress = current
+                end
             end
             
             if( ( self.LastProgress < 0.25 and current >= 0.25 ) or
                 ( self.LastProgress < 0.50 and current >= 0.50 ) or
                 ( self.LastProgress < 0.75 and current >= 0.75 ) ) then
-                unit:OnBuildProgress(self.LastProgress,current)
+                    unit:OnBuildProgress(self.LastProgress,current)
             end
             
             self.LastProgress = current
