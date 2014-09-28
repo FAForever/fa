1ae8320efbeb2116ce24075bbd6627aea2f9c9f3
Michael Sondergaard <msond@cs.au.dk>
2014-09-28 18:37:42 +0200

    Remove old dynamic shieldsize code

--- units/UAL0303/UAL0303_Script.lua
+++ units/UAL0303/UAL0303_Script.lua
@@ -59,30 +59,5 @@ UAL0303 = Class(AWalkingLandUnit) {
 
 --	End Shielding fix
 }
---	Dynamically change the hitbox so it's a sphere when the personal shield is on,
---  and a normal box when it's off.
-	OnShieldEnabled = function(self)
-		local bp = self:GetBlueprint()	
-		AWalkingLandUnit.OnShieldEnabled(self)
-		self:SetCollisionShape('Sphere', 0, bp.SizeY * 0.5, 0, (bp.Defense.Shield.ShieldSize * 0.5))
-	end,
-	
-	OnShieldDisabled = function(self)
-		local bp = self:GetBlueprint()			
-		AWalkingLandUnit.OnShieldDisabled(self)
-		self:SetCollisionShape('Box', 0, bp.SizeY * 0.5, 0, bp.SizeX * 0.5, bp.SizeY * 0.5, bp.SizeZ * 0.5)
-	end,
-
-	OnShieldHpDepleted = function(self)
-		local bp = self:GetBlueprint()
-		AWalkingLandUnit.OnShieldHpDepleted(self)
-		self:SetCollisionShape('Box', 0, bp.SizeY * 0.5, 0, bp.SizeX * 0.5, bp.SizeY * 0.5, bp.SizeZ * 0.5)
-	end,
-
-	OnShieldEnergyDepleted = function(self)
-		local bp = self:GetBlueprint()	
-		AWalkingLandUnit.OnShieldEnergyDepleted(self)
-		self:SetCollisionShape('Box', 0, bp.SizeY * 0.5, 0, bp.SizeX * 0.5, bp.SizeY * 0.5, bp.SizeZ * 0.5)
-	end,	
 
 TypeClass = UAL0303
