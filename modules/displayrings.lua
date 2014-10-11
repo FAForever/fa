24b0b48ec1ee5ee1f7b0de0efdc0e65dffa12056
Crotalus <->
2014-10-12 01:05:21 +0200

    Optimized build range ring on selection of ENGINEER units

--- modules/displayrings.lua
+++ modules/displayrings.lua
@@ -5,27 +5,13 @@ local Prefs = import('/lua/user/prefs.lua')
 
 local showRangeTable = false
 
---category tester function, returns true if the bp includes the given category
-function HasCategory(blueprint, category)
-	if blueprint then
-		if blueprint.Categories then
-			for i, cat in blueprint.Categories do
-				if cat == category then
-					return true
-				end
-			end
-		end
-		return false
-	end
-end
-
 --toggle show range of all selected units
 function ToggleShowRanges()
 	if showRangeTable then
 		import('/lua/ui/game/gamemain.lua').RemoveBeatFunction(ShowRanges)
-		for i, v in showRangeTable do
-			if v.BuildRangeRing then
-				v.BuildRangeRing:Destroy()
+		for i, ring in showRangeTable do
+			if ring then
+				ring:Destroy()
 			end
 		end
 		showRangeTable = false
@@ -36,47 +22,38 @@ function ToggleShowRanges()
 end
 
 --beat function, show all selected unit ranges
+
 function ShowRanges()
-	local worldview = import('/lua/ui/game/worldview.lua').viewLeft
-	if showRangeTable then
-		local selection = GetSelectedUnits()
-		local selectedUnitInfo = {}
-		if selection then
-			for i, unit in selection do
-				local id = unit:GetEntityId()
-				selectedUnitInfo[id] = unit
-			end
-		end
-		for id, unit in selectedUnitInfo do
+	local selection = EntityCategoryFilterDown(categories.ENGINEER, GetSelectedUnits())
+	local selected = {}
+
+	if selection then
+		local worldview = import('/lua/ui/game/worldview.lua').viewLeft
+
+		for _, unit in selection do
+			local id = unit:GetEntityId()
 			if not showRangeTable[id] then
-				showRangeTable[id] = {}
 				local bp = unit:GetBlueprint()
-				if HasCategory(bp, 'ENGINEER') then
-					showRangeTable[id].BuildRangeRing = Decal(GetFrame(0))
-					showRangeTable[id].BuildRangeRing:SetTexture('/textures/ring_orange.dds')
-					if bp.Economy.MaxBuildDistance then
-						showRangeTable[id].BuildRangeRing:SetScale({math.floor(2.03*(bp.Economy.MaxBuildDistance+2))+2, 0, math.floor(2.03*(bp.Economy.MaxBuildDistance+2))+2})
-					else
-						showRangeTable[id].BuildRangeRing:SetScale({22, 0, 22})
-					end
+				local ring =  Decal(GetFrame(0))
+				ring:SetTexture('/textures/ring_orange.dds')
+				if bp.Economy.MaxBuildDistance then
+					ring:SetScale({math.floor(2.03*(bp.Economy.MaxBuildDistance+2))+2, 0, math.floor(2.03*(bp.Economy.MaxBuildDistance+2))+2})
+				else
+					ring:SetScale({22, 0, 22})
 				end
+				showRangeTable[id] = ring
 			end
-			if showRangeTable[id] then
-				local unitpos = unit:GetPosition()
-				local temppos = worldview:Project(unitpos)
-				local screenpos = {temppos[1] + worldview.Left(), temppos[2] + worldview.Top()}
-				if showRangeTable[id].BuildRangeRing then
-					showRangeTable[id].BuildRangeRing:SetPositionByScreen(screenpos)
-				end
-			end
+
+			local pos = worldview:Project(unit:GetPosition())
+			showRangeTable[id]:SetPositionByScreen({pos[1]+worldview.Left(), pos[2]+worldview.Top()})
+			selected[id] = true
 		end
-		for id, overlay in showRangeTable do
-			if not selectedUnitInfo[id] then
-				if overlay.BuildRangeRing then
-					overlay.BuildRangeRing:Destroy()
-				end
-				showRangeTable[id] = nil
-			end
+	end
+
+	for id, ring in showRangeTable do
+		if not selected[id] then
+			ring:Destroy()
+			showRangeTable[id] = nil
 		end
 	end
 end
