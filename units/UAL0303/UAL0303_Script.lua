a7310419911152aacb7a477fdaff3594ece208f3
aeoncleanse <ben-knight@bluebottle.com>
2014-10-13 01:23:46 +0100

    Introduce New Shielding Method

--- units/UAL0303/UAL0303_Script.lua
+++ units/UAL0303/UAL0303_Script.lua
@@ -29,35 +29,6 @@ UAL0303 = Class(AWalkingLandUnit) {
         EffectUtil.CreateAeonCommanderBuildingEffects( self, unitBeingBuilt, self:GetBlueprint().General.BuildBones.BuildEffectBones, self.BuildEffectsBag )
     end,
 
-    --  Begin Shielding fix: Since Personal Shields no longer use their own collision, instead handling
-    --  all damage in unit.lua, and the harbinger is unique in having a personal bubble shield, we need
-    --  the hitbox to be the shape and size of the bubble when the shield is up, and the box of the unit
-    --  when the shield is down. Anti-collision for this unit is also handled here as it's not officially a personal shield.
-
-    --These functions are empty in unit.lua so no callback required
-    OnShieldEnabled = function(self)
-        local bp = self:GetBlueprint()
-        self:SetCollisionShape('Sphere', 0, bp.SizeY * 0.5, 0, (bp.Defense.Shield.ShieldSize * 0.5))
-        --Manually disable the bubble shield's collision sphere after its creation so it acts like the new personal shields
-        self.MyShield:SetCollisionShape('None')
-    end,
-
-    OnShieldDisabled = function(self)
-        local bp = self:GetBlueprint()
-        self:SetCollisionShape('Box', 0, bp.SizeY * 0.5, 0, bp.SizeX * 0.5, bp.SizeY * 0.5, bp.SizeZ * 0.5)
-    end,
-
-    OnShieldHpDepleted = function(self)
-        local bp = self:GetBlueprint()
-        self:SetCollisionShape('Box', 0, bp.SizeY * 0.5, 0, bp.SizeX * 0.5, bp.SizeY * 0.5, bp.SizeZ * 0.5)
-    end,
-
-    OnShieldEnergyDepleted = function(self)
-        local bp = self:GetBlueprint()
-        self:SetCollisionShape('Box', 0, bp.SizeY * 0.5, 0, bp.SizeX * 0.5, bp.SizeY * 0.5, bp.SizeZ * 0.5)
-    end,
-
-    --End Shielding fix
 }
 
 TypeClass = UAL0303
