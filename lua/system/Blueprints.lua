9a89b4b837543f23a715e1ccf924857b46f2198c
Crotalus <->
2014-11-19 21:45:26 +0100

    Restore engineer move-attack reclaim bug

--- lua/system/Blueprints.lua
+++ lua/system/Blueprints.lua
@@ -453,7 +453,7 @@ function PreModBlueprints(all_bps)
             local br = nil
 
             if(cats.ENGINEER) then
-                br = bp.Economy.MaxBuildDistance or 10
+                br = bp.Economy.MaxBuildDistance or 26
             elseif bp.Weapon then
                 local vision = bp.Intel.VisionRadius or 10
                 local max = 0
