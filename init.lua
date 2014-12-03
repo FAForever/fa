bf4531fcb95708f5bcd91dd51dd182dc3736526b
IceDreamer <ben-knight@bluebottle.com>
2014-12-03 00:23:16 +0000

    Clean init.lua Whitespace

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
--- this imports a path file that is written by Forged Alliance Forever right before it starts the game.
+-- This imports a path file that is written by Forged Alliance Forever right before it starts the game.
 dofile(InitFileDir .. '\\..\\..\\fa_path.lua')
- 
 path = {}
 
 blacklist = {
@@ -67,7 +66,6 @@ local function mount_dir(dir, mountpoint)
 	table.insert(path, { dir = dir, mountpoint = mountpoint } )
 end
 
-
 local function mount_contents(dir, mountpoint)
 	LOG('checking ' .. dir)
 	for _,entry in io.dir(dir .. '\\*') do
@@ -129,28 +127,25 @@ local function mount_dir_with_blacklist(dir, glob, mountpoint)
 	table.sort(sorted)
 	table.foreach(sorted, function(k,v) mount_dir(v,'/') end)
 end
--- these are the classic supcom directories. They don't work with accents or other foreign characters in usernames
+
+-- These are the classic supcom directories. They don't work with accents or other foreign characters in usernames
 mount_contents(SHGetFolderPath('PERSONAL') .. 'My Games\\Gas Powered Games\\Supreme Commander Forged Alliance\\mods', '/mods')
 mount_contents(SHGetFolderPath('PERSONAL') .. 'My Games\\Gas Powered Games\\Supreme Commander Forged Alliance\\maps', '/maps')
  
--- these are the local FAF directories. The My Games ones are only there for people with usernames that don't work in the uppder ones.
+-- These are the local FAF directories. The My Games ones are only there for people with usernames that don't work in the uppder ones.
 mount_contents(InitFileDir .. '\\..\\user\\My Games\\Gas Powered Games\\Supreme Commander Forged Alliance\\mods', '/mods')
 mount_contents(InitFileDir .. '\\..\\user\\My Games\\Gas Powered Games\\Supreme Commander Forged Alliance\\maps', '/maps')
 
--- InitFileDir = /repo/faf/
 mount_dir(InitFileDir, '/')
  
--- these are using the newly generated path from the dofile() statement at the beginning of this script
+-- These are using the newly generated path from the dofile() statement at the beginning of this script
 mount_dir_with_whitelist(fa_path .. '\\gamedata\\', '*.scd', '/')
 mount_dir(fa_path, '/')
- 
- 
+
 hook = {
 	'/schook'
 }
- 
- 
- 
+
 protocols = {
 	'http',
 	'https',
