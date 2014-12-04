0d32ab0c468fb6f038a2d35f542c8f73cd0ac0fe
IceDreamer <ben-knight@bluebottle.com>
2014-12-04 20:45:05 +0000

    Add new files

---
+++ mods/CampaignBranchE02/mod_info.lua
@@ -0,0 +1,15 @@
+name = "Supreme Commander Campaign Branch for map E02"
+version = 1
+copyright = "Copyright © 2006, Gas Powered Games"
+description = "Balance branch for the SC campaigns"
+author = "Gas Powered Games"
+url = "http://www.gaspoweredgames.com"
+uid = "e7846e9b-23a4-4b95-ae3a-fb69b289a585"
+
+exclusive = false
+
+ui_only = false
+
+selectable = false
+
+enabled = true
