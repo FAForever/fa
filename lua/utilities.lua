ef914b2c490bc32197cfbae9722bdfa8f229560f
IceDreamer <ben-knight@bluebottle.com>
2014-12-08 23:52:39 +0000

    Add new non-allied unit search sphere utility

--- lua/utilities.lua
+++ lua/utilities.lua
@@ -34,6 +34,29 @@ function GetEnemyUnitsInSphere(unit, position, radius)
     return RadEntities
 end
 
+-- Added to FAF so we can exclude allied units
+function GetTrueEnemiesInSphere(unit, position, radius)
+    local x1 = position.x - radius
+    local y1 = position.y - radius
+    local z1 = position.z - radius
+    local x2 = position.x + radius
+    local y2 = position.y + radius
+    local z2 = position.z + radius
+    local UnitsinRec = GetUnitsInRect( Rect(x1, z1, x2, z2) )
+    --Check for empty rectangle
+    if not UnitsinRec then
+        return UnitsinRec
+    end
+    local RadEntities = {}
+    for k, v in UnitsinRec do
+        local dist = VDist3(position, v:GetPosition())
+        if unit:GetArmy() != v:GetArmy() and dist <= radius and IsAlly(unit:GetArmy(), v:GetArmy()) == false then
+            table.insert(RadEntities, v)
+        end
+    end
+    return RadEntities
+end
+
 function GetDistanceBetweenTwoPoints(x1, y1, z1, x2, y2, z2)
     return ( math.sqrt( (x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2 ) )
 end
