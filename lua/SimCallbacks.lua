c6cc95d66f851c92bf38d17da8f9d8ddc716deb0
Michael Sondergaard <msond@cs.au.dk>
2014-12-12 02:43:49 +0100

    Revert merge error, null the methods for now

--- lua/SimCallbacks.lua
+++ lua/SimCallbacks.lua
@@ -119,32 +119,9 @@ Callbacks.PingGroupClick = import('/lua/SimPingGroup.lua').OnClickCallback
 
 
 Callbacks.AddTarget = function(data, units)
-	
-	if type(data.target) == 'string' then
-		local entity = GetEntityById(data.target)
-		if entity and IsBlip(entity) and entity.GetSource then
-			local blipentity = entity:GetSource()
-			if blipentity then
-				if IsUnit(blipentity) then
-					for id, unit in units or {} do
-						blipentity:addAttacker(unit)
-						unit:addTarget(blipentity)
-					end
-				end
-			end
-		end
-	end
-
-
 end
 
 Callbacks.ClearTargets = function(data, units)
-	for id, unit in units or {} do	
-		if unit then 
-			unit:clearTarget()
-		end
-	end
-	
 end
 
 Callbacks.GiveOrders = import('/lua/spreadattack.lua').GiveOrders
