2addac9c068d0cb7767b744d44ef50c9243f2ac2
Michael Sondergaard <msond@cs.au.dk>
2014-12-17 14:14:55 +0100

    Indent changelog using spaces

--- lua/ui/lobby/changelog.lua
+++ lua/ui/lobby/changelog.lua
@@ -1,75 +1,75 @@
 last_version = 6
 changelog = {
-	{
-		version = 3,
-		name = "Lobby 2.5 (3636 patch)",
-		description = {
-			"- Add Changelog dialog",
-			"- Add numbers of Restriction in OptionContainer",
-			"- Add new view in ModManager",
-			"- Add 'Show system message' option for see (connecting players, go observers, disconnected, ...)",
-			"- Add 'Player Swapped' message",
-			"- Add 'Connected with xxx (with FAF Proxy)' message see if is connected with FAFproxy",
-			"- Add 1250 and 1500 Units Caps",
-			"- Add Windowed options for launch Lobby windowed until launch [by Crotalus]",
-			"- Add Autobalance the teams when spawn is random [by Crotalus]",
-			"- Add new restrictions (NoEngineeringStation+EnhancementDrone), (NoEyeofRhianneandSoothsayer)",
-			"- Add translation for Unit Restrictions and re-order [by Xinnony, Navax's,  aeoncleanse]",
-			"- Fix the Mod UI not activated directly [by Crotalus]",
-			"- Fix Preset not loaded correctly (exemple FinalRushPro, but need recreate preset)",
-			"- Fix ModManager crash sometime",
-			"- Fix a bug in Game Options, the new value is reset if you scroll the list",
-			"- Fix sometime a PresetLobby not loaded (caused by map not available)",
-			"- Fix 'CH' and 'VA' and yellow Flag Country",
-			"- Replace Checkbox to Radiobox in ModManager",
-			"- Replace the 'world' flag country",
-			"- Delete 'Player go to Slot' message",
-			"- Delete 'Player go to Observer' message",
-			"- Remember a options 'Hide default Options'",
-			"- Show message if map is not available in PresetLobby",
-			"- SpeedUp UpdateGame(), merge 3 loops",
-			"- Typo in 'Lobby options'",
-			"- The ConnectDialog lasts until the lobby is fully loaded",
-			"- Typo 'x mods UI' to 'x UI mods' (KRAMP idea)",
-			"- Now you can see and select only the color available",
-			"- Transparent color the ACU in Minimap (just the grey color, not the black border)",
-			"- Typos in PresetLobby",
-		}
-	},
-	{
-		version = 4,
-		name = "Lobby 2.5b",
-		description = {
-			"- Disable Windowed options if the game is already set windowed",
-			"- Add color for the default options in Game Options window",
-			"- Add Tooltip for the Mod in Mod Manager with Little View",
-			"- Fix people can change color of other people",
-			"- Fix wrong description for Eye and Soothsayer Unit Restrictions",
-			"- Fix people can't join/host (black screen lobby)",
-			"- Fix problem with Preset Lobby",
-			"- Remove debug message",
-		}
-	},
-	{
-		version = 5,
-		name = "Lobby 2.5c",
-		description = {
-			"- Fix the Rating not show in observers list",
-			"- Observers font-size is reduced",
-			"- Fix the Country not show sometime",
-			"- Add debugger system",
-			"- Add slider for change the chat font-size",
-		}
-	},
-	{
-		version = 6,
-		name = "Lobby 2.5d",
-		description = {
-			"- Remove debug message",
-			"- Enhancement Debug System",
-			"- Fix UI bug with ConnectDialog",
-			"- Fix the player not removed in the slot when is leave the lobby",
-			"- Fix replaysync [by fafafaf]",
-		}
-	}
+    {
+        version = 3,
+        name = "Lobby 2.5 (3636 patch)",
+        description = {
+            "- Add Changelog dialog",
+            "- Add numbers of Restriction in OptionContainer",
+            "- Add new view in ModManager",
+            "- Add 'Show system message' option for see (connecting players, go observers, disconnected, ...)",
+            "- Add 'Player Swapped' message",
+            "- Add 'Connected with xxx (with FAF Proxy)' message see if is connected with FAFproxy",
+            "- Add 1250 and 1500 Units Caps",
+            "- Add Windowed options for launch Lobby windowed until launch [by Crotalus]",
+            "- Add Autobalance the teams when spawn is random [by Crotalus]",
+            "- Add new restrictions (NoEngineeringStation+EnhancementDrone), (NoEyeofRhianneandSoothsayer)",
+            "- Add translation for Unit Restrictions and re-order [by Xinnony, Navax's,  aeoncleanse]",
+            "- Fix the Mod UI not activated directly [by Crotalus]",
+            "- Fix Preset not loaded correctly (exemple FinalRushPro, but need recreate preset)",
+            "- Fix ModManager crash sometime",
+            "- Fix a bug in Game Options, the new value is reset if you scroll the list",
+            "- Fix sometime a PresetLobby not loaded (caused by map not available)",
+            "- Fix 'CH' and 'VA' and yellow Flag Country",
+            "- Replace Checkbox to Radiobox in ModManager",
+            "- Replace the 'world' flag country",
+            "- Delete 'Player go to Slot' message",
+            "- Delete 'Player go to Observer' message",
+            "- Remember a options 'Hide default Options'",
+            "- Show message if map is not available in PresetLobby",
+            "- SpeedUp UpdateGame(), merge 3 loops",
+            "- Typo in 'Lobby options'",
+            "- The ConnectDialog lasts until the lobby is fully loaded",
+            "- Typo 'x mods UI' to 'x UI mods' (KRAMP idea)",
+            "- Now you can see and select only the color available",
+            "- Transparent color the ACU in Minimap (just the grey color, not the black border)",
+            "- Typos in PresetLobby",
+        }
+    },
+    {
+        version = 4,
+        name = "Lobby 2.5b",
+        description = {
+            "- Disable Windowed options if the game is already set windowed",
+            "- Add color for the default options in Game Options window",
+            "- Add Tooltip for the Mod in Mod Manager with Little View",
+            "- Fix people can change color of other people",
+            "- Fix wrong description for Eye and Soothsayer Unit Restrictions",
+            "- Fix people can't join/host (black screen lobby)",
+            "- Fix problem with Preset Lobby",
+            "- Remove debug message",
+        }
+    },
+    {
+        version = 5,
+        name = "Lobby 2.5c",
+        description = {
+            "- Fix the Rating not show in observers list",
+            "- Observers font-size is reduced",
+            "- Fix the Country not show sometime",
+            "- Add debugger system",
+            "- Add slider for change the chat font-size",
+        }
+    },
+    {
+        version = 6,
+        name = "Lobby 2.5d",
+        description = {
+            "- Remove debug message",
+            "- Enhancement Debug System",
+            "- Fix UI bug with ConnectDialog",
+            "- Fix the player not removed in the slot when is leave the lobby",
+            "- Fix replaysync [by fafafaf]",
+        }
+    }
 }
