c34f3190b737586af324fd4ada8af68abce323a7
Chris Kitching <chriskitching@linux.com>
2014-12-22 17:46:53 +0000

    Simplify skinning logic

---
+++ textures/ui/AEON/game/unit-build-over-panel/unit-build-over-panel_layout.lua
@@ -0,0 +1,12 @@
+layout = {
+	['bracket-build_bmp'] = {left = 0, top = 0, width = 32, height = 116, leftOffset = 1, topOffset = 1, },
+	['fuelbar'] = {left = 79, top = 52, width = 188, height = 4, leftOffset = 0, topOffset = 0, },
+	['shieldbar'] = {left = 79, top = 48, width = 188, height = 4, leftOffset = 0, topOffset = 0, },
+	['healthbar_green'] = {left = 77, top = 35, width = 192, height = 24, leftOffset = 0, topOffset = 1, },
+	['healthbar_yellow'] = {left = 77, top = 35, width = 192, height = 24, leftOffset = 0, topOffset = 1, },
+	['healthbar_red'] = {left = 77, top = 35, width = 192, height = 24, leftOffset = 0, topOffset = 1, },
+	['healthbar_bg'] = {left = 79, top = 36, width = 188, height = 20, leftOffset = 0, topOffset = 0, },
+	['build-over-back_bmp'] = {left = 12, top = 0, width = 332, height = 116, leftOffset = 0, topOffset = 0, },
+	['bracket-unit_bmp'] = {left = 0, top = 0, width = 32, height = 116, leftOffset = 1, topOffset = 1, },
+	['unit-over-back_bmp'] = {left = 12, top = 0, width = 332, height = 116, leftOffset = 0, topOffset = 0, },
+}
---
+++ textures/ui/CYBRAN/game/unit-build-over-panel/unit-build-over-panel_layout.lua
@@ -0,0 +1,12 @@
+layout = {
+	['fuelbar'] = {left = 79, top = 52, width = 188, height = 4, leftOffset = 0, topOffset = 0, },
+	['shieldbar'] = {left = 79, top = 48, width = 188, height = 4, leftOffset = 0, topOffset = 0, },
+	['healthbar_green'] = {left = 77, top = 35, width = 192, height = 24, leftOffset = 0, topOffset = 1, },
+	['healthbar_yellow'] = {left = 77, top = 35, width = 192, height = 24, leftOffset = 0, topOffset = 1, },
+	['healthbar_red'] = {left = 77, top = 35, width = 192, height = 24, leftOffset = 0, topOffset = 1, },
+	['healthbar_bg'] = {left = 79, top = 36, width = 188, height = 20, leftOffset = 0, topOffset = 0, },
+	['bracket-build_bmp'] = {left = 0, top = 0, width = 32, height = 120, leftOffset = 1, topOffset = 1, },
+	['build-over-back_bmp'] = {left = 11, top = 1, width = 332, height = 116, leftOffset = 0, topOffset = 0, },
+	['bracket-unit_bmp'] = {left = 0, top = 0, width = 32, height = 120, leftOffset = 1, topOffset = 1, },
+	['unit-over-back_bmp'] = {left = 11, top = 1, width = 332, height = 116, leftOffset = 0, topOffset = 0, },
+}
