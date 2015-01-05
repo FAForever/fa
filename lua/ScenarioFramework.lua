d8a27a9e95e984b90284f25ae33456ed850d571b
IceDreamer <ben-knight@bluebottle.com>
2015-01-05 19:37:02 +0000

    Add missing functions to ScenarioFramework.lua

--- lua/ScenarioFramework.lua
+++ lua/ScenarioFramework.lua
@@ -25,6 +25,15 @@ function ExitGame()
     Sync.RequestingExit = true
 end
 
+function fillCoop()    
+    local tblArmy = ListArmies()
+    for iArmy, strArmy in pairs(tblArmy) do
+        if iArmy >= ScenarioInfo.Coop1 then
+            table.insert(ScenarioInfo.HumanPlayers, iArmy)
+        end
+    end
+end
+
 # Call to end an operation
 #   bool _success - instructs UI which dialog to show
 #   bool _allPrimary - true if all primary objectives completed, otherwise, false
@@ -95,6 +104,18 @@ local timerThread = nil
 function CreateTimerTrigger( cb, seconds, displayBool)
     timerThread = TriggerFile.CreateTimerTrigger(cb, seconds, displayBool)
     return timerThread
+end
+
+function CreateTimerTriggerUnlockCoop(cb, faction, seconds, displayBool)
+    local tblArmy = ListArmies()
+    for iArmy, strArmy in pairs(tblArmy) do
+        if iArmy >= ScenarioInfo.Coop1 then
+            factionIdx = GetArmyBrain(strArmy):GetFactionIndex()
+            if(factionIdx == faction) then
+                CreateTimerTrigger(cb, seconds, displayBool)
+            end
+        end
+    end
 end
 
 function ResetUITimer()
@@ -959,6 +980,23 @@ function RemoveRestriction(army, categories, isSilent)
     RemoveBuildRestriction(army, categories)
 end
 
+function RemoveRestrictionCoop(faction, categories, isSilent)
+    --for coop players
+    local tblArmy = ListArmies()
+    for iArmy, strArmy in pairs(tblArmy) do
+        if iArmy >= ScenarioInfo.Coop1 then     
+            factionIdx = GetArmyBrain(strArmy):GetFactionIndex()
+            if(factionIdx == faction) then
+                SimUIVars.SaveTechAllowance(categories)
+                if not isSilent then
+                    if not Sync.NewTech then Sync.NewTech = {} end
+                    table.insert(Sync.NewTech, EntityCategoryGetUnitList(categories))
+                end
+                RemoveBuildRestriction(iArmy, categories)
+            end
+        end
+    end
+end
 
 #### returns lists of factories by category
 #### <point> and <radius> are optional
