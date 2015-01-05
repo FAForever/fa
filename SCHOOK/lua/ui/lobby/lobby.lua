2860bc89f174f853c3cb9f96627c505dae3e11fc
Michael Sondergaard <msond@cs.au.dk>
2015-01-06 00:16:20 +0100

    Shadow schook/lua/ui/lobby.lua with an empty file
    This is to prevent unknown mods from hooking into the lobby using brittle file
    concatenation, causing issues.
    
    Closes #336

---
+++ SCHOOK/lua/ui/lobby/lobby.lua
@@ -0,0 +1,4 @@
+--
+-- This file is here to prevent .scd mods from hooking into lobby.lua using file concatenation.
+--
+-- See this issue for why this file was created: https://github.com/FAForever/fa/issues/336
