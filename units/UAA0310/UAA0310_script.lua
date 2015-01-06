0f7c364e6a876d5f6caed86220426a2a96852c61
Crotalus <Crotalus@users.noreply.github.com>
2015-01-06 18:25:25 +0100

    Czar now uses new death weapon

--- units/UAA0310/UAA0310_script.lua
+++ units/UAA0310/UAA0310_script.lua
@@ -15,7 +15,7 @@ local explosion = import('/lua/defaultexplosions.lua')
 UAA0310 = Class(AirTransport) {
     DestroyNoFallRandomChance = 1.1,
     BuildAttachBone = 'UAA0310',
-    
+
     Weapons = {
         QuantumBeamGeneratorWeapon = Class(AQuantumBeamGenerator){},
         SonicPulseBattery1 = Class(AAAZealotMissileWeapon) {},
@@ -34,26 +34,7 @@ UAA0310 = Class(AirTransport) {
             v.Beam:Disable()
         end
 
-        self.detector = CreateCollisionDetector(self)
-        self.Trash:Add(self.detector)
-        self.detector:WatchBone('Left_Turret01_Muzzle')
-        self.detector:WatchBone('Right_Turret01_Muzzle')
-        self.detector:WatchBone('Left_Turret02_WepFocus')
-        self.detector:WatchBone('Right_Turret02_WepFocus')
-        self.detector:WatchBone('Left_Turret03_Muzzle')
-        self.detector:WatchBone('Right_Turret03_Muzzle')
-        self.detector:WatchBone('Attachpoint01')
-        self.detector:WatchBone('Attachpoint02')
-        self.detector:EnableTerrainCheck(true)
-        self.detector:Enable()
-
         AirTransport.OnKilled(self, instigator, type, overkillRatio)
-    end,
-
-    OnAnimTerrainCollision = function(self, bone,x,y,z)
-        DamageArea(self, {x,y,z}, 5, 1000, 'Default', true, false)
-        explosion.CreateDefaultHitExplosionAtBone(self, bone, 5.0)
-        explosion.CreateDebrisProjectiles(self, explosion.GetAverageBoundingXYZRadius(self), {self:GetUnitSizes()})
     end,
 
     OnStopBeingBuilt = function(self,builder,layer)
