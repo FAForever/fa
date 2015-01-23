e8eb4664a7548d6a7579e2c9f8c97deb12f20aba
IceDreamer <ben-knight@bluebottle.com>
2015-01-23 01:49:01 +0000

    Kill idle factories icon

--- lua/ui/game/avatars.lua
+++ lua/ui/game/avatars.lua
@@ -793,7 +793,7 @@ function AvatarUpdate()
         end
     end
 
-    if factories and table.getn(EntityCategoryFilterDown(categories.ALLUNITS - categories.GATE, factories)) > 0 then
+    if factories and table.getn(EntityCategoryFilterDown(categories.ALLUNITS - categories.GATE - categories.ORBITALSYSTEM, factories)) > 0 then
         if controls.idleFactories then
             controls.idleFactories:Update(EntityCategoryFilterDown(categories.ALLUNITS - categories.GATE, factories))
         else
