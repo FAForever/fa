2e02feeff0d2d90976c42bea2f630a659f70059d
Michael Sondergaard <msond@cs.au.dk>
2015-01-30 23:44:19 +0100

    Fully disable steam discovery service
    This brings back the ability to connect by ip/port for steam users
    
    Closes #445

--- SCHOOK/lua/ui/lobby/gameselect.lua
+++ SCHOOK/lua/ui/lobby/gameselect.lua
@@ -1,3 +1,14 @@
--- This is a hack to disable the call to InternalStartSteamDiscoveryService,
--- which only exists when using the steam executable.
+--- A noop for the purpose of the FAF binary not containing this definition
+--
 InternalStartSteamDiscoveryService = function() end
+
+local oldCreateUI = CreateUI
+--- Overridden so that useSteam == false rather than nil
+--  since the steam implementation contains a direct comparison
+-- @param over
+-- @param exitBehavior
+-- @param useSteam always false
+--
+function CreateUI(over, exitBehavior, useSteam)
+    oldCreateUI(over, exitBehavior, false)
+end
