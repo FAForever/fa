1a5ae3226ae082facdd3fe03fb01f665e95cb7d9
Chris Kitching <chriskitching@linux.com>
2015-01-30 05:35:25 +0000

    Move ACUButton into its own file

--- lua/ui/controls/resmappreview.lua
+++ lua/ui/controls/resmappreview.lua
@@ -3,16 +3,14 @@ local Group = import('/lua/maui/group.lua').Group
 local MapPreview = import('/lua/ui/controls/mappreview.lua').MapPreview
 local LayoutHelpers = import('/lua/maui/layouthelpers.lua')
 local MapUtil = import('/lua/ui/maputil.lua')
-local Bitmap = import('/lua/maui/bitmap.lua').Bitmap
-local Text = import('/lua/maui/text.lua').Text
-local Button = import('/lua/maui/button.lua').Button
 local TexturePool = import('/lua/ui/texturepool.lua').TexturePool
+local ACUButton = import('/lua/ui/controls/acubutton.lua').ACUButton
 
 -- The default size of the mass/hydrocarbon icons
 local DEFAULT_HYDROCARBON_ICON_SIZE = 14
 local DEFAULT_MASS_ICON_SIZE = 10
 
--- UI control to show a preview image of a map.
+--- UI control to show a preview image of a map, with optional resource markers.
 ResourceMapPreview = Class(Group) {
     __init = function(self, parent, size, massIconSize, hydroIconSize, buttonsDisabled)
         Group.__init(self, parent)
@@ -38,7 +36,7 @@ ResourceMapPreview = Class(Group) {
         LayoutHelpers.AtLeftTopIn(self.mapPreview, self)
     end,
 
-    -- Discard all resource markers
+    --- Discard all resource markers
     DestroyResourceMarkers = function(self)
         for k, v in pairs(self.massmarkers) do
             self.massIconPool:Dispose(v)
@@ -57,17 +55,22 @@ ResourceMapPreview = Class(Group) {
         self.startPositions = {}
     end,
 
+    --- Delete all resource markers and clear the map preview.
     Clear = function(self)
         self:DestroyResourceMarkers()
         self.mapPreview:ClearTexture()
     end,
 
-    -- Update the control with a named scenario file.
+    --- Update the control to display the map given by a specified scenario file.
+    --
+    -- @param scenarioFile Path to the scenario file from which to load map data to display.
     SetScenarioFromFile = function(self, scenarioFile)
         self:SetScenario(MapUtil.LoadScenario(scenarioFile))
     end,
 
-    -- Update the control to reflect a new Scenario.
+    --- Update the control to display the map given by a specific scenario object.
+    --
+    -- @param scenarioInfo Scenario info object from which to extract map data.
     SetScenario = function(self, scenarioInfo)
         self:DestroyResourceMarkers()
 
@@ -154,158 +157,4 @@ ResourceMapPreview = Class(Group) {
         self.hydroIconPool:Destroy()
         Group.OnDestroy(self)
     end
-}
-
-local teamIcons = {
-    '/lobby/team_icons/team_no_icon.dds',
-    '/lobby/team_icons/team_1_icon.dds',
-    '/lobby/team_icons/team_2_icon.dds',
-    '/lobby/team_icons/team_3_icon.dds',
-    '/lobby/team_icons/team_4_icon.dds',
-    '/lobby/team_icons/team_5_icon.dds',
-    '/lobby/team_icons/team_6_icon.dds',
-}
-
--- A small button representing an ACU, with support for showing colour and team affiliation.
-ACUButton = Class(Group) {
-    __init = function(self, parent, enabled)
-        Group.__init(self, parent)
-        self.Height:Set(10)
-        self.Width:Set(8)
-
-        self.enabled = enabled
-
-        -- Provides the solid-colour filling of the little ACU. Default transparent.
-        local colourBmp = Bitmap(self)
-        colourBmp.Height:Set(10)
-        colourBmp.Width:Set(8)
-        colourBmp:SetSolidColor('00777777')
-        LayoutHelpers.AtLeftTopIn(colourBmp, self)
-
-        self.marker = colourBmp
-
-        -- Team number display, if any. Defaults to invisible.
-        local teamIndicator = Bitmap(colourBmp)
-        LayoutHelpers.AnchorToRight(teamIndicator, colourBmp, 1)
-        LayoutHelpers.AtTopIn(teamIndicator, colourBmp, 5)
-        teamIndicator:DisableHitTest()
-        self.teamIndicator = teamIndicator
-
-        -- The little ACU image.
-        local buttonImage = UIUtil.UIFile('/dialogs/mapselect02/commander_alpha.dds')
-        local markerOverlay = Button(colourBmp, buttonImage, buttonImage, buttonImage, buttonImage)
-        LayoutHelpers.AtCenterIn(markerOverlay, colourBmp)
-        markerOverlay.OnClick = function(this, modifiers)
-            if not self:IsEnabled() then
-                return
-            end
-
-            if modifiers.Left then
-                self:OnClick()
-            elseif modifiers.Right then
-                self:OnRightClick()
-            end
-        end
-
-        markerOverlay.OnRolloverEvent = function(this, state)
-            -- Don't respond to events if the control is disabled.
-            if not self:IsEnabled() then
-                return
-            end
-
-            if state == "enter" then
-                self.indicator:Play()
-            elseif state == "exit" then
-                self.indicator:Stop()
-            end
-        end
-        self.markerOverlay = markerOverlay
-
-        -- The exciting blue "Quantum gate" mouse-over graphic thing. That *pulsates*.
-        local indicator = Bitmap(self, UIUtil.UIFile('/game/beacons/beacon-quantum-gate_btn_up.dds'))
-        LayoutHelpers.AtCenterIn(indicator, colourBmp)
-        indicator.Height:Set(function() return indicator.BitmapHeight() * .3 end)
-        indicator.Width:Set(function() return indicator.BitmapWidth() * .3 end)
-        indicator.Depth:Set(function() return colourBmp.Depth() - 1 end)
-        indicator:Hide()
-        indicator:DisableHitTest()
-        indicator.Play = function(self)
-            self:SetAlpha(1)
-            self:Show()
-            self:SetNeedsFrameUpdate(true)
-            self.time = 0
-            self.OnFrame = function(control, time)
-                control.time = control.time + (time*4)
-                control:SetAlpha(MATH_Lerp(math.sin(control.time), -.5, .5, 0.3, 0.5))
-            end
-        end
-        indicator.Stop = function(self)
-            self:SetAlpha(0)
-            self:Hide()
-            self:SetNeedsFrameUpdate(false)
-        end
-
-        self.indicator = indicator
-
-        self.OnHide = function(self, hidden)
-            self.markerOverlay:SetHidden(hidden)
-            self.marker:SetHidden(hidden)
-            self.teamIndicator:SetHidden(hidden)
-            return true
-        end
-    end,
-
-    IsEnabled = function(self)
-        return self.enabled
-    end,
-
-    SetEnabled = function(self, enabled)
-        self.enabled = enabled
-    end,
-
-    SetColor = function(self, color)
-        self.marker:SetSolidColor(color)
-    end,
-
-    Clear = function(self)
-        self:SetColor('00777777')
-        self:SetTeam(1)
-    end,
-
-    SetTeam = function(self, team)
-        if team == 1 then
-            self.teamIndicator:SetSolidColor("00000000")
-        else
-            self.teamIndicator:SetTexture(UIUtil.UIFile(teamIcons[team]))
-        end
-    end,
-
-    SetClosed = function(self, closed)
-        -- Opening the slot is a simple matter of deleting the "X".
-        if not closed then
-            if self.textOverlay then
-                self.textOverlay:Destroy()
-                self.textOverlay = nil
-            end
-
-            return
-        end
-
-        -- Closing the slot requires clearing other data set on this button.
-        -- Remove any assigned colour and team.
-        self:Clear()
-
-        -- Put the little red "X" over the little ACU.
-        local textOverlay = Text(self)
-        textOverlay:SetFont(UIUtil.bodyFont, 14)
-        textOverlay:SetColor("Crimson")
-        textOverlay:SetText("X")
-        LayoutHelpers.AtCenterIn(textOverlay, self)
-
-        self.textOverlay = textOverlay
-    end,
-
-    -- Override for events...
-    OnClick = function(self) end,
-    OnRightClick = function(self) end
 }
