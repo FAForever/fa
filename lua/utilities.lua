450e384465bc891ae3308ba4b55ea12ef58147f9
Chris Kitching <chriskitching@linux.com>
2015-04-17 02:02:57 +0100

    Move TableCat into utils.lua
    Turns out that its many uses in EffectTemplates need the constructive concat, but let's put it in the right place (yet more handy functions on `table`, muwhaha)

--- lua/utilities.lua
+++ lua/utilities.lua
@@ -145,18 +145,3 @@ function UserConRequest(string)
     end
     table.insert(Sync.UserConRequests, string)
 end
-
-#---------------------------------------------------------------
-# TableCat - Concatenates multiple tables into one single table
-#---------------------------------------------------------------
-function TableCat( ... )
-    local ret = {}
-    for index = 1, table.getn(arg) do
-        if arg[index] != nil then
-            for k, v in arg[index] do
-                table.insert( ret, v )
-            end
-        end
-    end
-    return ret
-end
