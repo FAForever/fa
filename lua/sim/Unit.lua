aafb02174c2311e7e25f6553426f690d5d615c1b
Crotalus <Crotalus@users.noreply.github.com>
2015-04-21 06:30:13 +0200

    Refactor cybran build bots
    If the engineer is building something after the current finished project the
    same bots will be used.

--- lua/sim/Unit.lua
+++ lua/sim/Unit.lua
@@ -2275,6 +2275,11 @@ Unit = Class(moho.unit_methods) {
 
     StopBuildingEffects = function(self, unitBeingBuilt)
         self.BuildEffectsBag:Destroy()
+        if self.buildBots then
+            for _, b in self.buildBots do
+                ChangeState(b, b.IdleState)
+            end
+        end
     end,
 
     OnStartSacrifice = function(self, target_unit)
--- units/URA0001/URA0001_script.lua
+++ units/URA0001/URA0001_script.lua
@@ -12,6 +12,7 @@ local CAirUnit = import('/lua/cybranunits.lua').CAirUnit
 local CreateCybranBuildBeams = import('/lua/EffectUtilities.lua').CreateCybranBuildBeams
 
 URA0001 = Class(CAirUnit) {
+    spawnedBy = nil,
 
     OnCreate = function(self)
         CAirUnit.OnCreate(self)
@@ -24,7 +25,7 @@ URA0001 = Class(CAirUnit) {
     end,
 
     CreateBuildEffects = function( self, unitBeingBuilt, order )
-        self.BuildEffectsBag:Add(AttachBeamEntityToEntity(self, 'Muzzle_03', self, 'Muzzle_01', self:GetArmy(), '/effects/emitters/build_beam_02_emit.bp'))		
+        self.BuildEffectsBag:Add(AttachBeamEntityToEntity(self, 'Muzzle_03', self, 'Muzzle_01', self:GetArmy(), '/effects/emitters/build_beam_02_emit.bp'))
         self.BuildEffectsBag:Add(AttachBeamEntityToEntity(self, 'Muzzle_03', self, 'Muzzle_02', self:GetArmy(), '/effects/emitters/build_beam_02_emit.bp'))
         CreateCybranBuildBeams( self, unitBeingBuilt, {'Muzzle_03',}, self.BuildEffectsBag )
     end,
@@ -32,6 +33,50 @@ URA0001 = Class(CAirUnit) {
     OnStartReclaim = function(self, target)
         IssueStop( {self} )  -- you can't reclaim!
     end,
+
+    OnStopBuild = function(self, unitBeingBuilt)
+        CAirUnit.OnStopBuild(self, unitBeingBuilt)
+        ChangeState(self, self.IdleState)
+    end,
+
+    IdleState = State {
+        Main = function(self)
+            IssueClearCommands({self})
+            IssueMove({self}, self:GetPosition())
+            WaitSeconds(0.5)
+            IssueMove({self}, self.spawnedBy)
+
+            local delay = 0.1
+            local wait = 0
+
+            while wait < 4 do
+                local pos = self:GetPosition()
+                local bpos = self.spawnedBy:GetPosition()
+
+                if VDist2(pos[1], pos[3], bpos[1], bpos[3]) < 1 then
+                    break
+                end
+
+                wait = wait + delay
+                WaitSeconds(delay)
+            end
+
+            self:Destroy()
+        end,
+    },
+
+    BuildState = State {
+        Main = function(self)
+            local focus = self.spawnedBy:GetFocusUnit()
+
+            if not focus then
+                ChangeState(self, self.IdleState)
+            end
+
+            IssueClearCommands({self})
+            IssueGuard({self}, focus)
+        end,
+    },
 }
 
 TypeClass = URA0001
