f44e11d6984ea9355f00c16a77da67a51ac2546a
Crotalus <Crotalus@users.noreply.github.com>
2015-04-24 17:16:53 +0200

    Cleanup

--- lua/ui/game/gamemain.lua
+++ lua/ui/game/gamemain.lua
@@ -54,10 +54,10 @@ for key, action in currentKeyMap do
                 modifiersKeys["Alt-" .. key] = action
             else
                 WARN("Alt-" .. key .. " is already bind")
-            end        
+            end
         end
     end
-end  
+end
 IN_AddKeyMapTable(modifiersKeys)
 
 
@@ -121,7 +121,7 @@ function OnFirstUpdate()
         import('/lua/ui/game/score.lua').CreateScoreUI()
     end
     PlaySound( Sound { Bank='AmbientTest', Cue='AMB_Planet_Rumble_zoom'} )
-    ForkThread( 
+    ForkThread(
                function()
                    WaitSeconds(1.5)
                    UIZoomTo(avatars, 1)
@@ -393,7 +393,7 @@ function CreateWldUIProvider()
     end
 
     provider.GetPrefetchTextures = function(self)
-        return import('/lua/ui/game/prefetchtextures.lua').prefetchTextures        
+        return import('/lua/ui/game/prefetchtextures.lua').prefetchTextures
     end
 
 end
@@ -432,7 +432,7 @@ function OnSelectionChanged(oldSelection, newSelection, added, removed)
         import('/modules/selectedinfo.lua').ActivateSingleRangeOverlay()
     else
         import('/modules/selectedinfo.lua').DeactivateSingleRangeOverlay()
-    end   
+    end
 
 end
 
@@ -730,10 +730,10 @@ function ReceiveChat(sender, data)
 end
 
 function QuickSave(filename)
-    if SessionIsActive() and 
-        WorldIsPlaying() and 
-        not SessionIsGameOver() and 
-        not SessionIsMultiplayer() and 
+    if SessionIsActive() and
+        WorldIsPlaying() and
+        not SessionIsGameOver() and
+        not SessionIsMultiplayer() and
         not SessionIsReplay() and
         not IsNISMode() then
 
@@ -758,10 +758,10 @@ end
 
 defaultZoom = 1.4
 function SimChangeCameraZoom(newMult)
-    if SessionIsActive() and 
-        WorldIsPlaying() and 
-        not SessionIsGameOver() and 
-        not SessionIsMultiplayer() and 
+    if SessionIsActive() and
+        WorldIsPlaying() and
+        not SessionIsGameOver() and
+        not SessionIsMultiplayer() and
         not SessionIsReplay() and
         not IsNISMode() then
 
@@ -783,8 +783,8 @@ function UiBeat()
         lastObserving = observing
         import('/lua/ui/game/economy.lua').ToggleEconPanel(not observing)
     end
-    if HasCommandLineArg("/syncreplay") and HasCommandLineArg("/gpgnet") then	
-        GpgNetSend("BEAT",GameTick(),GetGameSpeed()) 
+    if HasCommandLineArg("/syncreplay") and HasCommandLineArg("/gpgnet") then
+        GpgNetSend("BEAT",GameTick(),GetGameSpeed())
     end
 end
 
