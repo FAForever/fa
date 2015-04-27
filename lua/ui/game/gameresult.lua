ed9addd196b2d42fc24edc46af469273ba0c1414
Chris Kitching <chriskitching@linux.com>
2015-04-27 20:12:24 +0100

    Don't do the same thing in both branches of an if...

--- lua/ui/game/gameresult.lua
+++ lua/ui/game/gameresult.lua
@@ -43,25 +43,14 @@ function DoGameResult(armyIndex, result)
 	if result != 'score' then
 	
 		if not announced[armyIndex] then
-			if armyIndex == GetFocusArmy() then
-				local armies = GetArmiesTable().armiesTable
-				if result == 'defeat' then
-					SimCallback({Func="GiveResourcesToPlayer", Args={ From=GetFocusArmy(), To=GetFocusArmy(), Mass=0, Energy=0, Loser=armies[armyIndex].nickname},} , true)
-				elseif result == 'victory' then
-					SimCallback({Func="GiveResourcesToPlayer", Args={ From=GetFocusArmy(), To=GetFocusArmy(), Mass=0, Energy=0, Winner=armies[armyIndex].nickname},} , true)
-				elseif result == 'draw' then
-					SimCallback({Func="GiveResourcesToPlayer", Args={ From=GetFocusArmy(), To=GetFocusArmy(), Mass=0, Energy=0, Draw=armies[armyIndex].nickname},} , true)
-				end
-			else
-				local armies = GetArmiesTable().armiesTable
-				if result == 'defeat' then
-					SimCallback({Func="GiveResourcesToPlayer", Args={ From=GetFocusArmy(), To=GetFocusArmy(), Mass=0, Energy=0, Loser=armies[armyIndex].nickname},} , true)
-				elseif result == 'victory' then
-					SimCallback({Func="GiveResourcesToPlayer", Args={ From=GetFocusArmy(), To=GetFocusArmy(), Mass=0, Energy=0, Winner=armies[armyIndex].nickname},} , true)
-				elseif result == 'draw' then
-					SimCallback({Func="GiveResourcesToPlayer", Args={ From=GetFocusArmy(), To=GetFocusArmy(), Mass=0, Energy=0, Draw=armies[armyIndex].nickname},} , true)
-				end
-			end
+            local armies = GetArmiesTable().armiesTable
+            if result == 'defeat' then
+                SimCallback({Func="GiveResourcesToPlayer", Args={ From=GetFocusArmy(), To=GetFocusArmy(), Mass=0, Energy=0, Loser=armies[armyIndex].nickname},} , true)
+            elseif result == 'victory' then
+                SimCallback({Func="GiveResourcesToPlayer", Args={ From=GetFocusArmy(), To=GetFocusArmy(), Mass=0, Energy=0, Winner=armies[armyIndex].nickname},} , true)
+            elseif result == 'draw' then
+                SimCallback({Func="GiveResourcesToPlayer", Args={ From=GetFocusArmy(), To=GetFocusArmy(), Mass=0, Energy=0, Draw=armies[armyIndex].nickname},} , true)
+            end
 
 			announced[armyIndex] = true
 			if armyIndex == GetFocusArmy() then
