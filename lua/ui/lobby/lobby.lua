FAForever/fa

commit 3f1191d70f9ca6f81cb3c2e9ce4c80c92a1f5254

Show clantag in ridiculous CalcConnectionStatus side effects
Fixes #685

lua/ui/lobby/lobby.lua
@@ -708,6 +708,16 @@ function UpdateSlotBackground(slotIndex)
     end
 end
 
+function GetPlayerDisplayName(playerInfo)
+    local playerName = playerInfo.PlayerName
+    local displayName = ""
+    if playerInfo.PlayerClan ~= "" then
+        return string.format("[%s] %s", playerInfo.PlayerClan, playerInfo.PlayerName)
+    else
+        return playerInfo.PlayerName
+    end
+end
+
 -- update the data in a player slot
 -- TODO: With lazyvars, this function should be eliminated. Lazy-value-callbacks should be used
 -- instead to incrementaly update things.
@@ -837,18 +847,9 @@ function SetSlotInfo(slotNum, playerInfo)
         slot.name._text:SetFont('Arial Gras', 12)
     end
 
-
-    local playerName = playerInfo.PlayerName
-    local displayName = ""
-    if playerInfo.PlayerClan ~= "" then
-        displayName = string.format("[%s] %s", playerInfo.PlayerClan, playerInfo.PlayerName)
-    else
-        displayName = playerInfo.PlayerName
-    end
-
     --\\ Stop - Color the Name in Slot by State
     if wasConnected(playerInfo.OwnerID) or isLocallyOwned or not playerInfo.Human then
-        slot.name:SetTitleText(displayName)
+        slot.name:SetTitleText(GetPlayerDisplayName(playerInfo))
         slot.name._text:SetFont('Arial Gras', 15)
         if not table.find(ConnectionEstablished, playerName) then
             if playerInfo.Human and not isLocallyOwned then
@@ -3163,10 +3164,13 @@ function CalcConnectionStatus(peer)
     else
         if not wasConnected(peer.id) then
             local peerSlot = FindSlotForID(peer.id)
-            GUI.slots[peerSlot].name:SetTitleText(peer.name)
-            GUI.slots[peerSlot].name._text:SetFont('Arial Gras', 15)
+            local slot = GUI.slots[peerSlot]
+            local playerInfo = gameInfo.PlayerOptions[peerSlot]
+
+            slot.name:SetTitleText(GetPlayerDisplayName(playerInfo))
+            slot.name._text:SetFont('Arial Gras', 15)
             if not table.find(ConnectionEstablished, peer.name) then
-                if gameInfo.PlayerOptions[peerSlot].Human and not IsLocallyOwned(peerSlot) then
+                if playerInfo.Human and not IsLocallyOwned(peerSlot) then
                     if table.find(ConnectedWithProxy, peer.id) then
                         AddChatText(LOCF("<LOC Engine0032>Connected to %s via the FAF proxy.", peer.name), "Engine0032")
                     end

commit 62d85cbd0c5b0069b79139cf613674c92b7b4501

Fix lobby-breaking regression from 3f1191d70f9ca6f81cb3c2e9ce4c80c92a1f5254

lua/ui/lobby/lobby.lua
@@ -848,6 +848,7 @@ function SetSlotInfo(slotNum, playerInfo)
     end
 
     --\\ Stop - Color the Name in Slot by State
+    local playerName = playerInfo.PlayerName
     if wasConnected(playerInfo.OwnerID) or isLocallyOwned or not playerInfo.Human then
         slot.name:SetTitleText(GetPlayerDisplayName(playerInfo))
         slot.name._text:SetFont('Arial Gras', 15)
@@ -860,7 +861,7 @@ function SetSlotInfo(slotNum, playerInfo)
                 end
 
                 table.insert(ConnectionEstablished, playerName)
-                for k, v in CurrentConnection do -- Remove PlayerName in this Table
+                for k, v in CurrentConnection do
                     if v == playerName then
                         CurrentConnection[k] = nil
                         break