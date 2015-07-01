bd2c7f73f19378d53e74cc69e851dd0a39efb922
Chris Kitching <chriskitching@linux.com>
2015-07-01 19:49:43 +0100

    Trivial cleanup of GetConstructEconomyModel

--- lua/game.lua
+++ lua/game.lua
@@ -73,9 +73,6 @@ BuffFieldBlueprint {                         -- Seraphim ACU Advanced Restoratio
 -- if DifferentialUpgradeCostCalculation is set to true, the base upgrade cost will be substracted
 
 function GetConstructEconomyModel(builder, targetData, upgradeBaseData)
-
-   local builder_bp = builder:GetBlueprint()
-   
    -- 'rate' here is how fast we build relative to a unit with build rate of 1
    local rate = builder:GetBuildRate()
    
@@ -84,36 +81,25 @@ function GetConstructEconomyModel(builder, targetData, upgradeBaseData)
    local energy = targetData.BuildCostEnergy or 0
    
    if upgradeBaseData and targetData.DifferentialUpgradeCostCalculation then
-
-      --LOG("Doing differential upgrade cost calculation")
-      --LOG(time, " ", mass, " ", energy)
-
-      -- We cant make a differential on buildtime. Not sure why but if we do it yields incorrect results. So just mass and energy
-      mass = mass - upgradeBaseData.BuildCostMass
-      energy = energy - upgradeBaseData.BuildCostEnergy
-
-      if mass < 0 then mass = 0 end
-      if energy < 0 then energy = 0 end
-
-      --LOG(time, " ", mass, " ", energy)
+      -- We cant make a differential on buildtime. Not sure why but if we do it yields incorrect
+      -- results. So just mass and energy.
+      mass = math.max(mass - upgradeBaseData.BuildCostMass, 0)
+      energy = math.max(energy - upgradeBaseData.BuildCostEnergy, 0)
    end
 
    -- apply penalties/bonuses to effective buildtime
    local time_mod = builder.BuildTimeModifier or 0
-   buildtime = buildtime * (100 + time_mod)*.01
-   if buildtime<.1 then buildtime = .1 end
-   
+   buildtime = math.max(buildtime * (100 + time_mod) * 0.01, 0.1)
+
    -- apply penalties/bonuses to effective energy cost
    local energy_mod = builder.EnergyModifier or 0
-   energy = energy * (100 + energy_mod)*.01
-   if energy<0 then energy = 0 end
+   energy = math.max(energy * (100 + energy_mod) * 0.01, 0)
    
    -- apply penalties/bonuses to effective mass cost
    local mass_mod = builder.MassModifier or 0
-   mass = mass * (100 + mass_mod)*.01
-   if mass<0 then mass = 0 end
-   
-   return buildtime/rate, energy, mass
+   mass = math.max(mass * (100 + mass_mod) * 0.01, 0)
+
+   return buildtime / rate, energy, mass
 end
 
 -- An EntityCategory set representing the banned units for this game.
