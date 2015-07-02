d59ecb8e0e32709d17991a12e63c8ae0fe4e9355
Chris Kitching <chriskitching@linux.com>
2015-07-02 22:40:59 +0100

    Hide bones at the right times for engineering stations
    Fixes #713

--- units/XRB0204/XRB0204_script.lua
+++ units/XRB0204/XRB0204_script.lua
@@ -13,12 +13,28 @@ XRB0204 = Class(CConstructionStructureUnit)
 {
     OnStartBeingBuilt = function(self, builder, layer)
         CConstructionStructureUnit.OnStartBeingBuilt(self, builder, layer)
-        self:HideBone('xrb0304', true)
-        self:ShowBone('TurretT2', true)
-        self:ShowBone('Door2_B02', true)
-        self:ShowBone('B02', true)
-        self:ShowBone('Attachpoint02', true)
-    end,   
+        local target = self:GetBlueprint().General.UpgradesFrom
+
+        -- Check if we're really being built on top of another unit (as an upgrade).
+        -- We might be being rebuild by the slightly bugtacular SCU REBUILDER behaviour, in which
+        -- case we want to show all our bones anyway.
+        local upos = self:GetPosition()
+        local candidates = GetUnitsInRect(Rect(upos[1], upos[3], upos[1], upos[3]))
+        for k, v in candidates do
+            if target == v:GetBlueprint().BlueprintId then
+                self:HideBone('xrb0304', true)
+                self:ShowBone('TurretT2', true)
+                self:ShowBone('Door2_B02', true)
+                self:ShowBone('B02', true)
+                self:ShowBone('Attachpoint02', true)
+                return
+            end
+        end
+    end,
+
+    OnStopBeingBuilt = function(self, builder, layer)
+        self:ShowBone('xrb0304', true)
+    end,
      
     OnStartBuild = function(self, unitBeingBuilt, order)
         CConstructionStructureUnit.OnStartBuild(self, unitBeingBuilt, order)
--- units/XRB0304/XRB0304_script.lua
+++ units/XRB0304/XRB0304_script.lua
@@ -13,13 +13,26 @@ XRB0304 = Class(CConstructionStructureUnit)
 {
     OnStartBeingBuilt = function(self, builder, layer)
         CConstructionStructureUnit.OnStartBeingBuilt(self, builder, layer)
-        self:HideBone('xrb0304', true)
-        self:ShowBone('TurretT3', true)
-        self:ShowBone('Door3_B03', true)
-        self:ShowBone('B03', true)
-        self:ShowBone('Attachpoint03', true)
-    end,   
-    
+
+        local target = self:GetBlueprint().General.UpgradesFrom
+
+        -- Check if we're really being built on top of another unit (as an upgrade).
+        -- We might be being rebuild by the slightly bugtacular SCU REBUILDER behaviour, in which
+        -- case we want to show all our bones anyway.
+        local upos = self:GetPosition()
+        local candidates = GetUnitsInRect(Rect(upos[1], upos[3], upos[1], upos[3]))
+        for k, v in candidates do
+            if target == v:GetBlueprint().BlueprintId then
+                self:HideBone('xrb0304', true)
+                self:ShowBone('TurretT3', true)
+                self:ShowBone('Door3_B03', true)
+                self:ShowBone('B03', true)
+                self:ShowBone('Attachpoint03', true)
+                return
+            end
+        end
+    end,
+
     OnStartBuild = function(self, unitBeingBuilt, order)
         CConstructionStructureUnit.OnStartBuild(self, unitBeingBuilt, order)
         
