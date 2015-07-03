FAForever/fa

commit 3c85c88d212e4f91912b9842fd144eeb99d84aac

Refactor SafeQuit()
Don't call both ExitGame() and ExitApplication(). The former is called for
non-online stuff.

Also, the function was in "c-boundary" so WaitSeconds() always failed,
thus risking SessionEndGame() not to complete.

Maybe this helps with desynced / corrupted replays, needs to be tested
ofc.

lua/ui/dialogs/eschandler.lua
@@ -12,19 +12,25 @@ local Utils = import('/lua/system/utils.lua')
 
 local quickDialog = false
 
--- Ridiculous evil hack to perform graceful game exits.
--- Instead of just killing the app (which leads everyone to have to wait for a timeout), we call
--- the methods involved in terminating a game session. This will broadcast a message on the next
--- tick that will have everyone else boot us: we just have to keep the app alive long enough for
--- that to happen. A second *should* be sufficient, as more than that would've been lagging like a
--- fish in play anyway...
--- We use safeCall because none of these things will work sensibly if not in a game. It allows those
--- useless calls to be skipped over and an exit to be achieved in the usual case.
 function SafeQuit()
-    ExitGame()
-    Utils.safecall("Don't panic.", SessionEndGame)
-    Utils.safecall("Don't panic.", WaitSeconds, 1)
-    ExitApplication()
+    local full_exit = HasCommandLineArg("/online") or HasCommandLineArg("/gpgnet") or HasCommandLineArg("/replay")
+
+    if not full_exit then
+        ExitGame()
+        return
+    end
+
+    if SessionIsActive() then
+        ForkThread(function ()
+            ConExecute('ren_oblivion true')
+            ConExecute('ren_ui false')
+            SessionEndGame()
+            WaitSeconds(1)
+            ExitApplication()
+        end)
+    else
+        ExitApplication()
+    end
 end
 
 -- Stack of escape handlers. The topmost one is called when escape is pressed.

lua/ui/dialogs/score.lua
@@ -23,6 +23,7 @@ local ItemList = import('/lua/maui/itemlist.lua').ItemList
 local CampaignManager = import('/lua/ui/campaign/campaignmanager.lua')
 local Prefs = import('/lua/user/prefs.lua')
 local hotstats = import('/lua/ui/dialogs/hotstats.lua')
+local EscapeHandler = import('/lua/ui/dialogs/eschandler.lua')
 
 dialog = false
 local currentPage = false
@@ -446,15 +447,11 @@ function CreateSkirmishScreen(victory, showCampaign, operationVictoryTable)
             operationVictoryTable.allSecondary = true
             CampaignManager.OperationVictory(operationVictoryTable, true)
         end
-        # Checking gpgnet too in case we switch to that
-        if HasCommandLineArg("/online") or HasCommandLineArg("/gpgnet") then
-            ExitApplication()
-        else
-            ExitGame()
-        end
+
+        EscapeHandler.SafeQuit()
     end
     Tooltip.AddButtonTooltip(bg.continueBtn, 'PostScore_Quit')
-    
+
     if showCampaign and not operationVictoryTable.success then
         bg.continueBtn.label:SetText(LOC('<LOC _Skip>Skip'))
         bg.continueBtn.HandleEvent = bg.continueBtn.oldHandleEvent

commit c11bdd850e947cdffd0b9fa3943ca3d62c675c81

Make SafeQuit actually close the game again
Regression from 3c85c88
Fixes #619

lua/ui/dialogs/eschandler.lua
@@ -12,14 +12,9 @@ local Utils = import('/lua/system/utils.lua')
 
 local quickDialog = false
 
+-- Terminate the game in a vaguely graceful fashion. This may or may not reduce the amount of times
+-- sudden quits lead to players having to wait for a timeout.
 function SafeQuit()
-    local full_exit = HasCommandLineArg("/online") or HasCommandLineArg("/gpgnet") or HasCommandLineArg("/replay")
-
-    if not full_exit then
-        ExitGame()
-        return
-    end
-
     if SessionIsActive() and not SessionIsReplay() then
         ForkThread(function ()
             ConExecute('ren_oblivion true')