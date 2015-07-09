FAForever/fa

commit 5b639b80675f2c9af2eba8ecadfbb1c1104a119c

Simplify secondary chat line backgrounds

lua/ui/game/chat.lua
@@ -231,14 +231,14 @@ function CreateChatLines()
                 end
             end
         end
-        
-        line.textBG2 = Bitmap(line)
-        line.textBG2.Depth:Set(function() return line.text.Depth() - 1 end)
-        line.textBG2.Left:Set(line.name.Left)
-        line.textBG2.Top:Set(line.teamColor.Top)
-        line.textBG2.Right:Set(line.text.Right)
-        line.textBG2.Bottom:Set(line.teamColor.Bottom)
-        line.textBG2:Disable()
+
+        -- A background for the line that persists after the chat panel is closed (to help with
+        -- readability against the simulation)
+        line.lineStickybg = Bitmap(line)
+        line.lineStickybg:DisableHitTest()
+        line.lineStickybg:SetSolidColor('aa000000')
+        LayoutHelpers.FillParent(line.lineStickybg, line.text)
+        LayoutHelpers.DepthUnderParent(line.lineStickybg, line.text)
 
         return line
     end
@@ -460,9 +460,9 @@ function SetupChatScroll()
                 
                 if GUI.bg:IsHidden() then
                     if ChatOptions.feed_background then
-                        line.textBG2:SetSolidColor('aa000000')
+                        line.lineStickybg:Show()
                     else
-                        line.textBG2:SetSolidColor('00000000')
+                        line.lineStickybg:Hide()
                     end
                 
                     if chatHistory[curEntry].new or line.time == nil then
@@ -859,7 +859,7 @@ function ToggleChat()
             GUI.bg.curTime = 0
         end
         for i, v in GUI.chatLines do
-            v.textBG2:SetSolidColor('00000000')
+            v.lineStickybg:Hide()
             v:SetNeedsFrameUpdate(false)
             v:Show()
         end

commit 6e84c7ea5336b62135b4ef59d05c7bc1594ae286

Fixing chat regression.
A couple regressions introduced in
commit: 5b639b80675f2c9af2eba8ecadfbb1c1104a119c

lua/ui/game/chat.lua
@@ -184,6 +184,7 @@ function CreateChatLines()
 
         -- Draw the faction icon with a colour representing the team behind it.
         line.teamColor = Bitmap(line)
+        line.teamColor.Depth:Set(function() return line.Depth() + 10 end)
         line.teamColor:SetSolidColor('00000000')
         line.teamColor.Height:Set(line.Height)
         line.teamColor.Width:Set(line.Height)
@@ -237,8 +238,9 @@ function CreateChatLines()
         line.lineStickybg = Bitmap(line)
         line.lineStickybg:DisableHitTest()
         line.lineStickybg:SetSolidColor('aa000000')
-        LayoutHelpers.FillParent(line.lineStickybg, line.text)
+        LayoutHelpers.FillParent(line.lineStickybg, line)
         LayoutHelpers.DepthUnderParent(line.lineStickybg, line.text)
+        line.lineStickybg:Hide()
 
         return line
     end
@@ -457,11 +459,6 @@ function SetupChatScroll()
                 line.EntryID = curEntry
                 
                 if GUI.bg:IsHidden() then
-                    if ChatOptions.feed_background then
-                        line.lineStickybg:Show()
-                    else
-                        line.lineStickybg:Hide()
-                    end
                 
                     line.curHistory = chatHistory[curEntry]
                     if line.curHistory.new or line.curHistory.time == nil then
@@ -470,6 +467,13 @@ function SetupChatScroll()
                     
                     if line.curHistory.time < ChatOptions.fade_time then
                         line:Show()
+                        
+                        if ChatOptions.feed_background then
+                            line.lineStickybg:Show()
+                        else
+                            line.lineStickybg:Hide()
+                        end
+                        
                         if line.name:GetText() == '' then
                             line.teamColor:Hide()
                         end
@@ -484,6 +488,7 @@ function SetupChatScroll()
                         end
                         line:SetNeedsFrameUpdate(true)
                     end
+                    
                 end
             else
                 line.name:Disable()