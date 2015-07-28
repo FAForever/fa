ef378ec240f2a18bf34b7b054755d6014f5edafa
Crotalus <Crotalus@users.noreply.github.com>
2015-07-29 01:06:19 +0200

    Unpause commander after upgrade completes
    In cases where commander is assisted by engineers to reduce total
    buildpower of upgrade the commander now unpauses after the upgrade is
    complete

--- lua/sim/tasks/EnhanceTask.lua
+++ lua/sim/tasks/EnhanceTask.lua
@@ -91,6 +91,11 @@ EnhanceTask = Class(ScriptTask) {
             end
 
             unit:OnWorkEnd(self.CommandData.Enhancement)
+
+            if unit:IsPaused() then
+                unit:SetPaused(false)
+            end
+
             self.Success = true
 
             return TASKSTATUS.Done
