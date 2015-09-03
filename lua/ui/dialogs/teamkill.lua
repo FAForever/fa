62f15605422976621037e28b07740b1bbc180602
quark036 <quark036@gmail.com>
2015-09-03 17:32:23 -0600

    Prevent accidentally closing, adjust dialogue

--- lua/ui/dialogs/teamkill.lua
+++ lua/ui/dialogs/teamkill.lua
@@ -21,41 +21,48 @@ function CreateDialog(teamkillTable)
     end
 	
     local dialogContent = Group(GetFrame(0))
-    dialogContent.Width:Set(600)
-    dialogContent.Height:Set(200)
+    dialogContent.Width:Set(360)
+    dialogContent.Height:Set(180)
 
     dialog = Popup(GetFrame(0), dialogContent)
 
     local title = UIUtil.CreateText(dialogContent, "<LOC teamkill_0001>Teamkill Detected", 14, UIUtil.titleFont)
-    LayoutHelpers.AtTopIn(title, dialogContent, 5)
+    LayoutHelpers.AtTopIn(title, dialogContent, 10)
     LayoutHelpers.AtHorizontalCenterIn(title, dialogContent)
 
-    local infoText = TextArea(dialogContent, 590, 100)
-    infoText:SetText(LOC("<LOC teamkill_0002>You have been killed by friendly fire. The deliberate killing of teammates is against FAF rules. If you feel your death was deliberate or unsportsmanlike, check the box below to report it."))
+    local infoText = TextArea(dialogContent, 340, 100)
+    infoText:SetText(LOC("<LOC teamkill_0002>You have been killed by friendly fire. The deliberate killing of teammates is against FAF rules. If you feel your death was deliberate or unsportsmanlike, press the button below to report it."))
     LayoutHelpers.Below(infoText, title)
-    LayoutHelpers.AtLeftIn(infoText, dialogContent, 5)
+    LayoutHelpers.AtLeftIn(infoText, dialogContent, 10)
 
-    local reportToMod = UIUtil.CreateCheckbox(dialogContent, '/CHECKBOX/', "<LOC teamkill_0003>Report this to a mod", true, 11)
-    LayoutHelpers.AtBottomIn(reportToMod, dialogContent, 15)
-    LayoutHelpers.AtLeftIn(reportToMod, dialogContent, 5)
-
-    local okBtn = UIUtil.CreateButtonWithDropshadow(dialogContent, '/BUTTON/medium/', "<LOC _Ok>")
-    LayoutHelpers.AtHorizontalCenterIn(okBtn, dialogContent)
-    LayoutHelpers.AtBottomIn(okBtn, dialogContent, 5)
-    okBtn.OnClick = function(self, modifiers)
+    local forgiveBtn = UIUtil.CreateButtonWithDropshadow(dialogContent, '/BUTTON/medium/', "<LOC teamkill_0004>Forgive")
+    LayoutHelpers.AtBottomIn(forgiveBtn, dialogContent, 10)
+    LayoutHelpers.AtRightIn(forgiveBtn, dialogContent, 10)
+    forgiveBtn.OnClick = function(self, modifiers)
         dialog:Close()
     end
-
+    
+    local reportBtn = UIUtil.CreateButtonWithDropshadow(dialogContent, '/BUTTON/medium/', "<LOC teamkill_0003>Report")
+    LayoutHelpers.AtBottomIn(reportBtn, dialogContent, 10)
+    LayoutHelpers.AtLeftIn(reportBtn, dialogContent, 10)
+    reportBtn.OnClick = function(self, modifiers)
+        local armiesInfo = GetArmiesTable()
+        local victimName = armiesInfo.armiesTable[teamkillTable.victim].nickname
+        local killerName = armiesInfo.armiesTable[teamkillTable.instigator].nickname
+        WARN("Was teamkilled: " .. victimName)
+        WARN("At time: " .. killTime)
+        WARN("Killed by: " .. killerName)
+        GpgNetSend('Teamkill',  killTime,victimName,killerName)
+        dialog:Close()
+    end
+    
     dialog.OnClosed = function(self)
         dialog = false
-        if reportToMod:IsChecked() then
-            local armiesInfo = GetArmiesTable()
-            local victimName = armiesInfo.armiesTable[teamkillTable.victim].nickname
-            local killerName = armiesInfo.armiesTable[teamkillTable.instigator].nickname
-            WARN("Was teamkilled: " .. victimName)
-            WARN("At time: " .. killTime)
-            WARN("Killed by: " .. killerName)
-            GpgNetSend('Teamkill',  killTime,victimName,killerName)
-        end
+    end
+    
+    dialog.OnEscapePressed = function(self)
+    end
+    
+    dialog.OnShadowClicked = function(self)
     end
 end
