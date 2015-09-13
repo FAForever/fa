e1c440e2754b48fa2e880b15e0725b5aed5f2046
speed2CZ <GTGTGT@seznam.cz>
2015-09-13 23:18:12 +0200

    Disable resources sharing for AI in Coop
    Fixes https://github.com/FAForever/fa-coop/issues/13

--- lua/aibrain.lua
+++ lua/aibrain.lua
@@ -423,7 +423,7 @@ AIBrain = Class(moho.aibrain_methods) {
             AMPlatoonCount = { DefaultGroupAir = 0, DefaultGroupLand = 0, DefaultGroupSea = 0, },
         }
 
-        if ScenarioInfo.type == 'campaign' then
+        if ScenarioInfo.type == 'campaign' or ScenarioInfo.type == 'campaign_coop' then
             self:SetResourceSharing(false)
         end
 
