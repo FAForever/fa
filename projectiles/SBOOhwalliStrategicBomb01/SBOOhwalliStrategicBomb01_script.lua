9b5eb04c6c03c37ad8ea3c6cba8047507b17dfe4
Crotalus <Crotalus@users.noreply.github.com>
2015-09-22 13:28:03 +0200

    Deal damage to air units that fly through ahwbomb

--- projectiles/SBOOhwalliStrategicBomb01/SBOOhwalliStrategicBomb01_script.lua
+++ projectiles/SBOOhwalliStrategicBomb01/SBOOhwalliStrategicBomb01_script.lua
@@ -9,10 +9,25 @@
 --****************************************************************************
 local SOhwalliStrategicBombProjectile = import('/lua/seraphimprojectiles.lua').SOhwalliStrategicBombProjectile
 
-SBOOhwalliStategicBomb01 = Class(SOhwalliStrategicBombProjectile){
+SBOOhwalliStategicBomb01 = Class(SOhwalliStrategicBombProjectile) {
+    Collisions = {},
+    OnCreate = function(self)
+        SOhwalliStrategicBombProjectile.OnCreate(self)
+        self:SetCollisionShape('Sphere', 0, 0, 0, 2)
+        self:CollideEntity(true)
+    end,
+
     OnImpact = function(self, TargetType, TargetEntity)
         self:CreateProjectile('/effects/entities/SBOOhwalliBombEffectController01/SBOOhwalliBombEffectController01_proj.bp', 0, 0, 0, 0, 0, 0):SetCollision(false)
         SOhwalliStrategicBombProjectile.OnImpact(self, TargetType, TargetEntity) 
+    end,
+
+    OnCollisionCheck = function(self, other)
+        if other ~= self and not self.Collisions[other:GetEntityId()] and EntityCategoryContains(categories.AIR, other) then
+            Damage(self:GetLauncher() or self, self:GetPosition(), other, self.DamageData.DamageAmount, 'Normal')
+        end
+        
+        return SOhwalliStrategicBombProjectile.OnCollisionCheck(self, other)
     end,
 }
 TypeClass = SBOOhwalliStategicBomb01
