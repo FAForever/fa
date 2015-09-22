84c28d1499221f6ec27852fb7362d63f1dc15232
Crotalus <Crotalus@users.noreply.github.com>
2015-09-22 13:17:51 +0200

    Add Ahwassa projectile blueprints

---
+++ projectiles/SBOOhwalliStrategicBomb01/SBOOhwalliStrategicBomb01_script.lua
@@ -0,0 +1,18 @@
+#****************************************************************************
+#**
+#**  File     :  /data/projectiles/SBOOhwalliStategicBomb01/SBOOhwalliStategicBomb01_script.lua
+#**  Author(s):  Greg Kohne, Gordon Duclos, Matt Vainio
+#**
+#**  Summary  :  Ohwalli-Strategic Bomb script, used on XSA402
+#**
+#**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+local SOhwalliStrategicBombProjectile = import('/lua/seraphimprojectiles.lua').SOhwalliStrategicBombProjectile
+
+SBOOhwalliStategicBomb01 = Class(SOhwalliStrategicBombProjectile){
+    OnImpact = function(self, TargetType, TargetEntity)
+        self:CreateProjectile('/effects/entities/SBOOhwalliBombEffectController01/SBOOhwalliBombEffectController01_proj.bp', 0, 0, 0, 0, 0, 0):SetCollision(false)
+        SOhwalliStrategicBombProjectile.OnImpact(self, TargetType, TargetEntity) 
+    end,
+}
+TypeClass = SBOOhwalliStategicBomb01
