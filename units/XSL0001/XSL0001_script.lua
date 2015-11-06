78e1d50fbaf34d1fa1b3534a21acf7667d947453
IceDreamer <ben-knight@bluebottle.com>
2015-11-06 21:00:27 +0000

    Fix problems with upgrades not stacking right, and properly execute a ceiling

--- units/XSL0001/XSL0001_script.lua
+++ units/XSL0001/XSL0001_script.lua
@@ -139,16 +139,16 @@ XSL0001 = Class(ACUUnit) {
                         Regen = {
                             Add = 0,
                             Mult = bp.RegenPerSecond or 0.1,
+                            Ceil = bp.RegenCeiling,
                         },
                     },
                 }
 
                 if enh == 'AdvancedRegenAura' then
-                    buff_bp.Affects.MaxHealth =
-                        {
-                            Add = 0,
-                            Mult = bp.MaxHealthFactor or 1.0,
-                            DoNoFill = true,
+                    buff_bp.Affects.MaxHealth = {
+                        Add = 0,
+                        Mult = bp.MaxHealthFactor or 1.0,
+                        DoNoFill = true,
                     }
                 end
 
