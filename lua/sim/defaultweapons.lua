0dd53f5914aaaecda59ed37ab0d62ed42b040fe6
IceDreamer <ben-knight@bluebottle.com>
2015-11-20 04:16:37 +0000

    Create default nuclear death weapon

--- lua/sim/defaultweapons.lua
+++ lua/sim/defaultweapons.lua
@@ -1221,3 +1221,31 @@ DefaultBeamWeapon = Class(DefaultProjectileWeapon) {
         return true
     end,
 }
+
+local NukeDamage = import('/lua/sim/NukeDamage.lua').NukeAOE
+DeathNukeWeapon = Class(BareBonesWeapon) {   
+    OnFire = function(self)
+    end,
+
+    Fire = function(self)
+        local bp = self:GetBlueprint()
+        local proj = self.unit:CreateProjectile(bp.ProjectileId, 0, 0, 0, nil, nil, nil):SetCollision(false)
+        proj:ForkThread(proj.EffectThread)
+        
+        -- Play the explosion sound
+        local projBp = proj:GetBlueprint()
+        if projBp.Audio.NukeExplosion then
+            self:PlaySound(projBp.Audio.NukeExplosion)
+        end
+        
+        proj.InnerRing = NukeDamage()
+        proj.InnerRing:OnCreate(bp.NukeInnerRingDamage, bp.NukeInnerRingRadius, bp.NukeInnerRingTicks, bp.NukeInnerRingTotalTime)
+        proj.OuterRing = NukeDamage()
+        proj.OuterRing:OnCreate(bp.NukeOuterRingDamage, bp.NukeOuterRingRadius, bp.NukeOuterRingTicks, bp.NukeOuterRingTotalTime)
+        
+        local firer = self.unit
+        local pos = proj:GetPosition()
+        proj.InnerRing:DoNukeDamage(firer, pos)
+        proj.OuterRing:DoNukeDamage(firer, pos)
+    end,
+}
