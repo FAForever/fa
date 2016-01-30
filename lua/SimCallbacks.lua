06eef2349e0095db3a7381378c509be4a6cc328b
Crotalus <Crotalus@users.noreply.github.com>
2016-01-30 04:49:46 +0100

    Fix random 'X is trying to cheat'
    By ordering one of your factories to assist an ally's factory you got the
    message "X is trying to cheat" when OkayToMessWithArmy was called on the
    non-controllable allied factory

--- lua/SimCallbacks.lua
+++ lua/SimCallbacks.lua
@@ -183,8 +183,10 @@ Callbacks.GiveOrders = import('/lua/spreadattack.lua').GiveOrders
 Callbacks.ValidateAssist = function(data, units)
     local target = GetEntityById(data.target)
     if units and target then
+        if GetFocusArmy() ~= target:GetArmy() then return end
+
         for k, u in units do
-            if IsEntity(u) and OkayToMessWithArmy(target:GetArmy()) and IsInvalidAssist(u, target) then
+            if IsEntity(u) and IsInvalidAssist(u, target) then
                 IssueClearCommands({target})
                 return
             end
