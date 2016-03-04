48428ea76261f977991f5a084bf5c532e3fe2892
Crotalus <Crotalus@users.noreply.github.com>
2016-03-04 15:31:23 +0100

    UnitsAroundPoints without argument covers all

--- lua/sim/DamageArea.lua
+++ lua/sim/DamageArea.lua
@@ -2,25 +2,22 @@ local oldDamageArea = DamageArea
 
 -- Trying to mimic DamageArea as good as possible, used for nukes to bypass the bubble damage absorbation of shields.
 DamageArea = function(instigator, location, radius, damage, type, damageAllies, damageSelf)
-    local rect = {location[1]-radius, location[3]-radius, location[1]+radius, location[1]+radius}
     local army = instigator:GetArmy()
+    local units = instigator:GetAIBrain():GetUnitsAroundPoint(categories.ALLUNITS, location, radius)
 
-    local units = GetUnitsInRect(unpack(rect)) or {}
     for _, u in units do
-        if VDist3(u:GetPosition(), location) <= radius then
-            if instigator == u then
-                if damageSelf then
-                    local vector = import('/lua/utilities.lua').GetDirectionVector(location, u:GetPosition())
-                    -- need this ugliness due to Damage() refuse to damage when instigator == u
-                    instigator:OnDamage(instigator, damage, vector, type)
-                end
-            elseif damageAllies or not IsAlly(army, u:GetArmy()) then
-                Damage(instigator, location, u, damage, type)
+        if instigator == u then
+            if damageSelf then
+                local vector = import('/lua/utilities.lua').GetDirectionVector(location, u:GetPosition())
+                -- need this ugliness due to Damage() refuse to damage when instigator == u
+                instigator:OnDamage(instigator, damage, vector, type)
             end
+        elseif damageAllies or not IsAlly(army, u:GetArmy()) then
+            Damage(instigator, location, u, damage, type)
         end
     end
 
-    local reclaim = GetReclaimablesInRect(unpack(rect)) or {}
+    local reclaim = GetReclaimablesInRect(location[1]-radius, location[3]-radius, location[1]+radius, location[1]+radius) or {}
     for _, r in reclaim do
         if IsProp(r) and VDist3(r:GetPosition(), location) <= radius then
             Damage(instigator, location, r, damage, type)
