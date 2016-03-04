577200029c6138db77c8ef43669a4ea7a7bd3921
Crotalus <Crotalus@users.noreply.github.com>
2016-03-04 14:45:04 +0100

    Fix bugs with the alternative DamageArea
    Due to DamageArea @ DamageArea.lua was defined local it actually returned
    the real DamageArea from that import, so half of the testing was on the
    original funcftion.
    
    brain:GetUnitsAroundPoints() sucks, can only return Enemy / Ally /
    Neutral, not all of them at the same time, so need to use GetUnitsInRect
    instead.
    
    GetReclaimablesInRect also returns reclaimable, alive units so we make
    sure it's a prop
    
    Damage() won't do damage if instigator == unit so have to call OnDamage
    manually in this case

--- lua/sim/DamageArea.lua
+++ lua/sim/DamageArea.lua
@@ -1,25 +1,32 @@
+local oldDamageArea = DamageArea
+
 -- Trying to mimic DamageArea as good as possible, used for nukes to bypass the bubble damage absorbation of shields.
-local DamageArea = function(instigator, location, radius, damage, type, damageAllies, damageSelf)
-    local units = instigator:GetAIBrain():GetUnitsAroundPoint(categories.ALLUNITS, location, radius, not damageAllies and 'Enemy' or 'Neutral')
+DamageArea = function(instigator, location, radius, damage, type, damageAllies, damageSelf)
+    local rect = {location[1]-radius, location[3]-radius, location[1]+radius, location[1]+radius}
+    local army = instigator:GetArmy()
 
-    -- edge case where you manually need to add the instigator
-    if not damageAllies and damageSelf and instigator and VDist3(instigator:GetPosition(), location) <= radius then
-        table.insert(units, instigator)
-    end
-
+    local units = GetUnitsInRect(unpack(rect)) or {}
     for _, u in units do
-        if u ~= instigator or damageSelf then
-            Damage(instigator, location, u, damage, type)
+        if VDist3(u:GetPosition(), location) <= radius then
+            if instigator == u then
+                if damageSelf then
+                    local vector = import('/lua/utilities.lua').GetDirectionVector(location, u:GetPosition())
+                    -- need this ugliness due to Damage() refuse to damage when instigator == u
+                    instigator:OnDamage(instigator, damage, vector, type)
+                end
+            elseif damageAllies or not IsAlly(army, u:GetArmy()) then
+                Damage(instigator, location, u, damage, type)
+            end
         end
     end
 
-    local reclaim = GetReclaimablesInRect(location[1]-radius, location[3]-radius, location[1]+radius, location[3]+radius)
+    local reclaim = GetReclaimablesInRect(unpack(rect)) or {}
     for _, r in reclaim do
-        if VDist3(r:GetPosition(), location) <= radius then
+        if IsProp(r) and VDist3(r:GetPosition(), location) <= radius then
             Damage(instigator, location, r, damage, type)
         end
     end
 
      -- Get rid of trees
-     DamageArea(instigator, location, radius, 1, 'Force', false, false)
+     oldDamageArea(instigator, location, radius, 1, 'Force', false, false)
 end
