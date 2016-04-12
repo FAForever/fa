255f54b41ba0e523c14ed898f4409179d6df983b
Crotalus <Crotalus@users.noreply.github.com>
2016-04-12 20:43:01 +0200

    New function: Damage.Area
    It supports named parameters i.e. order of arguments doesn't matter:
    Damage.Area{instigator=i, pos=pos, damage=10, radius=10}
    
    If an argument is not supplied it fall backs to default values when
    possible
    
    The function also handles DoT when supplied 'ticks' and 'duration' args

--- lua/sim/DamageArea.lua
+++ lua/sim/DamageArea.lua
@@ -1,30 +1,49 @@
+local utils = import('/lua/utilities.lua')
 local oldDamageArea = DamageArea
 
 -- Trying to mimic DamageArea as good as possible, used for nukes to bypass the bubble damage absorbation of shields.
-DamageArea = function(instigator, location, radius, damage, type, damageAllies, damageSelf, brain, army)
-    local rect = Rect(location[1]-radius, location[3]-radius, location[1]+radius, location[3]+radius)
+DamageArea = function(instigator, pos, radius, damage, type, damageAllies, damageSelf)
+    local rect = Rect(pos[1]-radius, pos[3]-radius, pos[1]+radius, pos[3]+radius)
     local units = GetUnitsInRect(rect) or {}
+    local army = instigator:GetArmy()
 
     for _, u in units do
-        if VDist3(u:GetPosition(), location) > radius then continue end
+        if VDist3(u:GetPosition(), pos) > radius then continue end
         if instigator == u then
             if damageSelf then
-                local vector = import('/lua/utilities.lua').GetDirectionVector(location, u:GetPosition())
                 -- need this ugliness due to Damage() refuse to damage when instigator == u
-                instigator:OnDamage(instigator, damage, vector, type)
+                instigator:OnDamage(instigator, damage, utils.GetDirectionVector(pos, u:GetPosition()), type)
             end
         elseif damageAllies or not IsAlly(army, u:GetArmy()) then
-            Damage(instigator, location, u, damage, type)
+            Damage(instigator, pos, u, damage, type)
         end
     end
-    
-    local reclaim = GetReclaimablesInRect(location[1]-radius, location[3]-radius, location[1]+radius, location[1]+radius) or {}
+
+    local reclaim = GetReclaimablesInRect(pos[1]-radius, pos[3]-radius, pos[1]+radius, pos[1]+radius) or {}
     for _, r in reclaim do
-        if IsProp(r) and VDist3(r:GetPosition(), location) <= radius then
-            Damage(instigator, location, r, damage, type)
+        if IsProp(r) and VDist3(r:GetPosition(), pos) <= radius then
+            Damage(instigator, pos, r, damage, type)
         end
     end
 
      -- Get rid of trees
-     oldDamageArea(instigator, location, radius, 1, 'Force', false, false)
+     oldDamageArea(instigator, pos, radius, 1, 'Force', false, false)
+end
+
+Area = function(p)
+    table.assimilate(p,  {type='Normal', allies=true, self=true, ticks=1, duration=0})
+    local width = p.ticks > 1 and p.duration > 0 and (p.radius / p.ticks) or p.radius
+
+    DamageArea(p.instigator, p.pos, width, p.damage, p.type, p.allies, p.self)
+
+    -- deal more damage over time
+    if width < p.radius then
+        local delay = (p.duration / p.ticks)
+        ForkThread(function()
+            for i=2, p.ticks do
+                WaitSeconds(delay)
+                DamageRing(p.instigator, p.pos, width * (i - 1), width * i, p.damage, p.type, p.allies, p.self)
+            end
+        end)
+    end
 end
