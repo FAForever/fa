f3dac815cdb516c50753cdb49aa3e7b1749cfd3d
IceDreamer <ben-knight@bluebottle.com>
2016-05-13 21:02:20 +0100

    Use the laser to control targeting exclusively

--- units/drlk001/DRLK001_Script.lua
+++ units/drlk001/DRLK001_Script.lua
@@ -12,66 +12,32 @@ local Effects = import('/lua/effecttemplates.lua')
 
 DRLK001 = Class(CWalkingLandUnit) {
     Weapons = {
-		AAGun = Class(CAANanoDartWeapon) {
-            IdleState = State(CAANanoDartWeapon.IdleState) {
-                Main = function(self)
-                    self.unit:SetWeaponEnabledByLabel('GroundGun', true)
-                    self.unit:GetWeaponManipulatorByLabel('GroundGun'):SetHeadingPitch(self:GetWeaponManipulatorByLabel('AAGun'):GetHeadingPitch())
-                    CAANanoDartWeapon.IdleState.Main(self)
-                end,
-                
-                OnGotTarget = function(self)
-                    self.unit:SetWeaponEnabledByLabel('GroundGun', false)
-                    CAANanoDartWeapon.IdleState.OnGotTarget(self)
-                end,
-                
-                OnFire = function(self)
-                    self.unit:SetWeaponEnabledByLabel('GroundGun', false)
-                    CAANanoDartWeapon.IdleState.OnFire(self)
-                end,
-            },
-        },    
+		AAGun = Class(CAANanoDartWeapon) {},    
 		Lazor = Class(TargetingLaser) {
             FxMuzzleFlash = {'/effects/emitters/particle_cannon_muzzle_02_emit.bp'},
             
             IdleState = State(TargetingLaser.IdleState) {
+            
+                -- Start with the AA gun off to reduce twitching of ground fire
                 Main = function(self)
+                    WARN('Idle')
                     self.unit:SetWeaponEnabledByLabel('GroundGun', true)
-                    self.unit:GetWeaponManipulatorByLabel('GroundGun'):SetHeadingPitch(self:GetWeaponManipulatorByLabel('AAGun'):GetHeadingPitch())
+                    self.unit:SetWeaponEnabledByLabel('AAGun', false)
+                    self.unit:GetWeaponManipulatorByLabel('GroundGun'):SetHeadingPitch(self.unit:GetWeaponManipulatorByLabel('AAGun'):GetHeadingPitch())
                     TargetingLaser.IdleState.Main(self)
                 end,
                 
-                OnGotTarget = function(self)
-                    self.unit:SetWeaponEnabledByLabel('GroundGun', false)
-                    TargetingLaser.IdleState.OnGotTarget(self)
-                end,
-                
+                -- Unit in range. Ceasee ground fire and turn on AA
                 OnFire = function(self)
                     self.unit:SetWeaponEnabledByLabel('GroundGun', false)
+                    self.unit:SetWeaponEnabledByLabel('AAGun', true)
+                    self.unit:GetWeaponManipulatorByLabel('AAGun'):SetHeadingPitch(self.unit:GetWeaponManipulatorByLabel('GroundGun'):GetHeadingPitch())
                     TargetingLaser.IdleState.OnFire(self)
                 end,
             },
             
         },
-		GroundGun = Class(CAANanoDartWeapon) {
-            IdleState = State(CAANanoDartWeapon.IdleState) {
-                Main = function(self)
-                    self.unit:SetWeaponEnabledByLabel('AAGun', true)
-                    self.unit:GetWeaponManipulatorByLabel('AAGun'):SetHeadingPitch(self:GetWeaponManipulatorByLabel('GroundGun'):GetHeadingPitch())
-                    CAANanoDartWeapon.IdleState.Main(self)
-                end,
-                
-                OnGotTarget = function(self)
-                    self.unit:SetWeaponEnabledByLabel('AAGun', false)
-                    CAANanoDartWeapon.IdleState.OnGotTarget(self)
-                end,
-                
-                OnFire = function(self)
-                    self.unit:SetWeaponEnabledByLabel('AAGun', false)
-                    CAANanoDartWeapon.IdleState.OnFire(self)
-                end,
-            },
-		},
+		GroundGun = Class(CAANanoDartWeapon) {},
     },
 }
 
