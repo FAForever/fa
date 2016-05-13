5aec29b855cc7c4e85b00729aaf547ddcb3a264c
IceDreamer <ben-knight@bluebottle.com>
2016-05-13 23:47:43 +0100

    Don't use OnFire

--- units/drlk001/DRLK001_Script.lua
+++ units/drlk001/DRLK001_Script.lua
@@ -16,23 +16,25 @@ DRLK001 = Class(CWalkingLandUnit) {
 		Lazor = Class(TargetingLaser) {
             FxMuzzleFlash = {'/effects/emitters/particle_cannon_muzzle_02_emit.bp'},
             
-            IdleState = State(TargetingLaser.IdleState) {
-            
-                -- Start with the AA gun off to reduce twitching of ground fire
-                Main = function(self)
-                    WARN('Idle')
-                    self.unit:SetWeaponEnabledByLabel('GroundGun', true)
-                    self.unit:SetWeaponEnabledByLabel('AAGun', false)
-                    self.unit:GetWeaponManipulatorByLabel('GroundGun'):SetHeadingPitch(self.unit:GetWeaponManipulatorByLabel('AAGun'):GetHeadingPitch())
-                    TargetingLaser.IdleState.Main(self)
-                end,
-                
-                -- Unit in range. Ceasee ground fire and turn on AA
-                OnFire = function(self)
+            -- Unit in range. Cease ground fire and turn on AA
+            OnWeaponFired = function(self)
+                if not self.AA then
                     self.unit:SetWeaponEnabledByLabel('GroundGun', false)
                     self.unit:SetWeaponEnabledByLabel('AAGun', true)
                     self.unit:GetWeaponManipulatorByLabel('AAGun'):SetHeadingPitch(self.unit:GetWeaponManipulatorByLabel('GroundGun'):GetHeadingPitch())
-                    TargetingLaser.IdleState.OnFire(self)
+                    self.AA = true
+                end
+                TargetingLaser.OnWeaponFired(self)
+            end,
+            
+            IdleState = State(TargetingLaser.IdleState) {
+                -- Start with the AA gun off to reduce twitching of ground fire
+                Main = function(self)
+                    self.unit:SetWeaponEnabledByLabel('GroundGun', true)
+                    self.unit:SetWeaponEnabledByLabel('AAGun', false)
+                    self.unit:GetWeaponManipulatorByLabel('GroundGun'):SetHeadingPitch(self.unit:GetWeaponManipulatorByLabel('AAGun'):GetHeadingPitch())
+                    self.AA = false
+                    TargetingLaser.IdleState.Main(self)
                 end,
             },
             
