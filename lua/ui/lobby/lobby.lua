cecca45c620d4d47a41f311f14c3a6c972c008b7
Crotalus <Crotalus@users.noreply.github.com>
2016-05-17 14:26:40 +0200

    Nicer message to someone kicked due to game launch

--- lua/ui/lobby/lobby.lua
+++ lua/ui/lobby/lobby.lua
@@ -1824,7 +1824,7 @@ local function TryLaunch(skipNoObserversCheck)
             return
         end
 
-        HostUtils.KickObservers()
+        HostUtils.KickObservers("GameLaunched")
     end
 
     if not EveryoneHasEstablishedConnections(gameInfo.GameOptions.AllowObservers) then
@@ -6091,9 +6091,9 @@ function InitHostUtils()
             return -1
         end,
 
-        KickObservers = function()
+        KickObservers = function(reason)
             for k,observer in gameInfo.Observers:pairs() do
-                lobbyComm:EjectPeer(observer.OwnerID, "KickedByHost")
+                lobbyComm:EjectPeer(observer.OwnerID, reason or "KickedByHost")
             end
             gameInfo.Observers = WatchedValueArray(LobbyComm.maxPlayerSlots)
         end
