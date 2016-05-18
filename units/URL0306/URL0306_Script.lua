a7ef551f9cca311aae64a7a8d22131629ad1c5d9
Crotalus <Crotalus@users.noreply.github.com>
2016-05-18 13:43:14 +0200

    Cleanup

--- units/URL0306/URL0306_Script.lua
+++ units/URL0306/URL0306_Script.lua
@@ -1,12 +1,12 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/URL0306/URL0306_script.lua
-#**  Author(s):  Jessica St. Croix
-#**
-#**  Summary  :  Cybran Mobile Radar Jammer Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+--****************************************************************************
+--**
+--**  File     :  /cdimage/units/URL0306/URL0306_script.lua
+--**  Author(s):  Jessica St. Croix
+--**
+--**  Summary  :  Cybran Mobile Radar Jammer Script
+--**
+--**  Copyright Â© 2005 Gas Powered Games, Inc.  All rights reserved.
+--****************************************************************************
 
 local CLandUnit = import('/lua/cybranunits.lua').CLandUnit
 local EffectUtil = import('/lua/EffectUtilities.lua')
@@ -16,35 +16,35 @@ URL0306 = Class(CLandUnit) {
         CLandUnit.OnStopBeingBuilt(self,builder,layer)
         self:SetMaintenanceConsumptionActive()
     end,
-    
+
     IntelEffects = {
-		{
-			Bones = {
-				'AttachPoint',
-			},
-			Offset = {
-				0,
-				0.3,
-				0,
-			},
-			Scale = 0.2,
-			Type = 'Jammer01',
-		},
+        {
+            Bones = {
+                'AttachPoint',
+            },
+            Offset = {
+                0,
+                0.3,
+                0,
+            },
+            Scale = 0.2,
+            Type = 'Jammer01',
+        },
     },
-    
+
     OnIntelEnabled = function(self)
         CLandUnit.OnIntelEnabled(self)
         if self.IntelEffects then
-			self.IntelEffectsBag = {}
-			self.CreateTerrainTypeEffects( self, self.IntelEffects, 'FXIdle',  self:GetCurrentLayer(), nil, self.IntelEffectsBag )
-		end
+            self.IntelEffectsBag = {}
+            self.CreateTerrainTypeEffects( self, self.IntelEffects, 'FXIdle',  self:GetCurrentLayer(), nil, self.IntelEffectsBag )
+        end
     end,
 
     OnIntelDisabled = function(self)
         CLandUnit.OnIntelDisabled(self)
         EffectUtil.CleanupEffectBag(self,'IntelEffectsBag')
-    end,    
-    
+    end,
+
 }
 
 TypeClass = URL0306
