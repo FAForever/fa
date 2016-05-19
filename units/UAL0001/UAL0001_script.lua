739982a777dcf050e34cd09c29a079c1a997627a
IceDreamer <ben-knight@bluebottle.com>
2016-05-19 22:41:37 +0100

    Remove regen from Tech and reduce HP boost at T2

--- units/UAL0001/UAL0001_script.lua
+++ units/UAL0001/UAL0001_script.lua
@@ -159,10 +159,6 @@ UAL0001 = Class(ACUUnit) {
                             Add = bp.NewHealth,
                             Mult = 1.0,
                         },
-                        Regen = {
-                            Add = bp.NewRegenRate,
-                            Mult = 1.0,
-                        },
                     },
                 }
             end
@@ -199,10 +195,6 @@ UAL0001 = Class(ACUUnit) {
                         },
                         MaxHealth = {
                             Add = bp.NewHealth,
-                            Mult = 1.0,
-                        },
-                        Regen = {
-                            Add = bp.NewRegenRate,
                             Mult = 1.0,
                         },
                     },
