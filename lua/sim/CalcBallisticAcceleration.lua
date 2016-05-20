aee806418780c81e945f62f79b5285b656fadcf0
Crotalus <Crotalus@users.noreply.github.com>
2016-05-20 13:39:35 +0200

    Use built-in VMult instead + add comments

--- lua/sim/CalcBallisticAcceleration.lua
+++ lua/sim/CalcBallisticAcceleration.lua
@@ -1,9 +1,5 @@
 local XZDist = import('/lua/utilities.lua').XZDistanceTwoVectors
 
-function ScaleV(v, scale)
-    return Vector(v.x * scale, v.y * scale, v.z * scale)
-end
-
 -- This table stores numbers of bombs left in a cluster bomb run
 -- format : bombs_left[entityId] = <n_left>
 local bombs_left = {}
@@ -21,33 +17,51 @@ CalculateBallisticAcceleration = function(weapon, projectile)
         bombs_left[id] = MuzzleSalvoSize
     end
 
-    local proj = {pos=projectile:GetPosition(), vel=ScaleV(Vector(launcher:GetVelocity()), 10)}
+    -- Get projectile position and velocity
+    -- velocity needs to multiplied by 10 due to being returned /tick instead of /s
+    local proj = {pos=projectile:GetPosition(), vel=VMult(Vector(launcher:GetVelocity()), 10)}
     local entity = launcher:GetTargetEntity()
     local target
     if entity and IsUnit(entity) then
-        target = {pos=entity:GetPosition(), vel=ScaleV(Vector(entity:GetVelocity()), 10)}
+        -- target is a entity
+        target = {pos=entity:GetPosition(), vel=VMult(Vector(entity:GetVelocity()), 10)}
     else
+        -- target is something else i.e. attack ground
         target = {pos=weapon:GetCurrentTargetPos(), vel=Vector(0, 0, 0)}
     end
 
+    -- calculate flat(exclude y-axis) distance and velocity between projectile and target
     local dist = {pos=XZDist(proj.pos, target.pos), vel=XZDist(proj.vel, target.vel)}
     if dist.vel == 0 then return acc end
 
     if bp.DropBombShort then
+        -- deliberately drop bomb short by % ratio, could be useful for torpedo bombers
         dist.pos = dist.pos * math.clamp(1 - bp.DropBombShort, 0, 1)
     end
 
-    if bombs_left[id] ~= nil then
+    if bombs_left[id] ~= nil then -- bomber will drop several bombs
+        -- calculate the total length of the carpet bombing, this is multiplied by 0.5
+        -- to get the bombs overlapping a bit
         local len = MuzzleSalvoDelay * dist.vel * 0.5
+        -- calculate the position for this particular bomb
         dist.pos = dist.pos + (len * MuzzleSalvoSize)/2 - len*bombs_left[id]
         bombs_left[id] = bombs_left[id] > 1 and bombs_left[id] - 1 or nil
     end
 
+    -- how many seconds until the bomb hits the target in xz-space
     local time = dist.pos / dist.vel
     if time == 0 then return acc end
 
+    -- find out where the target will be at that point in time (it could be moving)
     target.tpos = {target.pos[1] + time * target.vel[1], 0, target.pos[3] + time * target.vel[3]}
+    -- what is the height at that future position
     target.tpos[2] = GetSurfaceHeight(target.tpos[1], target.tpos[3])
+
+    -- The basic formula for displacement over time is x = v0*t + 0.5*a*t^2
+    -- x: displacement, v0: initial velocity, a: acceleration, t: time
+    -- v0 is zero due to projectiles not inheriting y-speed of bomber
+    -- now we can calculate what acceleration we need to make it hit the target in the y-axis
+    -- a = 2 * (1/t)^2 * x
 
     acc = 2 * math.pow(1 / time , 2) * (proj.pos[2] - target.tpos[2])
 
