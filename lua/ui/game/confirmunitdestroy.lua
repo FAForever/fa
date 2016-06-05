0792a41d60855fec5863198adcd5231ab6eaf724
speed2CZ <gtgtgt@seznam.cz>
2016-06-05 11:42:06 +0200

    Add Self Destruct Confirmation

--- lua/ui/game/confirmunitdestroy.lua
+++ lua/ui/game/confirmunitdestroy.lua
@@ -3,6 +3,7 @@ local UIUtil = import('/lua/ui/uiutil.lua')
 local LayoutHelpers = import('/lua/maui/layouthelpers.lua')
 local Group = import('/lua/maui/group.lua').Group
 local Prefs = import('/lua/user/prefs.lua')
+local options = Prefs.GetFromCurrentProfile('options')
 
 local destructingUnits = {}
 local controls = {}
@@ -16,12 +17,37 @@ function ConfirmUnitDestruction()
             true, {worldCover = false, enterButton = 1, escapeButton = 1})
     else
         local units = GetSelectedUnits()
+
         if units then
-            local unitIds = {}
-            for _, unit in units do
-                table.insert(unitIds, unit:GetEntityId())
+            local dialogue = nil
+
+            if options.confirm_self_destruct == 1 and table.getn(EntityCategoryFilterDown(categories.COMMAND, units)) > 0 then
+                dialogue = '<LOC confirm_0002>You are going to self destruct your ACU, are you sure?'
+            elseif options.confirm_self_destruct == 2 and table.getn(EntityCategoryFilterDown(categories.COMMAND, units)) > 0 then
+                dialogue = '<LOC confirm_0003>Your ACU is among the selected units for self destruction, do you wish to proceed?'
+            elseif options.confirm_self_destruct == 2 then
+                dialogue = '<LOC confirm_0004>Are you sure you wish to destroy the selected unit(s)?'
             end
-            SimCallback({Func = 'ToggleSelfDestruct', Args = {units = unitIds, owner = GetFocusArmy()}})
+
+            if dialogue then
+                UIUtil.QuickDialog(GetFrame(0), dialogue, 
+                '<LOC _Yes>', function()
+                    local unitIds = {}
+                    for _, unit in units do
+                        table.insert(unitIds, unit:GetEntityId())
+                    end
+                    SimCallback({Func = 'ToggleSelfDestruct', Args = {units = unitIds, owner = GetFocusArmy()}})
+                end,
+                '<LOC _No>', nil,
+                nil, nil,
+                true, {worldCover = false, enterButton = 1, escapeButton = 2})
+            else
+                local unitIds = {}
+                for _, unit in units do
+                    table.insert(unitIds, unit:GetEntityId())
+                end
+                SimCallback({Func = 'ToggleSelfDestruct', Args = {units = unitIds, owner = GetFocusArmy()}})
+            end
         end
     end
 end
