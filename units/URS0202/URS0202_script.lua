b59daaf199008600eb5ff1d3abd24479066400d0
IceDreamer <ben-knight@bluebottle.com>
2016-06-16 00:44:08 +0100

    Remove toggle on Cybran cruiser, use invisible beam

--- units/URS0202/URS0202_script.lua
+++ units/URS0202/URS0202_script.lua
@@ -10,37 +10,38 @@ local CybranWeaponsFile = import('/lua/cybranweapons.lua')
 local CDFProtonCannonWeapon = CybranWeaponsFile.CDFProtonCannonWeapon
 local CAANanoDartWeapon = CybranWeaponsFile.CAANanoDartWeapon
 local CAMZapperWeapon02 = CybranWeaponsFile.CAMZapperWeapon02
+local TargetingLaser = import('/lua/kirvesweapons.lua').TargetingLaserInvisible
 
 URS0202 = Class(CSeaUnit) {
     Weapons = {
+        TargetPainter = Class(TargetingLaser) {
+            -- Unit in range. Cease ground fire and turn on AA
+            OnWeaponFired = function(self)
+                if not self.AA then
+                    self.unit:SetWeaponEnabledByLabel('GroundGun', false)
+                    self.unit:SetWeaponEnabledByLabel('AAGun', true)
+                    self.unit:GetWeaponManipulatorByLabel('AAGun'):SetHeadingPitch(self.unit:GetWeaponManipulatorByLabel('GroundGun'):GetHeadingPitch())
+                    self.AA = true
+                end
+                TargetingLaser.OnWeaponFired(self)
+            end,
+
+            IdleState = State(TargetingLaser.IdleState) {
+                -- Start with the AA gun off to reduce twitching of ground fire
+                Main = function(self)
+                    self.unit:SetWeaponEnabledByLabel('GroundGun', true)
+                    self.unit:SetWeaponEnabledByLabel('AAGun', false)
+                    self.unit:GetWeaponManipulatorByLabel('GroundGun'):SetHeadingPitch(self.unit:GetWeaponManipulatorByLabel('AAGun'):GetHeadingPitch())
+                    self.AA = false
+                    TargetingLaser.IdleState.Main(self)
+                end,
+            },
+        },
         ParticleGun = Class(CDFProtonCannonWeapon) {},
         AAGun = Class(CAANanoDartWeapon) {},
         GroundGun = Class(CAANanoDartWeapon) {},
         Zapper = Class(CAMZapperWeapon02) {},
     },
-    
-    OnCreate = function(self)
-        CSeaUnit.OnCreate(self)
-        self:SetWeaponEnabledByLabel('GroundGun', false)
-    end,
-    
-    OnScriptBitSet = function(self, bit)
-        CSeaUnit.OnScriptBitSet(self, bit)
-        if bit == 1 then 
-            self:SetWeaponEnabledByLabel('GroundGun', true)
-            self:SetWeaponEnabledByLabel('AAGun', false)
-            self:GetWeaponManipulatorByLabel('GroundGun'):SetHeadingPitch( self:GetWeaponManipulatorByLabel('AAGun'):GetHeadingPitch() )
-        end
-    end,
-
-    OnScriptBitClear = function(self, bit)
-        CSeaUnit.OnScriptBitClear(self, bit)
-        if bit == 1 then 
-            self:SetWeaponEnabledByLabel('GroundGun', false)
-            self:SetWeaponEnabledByLabel('AAGun', true)
-            self:GetWeaponManipulatorByLabel('AAGun'):SetHeadingPitch( self:GetWeaponManipulatorByLabel('GroundGun'):GetHeadingPitch() )
-        end
-    end,
 }
 
 TypeClass = URS0202
