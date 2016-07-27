81f59f24a7a5173411300df84bffa57645b07eb1
Uveso <Archivar@Zwergenteam.de>
2016-07-27 21:58:36 +0200

    Removing doubled option Share Conditions
    Share Conditions is used from lobbyoptions.lua not options.lua

--- lua/options/options.lua
+++ lua/options/options.lua
@@ -1114,25 +1114,6 @@ extraOpts = {
     },
     {
         default = 1,
-        label = "<LOC lobui_0714>Share Conditions",
-        help = "<LOC lobui_0715>Kill all the units you shared to your allies and send back the units your allies shared with you when you die",
-        key = 'Share',
-        pref = 'Lobby_Share',
-        values = {
-            {
-                text = "<LOC lobui_0716>Full Share",
-                help = "<LOC lobui_0717>You can give units to your allies and they will not be destroyed when you die",
-                key = 'no',
-            },
-            {
-                text = "<LOC lobui_0718>Share Until Death",
-                help = "<LOC lobui_0719>All the units you gave to your allies will be destroyed when you die",
-                key = 'yes',
-            },
-        },
-    },
-    {
-        default = 1,
         label = "<LOC lobui_0720>Score",
         help = "<LOC lobui_0721>Set score on or off during the game",
         key = 'Score',
