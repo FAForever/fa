d075395bec56a8571c5c2767b31951947212688d
HUSSAR-mtrela <martin.trela@gmail.com>
2016-08-13 12:37:28 -0400

    fixed file headers

--- lua/simInit.lua
+++ lua/simInit.lua
@@ -1,5 +1,5 @@
 -- ==========================================================================================
--- * File       : lua/modules/ui/lobby/UnitsManager.lua 
+-- * File       : lua/simInit.lua 
 -- * Authors    : Gas Powered Games, FAF Community, HUSSAR
 -- * Summary    : This is the sim-specific top-level lua initialization file. It is run at initialization time to set up all lua state for the sim.
 -- * Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
