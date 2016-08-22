03c80ccf84d0e8d38afbdced290fb57deb676b3f
Uveso <Archivar@Zwergenteam.de>
2016-08-22 19:35:45 +0200

    Adding new files to repo

---
+++ lua/system/GlobalBaseTemplate.lua
@@ -0,0 +1,61 @@
+#****************************************************************************
+#**
+#**  File     :  /lua/system/GlobalBaseTemplate.lua
+#**
+#**  Summary  :  Global base table and template methods
+#**
+#**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+# Global list of all BaseBuilderTemplates found in the system.
+BaseBuilderTemplates = {}
+
+# 
+BaseBuilderTemplate = {}
+BaseBuilderTemplateDefMeta = {}
+
+BaseBuilderTemplateDefMeta.__index = BaseBuilderTemplateDefMeta
+BaseBuilderTemplateDefMeta.__call = function(...)
+    if type(arg[2]) ~= 'table' then
+        LOG('Invalid BaseBuilderTemplate: ', repr(arg))
+        return
+    end
+    
+    if not arg[2].BaseTemplateName then
+        WARN('Missing BaseTemplateName for BaseBuilderTemplate definition: ',repr(arg))
+        return
+    end
+    
+    if not arg[2].Builders then
+        WARN('Missing Builders for BaseBuilderTemplate definition - BaseTemplateName = ' .. arg[2].BaseTemplateName)
+        return
+    end
+    for k,v in arg[2].Builders do
+        if not BuilderGroups[v] then
+            WARN('Invalid BuilderGroup named - ' .. v .. ' - in BaseBuilderTemplate: ' .. arg[2].BaseTemplateName)
+        end
+    end
+    
+    if not arg[2].ExpansionFunction then
+        WARN('Missing Builders for ExpansionFunction definition - BaseTemplateName = ' .. arg[2].BaseTemplateName)
+        return
+    end
+    
+    if not arg[2].BaseSettings then
+        WARN('Missing BaseSettings for BaseBuilderTemplate definition - BaseTemplateName = ' .. arg[2].BaseTemplateName)
+        return
+    end
+    
+    if BaseBuilderTemplates[arg[2].BaseTemplateName] then
+        WARN('Duplicate BaseBuilderTemplate detected - overriding/appending old: ', arg[2].BaseTemplateName)
+        for k,v in arg[2] do
+            BaseBuilderTemplates[arg[2].BaseTemplateName][k] = v
+        end
+    else
+        BaseBuilderTemplates[arg[2].BaseTemplateName] = arg[2]
+    end
+    #SPEW('BaseBuilderTemplate Registered: ', arg[2].BaseTemplateName)
+    return arg[2].BaseTemplateName
+end
+
+setmetatable(BaseBuilderTemplate, BaseBuilderTemplateDefMeta)
---
+++ lua/system/GlobalPlatoonTemplate.lua
@@ -0,0 +1,62 @@
+#****************************************************************************
+#**
+#**  File     :  /lua/system/GlobalPlatoonTemplate.lua
+#**
+#**  Summary  :  Global buff table and blueprint methods
+#**
+#**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+
+# Global list of all buffs found in the system.
+PlatoonTemplates = {}
+
+# 
+PlatoonTemplate = {}
+PlatoonTemplateDefMeta = {}
+
+PlatoonTemplateDefMeta.__index = PlatoonTemplateDefMeta
+PlatoonTemplateDefMeta.__call = function(...)
+    
+    if type(arg[2]) ~= 'table' then
+        LOG('Invalid PlatoonTemplate: ', repr(arg))
+        return
+    end
+    
+    if not arg[2].Name then
+        LOG('Missing name for PlatoonTemplate definition: ',repr(arg))
+        return
+    end
+    
+    if not arg[2].GlobalSquads and not arg[2].FactionSquads then
+        LOG('Missing GlobalSquads and FactionSquads for PlatoonTemplate definition - requires one: ',repr(arg))
+        return
+    end
+    
+    local oldFactionSquads = false
+    if InitialRegistration and PlatoonTemplates[arg[2].Name] then
+        WARN('Duplicate PlatoonTemplate detected - overriding old: ', arg[2].Name)
+        # Save out any old faction squads in case they aren't being overwritten
+        oldFactionSquads = PlatoonTemplates[arg[2].Name]
+    end
+
+    if not PlatoonTemplates[arg[2].Name] then
+        PlatoonTemplates[arg[2].Name] = {}
+    end
+
+    #SPEW('PlatoonTemplate Registered: ', arg[2].Name)
+    
+    PlatoonTemplates[arg[2].Name] = arg[2]
+    
+    # if there are old faction squads insert the ones that aren't being overridden
+    if oldFactionSquads then
+        for k,v in oldFactionSquads do
+            if not PlatoonTemplates[arg[2].Name].FactionSquads[k] then
+                PlatoonTemplates[arg[2].Name].FactionSquads[k] = v
+            end
+        end
+    end
+    
+    return arg[2].Name
+end
+
+setmetatable(PlatoonTemplate, PlatoonTemplateDefMeta)
