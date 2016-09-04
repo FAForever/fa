b3d885af0156b0fbbd43f598af7b2c14e52bb0ac
IceDreamer <ben-knight@bluebottle.com>
2016-09-04 22:16:40 +0100

    Invert the logic to exclude non-number values as well

--- lua/ui/lobby/trueskill.lua
+++ lua/ui/lobby/trueskill.lua
@@ -461,6 +461,6 @@ function computeQuality(team)
 
     local result = math.exp(expPart) * math.sqrt(sqrtPart)
 
-    if quality <= 0 or quality > 100 then return 0 end
+    if not result >= 0 and not result <= 100 then return 0 end
     return round((result * 100), 2)
 end
