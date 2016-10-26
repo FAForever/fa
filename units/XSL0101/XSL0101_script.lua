df05080f3fafc46c23f4f37bd348cdbdc773e975
IceDreamer <ben-knight@bluebottle.com>
2016-10-26 21:53:17 +0100

    Rework approach to Selen behaviour. Fixes #1627

--- units/XSL0101/XSL0101_script.lua
+++ units/XSL0101/XSL0101_script.lua
@@ -9,13 +9,23 @@ local SDFPhasicAutoGunWeapon = import('/lua/seraphimweapons.lua').SDFPhasicAutoG
 
 XSL0101 = Class(SWalkingLandUnit) {
     Weapons = {
-		LaserTurret = Class(SDFPhasicAutoGunWeapon) {},
+		LaserTurret = Class(SDFPhasicAutoGunWeapon) {
+            OnWeaponFired = function(self)
+                self.unit:RevealUnit()
+
+                -- Each time we fire, reveal cancels CloakThread so we only cloak after firing is over
+                -- Messy, but can't use weapon IdleState because it's called during and immediately after
+                -- construction, and before motion events
+                self.unit.CloakThread = self.unit:ForkThread(self.unit.HideUnit)
+            end,
+        },
     },
 
     -- Toggle disabled
     OnScriptBitSet = function(self, bit)
         if bit == 8 then
             self.Sync.LowPriority = false
+            self:SetWeaponEnabledByLabel('LaserTurret', true)
         else
             SWalkingLandUnit.OnScriptBitSet(self, bit)
         end
@@ -25,6 +35,7 @@ XSL0101 = Class(SWalkingLandUnit) {
     OnScriptBitClear = function(self, bit)
         if bit == 8 then
             self.Sync.LowPriority = true
+            self:SetWeaponEnabledByLabel('LaserTurret', false)
         else
             SWalkingLandUnit.OnScriptBitClear(self, bit)
         end
@@ -36,43 +47,51 @@ XSL0101 = Class(SWalkingLandUnit) {
             self.CloakThread = nil
         end
 
-        self:SetFireState(0)
+        -- Ensure weapon state
+        self:SetWeaponEnabledByLabel('LaserTurret', true)
+        
+        -- Toggle stealth off
         self:SetMaintenanceConsumptionInactive()
         self:DisableUnitIntel('ToggleBit5', 'RadarStealth')
         self:DisableUnitIntel('ToggleBit8', 'Cloak')
+    end,
+
+    HideUnit = function(self)
+        if not self.Dead and self:GetFractionComplete() == 1 then
+            WaitSeconds(self:GetBlueprint().Intel.StealthWaitTime)
+
+            -- Ensure weapon state
+            if self.Sync.LowPriority then
+                self:SetWeaponEnabledByLabel('LaserTurret', false)
+            end
+
+            -- Toggle stealth on
+            self:SetMaintenanceConsumptionActive()
+            self:EnableUnitIntel('ToggleBit5', 'RadarStealth')
+            self:EnableUnitIntel('ToggleBit8', 'Cloak')
+
+            self.CloakThread = nil
+        end
     end,
 
     -- Turn off the cloak to begin with
     OnStopBeingBuilt = function(self, builder, layer)
         SWalkingLandUnit.OnStopBeingBuilt(self, builder, layer)
         self:SetScriptBit('RULEUTC_CloakToggle', true)
+        self:ForkThread(self.HideUnit)
     end,
 
     OnMotionHorzEventChange = function(self, new, old)
-        if self.Sync.LowPriority then
-            -- If we stopped moving, hide
-            if new == 'Stopped' then
-                -- We need to fork in order to use WaitSeconds
-                self.CloakThread = ForkThread(function()
-                    WaitSeconds(2)
-
-                    if not self.Dead then
-                        self:SetFireState(1)
-                        self:SetMaintenanceConsumptionActive()
-                        self:EnableUnitIntel('ToggleBit5', 'RadarStealth')
-                        self:EnableUnitIntel('ToggleBit8', 'Cloak')
-
-                        self.CloakThread = nil
-                    end
-                end)
-            end
+        -- If we stopped moving, hide
+        if new == 'Stopped' then
+            -- We need to fork in order to use WaitSeconds
+            self.CloakThread = self:ForkThread(self.HideUnit)
         end
 
         -- If we begin moving, reveal ourselves
         if old == 'Stopped' then
             self:RevealUnit()
         end
-
 
         SWalkingLandUnit.OnMotionHorzEventChange(self, new, old)
     end,
