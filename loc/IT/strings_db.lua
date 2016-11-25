a07f54a572873d367e323ebb6dbf86c7ed771435
Lukas Erlacher <l.erlacher@googlemail.com>
2016-11-25 16:08:49 +0100

    Add localizations for #1687

--- loc/IT/strings_db.lua
+++ loc/IT/strings_db.lua
@@ -7671,7 +7671,7 @@ lobui_0764="Civilian Desertion"
 lobui_0765="Your units will be transferred to the Civilian AI, if there is one, when you die."
 lobui_0766="Defectors"
 lobui_0767="Your units will be transferred to the enemy with the highest score when you die."
-lobui_0768="Your rating is %s +/- %s"
+lobui_0768="Your Trueskill rating is %s +/- %s"
 
 lobui_0592="Permetti osservatori"
 lobui_0593="Gli osservatori sono permessi dopo che il gioco e iniziato?"
