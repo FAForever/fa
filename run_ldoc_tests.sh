c056d78065c15a9c35b2da6454ca9589a479a58b
Stefan Falk <falk.stefan@gmx.at>
2016-12-21 19:54:40 +0100

    removing tabs from css; beautifying run_ldoc_tests.sh

--- run_ldoc_tests.sh
+++ run_ldoc_tests.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+
+# ################################################################
+# Initializing ..
+
 if [ -z "$TRAVIS_BUILD_DIR" ]; then
   TRAVIS_BUILD_DIR=$PWD  # Dev-Mode
 fi
 
-echo "Before install .." 
+echo "Environment:"
 HOME="$TRAVIS_BUILD_DIR/.." 
 echo "HOME="$HOME 
 echo "TRAVIS_BUILD_DIR="$TRAVIS_BUILD_DIR
 
-cd $HOME
 
 # ################################################################
 # Setting up LDoc
 
-sudo apt-get install tree # Just to debug LDoc output 
-mkdir ldoc-build # ldoc result
+cd $HOME
 
-# Download & install luarocks
+# Download, configure and install luarocks
 
 if [ ! -f luarocks-2.4.1.tar.gz ]; then
   echo "File not found!"
@@ -26,18 +28,37 @@ fi
 
 tar zxpf luarocks-2.4.1.tar.gz
 
-cd luarocks-2.4.1 # We're now in ~/luarocks-2.4.1
+cd luarocks-2.4.1 
 
 ./configure; sudo make bootstrap
 
-# Now we can install luasocket, luafilesystem and ldoc
+# With luarocks we can install luasocket, luafilesystem and ldoc
 sudo luarocks install luasocket
 sudo luarocks install luafilesystem
 sudo luarocks install ldoc
 
+
+# ################################################################
+# Running LDoc
+
 cd $HOME
 
+# Let's run LDoc now ..
+
+mkdir ldoc-build # ldoc result
+
+echo "Starting LDoc .."
 ldoc -a -c fa/.ldoc/.cfg/config.ld -s fa/.ldoc/.cfg -d ldoc-build/ fa/
 
-tree ldoc-build/ # Show me what you've got!
 
+# ################################################################
+# Deploy documentation
+
+echo "Deploying documentation .."
+
+# From here we can deploy the website
+sudo apt-get install tree  # Just to debug LDoc output 
+tree ldoc-build/           # Show me what you've got!
+
+
+echo "All done."
