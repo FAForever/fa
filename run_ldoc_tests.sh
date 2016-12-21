f777e1cd1be8dfb6c86a7d3f45d4d9d9a3f12193
Stefan Falk <falk.stefan@gmx.at>
2016-12-21 19:31:19 +0100

    introducing run_ldoc_tests.sh

---
+++ run_ldoc_tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ -z "$TRAVIS_BUILD_DIR" ]; then
+  TRAVIS_BUILD_DIR=$PWD  # Dev-Mode
+fi
+
+echo "Before install .." 
+HOME="$TRAVIS_BUILD_DIR/.." 
+echo "HOME="$HOME 
+echo "TRAVIS_BUILD_DIR="$TRAVIS_BUILD_DIR
+
+cd $HOME
+
+# ################################################################
+# Setting up LDoc
+
+sudo apt-get install tree # Just to debug LDoc output 
+mkdir ldoc-build # ldoc result
+
+# Download & install luarocks
+
+if [ ! -f luarocks-2.4.1.tar.gz ]; then
+  echo "File not found!"
+  wget https://luarocks.org/releases/luarocks-2.4.1.tar.gz
+fi
+
+tar zxpf luarocks-2.4.1.tar.gz
+
+cd luarocks-2.4.1 # We're now in ~/luarocks-2.4.1
+
+./configure; sudo make bootstrap
+
+cd $HOME
+
+ldoc -a -d ldoc-build/ fa/
+
+tree ldoc-build/ # Show me what you've got!
+
