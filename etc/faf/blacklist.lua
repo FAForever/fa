e61b51c73a378d7d404811be53c2b9d7530318fb
IceDreamer <ben-knight@bluebottle.com>
2016-12-30 20:56:56 +0000

    Rearrange blacklist.lua

--- etc/faf/blacklist.lua
+++ etc/faf/blacklist.lua
@@ -8,8 +8,17 @@ local OBSOLETE = "<LOC uimod_0025>Obsolete"
 local HARMFUL = "<LOC uimod_0026>Considered harmful"
 
 Blacklist = {
-    -- PWN Desync
-    ['C707618F-D9B3-43CE-A181-1AC80D582078'] = HARMFUL,
+-- Harmful --
+    ['C707618F-D9B3-43CE-A181-1AC80D582078'] = HARMFUL, -- PWN Desync
+
+    -- Auto-adjust netlag (causes desyncs due to interesting race conditions).
+    ['9033139e-2701-4ac1-b330-ec984ebc23f9'] = HARMFUL,
+    ['xxxx-NetFag-Experimental-v2'] = HARMFUL,
+    ['xxxx-NetLag-Experimental-v1'] = HARMFUL,
+    ['xxxx-NetLag-Experimental-v2'] = HARMFUL,
+    ['xxxx-NetLag-Experimental-v3'] = HARMFUL,
+
+-- Obselete --
 
     -- XINNONY TRY FIX CORRECT DISCONNECT
     ['9B308860-AFD9-11E3-A5E2-0800200C9A66'] = OBSOLETE,
@@ -18,16 +27,10 @@ Blacklist = {
     ['56EF9738-2F96-4941-90B9-DDDDFAC50D05'] = OBSOLETE,
     ['56EF9738-2F96-4941-90B9-DDDDFAC50D06'] = OBSOLETE,
 
+ -- Integrated --
     -- UI Lagfix (everything interesting from it is merged)
     ['9cef4ff7-eccd-4d2d-bee7-cbc7bce0de38'] = INTEGRATED,
     ['2a022e6d-1ceb-4353-a8c5-4cd65709bc33'] = INTEGRATED,
-
-    -- Auto-adjust netlag (causes desyncs due to interesting race conditions).
-    ['9033139e-2701-4ac1-b330-ec984ebc23f9'] = HARMFUL,
-    ['xxxx-NetFag-Experimental-v2'] = HARMFUL,
-    ['xxxx-NetLag-Experimental-v1'] = HARMFUL,
-    ['xxxx-NetLag-Experimental-v2'] = HARMFUL,
-    ['xxxx-NetLag-Experimental-v3'] = HARMFUL,
 
     -- Hotbuild (already merged)
     ['f8785b7a-9e9e-4863-abb3-46aaf1caef80'] = INTEGRATED,
@@ -39,6 +42,41 @@ Blacklist = {
     ['e9bb4b11-2952-4b19-bf4e-11fb6e5fcbe9'] = INTEGRATED,
     ['CC5A5288-18E3-11DD-BF33-005055D89593'] = INTEGRATED,
 
+    -- MoreUnitInfo
+    ['CC51528A-1823-113D-BF33-044655D89593'] = INTEGRATED,
+    ['9B5F858A-163C-4AF1-B846-A884572E61A4'] = INTEGRATED,
+
+    -- Enhanced chat legibility (we merged it)
+    ['11c903ef-f42f-49fe-933a-425de3944d20'] = INTEGRATED,
+    ['9aa24bb2-1f63-4f77-8292-4aaf5414cdb2'] = INTEGRATED,
+
+    -- AZUI (Merged)
+    ['8c5dc7d0-4fa4-11dd-ae16-0800200c9a66'] = INTEGRATED,
+
+    -- Selection Sort
+    ['cfe9131e-9324-4753-a9ca-46972127fe8c'] = INTEGRATED,
+    ['cfe9131e-9324-4753-a9ca-46972127fe8b'] = INTEGRATED,
+
+    -- Hotkey Labels
+    ['e338e34e-58da-11e6-8b77-86f30ca893d3'] = INTEGRATED,
+
+    -- INTEGRATED mods due to added preset restrictions in Units Manager:
+    -- No TML v1.0
+    ['63472293-f7d7-4b77-ba7b-d04ad09f63de'] = INTEGRATED,
+    -- No Tactical Missile Launchers v1.0
+    ['922b87a0-31b9-12dd-bd11-0800200c9a66'] = INTEGRATED,
+    -- No Mobile Missile Launchers v1.0
+    ['dcdbdbcb-b5f1-4828-9298-dca2f4f17a9a'] = INTEGRATED,
+    -- No MML v1.0
+    ['bcd271f7-92a4-4b8b-b0c3-ec20189f7663'] = INTEGRATED,
+    -- No Heavy Art v1.0
+    ['ffeca075-c0cd-4355-861b-0bb29c75af10'] = INTEGRATED,
+    -- No T3 Mobile Anti-Air units v5.0
+    ['ba3d087d-ceb7-495a-b606-3e1593750a5d'] = INTEGRATED,
+    -- No Strategic Missile Launchers v1.0
+    ['9-22b87a0-31c9-12dd-bd11-4583459c9a66'] = INTEGRATED,
+
+ -- Upgrade --
     -- Ally overflow (old, broken versions)
     ['ecbf6277-24e3-437a-b968-AllyOverflow-v1'] = UPGRADE,
     ['ecbf6277-24e3-437a-b968-AllyOverflow-v2'] = UPGRADE,
@@ -68,20 +106,9 @@ Blacklist = {
     ['4767b4d5-e74e-489a-8542-3e993dd452b3'] = UPGRADE,
     ['3501ac36-6f32-4299-9bdb-e323f37a55a2'] = UPGRADE,
 
-    -- MoreUnitInfo
-    ['CC51528A-1823-113D-BF33-044655D89593'] = INTEGRATED,
-    ['9B5F858A-163C-4AF1-B846-A884572E61A4'] = INTEGRATED,
-
-    -- Enhanced chat legibility (we merged it)
-    ['11c903ef-f42f-49fe-933a-425de3944d20'] = INTEGRATED,
-    ['9aa24bb2-1f63-4f77-8292-4aaf5414cdb2'] = INTEGRATED,
-
     -- Supreme economy (old ones)
     ['f8d8c95a-71e7-4978-921e-8765beb328e8'] = UPGRADE,
     ['89BF1572-9EA8-11DC-1313-635F56D89591'] = UPGRADE,
-
-    -- AZUI (Merged)
-    ['8c5dc7d0-4fa4-11dd-ae16-0800200c9a66'] = INTEGRATED,
 
     -- rks explosions
     ['7749EEA3-DE86-5CC2-39AC-35672BDE74DD'] = UPGRADE,
@@ -93,33 +120,4 @@ Blacklist = {
     ['7749EEN4-DE86-5CC2-39AC-35692BDE76XF'] = UPGRADE,
     ['1749EEN4-DE86-5CC2-39AC-35692BDE76XF'] = UPGRADE,
     ['EEFFA8C6-96D9-11E4-9DA1-460D1D5D46B0'] = UPGRADE,
-
-    -- Selection Sort
-    ['cfe9131e-9324-4753-a9ca-46972127fe8c'] = INTEGRATED,
-    ['cfe9131e-9324-4753-a9ca-46972127fe8b'] = INTEGRATED,
-
-    -- Hotkey Labels
-    ['e338e34e-58da-11e6-8b77-86f30ca893d3'] = INTEGRATED,
-    
-    -- ==================================================================
-    -- INTEGRATED mods due to added preset restrictions in Units Manager:
-    -- ==================================================================
-    -- No TML v1.0
-    ['63472293-f7d7-4b77-ba7b-d04ad09f63de'] = INTEGRATED, 
-    -- No Tactical Missile Launchers v1.0 
-    ['922b87a0-31b9-12dd-bd11-0800200c9a66'] = INTEGRATED, 
-    -- No Mobile Missile Launchers v1.0  
-    ['dcdbdbcb-b5f1-4828-9298-dca2f4f17a9a'] = INTEGRATED, 
-    -- No MML v1.0 
-    ['bcd271f7-92a4-4b8b-b0c3-ec20189f7663'] = INTEGRATED, 
-    -- No Heavy Art v1.0 
-    ['ffeca075-c0cd-4355-861b-0bb29c75af10'] = INTEGRATED, 
-    -- No T3 Mobile Anti-Air units v5.0  
-    ['ba3d087d-ceb7-495a-b606-3e1593750a5d'] = INTEGRATED, 
-    -- No Strategic Missile Launchers v1.0 
-    ['9-22b87a0-31c9-12dd-bd11-4583459c9a66'] = INTEGRATED,
-    -- ==================================================================
-
-
-
 }
