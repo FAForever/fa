cd349b72e7a53806c24ff3ff54c18d2c5f196e47
IceDreamer <ben-knight@bluebottle.com>
2017-01-27 00:19:42 +0000

    Cleanup prep for removing SCU Manager

--- lua/keymap/misckeyactions.lua
+++ lua/keymap/misckeyactions.lua
@@ -1,28 +1,28 @@
+-- This file contains key bindable actions that don't fit elsewhere
+
 local Prefs = import('/lua/user/prefs.lua')
 
 local lockZoomEnable = false
 function lockZoom()
-	if lockZoomEnable then
-			local options = Prefs.GetFromCurrentProfile('options')
-			value = options.wheel_sensitivity
-			ConExecute("cam_ZoomAmount " .. tostring(value / 100))
-			
-			lockZoomEnable = false
-	else
-			ConExecute("cam_ZoomAmount 0")
-			lockZoomEnable = true
-	end
+    if lockZoomEnable then
+        local options = Prefs.GetFromCurrentProfile('options')
+        value = options.wheel_sensitivity
+        ConExecute("cam_ZoomAmount " .. tostring(value / 100))
 
-
+        lockZoomEnable = false
+    else
+        ConExecute("cam_ZoomAmount 0")
+        lockZoomEnable = true
+    end
 end
 
 
 function airNoTransports()
-UISelectionByCategory("AIR + MOBILE", false,false,false,false) SelectUnits(EntityCategoryFilterDown(categories.ALLUNITS - categories.TRANSPORTATION, GetSelectedUnits()))
+    UISelectionByCategory("AIR + MOBILE", false, false, false, false) SelectUnits(EntityCategoryFilterDown(categories.ALLUNITS - categories.TRANSPORTATION, GetSelectedUnits()))
 end
 
 function airTransports()
-UISelectionByCategory("TRANSPORTATION", false,false,false,false)
+    UISelectionByCategory("TRANSPORTATION", false, false, false, false)
 end
 
 function ToggleRepeatBuild()
@@ -30,16 +30,18 @@ function ToggleRepeatBuild()
     if selection then
         local allFactories = true
         local currentInfiniteQueueCheckStatus = false
-        for i,v in selection do
+        for _, v in selection do
             if v:IsRepeatQueue() then
                 currentInfiniteQueueCheckStatus = true
             end
+
             if not v:IsInCategory('FACTORY') then
                 allFactories = false
             end
         end
+
         if allFactories then
-            for i,v in selection do
+            for _, v in selection do
                 if currentInfiniteQueueCheckStatus then
                     v:ProcessInfo('SetRepeatQueue', 'false')
                 else
@@ -50,7 +52,7 @@ function ToggleRepeatBuild()
     end
 end
 
---function to toggle things like shields etc
+-- Function to toggle things like shields etc
 -- Unit toggle rules copied from orders.lua, used for converting to the numbers needed for the togglescriptbit function
 unitToggleRules = {
     Shield =  0,
@@ -61,7 +63,8 @@ unitToggleRules = {
     Stealth = 5,
     Generic = 6,
     Special = 7,
-Cloak = 8,}
+    Cloak = 8,
+}
 
 function toggleScript(name)
     local selection = GetSelectedUnits()
@@ -70,9 +73,9 @@ function toggleScript(name)
     ToggleScriptBit(selection, number, currentBit)
 end
 
-function toggleAllScript(name)
+function toggleAllScript()
     local selection = GetSelectedUnits()
-    for i = 0,8 do
+    for i = 0, 8 do
         local currentBit = GetScriptBit(selection, i)
         ToggleScriptBit(selection, i, currentBit)
     end
@@ -85,12 +88,11 @@ local FILTERS = {
 
 function toggleOverlay(type)
     local currentFilters = Prefs.GetFromCurrentProfile('activeFilters') or {}
-
     local filterSet = FILTERS[type]
 
     -- Determine if any of the filters from the selected filter set are active.
     local filtersAreActive = false
-    for i, filter in filterSet do
+    for _, filter in filterSet do
         if currentFilters[filter] then
             filtersAreActive = true
             break
@@ -98,7 +100,7 @@ function toggleOverlay(type)
     end
 
     -- If any filters are active, turn them all off. Otherwise turn them all on.
-    for i, filter in filterSet do
+    for _, filter in filterSet do
         if filtersAreActive then
             currentFilters[filter] = nil
         else
@@ -111,7 +113,6 @@ function toggleOverlay(type)
 end
 
 --- Function builder for "Get next factory of type" functions
---
 -- @param factoryType The type of factory (LAND, AIR, NAVAL) for which a cycling function is wanted.
 function getGetNextFactory(factoryType)
     local currentFactoryIndex = 1
@@ -137,11 +138,11 @@ GetNextNavalFactory = getGetNextFactory("NAVAL")
 
 function GetNearestIdleLTMex()
     local tech = 1
-    while (tech < 4) do
+    while tech < 4 do
         ConExecute('UI_SelectByCategory +nearest +idle +inview MASSEXTRACTION TECH' .. tech)
         tech = tech + 1
         local tempList = GetSelectedUnits()
-        if (tempList ~= nil) and (table.getn(tempList) > 0) then
+        if tempList ~= nil and table.getn(tempList) > 0 then
             break
         end
     end
@@ -158,24 +159,24 @@ function toggleIntelShieldScript()
     toggleScript("Shield")
 end
 
---this function might be too slow in larger games, needs testing
+-- This function might be too slow in larger games, needs testing
 function GetSimilarUnits()
     local enhance = import('/lua/enhancementcommon.lua')
     local curSelection = GetSelectedUnits()
     if curSelection then
-        --find out what enhancements the current unit has
+        -- Find out what enhancements the current unit has
         local curUnitId = curSelection[1]:GetEntityId()
         local curUnitEnhancements = enhance.GetEnhancements(curUnitId)
 
-        --select all similar units by category
+        -- Select all similar units by category
         local bp = curSelection[1]:GetBlueprint()
         local catString = table.concatkeys(bp.CategoriesHash, " * ")
         UISelectionByCategory(catString, false, false, false, false)
 
-        --get enhancements on each unit and filter down to only those with the same as the first unit
+        -- Get enhancements on each unit and filter down to only those with the same as the first unit
         local newSelection = GetSelectedUnits()
         local tempSelectionTable = {}
-        for i, unit in newSelection do
+        for _, unit in newSelection do
             local unitId = unit:GetEntityId()
             local unitEnhancements = enhance.GetEnhancements(unitId)
             if curUnitEnhancements and unitEnhancements then
@@ -191,9 +192,7 @@ function GetSimilarUnits()
     end
 end
 
--- by norem
 local lastACUSelectionTime = 0
-
 function ACUSelectCG()
     local curTime = GetSystemTimeSeconds()
     local diffTime = curTime - lastACUSelectionTime
@@ -213,7 +212,6 @@ function ACUAppendCG()
 end
 
 local GetDistanceBetweenTwoVectors = import('/lua/utilities.lua').GetDistanceBetweenTwoVectors
-
 function GetNearestIdleEngineerNotACU()
     local idleEngineers = GetIdleEngineers()
     if not idleEngineers then
@@ -238,25 +236,26 @@ function AddNearestIdleEngineersSeq()
     local allIdleEngineers = GetIdleEngineers() or {}
     local currentSelection = GetSelectedUnits() or {}
 
-    -- check if current selection contains only idle engineers
+    -- Check if current selection contains only idle engineers
     local idleEngineers = allIdleEngineers
-    for i, unit in currentSelection do
+    for _, unit in currentSelection do
         local key = table.find(idleEngineers, unit)
         if key then
             table.remove(idleEngineers, key)
         else
-            -- not an idle engineer, clear selection
+            -- Not an idle engineer, clear selection
             SelectUnits(nil)
             currentSelection = {}
             idleEngineers = allIdleEngineers
             break
         end
     end
+
     if table.empty(idleEngineers) then
         return
     end
 
-    -- get nearest in list of unselected, idle
+    -- Get nearest in list of unselected, idle
     local mousePos = GetMouseWorldPos()
     local nearestIndex = 1
     local nearestDist = GetDistanceBetweenTwoVectors(mousePos, idleEngineers[nearestIndex]:GetPosition())
@@ -268,22 +267,11 @@ function AddNearestIdleEngineersSeq()
         end
     end
 
-    -- compare nearest with already selected
-    -- if it is closer than any of them, select it and deselect the others
-    -- can be confusing in some situations
-    --[[for i, unit in currentSelection do
-    if GetDistanceBetweenTwoVectors(mousePos, unit:GetPosition()) > nearestDist then
-        SelectUnits({idleEngineers[nearestIndex]})
-        return
-    end
-end]]
-
-AddSelectUnits({idleEngineers[nearestIndex]})
+    AddSelectUnits({idleEngineers[nearestIndex]})
 end
 
 local categoryTable = {'LAND','AIR','NAVAL'}
 local curFacIndex = 1
-
 function CycleIdleFactories()
     local idleFactories = GetIdleFactories()
     if not idleFactories then
@@ -330,7 +318,7 @@ local unitTypes = {
     categories.AIR * categories.MOBILE,
 }
 local sortedUnits = {}
-local unitCurType = nil
+local unitCurType = false
 
 function CycleUnitTypesInSel()
     local selection = GetSelectedUnits()
@@ -340,7 +328,7 @@ function CycleUnitTypesInSel()
 
     local isNewSel = false
     if sortedUnits[unitCurType] then
-        for i, unit in selection do
+        for _, unit in selection do
             if not table.find(sortedUnits[unitCurType], unit) then
                 isNewSel = true
                 break
@@ -351,7 +339,7 @@ function CycleUnitTypesInSel()
     end
 
     if isNewSel then
-        -- sort units
+        -- Sort units
         sortedUnits = {}
         for i, cat in ipairs(unitTypes) do
             local units = EntityCategoryFilterDown(cat, selection)
@@ -360,7 +348,7 @@ function CycleUnitTypesInSel()
             end
         end
 
-        -- first type should be selected
+        -- First type should be selected
         if not table.empty(sortedUnits) then
             unitCurType = 1
         else
@@ -368,7 +356,7 @@ function CycleUnitTypesInSel()
             return
         end
     else
-        -- next type should be selected
+        -- Next type should be selected
         unitCurType = unitCurType + 1
         if not sortedUnits[unitCurType] then
             unitCurType = 1
@@ -379,12 +367,10 @@ function CycleUnitTypesInSel()
 end
 
 function CreateTemplateFactory()
-    local currentCommandQueue = nil
+    local currentCommandQueue
     local selection = GetSelectedUnits()
     if selection and table.getn(selection) == 1 and selection[1]:IsInCategory('FACTORY') then
         currentCommandQueue = SetCurrentFactoryForQueueDisplay(selection[1])
     end
     import('/modules/templates_factory.lua').CreateBuildTemplate(currentCommandQueue)
 end
-
--- end by norem
