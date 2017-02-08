06eae2c58dd46e360d3b0e671d846348f00f46be
IceDreamer <ben-knight@bluebottle.com>
2017-02-08 00:12:09 +0000

    Cleanup

--- units/XSB2108/XSB2108_script.lua
+++ units/XSB2108/XSB2108_script.lua
@@ -1,12 +1,9 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/UAB2108/UAB2108_script.lua
-#**  Author(s):  Greg Kohne, Aaron Lundquist
-#**
-#**  Summary  :  Seraphim Tactical Missile Launcher Script
-#**
-#**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+-----------------------------------------------------------------
+-- File     :  /cdimage/units/UAB2108/UAB2108_script.lua
+-- Author(s):  Greg Kohne, Aaron Lundquist
+-- Summary  :  Seraphim Tactical Missile Launcher Script
+-- Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+-----------------------------------------------------------------
 
 local SStructureUnit = import('/lua/seraphimunits.lua').SStructureUnit
 local SLaanseMissileWeapon = import('/lua/seraphimweapons.lua').SLaanseMissileWeapon
@@ -16,4 +13,5 @@ XSB2108 = Class(SStructureUnit) {
         CruiseMissile = Class(SLaanseMissileWeapon) {},
     },
 }
+
 TypeClass = XSB2108
