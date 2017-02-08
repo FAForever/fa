8864db8f0e369b923385df51c9ad3327694af5f1
IceDreamer <ben-knight@bluebottle.com>
2017-02-08 00:12:09 +0000

    Cleanup

--- units/UAB2305/UAB2305_script.lua
+++ units/UAB2305/UAB2305_script.lua
@@ -1,33 +1,26 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/UAB2305/UAB2305_script.lua
-#**  Author(s):  John Comes, David Tomandl
-#**
-#**  Summary  :  Aeon Tactical Missile Launcher Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+-----------------------------------------------------------------
+-- File     :  /cdimage/units/UAB2305/UAB2305_script.lua
+-- Author(s):  John Comes, David Tomandl
+-- Summary  :  Aeon Tactical Missile Launcher Script
+-- Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+-----------------------------------------------------------------
 
 local AStructureUnit = import('/lua/aeonunits.lua').AStructureUnit
 local AIFQuantumWarhead = import('/lua/aeonweapons.lua').AIFQuantumWarhead
 local EffectUtil = import('/lua/EffectUtilities.lua')
 
 UAB2305 = Class(AStructureUnit) {
-
-
     Weapons = {
-        QuantumMissiles = Class(AIFQuantumWarhead) 
-        {
+        QuantumMissiles = Class(AIFQuantumWarhead) {
             UnpackEffects01 = {'/effects/emitters/aeon_nuke_unpack_01_emit.bp',},
 
             PlayFxWeaponUnpackSequence = function(self)
                 for k, v in self.UnpackEffects01 do
-                    CreateAttachedEmitter( self.unit, 'B04', self.unit:GetArmy(), v )
+                    CreateAttachedEmitter( self.unit, 'B04', self.unit:GetArmy(), v)
                 end
 
                 AIFQuantumWarhead.PlayFxWeaponUnpackSequence(self)
             end,
-    
         },
     },
 
@@ -49,8 +42,6 @@ UAB2305 = Class(AStructureUnit) {
             self:PlaySound(myBlueprint.Audio.Activate)
         end
     end,
-    
-
 }
 
 TypeClass = UAB2305
--- units/UEB2305/UEB2305_script.lua
+++ units/UEB2305/UEB2305_script.lua
@@ -1,12 +1,10 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/UEB2305/UEB2305_script.lua
-#**  Author(s):  John Comes, David Tomandl
-#**
-#**  Summary  :  UEF Strategic Missile Launcher Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+-----------------------------------------------------------------
+-- File     :  /cdimage/units/UEB2305/UEB2305_script.lua
+-- Author(s):  John Comes, David Tomandl
+-- Summary  :  UEF Strategic Missile Launcher Script
+-- Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+-----------------------------------------------------------------
+
 local TStructureUnit = import('/lua/terranunits.lua').TStructureUnit
 local TIFStrategicMissileWeapon = import('/lua/terranweapons.lua').TIFStrategicMissileWeapon
 
