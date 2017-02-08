ee8723b1e7f6e8f977a6bef0d478c74f75f44725
IceDreamer <ben-knight@bluebottle.com>
2017-02-08 00:12:09 +0000

    Cleanup

--- units/UAB2108/UAB2108_script.lua
+++ units/UAB2108/UAB2108_script.lua
@@ -1,12 +1,9 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/UAB2108/UAB2108_script.lua
-#**  Author(s):  John Comes, David Tomandl
-#**
-#**  Summary  :  Aeon Tactical Missile Launcher Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+-----------------------------------------------------------------
+-- File     :  /cdimage/units/UAB2108/UAB2108_script.lua
+-- Author(s):  John Comes, David Tomandl
+-- Summary  :  Aeon Tactical Missile Launcher Script
+-- Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+-----------------------------------------------------------------
 
 local AStructureUnit = import('/lua/aeonunits.lua').AStructureUnit
 local ACruiseMissileWeapon = import('/lua/aeonweapons.lua').ACruiseMissileWeapon
@@ -16,4 +13,5 @@ UAB2108 = Class(AStructureUnit) {
         CruiseMissile = Class(ACruiseMissileWeapon) {},
     },
 }
+
 TypeClass = UAB2108
