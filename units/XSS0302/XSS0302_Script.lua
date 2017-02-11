8df7e4532c3d91f9010607dc19cf34d7cab72fca
IceDreamer <ben-knight@bluebottle.com>
2017-02-11 01:19:40 +0000

    Add fire control to manually launched missiles on more units

--- units/XSS0302/XSS0302_Script.lua
+++ units/XSS0302/XSS0302_Script.lua
@@ -11,6 +11,7 @@ local SDFHeavyQuarnonCannon = SeraphimWeapons.SDFHeavyQuarnonCannon
 local SAMElectrumMissileDefense = SeraphimWeapons.SAMElectrumMissileDefense
 local SAAOlarisCannonWeapon = SeraphimWeapons.SAAOlarisCannonWeapon
 local SIFInainoWeapon = SeraphimWeapons.SIFInainoWeapon
+local ManualLaunchWeapon = import('/lua/sim/defaultweapons.lua').ManualLaunchWeapon
 
 XSS0302 = Class(SSeaUnit) {
     FxDamageScale = 2,
@@ -24,7 +25,7 @@ XSS0302 = Class(SSeaUnit) {
         AntiMissileRight = Class(SAMElectrumMissileDefense) {},
         AntiAirLeft = Class(SAAOlarisCannonWeapon) {},
         AntiAirRight = Class(SAAOlarisCannonWeapon) {},
-        InainoMissiles = Class(SIFInainoWeapon) {},
+        InainoMissiles = Class(SIFInainoWeapon, ManualLaunchWeapon) {},
     },
 }
 
