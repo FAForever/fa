03695561162051741d74905758533009c8a048ea
TheKeyBlue <keyblue357@hotmail.com>
2017-02-22 19:23:17 +0100

    Only send NoUnitsAvailable message to spot requesting observer
    To remove the ability of people to spam chat with the observer button.

--- lua/ui/lobby/lobby.lua
+++ lua/ui/lobby/lobby.lua
@@ -1592,6 +1592,18 @@ function SendSystemMessage(id, ...)
     PrintSystemMessage(id, arg)
 end
 
+function SendPersonalSystemMessage(targetID, id, ...)
+    if targetID ~= localPlayerID then
+        local data = {
+            Type = "SystemMessage",
+            Id = id,
+            Args = arg
+        }
+
+        lobbyComm:SendData(targetID, data)
+    end
+end
+
 function PublicChat(text)
     lobbyComm:BroadcastData(
         {
@@ -5658,8 +5670,8 @@ function InitHostUtils()
                     end
 
                     -- There are no AIs and no slots, so break out with a message
-                    if toPlayerSlot < 1 then
-                        SendSystemMessage("lobui_0608")
+                    if toPlayerSlot < 1 and gameInfo.Observers[fromObserverSlot] then
+                        SendPersonalSystemMessage(gameInfo.Observers[fromObserverSlot].OwnerID, "lobui_0608")
                         return
                     end
                 end
