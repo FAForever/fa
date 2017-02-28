5dcabee2dfb02f08fa3dfc197fe07ab2a7af38cc
TheKeyBlue <keyblue357@hotmail.com>
2017-03-01 00:36:25 +0100

    Create a new button for attack move command

--- lua/abilitydefinition.lua
+++ lua/abilitydefinition.lua
@@ -7,4 +7,11 @@ abilities = {
         preferredSlot = 7,
         script = 'TargetLocation',
     },
+    ['AttackMove'] = {
+        bitmapId = 'attack_move',
+        helpText = 'attack_move',
+        preferredSlot = 7,
+        script = 'AttackMove',
+        cursor = 'ATTACK_MOVE',
+    },
 }
