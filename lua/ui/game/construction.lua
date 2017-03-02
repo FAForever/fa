2a592024469232a01cfa777fcaa957fd4c8922ce
TheKeyBlue <keyblue357@hotmail.com>
2017-03-02 01:45:41 +0100

    Make sure to clean up variable that saves current selection.
    fixes #1624

--- lua/ui/game/construction.lua
+++ lua/ui/game/construction.lua
@@ -2115,6 +2115,11 @@ function RefreshUI()
 end
 
 function OnSelection(buildableCategories, selection, isOldSelection)
+    if table.empty(selection) then
+        sortedOptions.selection = {}
+        return
+    end
+    
     if options.gui_templates_factory ~= 0 then
         if table.empty(selection) then
             allFactories = false
