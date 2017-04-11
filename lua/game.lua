09a2cb3ccbaa1babe529a008b0a5092caa88832d
IceDreamer <ben-knight@bluebottle.com>
2017-04-11 22:26:00 +0100

    Fix bad merge leading to variables in the wrong order/missing

--- lua/game.lua
+++ lua/game.lua
@@ -67,27 +67,27 @@ BuffFieldBlueprint { -- Seraphim ACU Advanced Restoration
 -- In order to keep backwards compatibility, there is a new option in the blueprint economy section.
 -- if DifferentialUpgradeCostCalculation is set to true, the base upgrade cost will be subtracted
 function GetConstructEconomyModel(builder, targetData, upgradeBaseData)
-    mass = math.max(mass - upgradeBaseData.BuildCostMass, 0)
-    energy = math.max(energy - upgradeBaseData.BuildCostEnergy, 0)
-    
     -- 'rate' here is how fast we build relative to a unit with build rate of 1
     local rate = builder:GetBuildRate()
     local buildtime = targetData.BuildTime or 0.1
     local mass = targetData.BuildCostMass or 0
     local energy = targetData.BuildCostEnergy or 0
 
-    -- Apply penalties/bonuses to effective buildtime
-    local time_mod = builder.BuildTimeModifier or 0
-    buildtime = math.max(buildtime * (100 + time_mod) * 0.01, 0.1)
-    
-    -- Apply penalties/bonuses to effective energy cost
-    local energy_mod = builder.EnergyModifier or 0
+    if upgradeBaseData and targetData.DifferentialUpgradeCostCalculation then
+        -- We cant make a differential on buildtime. Not sure why but if we do it yields incorrect
+        -- results. So just mass and energy.
+        mass = math.max(mass - upgradeBaseData.BuildCostMass, 0)
+        energy = math.max(energy - upgradeBaseData.BuildCostEnergy, 0)
+    end
 
+    -- Apply penalties/bonuses to effective costs
+    local time_mod = builder.BuildTimeModifier or 0
+    local energy_mod = builder.EnergyModifier or 0
+    local mass_mod = builder.MassModifier or 0
+
+    buildtime = math.max(buildtime * (100 + time_mod) * 0.01, 0.1)
     energy = math.max(energy * (100 + energy_mod) * 0.01, 0)
     mass = math.max(mass * (100 + mass_mod) * 0.01, 0)
-
-    -- Apply penalties/bonuses to effective mass cost
-    local mass_mod = builder.MassModifier or 0
 
     return buildtime / rate, energy, mass
 end
