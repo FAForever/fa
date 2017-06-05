884e3c391433ccd6d3ce6821a35befac10574de0
Uveso <Archivar@Zwergenteam.de>
2017-06-05 19:45:50 +0200

    Bugfix and optimizing LeadTarget function

--- lua/AI/sorianutilities.lua
+++ lua/AI/sorianutilities.lua
@@ -23,7 +23,6 @@ local AITaunts = {
 }
 
 function T4Timeout(aiBrain)
-    SPEW('Uveso Debugger: [sorianutilities.lua '..debug.getinfo(1).currentline..'] - function T4Timeout(aiBrain) called')
     WaitSeconds(30)
     aiBrain.T4Building = false
 end
@@ -815,79 +814,115 @@ end
 --                                 Launch Time: ~3 seconds
 -- -----------------------------------------------------
 function LeadTarget(platoon, target)
-    local TMLRandom = tonumber(ScenarioInfo.Options.TMLRandom) or 0
-    local position = platoon:GetPlatoonPosition()
-    local pos = target:GetPosition()
-
-    -- Get firing position height
-    local fromheight = GetTerrainHeight(position[1], position[3])
-    if GetSurfaceHeight(position[1], position[3]) > GetTerrainHeight(position[1], position[3]) then
-        fromheight = GetSurfaceHeight(position[1], position[3])
+    -- Get launcher and target position
+    local LauncherPos = platoon:GetPlatoonPosition()
+    local TargetPos = target:GetPosition()
+    -- Get target position in 1 second intervals.
+    -- This allows us to get speed and direction from the target
+    local TargetStartPosition=0
+    local Target1SecPos=0
+    local Target2SecPos=0
+    local XmovePerSec=0
+    local YmovePerSec=0
+    local XmovePerSecCheck=-1
+    local YmovePerSecCheck=-1
+    -- Check if the target is runing straight or circling
+    -- If x/y and xcheck/ycheck are equal, we can be sure the target is moving straight
+    -- in one direction. At least for the last 2 seconds.
+    local LoopSaveGuard = 0
+    while (XmovePerSec ~= XmovePerSecCheck or YmovePerSec ~= YmovePerSecCheck) and LoopSaveGuard < 10 do
+        -- 1st position of target
+        TargetPos = target:GetPosition()
+        TargetStartPosition = {TargetPos[1], 0, TargetPos[3]}
+        WaitSeconds(1)
+        -- 2nd position of target after 1 second
+        TargetPos = target:GetPosition()
+        Target1SecPos = {TargetPos[1], 0, TargetPos[3]}
+        XmovePerSec = (TargetStartPosition[1] - Target1SecPos[1])
+        YmovePerSec = (TargetStartPosition[3] - Target1SecPos[3])
+        WaitSeconds(1)
+        -- 3rd position of target after 2 seconds to verify straight movement
+        TargetPos = target:GetPosition()
+        Target2SecPos = {TargetPos[1], TargetPos[2], TargetPos[3]}
+        XmovePerSecCheck = (Target1SecPos[1] - Target2SecPos[1])
+        YmovePerSecCheck = (Target1SecPos[3] - Target2SecPos[3])
+        --We leave the while-do check after 10 loops (20 seconds) and try collateral damage
+        --This can happen if a player try to fool the targetingsystem by circling a unit.
+        LoopSaveGuard = LoopSaveGuard + 1
+    end
+    -- Get launcher position height
+    local fromheight = GetTerrainHeight(LauncherPos[1], LauncherPos[3])
+    if GetSurfaceHeight(LauncherPos[1], LauncherPos[3]) > fromheight then
+        fromheight = GetSurfaceHeight(LauncherPos[1], LauncherPos[3])
     end
     -- Get target position height
-    local toheight = GetTerrainHeight(pos[1], pos[3])
-    if GetSurfaceHeight(pos[1], pos[3]) > GetTerrainHeight(pos[1], pos[3]) then
-        toheight = GetSurfaceHeight(pos[1], pos[3])
+    local toheight = GetTerrainHeight(Target2SecPos[1], Target2SecPos[3])
+    if GetSurfaceHeight(Target2SecPos[1], Target2SecPos[3]) > toheight then
+        toheight = GetSurfaceHeight(Target2SecPos[1], Target2SecPos[3])
     end
-
-    -- Get height difference between firing position and target position
-    local heightdiff = math.abs(fromheight - toheight)
-
-    -- Get target position and then again after 1 second
-    -- Allows us to get speed and direction
-    local Tpos1 = {pos[1], 0, pos[3]}
-    WaitSeconds(1)
-    pos = target:GetPosition()
-    local Tpos2 = {pos[1], 0, pos[3]}
-
-    -- Get distance moved on X and Y axis
-    local xmove = (Tpos1[1] - Tpos2[1])
-    local ymove = (Tpos1[3] - Tpos2[3])
-
-    -- Get distance from firing position to targets starting position and position it moved
-    -- to after 1 second
-    local dist1 = VDist2Sq(position[1], position[3], Tpos1[1], Tpos1[3])
-    local dist2 = VDist2Sq(position[1], position[3], Tpos2[1], Tpos2[3])
-    dist1 = math.sqrt(dist1)
-    dist2 = math.sqrt(dist2)
-
-    -- Adjust for level off time.
-    local distadjust = 0.25
-
-    -- Missile has a faster turn rate when targeting targets < 50 MU away
-    -- so will level off faster
-    if dist2 < 50 then
-        distadjust = 0.02
-    end
-
-    -- Divide both distances by missiles max speed to get time to impact
-    local time1 = (dist1 / 12)
-    local time2 = (dist2 / 12)
-
+    -- Get height difference between launcher position and target position
     -- Adjust for height difference by dividing the height difference by the missiles max speed
-    local heightadjust = heightdiff / 12
-
-    -- Speed up time is distance the missile will travel while reaching max speed
-    -- (~22.47 MU) divided by the missiles max speed which equals 1.8725 seconds flight time
-
-    -- total travel time + 1.87 (time for missile to speed up, rounded) + 3 seconds for launch
-    -- + adjustment for turn rate + adjustment for height difference
-    local newtime = time2 - (time1 - time2) + 4.87 + distadjust + heightadjust
-
+    local HeightDifference = math.abs(fromheight - toheight) / 12
+    -- Speed up time is distance the missile will travel while reaching max speed (~22.47 MapUnits)
+    -- divided by the missiles max speed (12) which is equal to 1.8725 seconds flight time
+    local SpeedUpTime = 22.47 / 12
+    --  Missile needs 3 seconds to launch
+    local LaunchTime = 3
+    -- Get distance from launcher position to targets starting position and position it moved to after 1 second
+    local dist1 = VDist2(LauncherPos[1], LauncherPos[3], Target1SecPos[1], Target1SecPos[3])
+    local dist2 = VDist2(LauncherPos[1], LauncherPos[3], Target2SecPos[1], Target2SecPos[3])
+    -- Missile has a faster turn rate when targeting targets < 50 MU away, so it will level off faster
+    local LevelOffTime = 0.25
+    local CollisionRangeAdjust = 0
+    if dist2 < 50 then
+        LevelOffTime = 0.02
+        CollisionRangeAdjust = 2
+    end
+    -- Divide both distances by missiles max speed to get time to impact
+    local time1 = (dist1 / 12) + LaunchTime + SpeedUpTime + LevelOffTime + HeightDifference
+    local time2 = (dist2 / 12) + LaunchTime + SpeedUpTime + LevelOffTime + HeightDifference
+    -- Get the missile travel time by extrapolating speed and time from dist1 and dist2
+    local MissileTravelTime = (time2 + (time2 - time1)) + ((time2 - time1) * time2)
+    -- Now adding all times to get final missile flight time to the position where the target will be
+    local MissileImpactTime = MissileTravelTime + LaunchTime + SpeedUpTime + LevelOffTime + HeightDifference
+    -- Create missile impact corrdinates based on movePerSec * MissileImpactTime
+    local MissileImpactX = Target2SecPos[1] - (XmovePerSec * MissileImpactTime)
+    local MissileImpactY = Target2SecPos[3] - (YmovePerSec * MissileImpactTime)
+    -- Adjust for targets CollisionOffsetY. If the hitbox of the unit is above the ground
+    -- we nedd to fire "behind" the target, so we hit the unit in midair.
+    local TargetCollisionBoxAdjust = 0
+    local TargetBluePrint = target:GetBlueprint()
+    if TargetBluePrint.CollisionOffsetY and TargetBluePrint.CollisionOffsetY > 0 then
+        -- if the unit is far away we need to target farther behind the target because of the projectile flight angel
+        local DistanceOffset = (100 / 256 * dist2) * 0.06
+        TargetCollisionBoxAdjust = TargetBluePrint.CollisionOffsetY * CollisionRangeAdjust + DistanceOffset
+    end
+    -- To calculate the Adjustment behind the target we use a variation of the Pythagorean theorem. (Percent scale technique)
+    -- (a²+b²=c²) If we add x% to c² then also a² and b² are x% larger. (a²)*x% + (b²)*x% = (c²)*x%
+    local Hypotenuse = VDist2(LauncherPos[1], LauncherPos[3], MissileImpactX, MissileImpactY)
+    local HypotenuseScale = 100 / Hypotenuse * TargetCollisionBoxAdjust
+    local aLegScale = (MissileImpactX - LauncherPos[1]) / 100 * HypotenuseScale
+    local bLegScale = (MissileImpactY - LauncherPos[3]) / 100 * HypotenuseScale
+    -- Add x percent (behind) the target coordinates to get our final missile impact coordinates
+    MissileImpactX = MissileImpactX + aLegScale
+    MissileImpactY = MissileImpactY + bLegScale
     -- Add some optional randomization to make the AI easier
-    local randomize = (100 - Random(0, TMLRandom)) / 100
-    newtime = newtime * randomize
-
-    -- Create target corrdinates
-    local newx = xmove * newtime
-    local newy = ymove * newtime
-
+    local TMLRandom = tonumber(ScenarioInfo.Options.TMLRandom) or 0
+    MissileImpactX = MissileImpactX + (Random(0, TMLRandom) - TMLRandom / 2) / 5
+    MissileImpactY = MissileImpactY + (Random(0, TMLRandom) - TMLRandom / 2) / 5
     -- Cancel firing if target is outside map boundries
-    if Tpos2[1] - newx < 0 or Tpos2[3] - newy < 0 or
-      Tpos2[1] - newx > ScenarioInfo.size[1] or Tpos2[3] - newy > ScenarioInfo.size[2] then
+    if MissileImpactX < 0 or MissileImpactY < 0 or MissileImpactX > ScenarioInfo.size[1] or MissileImpactY > ScenarioInfo.size[2] then
         return false
     end
-    return {Tpos2[1] - newx, 0, Tpos2[3] - newy}
+    -- Also cancel if target would be out of weaponrange or inside minimum range.
+    local maxRadius = 256
+    local minRadius = 15
+    local dist3 = VDist2(LauncherPos[1], LauncherPos[3], MissileImpactX, MissileImpactY)
+    if dist3 < minRadius or dist3 > maxRadius then
+        return false
+    end
+    -- return extrapolated target position / missile impact coordinates
+    return {MissileImpactX, Target2SecPos[2], MissileImpactY}
 end
 
 -- -----------------------------------------------------
