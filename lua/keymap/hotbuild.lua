e338384df7cad06f7f729848b9fd320ec8607bef
IceDreamer <ben-knight@bluebottle.com>
2017-06-17 00:57:55 +0100

    Alter hotbuild behaviour during queued upgrades

--- lua/keymap/hotbuild.lua
+++ lua/keymap/hotbuild.lua
@@ -15,6 +15,7 @@ local LayoutHelpers = import('/lua/maui/layouthelpers.lua')
 local Effect = import('/lua/maui/effecthelpers.lua')
 
 local upgradeTab = import('/lua/keymap/upgradeTab.lua').upgradeTab
+local ModifyBuildables = import('/lua/ui/notify/enhancementqueue.lua').ModifyBuildablesForACU
 
 local unitkeygroups
 local cyclePos
@@ -205,7 +206,8 @@ function buildActionBuilding(name, modifier)
     -- Filter the values
     local selection = GetSelectedUnits()
     local availableOrders, availableToggles, buildableCategories = GetUnitCommandData(selection)
-    local buildable = EntityCategoryGetUnitList(buildableCategories)
+    local newBuildableCategories = ModifyBuildables(buildableCategories, selection)
+    local buildable = EntityCategoryGetUnitList(newBuildableCategories)
     for _, value in allValues do
         for i, buildableValue in buildable do
             if value == buildableValue then
@@ -247,6 +249,7 @@ function buildActionBuilding(name, modifier)
         cycleThread = ForkThread(function()
             local stayTime = options.hotbuild_cycle_reset_time / 2000.0
             local fadeTime = options.hotbuild_cycle_reset_time / 2000.0
+
             WaitSeconds(stayTime)
             if not cycleMap:IsHidden() then
                 Effect.FadeOut(cycleMap, fadeTime, 0.6, 0.1)
