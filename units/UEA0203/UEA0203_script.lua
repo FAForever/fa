31155c09fdf7eff044c938a2767175bbba15830b
Mr. Nukealizer <mr.nukealizer@gmail.com>
2017-06-16 18:55:41 -0700

    Fix for the broken gunship cargo check

--- units/UEA0203/UEA0203_script.lua
+++ units/UEA0203/UEA0203_script.lua
@@ -46,7 +46,7 @@ UEA0203 = Class(AirTransport, TAirUnit) {
         -- We need to disable the weapon on that unit in case it's a LAB
         -- Use SetEnabled rather than SetOnTransport to ignore units like LABs which can fire from transports
         local units = self:GetCargo()
-        if units[1] then
+        for _, unit in units do
             for i = 1, unit:GetWeaponCount() do
                 local wep = unit:GetWeapon(i)
                 wep:SetEnabled(not bool)
