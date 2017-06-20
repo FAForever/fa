8abe20ae4bc1239477e132eef5a1d205dbc8f35f
dk0x <igor.lapay@gmail.com>
2017-06-20 16:05:48 +0600

    replace lot of code to STR_Utf8SubString

--- lua/UTF.lua
+++ lua/UTF.lua
@@ -30,51 +30,16 @@ function UTF(unicode)
     WARN('Unicode cannot be greater than U+10FFFF! (UTF('.. unicode ..'))')
     return ""
 end
+
 function InsertChar(str, chr, pos)
     -- if we insert char to start or end of text just glue chr and str
     if pos == 0 then return chr .. str end 
-    if pos == STR_Utf8Len(str) then return str .. chr end
-    -- otherwise lets calculate how many bytes contain chars before position
-    local byteArrayOfText={}
-    str:gsub(".", function(c) table.insert(byteArrayOfText,c) end)  -- split str to bytes and add it to byteArrayOfText
-    local offsetInBytes = 0  -- offset in bytes (add it to pos future)
-    local posWithUnicodeChars = 1  -- normaly iteration position by char 
-    while posWithUnicodeChars <= pos do
-        -- first byte always >=0xC0 (192). look at function UTF(unicode) local Byte0 = 0xC0 + ...
-        if string.byte(byteArrayOfText[posWithUnicodeChars+offsetInBytes]) >= 192 then    -- and if we have there unicode char
-            offsetInBytes = offsetInBytes + 1  -- add offset (because unicode char contain 2 bytes)
-        end
-        posWithUnicodeChars = posWithUnicodeChars + 1
-    end
-    return str:sub(1, pos+offsetInBytes) .. chr .. str:sub(pos+offsetInBytes+1) -- split str and add chr between parts
+    local strLen = STR_Utf8Len(str)
+    if pos == strLen then return str .. chr end
+    -- otherwise insert
+    return STR_Utf8SubString(str, 1, pos) .. chr .. STR_Utf8SubString(str, pos+1, strLen - pos) -- split str and add chr between parts
 end
 
---[[
-function InsertChar2(str, chr, pos)
-    -- if we insert char to start or end of text just glue chr and str
-    if pos == 0 then return chr .. str end 
-    if pos == STR_Utf8Len(str) then return str .. chr end
-    -- otherwise lets calculate how many bytes contain chars before position
-    local offset = 0
-    local posWithUnicodeChars = 1
-    local skipSecondByteOfUnicodeChar = false
-    str:gsub(".", 
-        function(c) 
-            if posWithUnicodeChars <= pos then 
-                -- first byte always >=0xC0 (192). look at function UTF(unicode) local Byte0 = 0xC0 + ...
-                if string.byte(c) >= 192 and not skipSecondByteOfUnicodeChar then -- if byte is first part and not second part of unicode char 
-                    offset = offset + 1     -- increase offset
-                    skipSecondByteOfUnicodeChar = true -- and skip next byte because is second part of unicode char
-                else        -- otherwise skip second byte and reset skipSecondByteOfUnicodeChar to false
-                    skipSecondByteOfUnicodeChar = false
-                    posWithUnicodeChars = posWithUnicodeChars + 1  -- this inc fires all iteration of bytes one by char except unicode char (fires one by two byte)
-                end
-            end
-        end
-    )
-   return str:sub(1, pos+offset) .. chr .. str:sub(pos+offset+1) -- split str and add chr between parts
-end
-]]
 
 function AddUnicodeCharToEditText(edit, unicode)
     if unicode <= 0x7F then return "" end
