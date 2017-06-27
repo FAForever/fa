8c0a96e0c75f295f09a9f71c2295f670ef953723
Uveso <Archivar@Zwergenteam.de>
2017-06-27 18:02:40 +0200

    Identify Sorian with variable instead of String find

--- lua/sim/EngineerManager.lua
+++ lua/sim/EngineerManager.lua
@@ -715,8 +715,7 @@ EngineerManager = Class(BuilderManager) {
         local guards = unit:GetGuards()
         for k,v in guards do
             if not v.Dead and v.AssistPlatoon then
-                local per = ScenarioInfo.ArmySetup[self.Brain.Name].AIPersonality
-                if string.find(per, 'sorian') and self.Brain:PlatoonExists(v.AssistPlatoon) then
+                if self.Brain.Sorian and self.Brain:PlatoonExists(v.AssistPlatoon) then
                     v.AssistPlatoon:ForkThread(v.AssistPlatoon.SorianEconAssistBody)
                 elseif self.Brain:PlatoonExists(v.AssistPlatoon) then
                     v.AssistPlatoon:ForkThread(v.AssistPlatoon.EconAssistBody)
@@ -807,8 +806,7 @@ EngineerManager = Class(BuilderManager) {
         local guards = unit:GetGuards()
         for k,v in guards do
             if not v.Dead and v.AssistPlatoon then
-                local per = ScenarioInfo.ArmySetup[self.Brain.Name].AIPersonality
-                if string.find(per, 'sorian') and self.Brain:PlatoonExists(v.AssistPlatoon) then
+                if self.Brain.Sorian and self.Brain:PlatoonExists(v.AssistPlatoon) then
                     v.AssistPlatoon:ForkThread(v.AssistPlatoon.SorianEconAssistBody)
                 elseif self.Brain:PlatoonExists(v.AssistPlatoon) then
                     v.AssistPlatoon:ForkThread(v.AssistPlatoon.EconAssistBody)
