8a67b74c9c9ee9a4c837623139c851f55ea7f2ac
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-08-17 17:04:47 +0200

    disable build ability of acu before warping in
    fixes #2147
    
    due to the short delay before the acu is warped in, the acu was able to
    start building stuff before the animation is played.

--- lua/defaultunits.lua
+++ lua/defaultunits.lua
@@ -2189,7 +2189,6 @@ CommandUnit = Class(WalkingLandUnit) {
         self:HideBone(0, true)
         self:SetUnSelectable(true)
         self:SetBusy(true)
-        self:SetBlockCommandQueue(true)
         self:ForkThread(self.WarpInEffectThread, bones)
     end,
 
