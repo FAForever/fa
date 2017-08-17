e1165ff43ac0bb43db14d86b33ed7e5420e759da
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-08-18 00:37:06 +0200

    disable vision for submerged amphibious units
    fixes #2056
    
    the vision is still enabled for acus and sacus (and other units with
    omni sensors)

--- lua/sim/Unit.lua
+++ lua/sim/Unit.lua
@@ -2840,6 +2840,14 @@ Unit = Class(moho.unit_methods) {
         if self.LayerChangeTrigger then
             self:LayerChangeTrigger(new, old)
         end
+        
+        if new == 'Seabed' then
+            if self:GetBlueprint().Intel.OmniRadius == 0 then
+                self:DisableIntel('Vision')
+            end
+        else
+            self:EnableIntel('Vision')
+        end
     end,
 
     OnMotionHorzEventChange = function(self, new, old)
