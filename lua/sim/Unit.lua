aeff5c92aacbfc4d6890776342b5f8612cdf37a6
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-08-20 01:10:08 +0200

    small change in syntax

--- lua/sim/Unit.lua
+++ lua/sim/Unit.lua
@@ -3021,7 +3021,7 @@ Unit = Class(moho.unit_methods) {
         end
         
         if new == 'Seabed' then
-            if self:GetBlueprint().Intel.OmniRadius == 0 then
+            if not self:GetBlueprint().Intel.OmniRadius > 0 then
                 self:DisableIntel('Vision')
             end
         else
