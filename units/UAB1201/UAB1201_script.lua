cf8b4d5a935d9dcff0f56ad3cd86b02e1c6ff026
Mr. Nukealizer <mr.nukealizer@gmail.com>
2017-08-21 17:56:44 -0700

    Make Aeon T2 generator ball spin like the T3 version

--- units/UAB1201/UAB1201_script.lua
+++ units/UAB1201/UAB1201_script.lua
@@ -11,6 +11,13 @@ local AEnergyCreationUnit = import('/lua/aeonunits.lua').AEnergyCreationUnit
 
 UAB1201 = Class(AEnergyCreationUnit) {
     AmbientEffects = 'AT2PowerAmbient',
+
+    OnStopBeingBuilt = function(self, builder, layer)
+        AEnergyCreationUnit.OnStopBeingBuilt(self, builder, layer)
+        self.Trash:Add(CreateRotator(self, 'Sphere', 'x', nil, 0, 15, 80 + Random(0, 20)))
+        self.Trash:Add(CreateRotator(self, 'Sphere', 'y', nil, 0, 15, 80 + Random(0, 20)))
+        self.Trash:Add(CreateRotator(self, 'Sphere', 'z', nil, 0, 15, 80 + Random(0, 20)))
+    end,
 }
 
 TypeClass = UAB1201
