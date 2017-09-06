8bb98e791c607837aa6d74323840a6b4313efbe3
speed2CZ <GTGTGT@seznam.cz>
2017-09-06 19:06:23 +0200

    Redo army colours
    Pink for the win

--- lua/GameColors.lua
+++ lua/GameColors.lua
@@ -3,47 +3,49 @@ GameColors = {
 
     -- Faction colours
     ArmyColors = {
-    "ff436eee",      -- new blue1
-    "FFe80a0a",      -- Cybran red
-    "ff616d7e",      -- grey
-    "fffafa00",      -- new yellow
-    "FFFF873E",      -- Nomads orange
-    "ffffffff",      -- white
-    "ff9161ff",      -- purple
-    "ffff88ff",      -- pink
-    "ff2e8b57",      -- new green
-    "FF131cd3",      -- UEF blue
-    "FF5F01A7",      -- dark purple
-    "ffff32ff",      -- new fuschia
-    "ffffbf80",      -- lt orange
-    "ffb76518",      -- new brown
-    "ff901427",      -- dark red
-    "FF2F4F4F",      -- olive (dark green)
-    "ff40bf40",      -- mid green
-    "ff66ffcc",      -- aqua
-    "ff76a101",      -- Order Green
+    "FFF4CDD4",      -- Pink
+    "FFE06287",      -- Pink
+    "FFE24585",      -- Pink
+    "FFB52555",      -- Pink
+    "FFA4123F",      -- Pink
+    "FFF5B6CD",      -- Pink
+    "FFF59BBB",      -- Pink
+    "FFEF4A81",      -- Pink
+    "FFE0004D",      -- Pink
+    "FFC5003E",      -- Pink
+    "FFF99FC9",      -- Pink
+    "FFF57EB6",      -- Pink
+    "FFF04E98",      -- Pink
+    "FFE31C79",      -- Pink
+    "FFCE0F69",      -- Pink
+    "FFF5DADF",      -- Pink
+    "FFF7CED7",      -- Pink
+    "FFF9B5C4",      -- Pink
+    "FFF890A5",      -- Pink
+    "FFEF6079",      -- Pink
     },
 
     PlayerColors = {
-    "ff436eee",      -- new blue1
-    "FFe80a0a",      -- Cybran red
-    "ff616d7e",      -- grey
-    "fffafa00",      -- new yellow
-    "FFFF873E",      -- Nomads orange
-    "ffffffff",      -- white
-    "ff9161ff",      -- purple
-    "ffff88ff",      -- pink
-    "ff2e8b57",      -- new green
-    "FF131cd3",      -- UEF blue
-    "FF5F01A7",      -- dark purple
-    "ffff32ff",      -- new fuschia
-    "ffffbf80",      -- lt orange
-    "ffb76518",      -- new brown
-    "ff901427",      -- dark red
-    "FF2F4F4F",      -- olive (dark green)
-    "ff40bf40",      -- mid green
-    "ff66ffcc",      -- aqua
-    "ff76a101",      -- Order Green
+    "FFF4CDD4",      -- Pink
+    "FFE06287",      -- Pink
+    "FFE24585",      -- Pink
+    "FFB52555",      -- Pink
+    "FFA4123F",      -- Pink
+    "FFF5B6CD",      -- Pink
+    "FFF59BBB",      -- Pink
+    "FFEF4A81",      -- Pink
+    "FFE0004D",      -- Pink
+    "FFC5003E",      -- Pink
+    "FFF99FC9",      -- Pink
+    "FFF57EB6",      -- Pink
+    "FFF04E98",      -- Pink
+    "FFE31C79",      -- Pink
+    "FFCE0F69",      -- Pink
+    "FFF5DADF",      -- Pink
+    "FFF7CED7",      -- Pink
+    "FFF9B5C4",      -- Pink
+    "FFF890A5",      -- Pink
+    "FFEF6079",      -- Pink
     },
 
     TeamColorMode = {
