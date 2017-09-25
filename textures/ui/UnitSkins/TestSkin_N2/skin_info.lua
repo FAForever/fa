48903ed54add17dae6edd74ae9627f55e936a4c4
HUSSAR-mtrela <martin.trela@gmail.com>
2017-09-24 21:55:01 -0400

    added tmp. unit skins for testing Unit Skin Manager

---
+++ textures/ui/UnitSkins/TestSkin_N2/skin_info.lua
@@ -0,0 +1,14 @@
+uid = "4dfc9962-n002-4a86-bb4f-3d8b4ecc04b0"
+name = "NOMADS Rusty Skin2"
+description = "A Rusty skin variant for the NOMADS ACU." 
+author = "Exotic_Retard"
+faction = "NOMADS"
+version = 1.0
+
+reward = false
+target = "ACU"
+--enabled = true
+
+
+
+
---
+++ textures/ui/UnitSkins/TestSkin_S1/skin_info.lua
@@ -0,0 +1,14 @@
+uid = "4dfc9962-s001-4a86-bb4f-3d8b4ecc04b0"
+name = "SERAPHIM Rusty Skin"
+description = "A Rusty skin variant for the SERAPHIM ACU." 
+author = "Exotic_Retard"
+faction = "SERAPHIM"
+version = 1.0
+
+reward = false
+target = "ACU"
+--enabled = true
+
+
+
+
