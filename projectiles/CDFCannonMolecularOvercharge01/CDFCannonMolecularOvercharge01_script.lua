1fdc9a19be839fe43ceb3a2dcb95b9f9e481cf47
IceDreamer <bknight4125@gmail.com>
2017-11-10 21:51:13 +0000

    Modify proj scripts

--- projectiles/CDFCannonMolecularOvercharge01/CDFCannonMolecularOvercharge01_script.lua
+++ projectiles/CDFCannonMolecularOvercharge01/CDFCannonMolecularOvercharge01_script.lua
@@ -2,8 +2,9 @@
 
 local CMolecularCannonProjectile = import('/lua/cybranprojectiles.lua').CMolecularCannonProjectile
 local EffectTemplate = import('/lua/EffectTemplates.lua')
+local OverchargeProjectile = import('/lua/sim/DefaultProjectiles.lua').OverchargeProjectile
 
-CDFCannonMolecular01 = Class(CMolecularCannonProjectile) {
+CDFCannonMolecular01 = Class(CMolecularCannonProjectile, OverchargeProjectile) {
     PolyTrail = '/effects/emitters/default_polytrail_03_emit.bp',
     FxTrails = EffectTemplate.CCommanderOverchargeFxTrail01,
 
@@ -11,6 +12,11 @@ CDFCannonMolecular01 = Class(CMolecularCannonProjectile) {
     FxImpactUnit = EffectTemplate.CCommanderOverchargeHit01,
     FxImpactProp = EffectTemplate.CCommanderOverchargeHit01,
     FxImpactLand = EffectTemplate.CCommanderOverchargeHit01,
+
+    OnImpact = function(self, targetType, targetEntity)
+        OverchargeProjectile.OnImpact(self, targetType, targetEntity)
+        CMolecularCannonProjectile.OnImpact(self, targetType, targetEntity)
+    end,
 }
 
 TypeClass = CDFCannonMolecular01
