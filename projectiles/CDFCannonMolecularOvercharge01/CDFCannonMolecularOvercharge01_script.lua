f4d58fbb204b6b29da3cddc237df35adb806e93c
IceDreamer <bknight4125@gmail.com>
2017-11-10 21:39:44 +0000

    Cleanup

--- projectiles/CDFCannonMolecularOvercharge01/CDFCannonMolecularOvercharge01_script.lua
+++ projectiles/CDFCannonMolecularOvercharge01/CDFCannonMolecularOvercharge01_script.lua
@@ -1,18 +1,16 @@
-#
-# Cybran Molecular Cannon
-#
+-- Cybran Molecular Cannon
+
 local CMolecularCannonProjectile = import('/lua/cybranprojectiles.lua').CMolecularCannonProjectile
 local EffectTemplate = import('/lua/EffectTemplates.lua')
 
 CDFCannonMolecular01 = Class(CMolecularCannonProjectile) {
-
     PolyTrail = '/effects/emitters/default_polytrail_03_emit.bp',
     FxTrails = EffectTemplate.CCommanderOverchargeFxTrail01,
 
-    # Hit Effects
+    -- Hit Effects
     FxImpactUnit = EffectTemplate.CCommanderOverchargeHit01,
     FxImpactProp = EffectTemplate.CCommanderOverchargeHit01,
     FxImpactLand = EffectTemplate.CCommanderOverchargeHit01,
 }
-TypeClass = CDFCannonMolecular01
 
+TypeClass = CDFCannonMolecular01
