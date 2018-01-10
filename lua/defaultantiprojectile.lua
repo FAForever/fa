099cdc4ed08f467513c085751900c961b110a919
Kyryl (Etherr) <33268383+FAETHER@users.noreply.github.com>
2018-01-09 21:48:38 -0500

    Cleanup defaultantimissile.lua

--- lua/defaultantiprojectile.lua
+++ lua/defaultantiprojectile.lua
@@ -1,12 +1,12 @@
-#****************************************************************************
-#**
-#**  File     :  /lua/defaultantimissile.lua
-#**  Author(s):  Gordon Duclos
-#**
-#**  Summary  :  Default definitions collision beams
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+--****************************************************************************
+--**
+--**  File     :  /lua/defaultantimissile.lua
+--**  Author(s):  Gordon Duclos
+--**
+--**  Summary  :  Default definitions collision beams
+--**
+--**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+--****************************************************************************
 local Entity = import('/lua/sim/Entity.lua').Entity
 
 Flare = Class(Entity) {
@@ -20,11 +20,11 @@ Flare = Class(Entity) {
         self.RedirectCat = spec.Category or 'MISSILE'
     end,
 
-    # We only divert projectiles. The flare-projectile itself will be responsible for
-    # accepting the collision and causing the hostile projectile to impact.
+   -- We only divert projectiles. The flare-projectile itself will be responsible for
+   -- accepting the collision and causing the hostile projectile to impact.
     OnCollisionCheck = function(self,other)
         if EntityCategoryContains(ParseEntityCategory(self.RedirectCat), other) and (self:GetArmy() != other:GetArmy())then
-            LOG('*DEBUG FLARE COLLISION CHECK')
+        --  LOG('*DEBUG FLARE COLLISION CHECK')
             other:SetNewTarget(self.Owner)
         end
         return false
@@ -41,8 +41,8 @@ DepthCharge = Class(Entity) {
         self:AttachTo(spec.Owner, -1)
     end,
 
-    # We only divert projectiles. The flare-projectile itself will be responsible for
-    # accepting the collision and causing the hostile projectile to impact.
+   -- We only divert projectiles. The flare-projectile itself will be responsible for
+   -- accepting the collision and causing the hostile projectile to impact.
     OnCollisionCheck = function(self,other)
         if EntityCategoryContains(categories.TORPEDO, other) and self:GetArmy() != other:GetArmy() then
             other:SetNewTarget(self.Owner)
@@ -57,13 +57,13 @@ MissileRedirect = Class(Entity) {
                    '/effects/emitters/particle_cannon_beam_02_emit.bp'},
     EndPointEffects = {'/effects/emitters/particle_cannon_end_01_emit.bp',},
     
-    #AttachBone = function( AttachBone )
-    #    self:AttachTo(spec.Owner, self.AttachBone)
-    #end, 
+    --AttachBone = function( AttachBone )
+    --    self:AttachTo(spec.Owner, self.AttachBone)
+    --end, 
 
     OnCreate = function(self, spec)
         Entity.OnCreate(self, spec)
-        LOG('*DEBUG MISSILEREDIRECT START BEING CREATED')
+    --  LOG('*DEBUG MISSILEREDIRECT START BEING CREATED')
         self.Owner = spec.Owner
         self.Radius = spec.Radius
         self.RedirectRateOfFire = spec.RedirectRateOfFire or 1
@@ -72,7 +72,7 @@ MissileRedirect = Class(Entity) {
         self.AttachBone = spec.AttachBone
         self:AttachTo(spec.Owner, spec.AttachBone)
         ChangeState(self, self.WaitingState)
-        #LOG('*DEBUG MISSILEREDIRECT DONE BEING CREATED')
+     -- LOG('*DEBUG MISSILEREDIRECT DONE BEING CREATED')
     end,
 
     OnDestroy = function(self)
@@ -80,57 +80,57 @@ MissileRedirect = Class(Entity) {
         ChangeState(self, self.DeadState)
     end,
 
-    DeadState = State {
-        Main = function(self)
-        end,
+DeadState = State {
+    Main = function(self)
+    end,
     },
 
-    # Return true to process this collision, false to ignore it.
+    -- Return true to process this collision, false to ignore it.
 
-    WaitingState = State{
-        OnCollisionCheck = function(self, other)
-            LOG('*DEBUG MISSILE REDIRECT COLLISION CHECK')
-            if EntityCategoryContains(categories.MISSILE, other) and not EntityCategoryContains(categories.STRATEGIC, other) 
-                        and other != self.EnemyProj and IsEnemy( self:GetArmy(), other:GetArmy() ) then
-                self.Enemy = other:GetLauncher()
-                self.EnemyProj = other
-                #NOTE: Fix me We need to test enemy validity if there is no enemy 
-                #      set target to 180 of the unit
-                if self.Enemy then
-                    other:SetNewTarget(self.Enemy)
-                    other:TrackTarget(true)
-                    other:SetTurnRate(720)
-                end
-                ChangeState(self, self.RedirectingState)
-            end
-            return false
-        end,
+WaitingState = State{
+    OnCollisionCheck = function(self, other)
+         --LOG('*DEBUG MISSILE REDIRECT COLLISION CHECK')
+        if EntityCategoryContains(categories.MISSILE, other) and not EntityCategoryContains(categories.STRATEGIC, other) 
+           and other != self.EnemyProj and IsEnemy( self:GetArmy(), other:GetArmy() ) then
+           self.Enemy = other:GetLauncher()
+           self.EnemyProj = other
+           --NOTE: Fix me We need to test enemy validity if there is no enemy 
+           --      set target to 180 of the unit
+           if self.Enemy then
+              other:SetNewTarget(self.Enemy)
+              other:TrackTarget(true)
+              other:SetTurnRate(720)
+           end
+           ChangeState(self, self.RedirectingState)
+        end
+           return false
+    end,
     },
 
-    RedirectingState = State{
+RedirectingState = State{
 
-        Main = function(self)
-            if not self or self:BeenDestroyed() 
-            or not self.EnemyProj or self.EnemyProj:BeenDestroyed() 
-            or not self.Owner or self.Owner:IsDead() then
-                return
-            end
+    Main = function(self)
+        if not self or self:BeenDestroyed() 
+           or not self.EnemyProj or self.EnemyProj:BeenDestroyed() 
+           or not self.Owner or self.Owner:IsDead() then
+           return
+        end
             
-            local beams = {}
-            for k, v in self.RedirectBeams do               
-                table.insert(beams, AttachBeamEntityToEntity(self.EnemyProj, -1, self.Owner, self.AttachBone, self:GetArmy(), v))
+    local beams = {}
+    for k, v in self.RedirectBeams do               
+        table.insert(beams, AttachBeamEntityToEntity(self.EnemyProj, -1, self.Owner, self.AttachBone, self:GetArmy(), v))
+    end
+        if self.Enemy then
+         -- Set collision to friends active so that when the missile reaches its source it can deal damage. 
+            self.EnemyProj.DamageData.CollideFriendly = true         
+            self.EnemyProj.DamageData.DamageFriendly = true 
+            self.EnemyProj.DamageData.DamageSelf = true 
+        end
+        if self.Enemy and not self.Enemy:BeenDestroyed() then
+            WaitSeconds(1/self.RedirectRateOfFire)
+            if not self.EnemyProj:BeenDestroyed() then
+                self.EnemyProj:TrackTarget(false)
             end
-            if self.Enemy then
-            # Set collision to friends active so that when the missile reaches its source it can deal damage. 
-			    self.EnemyProj.DamageData.CollideFriendly = true         
-			    self.EnemyProj.DamageData.DamageFriendly = true 
-			    self.EnemyProj.DamageData.DamageSelf = true 
-			end
-            if self.Enemy and not self.Enemy:BeenDestroyed() then
-                WaitSeconds(1/self.RedirectRateOfFire)
-                if not self.EnemyProj:BeenDestroyed() then
-                     self.EnemyProj:TrackTarget(false)
-                end
             else
                 WaitSeconds(1/self.RedirectRateOfFire)
                 local vectordam = {}
@@ -143,11 +143,11 @@ MissileRedirect = Class(Entity) {
                 v:Destroy()
             end
             ChangeState(self, self.WaitingState)
-        end,
+    end,
 
-        OnCollisionCheck = function(self, other)
-            return false
-        end,
+    OnCollisionCheck = function(self, other)
+    return false
+    end,
     },
 
 }
