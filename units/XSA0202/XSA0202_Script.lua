1bbc5b871bf3412166e1bfeb8cd53a0543e6e833
Kyryl Melekhin <k.melekhin@gmail.com>
2018-01-15 12:47:23 -0500

    New Gpg unit scripts.

---
+++ units/XSA0202/XSA0202_Script.lua
@@ -0,0 +1,54 @@
+#****************************************************************************
+#**
+#**  File     :  /data/units/XSA0202/XSA0202_script.lua
+#**  Author(s):  Jessica St. Croix, Gordon Duclos, Matt Vainio, Aaron Lundquist
+#**
+#**  Summary  :  Seraphim Fighter/Bomber Script
+#**
+#**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+local SAirUnit = import('/lua/seraphimunits.lua').SAirUnit
+local SeraphimWeapons = import('/lua/seraphimweapons.lua')
+local SAAShleoCannonWeapon = SeraphimWeapons.SAAShleoCannonWeapon
+local SDFBombOtheWeapon = SeraphimWeapons.SDFBombOtheWeapon
+
+XSA0202 = Class(SAirUnit) {
+    Weapons = {
+        ShleoAAGun01 = Class(SAAShleoCannonWeapon) {
+			FxMuzzleFlash = {'/effects/emitters/sonic_pulse_muzzle_flash_02_emit.bp',},
+        },
+        ShleoAAGun02 = Class(SAAShleoCannonWeapon) {
+			FxMuzzleFlash = {'/effects/emitters/sonic_pulse_muzzle_flash_02_emit.bp',},
+        },
+        Bomb = Class(SDFBombOtheWeapon) {
+                
+            IdleState = State (SDFBombOtheWeapon.IdleState) {
+                Main = function(self)
+                    SDFBombOtheWeapon.IdleState.Main(self)
+                end,
+                
+                OnGotTarget = function(self)
+                    self.unit:SetBreakOffTriggerMult(2.0)
+                    self.unit:SetBreakOffDistanceMult(8.0)
+                    self.unit:SetSpeedMult(0.67)
+                    SDFBombOtheWeapon.OnGotTarget(self)
+                end,                
+            },
+        
+            OnGotTarget = function(self)
+                self.unit:SetBreakOffTriggerMult(2.0)
+                self.unit:SetBreakOffDistanceMult(8.0)
+                self.unit:SetSpeedMult(0.67)
+                SDFBombOtheWeapon.OnGotTarget(self)
+            end,
+        
+            OnLostTarget = function(self)
+                self.unit:SetBreakOffTriggerMult(1.0)
+                self.unit:SetBreakOffDistanceMult(1.0)
+                self.unit:SetSpeedMult(1.0)
+                SDFBombOtheWeapon.OnLostTarget(self)
+            end,  	
+        },
+    },
+}
+TypeClass = XSA0202
