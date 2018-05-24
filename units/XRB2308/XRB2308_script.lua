ff2dfa473545f707df599b0fba791ffba05eeacd
Strogoo <Pilot9.65@gmail.com>
2018-05-24 21:37:05 +0300

    Remove old code

--- units/XRB2308/XRB2308_script.lua
+++ units/XRB2308/XRB2308_script.lua
@@ -16,7 +16,6 @@ XRB2308 = Class(CStructureUnit) {
 
     OnStopBeingBuilt = function(self, builder, layer)
         CStructureUnit.OnStopBeingBuilt(self, builder, layer)
-        --self:StartSinkingFromBuild() --previous sinking code
         
         IssueDive({self})
         self:FinalAnimation()
@@ -26,56 +25,6 @@ XRB2308 = Class(CStructureUnit) {
         self.Trash:Add(CreateAttachedEmitter(self, 'xrb2308', army, '/effects/emitters/tt_snowy01_landing01_01_emit.bp'):ScaleEmitter(1.5)) -- One-off
 
         ChangeState(self, self.IdleState)
-    end,
-
-    StartSinkingFromBuild = function(self)
-        -- do not start sinking when unit is below water surface already
-        local position = self:GetPosition()
-        if GetSurfaceHeight(position[1], position[3]) > position[2] then return end
-
-        -- Add sinking effect for the duration of the sinking
-        local army = self:GetArmy()
-        self.Trash:Add(CreateAttachedEmitter(self, 'xrb2308', army, '/effects/emitters/tt_water_submerge02_01_emit.bp'):ScaleEmitter(1.5)) -- Continuous
-
-        -- Create sinker projectile
-        local bone = 0
-        local proj = self:CreateProjectileAtBone('/projectiles/Sinker/Sinker_proj.bp', bone)
-        self.sinkProjectile = proj
-
-        -- Start the sinking after a delay of the given number of seconds, attaching to a given bone and entity.
-        proj:SetLocalAngularVelocity(0, 0, 0) -- Change this to make it rotate some while sinking
-        proj:Start(0, self, bone)
-        proj:SetBallisticAcceleration(-0.75)
-
-        self.Trash:Add(proj)
-        self.Depthwatcher = self:ForkThread(self.DepthWatcher)
-    end,
-
-    -- Sink to the bottom, or to a pre-set depth
-    DepthWatcher = function(self)
-        self.sinkingFromBuild = true
-
-        local sinkFor = 3.4 -- Use this to set the depth - Basic maths required
-        while self.sinkProjectile and sinkFor > 0 do
-            WaitTicks(1)
-            sinkFor = sinkFor - 0.1
-        end
-
-        local bottom = true
-        if not self.Dead then
-            if self.sinkProjectile then
-                bottom = false -- We must have timed out
-                self.sinkProjectile:Destroy()
-                self.sinkProjectile = nil
-            end
-
-            -- Stop the unit's momentum
-            self:SetPosition(self:GetPosition(), true)
-            self:FinalAnimation()
-        end
-
-        self.sinkingFromBuild = false
-        self.Bottom = bottom
     end,
 
     -- Do the deploy animation
