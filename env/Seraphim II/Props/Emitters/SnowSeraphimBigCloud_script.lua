985f91e19e664847683e346d0b6ab7baab2225ea
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2018-06-10 00:46:56 +0200

    add new textures, decals and props
    using the material that Deep_ made in 2013, these additional map materials will help to create a new set of different looking maps.
    
    http://forums.faforever.com/viewtopic.php?f=53&t=4651&p=49933#p50157
    
    He created all of these assets by himself and ok'd its use.
    > Yes all used textures and meshes are done by myself and sure i would like to share my stuff.

---
+++ env/Seraphim II/Props/Emitters/SnowSeraphimBigCloud_script.lua
@@ -0,0 +1,14 @@
+#
+# LavaSteam01
+#
+BlowingSnow01 = Class(import('/lua/sim/Prop.lua').Prop) {
+
+    OnCreate = function(self)
+        CreateEmitterAtBone(self, -2, -1, '/effects/emitters/weather_snowseraphim_03_emit.bp')
+    end,
+
+    OnDestroy = function()
+    end,
+}
+
+TypeClass = BlowingSnow01
