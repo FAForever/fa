fadb68e839ace2aa53d3fbaace31a2d44379c506
Strogoo <Pilot9.65@gmail.com>
2018-08-07 05:46:58 +0300

    Fix assist for mobile shields (EQ)
    Thanks to Exotic

--- lua/SimCallbacks.lua
+++ lua/SimCallbacks.lua
@@ -250,3 +250,17 @@ Callbacks.AttackMove = function(data, units)
     end
     IssueAggressiveMove(units, data.Target)
 end
+
+--tells a unit to toggle its pointer
+Callbacks.FlagShield = function(data, units)
+    units = SecureUnits(units)
+    local target = GetEntityById(data.target)
+    if units and target then
+        for k, u in units do
+            if IsEntity(u) and u.PointerEnabled == true then
+                u.PointerEnabled = false --turn the pointer flag off
+                u:DisablePointer() --turn the pointer off
+            end
+        end
+    end
+end
