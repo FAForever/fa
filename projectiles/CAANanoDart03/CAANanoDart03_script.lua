2b97cb7f675f3523b7b04fd13401fe4c43d6873f
Petricpwnz <petrasanic@yandex.ru>
2018-09-20 06:34:47 +0300

    Omlette du balancè
    -Cruiser projectile further tweaks
    -T3 MAA speed and range buffs
    -T2 air HQ cost increase
    -Torp bomber cost increase
    -Novax intel improvement
    -Chrono stun nerf and add extra hp
    -Fix incorrect aeon BS range
    -Make atlantis non-groundifreable except for tempest
    -Restorer speed and air-to-ground dps buff
    -Things I forgot

--- projectiles/CAANanoDart03/CAANanoDart03_script.lua
+++ projectiles/CAANanoDart03/CAANanoDart03_script.lua
@@ -13,8 +13,8 @@ CAANanoDart01 = Class(CAANanoDartProjectile) {
 
 
     UpdateThread = function(self)
-        WaitSeconds(0.15)
-        self:SetMaxSpeed(15)
+        WaitSeconds(0.25)
+        self:SetMaxSpeed(10)
         self:SetBallisticAcceleration(-0.2)
         local army = self:GetArmy()
 
@@ -22,7 +22,7 @@ CAANanoDart01 = Class(CAANanoDartProjectile) {
             CreateEmitterOnEntity(self,army,self.FxTrails[i])
         end
 
-        WaitSeconds(0.3)
+        WaitSeconds(0.25)
         self:SetMesh('/projectiles/CAANanoDart01/CAANanoDartUnPacked01_mesh')
         self:SetMaxSpeed(60)
         self:SetAcceleration(20 + Random() * 5)
