68f1ed8d1185b14a464bc9337322ce8de07dd530
AceWindu <kidofthecabbagepatch@hotmail.com>
2018-09-29 10:15:00 +0100

    add aeon anti torp projectile bp and script file

---
+++ projectiles/AIMAntiTorpedo01/AIMAntiTorpedo01_script.lua
@@ -0,0 +1,8 @@
+#
+# Ship-based Anti-Torpedo Script
+#
+local QuasarAntiTorpedoChargeSubProjectile = import('/lua/aeonprojectiles.lua').ATorpedoSubProjectile
+
+AIMAntiTorpedo01 = Class(QuasarAntiTorpedoChargeSubProjectile) {}
+
+TypeClass = AIMAntiTorpedo01
