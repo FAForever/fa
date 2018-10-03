e3955b35c12b7aaeb011a57b18f91b370e1dcaaf
Strogoo <Pilot9.65@gmail.com>
2018-10-03 04:41:15 +0300

    Strats script files cleanup

--- units/UAA0304/UAA0304_Script.lua
+++ units/UAA0304/UAA0304_Script.lua
@@ -1,12 +1,9 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/UAA0304/UAA0304_script.lua
-#**  Author(s):  John Comes, David Tomandl
-#**
-#**  Summary  :  Aeon Strategic Bomber Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+------------------------------------------------------------------
+--  File     :  /cdimage/units/UAA0304/UAA0304_script.lua
+--  Author(s):  John Comes, David Tomandl
+--  Summary  :  Aeon Strategic Bomber Script
+--  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+------------------------------------------------------------------
 
 local AAirUnit = import('/lua/aeonunits.lua').AAirUnit
 local AIFBombQuarkWeapon = import('/lua/aeonweapons.lua').AIFBombQuarkWeapon
--- units/UEA0304/UEA0304_Script.lua
+++ units/UEA0304/UEA0304_Script.lua
@@ -1,12 +1,9 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/UEA0304/UEA0304_script.lua
-#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
-#**
-#**  Summary  :  UEF Strategic Bomber Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+------------------------------------------------------------------
+--  File     :  /cdimage/units/UEA0304/UEA0304_script.lua
+--  Author(s):  John Comes, David Tomandl, Jessica St. Croix
+--  Summary  :  UEF Strategic Bomber Script
+--  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+------------------------------------------------------------------
 
 local TAirUnit = import('/lua/terranunits.lua').TAirUnit
 local TIFSmallYieldNuclearBombWeapon = import('/lua/terranweapons.lua').TIFSmallYieldNuclearBombWeapon
