6a7f95304950cff2d700efe4b122808da21e45df
speed2 <GTGTGT@seznam.cz>
2018-10-28 17:45:02 +0100

    Move the changelog out of lobby.lua

---
+++ lua/ui/lobby/changelog.lua
@@ -0,0 +1,78 @@
+local Group = import('/lua/maui/group.lua').Group
+local ItemList = import('/lua/maui/itemlist.lua').ItemList
+local LayoutHelpers = import('/lua/maui/layouthelpers.lua')
+local Popup = import('/lua/ui/controls/popups/popup.lua').Popup
+local Prefs = import('/lua/user/prefs.lua')
+local UIUtil = import('/lua/ui/uiutil.lua')
+
+function CreateUI(parent, showPatch)
+    local dialogContent = Group(parent)
+    dialogContent.Width:Set(1000)
+    dialogContent.Height:Set(700)
+
+    local Changelog = import('/lua/ui/lobby/changelogData.lua')
+    local changelogPopup = Popup(parent, dialogContent)
+    changelogPopup.OnClosed = function()
+        Prefs.SetToCurrentProfile('LobbyChangelog', Changelog.last_version)
+    end
+
+    -- Title --
+    local text0 = UIUtil.CreateText(dialogContent, LOC("<LOC lobui_0412>"), 17, 'Arial Gras', true)
+    LayoutHelpers.AtHorizontalCenterIn(text0, dialogContent, 0)
+    LayoutHelpers.AtTopIn(text0, dialogContent, 10)
+
+    -- Info List --
+    local InfoList = ItemList(dialogContent)
+    InfoList:SetFont(UIUtil.bodyFont, 11)
+    InfoList:SetColors(nil, "00000000")
+    InfoList.Width:Set(972)
+    InfoList.Height:Set(610)
+    LayoutHelpers.AtLeftIn(InfoList, dialogContent, 10)
+    LayoutHelpers.AtRightIn(InfoList, dialogContent, 26)
+    LayoutHelpers.AtTopIn(InfoList, dialogContent, 38)
+    UIUtil.CreateLobbyVertScrollbar(InfoList)
+    InfoList.OnClick = function(self) end
+    -- See only new Changelog by version
+    local Last_Changelog_Version = Prefs.GetFromCurrentProfile('LobbyChangelog') or 0
+	if showPatch == true then
+		Last_Changelog_Version = Last_Changelog_Version -1
+	end
+    for i, d in Changelog.changelog do
+        if Last_Changelog_Version < d.version then
+            InfoList:AddItem(d.name)
+            for k, v in d.description do
+                InfoList:AddItem(v)
+            end
+            InfoList:AddItem('')
+        end
+    end
+
+    -- OK button --
+    local OkButton = UIUtil.CreateButtonWithDropshadow(dialogContent, '/BUTTON/medium/', "Ok")
+    LayoutHelpers.AtLeftIn(OkButton, dialogContent, 0)
+    LayoutHelpers.AtBottomIn(OkButton, dialogContent, 10)
+    OkButton.OnClick = function()
+        changelogPopup:Close()
+    end
+	-- Link to the changelog on github --
+	local ChangelogButton = UIUtil.CreateButtonWithDropshadow(dialogContent, '/BUTTON/medium/', "All Changes")
+	LayoutHelpers.AtRightIn(ChangelogButton, dialogContent, 0)
+	LayoutHelpers.AtBottomIn(ChangelogButton, dialogContent, 10)
+    ChangelogButton.OnClick = function()
+		OpenURL('http://github.com/FAForever/fa/blob/develop/changelog.md')
+    end
+end
+
+-- Changelog dialog
+function NeedChangelog()
+    local Changelog = import('/lua/ui/lobby/changelogData.lua').changelog
+    local Last_Changelog_Version = Prefs.GetFromCurrentProfile('LobbyChangelog') or 0
+    local result = false
+    for i, d in Changelog do
+        if Last_Changelog_Version < d.version then
+            result = true
+            break
+        end
+    end
+    return result
+end
