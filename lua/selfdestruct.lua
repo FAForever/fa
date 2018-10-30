67b64c584aa2bea3948d34f6df07819664271ed0
IceDreamer <bknight4125@gmail.com>
2018-10-30 21:36:46 +0000

    Refactor selfdestruct code

--- lua/selfdestruct.lua
+++ lua/selfdestruct.lua
@@ -12,53 +12,42 @@ function ToggleSelfDestruct(data)
                 table.insert(unitEntities, unit)
             end
         end
-        if table.getsize(unitEntities) > 0 then
-            if data.noDelay then -- Kill these units instantly
-                for _, unit in unitEntities do
-                    if unit:BeenDestroyed() or unit.Dead then return end
 
-                    FireSelfdestructWeapons(unit)
-                    unit:Kill()
-                end
-            else
-                local togglingOff = false
-                for _, unit in unitEntities do -- Rescue anything in the process of dying, and skip the next bit
-                    if unit.SelfDestructThread then
-                        togglingOff = true
-                        KillThread(unit.SelfDestructThread)
-                        unit.SelfDestructThread = false
-                        local entityId = unit:GetEntityId()
-                        CancelCountdown(entityId)
-                    end
-                end
+        local off = false
+        for _, unit in unitEntities do
+            if data.noDelay or unit:GetBlueprint().General.InstantDeathOnSelfDestruct then -- Kill these units instantly
+                TriggerDestruction(unit)
+            elseif unit.SelfDestructThread then
+                KillThread(unit.SelfDestructThread)
+                unit.SelfDestructThread = false
+                CancelCountdown(unit:GetEntityId())
 
-                if not togglingOff then
-                    for _, unitEnt in unitEntities do
-                        local unit = unitEnt
-
-                        -- Unit and weapon bp flags can be used to control behaviour on SelfDestruct
-                        -- Instant kill if InstantDeathOnSelfDestruct = true variable set in units general table
-                        -- Fires weapons with FireOnSelfDestruct = true in units weapon table
-                        local bp = unit:GetBlueprint()
-                        if bp.General.InstantDeathOnSelfDestruct then
-                            FireSelfdestructWeapons(unit)
-                            unit:Kill()
-                        else
-                            -- Regular self destruct cycle
-                            local entityId = unit:GetEntityId()
-                            StartCountdown(entityId)
-                            unit.SelfDestructThread = ForkThread(function()
-                                WaitSeconds(5)
-                                if unit:BeenDestroyed() then return end
-                                FireSelfdestructWeapons(unit)
-                                unit:Kill()
-                            end)
-                        end
-                    end
-                end
+                off = true
             end
         end
+
+        if off then return end
+
+        -- Begin regular self destruct cycle
+        for _, unit in unitEntities do
+            if unit:BeenDestroyed() or unit.Dead then continue end
+
+            local u = unit -- Save the looped entity for the forked thread to work
+            StartCountdown(u:GetEntityId())
+            u.SelfDestructThread = ForkThread(
+            function()
+                WaitTicks(50)
+                TriggerDestruction(u)
+            end)
+        end
     end
+end
+
+function TriggerDestruction(unit)
+    if unit:BeenDestroyed() or unit.Dead then return end
+
+    FireSelfdestructWeapons(unit)
+    unit:Kill()
 end
 
 function FireSelfdestructWeapons(unit)
