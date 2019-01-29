c8abda3eefd056975f8aac60a6dfe0332da77e83
TurinturambarFAF <41325352+TurinturambarFAF@users.noreply.github.com>
2019-01-29 07:47:37 +0100

    Scathis rotationbug fix (#2699)
    * Scathis rework
    
    makes the scathis into a gameender with increasd cost range higher dps and an adjusted firingcycle.
    
    * scathis mobility increase
    
    makes the scathis faster and thereby makes it possible to dodge nukes
    
    * adding bps
    
    adds projectile bps to make it possible to adjust AoE
    
    * fixes scathis not shoting
    
    fixes some bugg causing scathis to not shoot
    
    * bigger decalls
    
    increases scathis projectile explosion and crater effect to match the new AoE value AoE.
    
    * adding bps
    
    adds scathis script file fox rotation bugg fix
    
    * scathis rotationbug fix
    
    fixes scathis barrel not rotationg after shooting
    
    * faster scathis projectiles
    
    increases scathis projectile speed to mavor level
    
    * Update URL0401_unit.bp
    
    * revertint speedbuff and adjusting firing e cost
    
    reverts maxspeed to old value and ajusts required e and e cost to gameender level

---
+++ projectiles/CIFArtilleryProton01/CIFArtilleryProton01_script.lua
@@ -0,0 +1,28 @@
+#
+# Cybran Artillery Projectile
+#
+local CArtilleryProtonProjectile = import('/lua/cybranprojectiles.lua').CArtilleryProtonProjectile
+local RandomFloat = import('/lua/utilities.lua').GetRandomFloat
+
+CIFArtilleryProton01 = Class(CArtilleryProtonProjectile) {
+
+    OnImpact = function(self, targetType, targetEntity)
+        CArtilleryProtonProjectile.OnImpact(self, targetType, targetEntity)
+        local army = self:GetArmy()
+        CreateLightParticle( self, -1, army, 24, 12, 'glow_03', 'ramp_red_06' )
+        CreateLightParticle( self, -1, army, 18, 22, 'glow_03', 'ramp_antimatter_02' )
+        if targetType == 'Terrain' or targetType == 'Prop' then
+            CreateDecal( self:GetPosition(), RandomFloat(0.0,6.28), 'scorch_011_albedo', '', 'Albedo', 20, 20, 350, 200, army )  
+        end
+        ForkThread(self.ForceThread, self, self:GetPosition())
+        self:ShakeCamera( 20, 3, 0, 1 )        
+    end,
+
+    ForceThread = function(self, pos)
+        DamageArea(self, pos, 10, 1, 'Force', true)
+        WaitTicks(2)
+        DamageArea(self, pos, 10, 1, 'Force', true)
+        DamageRing(self, pos, 10, 15, 1, 'Fire', true)
+    end,
+}
+TypeClass = CIFArtilleryProton01
