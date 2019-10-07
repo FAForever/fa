67870ed1560771f29fdf01a9b790d5656a475f82
keyser <PYL35@hotmail.fr>
2019-10-07 16:59:50 +0200

    clean up wagner script

--- units/URL0203/URL0203_script.lua
+++ units/URL0203/URL0203_script.lua
@@ -1,12 +1,9 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/URL0203/URL0203_script.lua
-#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
-#**
-#**  Summary  :  Cybran Ambphibious Tank Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+-----------------------------------------------------------------
+--  File     :  /cdimage/units/URL0203/URL0203_script.lua
+--  Author(s):  John Comes, David Tomandl, Jessica St. Croix
+--  Summary  :  Cybran Ambphibious Tank Script
+--  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+-----------------------------------------------------------------
 
 local CLandUnit = import('/lua/cybranunits.lua').CLandUnit
 local CybranWeaponsFile = import('/lua/cybranweapons.lua')
