6011718d0d4a542b62c518e0bacc497819b1c0f3
keyser <PYL35@hotmail.fr>
2019-10-10 01:20:00 +0200

    clean up sera T2 sonar script

--- units/XSB3202/XSB3202_script.lua
+++ units/XSB3202/XSB3202_script.lua
@@ -1,12 +1,9 @@
-#****************************************************************************
-#**
-#**  File     :  /cdimage/units/UAB3202/UAB3202_script.lua
-#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
-#**
-#**  Summary  :  Aeon Long Range Sonar Script
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+-----------------------------------------------------------------
+--  File     :  /cdimage/units/UAB3202/UAB3202_script.lua
+--  Author(s):  John Comes, David Tomandl, Jessica St. Croix
+--  Summary  :  Aeon Long Range Sonar Script
+--  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+-----------------------------------------------------------------
 
 local SSonarUnit = import('/lua/seraphimunits.lua').SSonarUnit
 
