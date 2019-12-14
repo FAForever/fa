f30fb54eb962fa14005b177a344fa21cc876493c
wodzu_93 <58888496+wodzu93@users.noreply.github.com>
2019-12-14 23:11:55 +0100

    Add files via upload

---
+++ units/XEB2306/XEB2306_Script.lua
@@ -0,0 +1,29 @@
+local TStructureUnit = import('/lua/terranunits.lua').TStructureUnit
+local TDFHeavyPlasmaCannonWeapon = import('/lua/terranweapons.lua').TDFHeavyPlasmaGatlingCannonWeapon
+
+local EffectUtils = import('/lua/effectutilities.lua')
+local Effects = import('/lua/effecttemplates.lua')
+
+XEB2306 = Class(TStructureUnit) {
+    Weapons = {
+        MainGun = Class(TDFHeavyPlasmaCannonWeapon) 
+        {       
+            PlayFxWeaponPackSequence = function(self)
+                self.ExhaustEffects = EffectUtils.CreateBoneEffects( self.unit, 'Exhaust', self.unit:GetArmy(), Effects.WeaponSteam01 )
+                TDFHeavyPlasmaCannonWeapon.PlayFxWeaponPackSequence(self)
+            end,
+            
+            PlayFxRackSalvoReloadSequence = function(self)
+                self.ExhaustEffects = EffectUtils.CreateBoneEffects( self.unit, 'Exhaust', self.unit:GetArmy(), Effects.WeaponSteam01 )
+                TDFHeavyPlasmaCannonWeapon.PlayFxRackSalvoChargeSequence(self)
+            end,    
+        }
+    },
+
+    OnStopBeingBuilt = function(self,builder,layer)
+        TStructureUnit.OnStopBeingBuilt(self,builder,layer)
+
+	self.Trash:Add(CreateRotator(self, 'Gun_Barrel', 'z', nil, 270, 0, 0))
+    end,
+}
+TypeClass = XEB2306
