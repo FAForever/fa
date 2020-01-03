51c56eb372a668dc1a74e5d74578356936afda59
keyser <PYL35@hotmail.fr>
2020-01-03 14:55:57 +0100

    remove vet bar for unvettable units (#2939)
    * remove vet bar for unvettable units
    
    this change apply to cybran land scouts, T1/T3 air scouts, stealth boat, mobile stealth, mobile shields, shield boat, tmd, smd, T1 transports, aeon/cybran sonars, drones, mercy, beetle
    
    * improve code vet bar
    
    * add flag to weapon unvettable
    
    * improved unvettable flag
    
    * refactor ShouldUseVetSystem
    
    * more refactoring

--- lua/sim/Unit.lua
+++ lua/sim/Unit.lua
@@ -1445,21 +1445,25 @@ Unit = Class(moho.unit_methods) {
 
     -- Returns true if a unit can gain veterancy (Has a weapon)
     ShouldUseVetSystem = function(self)
-        local weps = self:GetBlueprint().Weapon
+        local bp = self:GetBlueprint()
 
-        -- Bail if we don't have any weapons
-        if not weps[1] then
+        -- Bail if we don't have any weapons or have the ExcludeFromVeterancy flag (TMD, SMD, stealth boat, mobile stealth, mobile shields, aeon T3 sonar, mercy, beetle)
+        if not bp.Weapon[1] or bp.General.ExcludeFromVeterancy then
             return false
         end
 
-        -- Find a weapon which is not a DeathWeapon
-        for index, wep in weps do
-            if wep.Label ~= 'DeathWeapon' then
+        -- Find a weapon which is not a DeathWeapon / DeathImpact
+        local No_vet_label = {
+        ['DeathWeapon'] = true,
+        ['DeathImpact'] = true,
+        }
+        for index, wep in bp.Weapon do
+            if not No_vet_label[wep.Label] then
                 return true
             end
         end
 
-        -- We only have a DeathWeapon. Bail.
+        -- We only have DeathWeapon / DeathImpact labels. Bail.
         return false
     end,
 
