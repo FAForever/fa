7e35c37c121ae22eae0e61ff37d93bfe3a4f6ccd
TurinturambarFAF <konrad.schulz@posteo.net>
2020-02-20 10:14:10 +0100

    adjust explosion decalls
    changes size of explosion decalls for cyb T3 mobile arty, T3 static arty and scathis to the respective AoE sizes

--- projectiles/CIFArtilleryProton03/CIFArtilleryProton03_script.lua
+++ projectiles/CIFArtilleryProton03/CIFArtilleryProton03_script.lua
@@ -4,20 +4,22 @@
 local CArtilleryProtonProjectile = import('/lua/cybranprojectiles.lua').CArtilleryProtonProjectile
 local RandomFloat = import('/lua/utilities.lua').GetRandomFloat
 
-CIFArtilleryProton01 = Class(CArtilleryProtonProjectile) {
+CIFArtilleryProton03 = Class(CArtilleryProtonProjectile) {
 
     OnImpact = function(self, targetType, targetEntity)
         CArtilleryProtonProjectile.OnImpact(self, targetType, targetEntity)
-        local army = self:GetArmy()
-        CreateLightParticle( self, -1, army, 24, 12, 'glow_03', 'ramp_red_06' )
-        CreateLightParticle( self, -1, army, 18, 22, 'glow_03', 'ramp_antimatter_02' )
+        local army = self.Army
+        CreateLightParticle( self, -1, army, 29, 12, 'glow_03', 'ramp_red_06' )
+        CreateLightParticle( self, -1, army, 29, 22, 'glow_03', 'ramp_antimatter_02' )
         if targetType == 'Terrain' or targetType == 'Prop' then
-            CreateDecal( self:GetPosition(), RandomFloat(0.0,6.28), 'scorch_011_albedo', '', 'Albedo', 20, 20, 350, 200, army )  
+            CreateDecal( self:GetPosition(), RandomFloat(0.0,6.28), 'scorch_011_albedo', '', 'Albedo', 29, 29, 350, 200, army )  
         end
         ForkThread(self.ForceThread, self, self:GetPosition())
         self:ShakeCamera( 20, 3, 0, 1 )        
     end,
-
+    FxLandHitScale = 1.6,
+    FxPropHitScale = 1.6,
+    FxUnitHitScale = 1.6,
     ForceThread = function(self, pos)
         DamageArea(self, pos, 10, 1, 'Force', true)
         WaitTicks(2)
@@ -25,4 +27,4 @@ CIFArtilleryProton01 = Class(CArtilleryProtonProjectile) {
         DamageRing(self, pos, 10, 15, 1, 'Fire', true)
     end,
 }
-TypeClass = CIFArtilleryProton01
+TypeClass = CIFArtilleryProton03
