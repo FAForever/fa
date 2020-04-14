001d82f055d3e5079169b81891a544b41d34aeaf
keyser <PYL35@hotmail.fr>
2020-04-14 12:29:44 +0200

    readd forcethread (#3048)

--- projectiles/CIFArtilleryProton01/CIFArtilleryProton01_script.lua
+++ projectiles/CIFArtilleryProton01/CIFArtilleryProton01_script.lua
@@ -13,8 +13,18 @@ CIFArtilleryProton01 = Class(CArtilleryProtonProjectile) {
         CreateLightParticle( self, -1, army, 12, 22, 'glow_03', 'ramp_antimatter_02' )
 		if targetType == 'Terrain' or targetType == 'Prop' then
 			CreateDecal( self:GetPosition(), RandomFloat(0.0,6.28), 'scorch_011_albedo', '', 'Albedo', 14, 14, 350, 200, army )  
-        end      
+        end
+        ForkThread(self.ForceThread, self, self:GetPosition())
+        self:ShakeCamera( 20, 3, 0, 1 )
     end,
+    
+    ForceThread = function(self, pos)
+        DamageArea(self, pos, 7, 1, 'Force', true)
+        WaitTicks(2)
+        DamageArea(self, pos, 7, 1, 'Force', true)
+        DamageRing(self, pos, 7, 10, 1, 'Fire', true)
+    end,
+    
     FxLandHitScale = 0.65,
     FxPropHitScale = 0.65,
     FxUnitHitScale = 0.65,
