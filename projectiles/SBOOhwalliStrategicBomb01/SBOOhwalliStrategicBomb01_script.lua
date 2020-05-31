aee547772e7533dac7f49feacf223109fe5946e5
shalkya <PYL35@hotmail.fr>
2020-05-31 17:32:23 +0200

    clean up files

--- projectiles/SBOOhwalliStrategicBomb01/SBOOhwalliStrategicBomb01_script.lua
+++ projectiles/SBOOhwalliStrategicBomb01/SBOOhwalliStrategicBomb01_script.lua
@@ -1,12 +1,12 @@
-#****************************************************************************
-#**
-#**  File     :  /data/projectiles/SBOOhwalliStategicBomb01/SBOOhwalliStategicBomb01_script.lua
-#**  Author(s):  Greg Kohne, Gordon Duclos, Matt Vainio
-#**
-#**  Summary  :  Ohwalli-Strategic Bomb script, used on XSA402
-#**
-#**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+-------------------------------------------------------------------------------
+--
+--  File     :  /data/projectiles/SBOOhwalliStategicBomb01/SBOOhwalliStategicBomb01_script.lua
+--  Author(s):  Greg Kohne, Gordon Duclos, Matt Vainio
+--
+--  Summary  :  Ohwalli-Strategic Bomb script, used on XSA402
+--
+--  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+-------------------------------------------------------------------------------
 local SOhwalliStrategicBombProjectile = import('/lua/seraphimprojectiles.lua').SOhwalliStrategicBombProjectile
 
 SBOOhwalliStategicBomb01 = Class(SOhwalliStrategicBombProjectile){
