b0df01f1bcac6efcd9aa1511af4b8f775a28cbdc
shalkya <PYL35@hotmail.fr>
2020-08-10 14:50:14 +0200

    mercy projectile fix
    - properly kill trees
    - spawn decals

--- projectiles/AIFGuidedMissile02/AIFGuidedMissile02_Script.lua
+++ projectiles/AIFGuidedMissile02/AIFGuidedMissile02_Script.lua
@@ -7,7 +7,9 @@
 --
 --  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
 -------------------------------------------------------------------------------
+
 local AGuidedMissileProjectile = import('/lua/aeonprojectiles.lua').AGuidedMissileProjectile
+local DefaultExplosion = import('/lua/defaultexplosions.lua')
 
 AIFGuidedMissile02 = Class(AGuidedMissileProjectile) {
 	-- FxTrailScale = 0.5,
@@ -20,7 +22,23 @@ AIFGuidedMissile02 = Class(AGuidedMissileProjectile) {
 	MovementThread = function(self)
 		WaitSeconds(0.6)
 		self:TrackTarget(true)
-	end,    
+	end,
+    
+    OnImpact = function(self, targetType, targetEntity)
+        if targetType ~= 'Shield' and targetType ~= 'Water' and targetType ~= 'Air' and targetType ~= 'UnitAir' and targetType ~= 'Projectile' then
+			local RandomFloat = import('/lua/utilities.lua').GetRandomFloat
+            local rotation = RandomFloat(0,2*math.pi)
+            local radius = self.DamageData.DamageRadius
+            local pos = self:GetPosition()
+            local army = self.Army
+
+            DamageArea(self, pos, radius, 1, 'Force', true)
+            DamageArea(self, pos, radius, 1, 'Force', true)
+            CreateDecal(pos, rotation, 'scorch_001_albedo', '', 'Albedo', radius*3, radius*3, 200, 70, army)
+        end
+        
+        AGuidedMissileProjectile.OnImpact(self, targetType, targetEntity)
+    end,
 }
 TypeClass = AIFGuidedMissile02
 
