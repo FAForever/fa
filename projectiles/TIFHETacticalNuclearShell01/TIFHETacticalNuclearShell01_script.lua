02adfde1ec5d74fda58a88976fd2558df7a96e3a
shalkya <PYL35@hotmail.fr>
2020-08-12 16:42:28 +0200

    add mavor projectile script

---
+++ projectiles/TIFHETacticalNuclearShell01/TIFHETacticalNuclearShell01_script.lua
@@ -0,0 +1,8 @@
+#
+# UEF Anti-Matter Shells
+#
+local TArtilleryAntiMatterProjectile = import('/lua/terranprojectiles.lua').TArtilleryAntiMatterProjectile
+TIFAntiMatterShells01 = Class(TArtilleryAntiMatterProjectile) {
+}
+
+TypeClass = TIFAntiMatterShells01
