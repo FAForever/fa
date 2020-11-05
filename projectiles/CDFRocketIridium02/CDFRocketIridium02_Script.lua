ce54f9a4e23e3d86b7369980d104dfdb4bc7fcb1
shalkya <PYL35@hotmail.fr>
2020-11-05 10:14:55 +0100

    clean up files

--- projectiles/CDFRocketIridium02/CDFRocketIridium02_Script.lua
+++ projectiles/CDFRocketIridium02/CDFRocketIridium02_Script.lua
@@ -1,13 +1,15 @@
-#****************************************************************************
-#**
-#**  File     :  /data/projectiles/CDFRocketIridium02/CDFRocketIridium02_script.lua
-#**  Author(s):  Matt Vainio
-#**
-#**  Summary  :  Cybran Iridium Rocket Tubes, DRL0204
-#**
-#**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+------------------------------------------------------------
+--
+--  File     :  /data/projectiles/CDFRocketIridium02/CDFRocketIridium02_script.lua
+--  Author(s):  Matt Vainio
+--
+--  Summary  :  Cybran Iridium Rocket Tubes, DRL0204
+--
+--  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+------------------------------------------------------------
+
 local CIridiumRocketProjectile = import('/lua/cybranprojectiles.lua').CIridiumRocketProjectile
+
 CDFRocketIridium02 = Class(CIridiumRocketProjectile) {}
 
 TypeClass = CDFRocketIridium02
