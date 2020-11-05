eb4acf262b28652859e28465a0de0e0dfcfb1f61
shalkya <PYL35@hotmail.fr>
2020-11-05 11:06:49 +0100

    add decals to range bot projectiles

--- projectiles/CDFRocketIridium02/CDFRocketIridium02_Script.lua
+++ projectiles/CDFRocketIridium02/CDFRocketIridium02_Script.lua
@@ -9,7 +9,27 @@
 ------------------------------------------------------------
 
 local CIridiumRocketProjectile = import('/lua/cybranprojectiles.lua').CIridiumRocketProjectile
+local DefaultProjectileFile = import('/lua/sim/defaultprojectiles.lua')
+local SinglePolyTrailProjectile = DefaultProjectileFile.SinglePolyTrailProjectile
 
-CDFRocketIridium02 = Class(CIridiumRocketProjectile) {}
+CDFRocketIridium02 = Class(CIridiumRocketProjectile) {
+    OnImpact = function(self, targetType, targetEntity)
+        if targetType ~= 'Shield' and targetType ~= 'Water' and targetType ~= 'Air' and targetType ~= 'UnitAir' and targetType ~= 'Projectile' then
+            local RandomFloat = import('/lua/utilities.lua').GetRandomFloat
+            local rotation = RandomFloat(0,2*math.pi)
+            local radius = self.DamageData.DamageRadius
+            local pos = self:GetPosition()
+            local army = self.Army
+            
+            DamageArea(self, pos, radius, 1, 'Force', true)
+            DamageArea(self, pos, radius, 1, 'Force', true)
+            DamageRing( self, pos, radius, 5/4 * radius, 1, 'Fire', true )
+            
+            CreateDecal(pos, rotation, 'scorch_001_albedo', '', 'Albedo', radius+1, radius+1, 100, 50, army)
+        end
+        
+        SinglePolyTrailProjectile.OnImpact(self, targetType, targetEntity)
+    end,
+}
 
 TypeClass = CDFRocketIridium02
--- projectiles/TDFFragmentationGrenade01/TDFFragmentationGrenade01_script.lua
+++ projectiles/TDFFragmentationGrenade01/TDFFragmentationGrenade01_script.lua
@@ -9,8 +9,27 @@
 ------------------------------------------------------------
 
 local TFragmentationGrenade = import('/lua/terranprojectiles.lua').TFragmentationGrenade
+local DefaultProjectileFile = import('/lua/sim/defaultprojectiles.lua')
+local EmitterProjectile = DefaultProjectileFile.EmitterProjectile
 
 TDFFragmentationGrenade01 = Class(TFragmentationGrenade) {
+    OnImpact = function(self, targetType, targetEntity)
+        if targetType ~= 'Shield' and targetType ~= 'Water' and targetType ~= 'Air' and targetType ~= 'UnitAir' and targetType ~= 'Projectile' then
+            local RandomFloat = import('/lua/utilities.lua').GetRandomFloat
+            local rotation = RandomFloat(0,2*math.pi)
+            local radius = self.DamageData.DamageRadius
+            local pos = self:GetPosition()
+            local army = self.Army
+            
+            DamageArea(self, pos, radius, 1, 'Force', true)
+            DamageArea(self, pos, radius, 1, 'Force', true)
+            DamageRing( self, pos, radius, 5/4 * radius, 1, 'Fire', true )
+            
+            CreateDecal(pos, rotation, 'scorch_001_albedo', '', 'Albedo', radius+1, radius+1, 100, 50, army)
+        end
+        
+        EmitterProjectile.OnImpact(self, targetType, targetEntity)
+    end,
 }
 
 TypeClass = TDFFragmentationGrenade01
