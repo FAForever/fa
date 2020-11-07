6f7798ed2ccd54ab1224e2ec1c99ccfd2c969143
shalkya <PYL35@hotmail.fr>
2020-11-07 14:59:58 +0100

    add TDFGauss02_script.lua

---
+++ projectiles/TDFGauss02/TDFGauss02_script.lua
@@ -0,0 +1,9 @@
+#
+# Terran Gauss Cannon Projectile
+#
+local TDFGaussCannonProjectile = import('/lua/terranprojectiles.lua').TDFGaussCannonProjectile
+TDFGauss01 = Class(TDFGaussCannonProjectile) {
+
+}
+TypeClass = TDFGauss01
+
