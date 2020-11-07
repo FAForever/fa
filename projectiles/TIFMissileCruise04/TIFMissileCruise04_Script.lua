d95eb6d2090cee30d95a5fdb9f2c1c1c50c314bf
shalkya <PYL35@hotmail.fr>
2020-11-07 16:51:24 +0100

    add cruise missile projectile files

---
+++ projectiles/TIFMissileCruise04/TIFMissileCruise04_Script.lua
@@ -0,0 +1,69 @@
+#
+# Terran Land-Based Cruise Missile
+#
+local TMissileCruiseProjectile = import('/lua/terranprojectiles.lua').TMissileCruiseProjectile
+local Explosion = import('/lua/defaultexplosions.lua')
+
+TIFMissileCruise04 = Class(TMissileCruiseProjectile) {
+
+    FxAirUnitHitScale = 1.5,
+    FxLandHitScale = 1.5,
+    FxNoneHitScale = 1.5,
+    FxPropHitScale = 1.5,
+    FxProjectileHitScale = 1.5,
+    FxProjectileUnderWaterHitScale = 1.5,
+    FxShieldHitScale = 1.5,
+    FxUnderWaterHitScale = 1.5,
+    FxUnitHitScale = 1.5,
+    FxWaterHitScale = 1.5,
+    FxOnKilledScale = 1.5,
+
+    OnCreate = function(self)
+        TMissileCruiseProjectile.OnCreate(self)
+        self:SetCollisionShape('Sphere', 0, 0, 0, 2.0)
+        self.MovementTurnLevel = 1
+        self:ForkThread( self.MovementThread )
+    end,
+
+    MovementThread = function(self)        
+        self.WaitTime = 0.1
+        self:SetTurnRate(8)
+        WaitSeconds(0.3)        
+        while not self:BeenDestroyed() do
+            self:SetTurnRateByDist()
+            WaitSeconds(self.WaitTime)
+        end
+    end,
+
+    SetTurnRateByDist = function(self)
+        local dist = self:GetDistanceToTarget()
+        #Get the nuke as close to 90 deg as possible
+        if dist > 50 then        
+            #Freeze the turn rate as to prevent steep angles at long distance targets
+            WaitSeconds(2)
+            self:SetTurnRate(20)
+        elseif dist > 64 and dist <= 107 then
+						# Increase check intervals
+						self:SetTurnRate(30)
+						WaitSeconds(1.5)
+            self:SetTurnRate(30)
+        elseif dist > 21 and dist <= 53 then
+						# Further increase check intervals
+            WaitSeconds(0.3)
+            self:SetTurnRate(50)
+				elseif dist > 0 and dist <= 21 then
+						# Further increase check intervals            
+            self:SetTurnRate(100)   
+            KillThread(self.MoveThread)         
+        end
+    end,        
+
+    GetDistanceToTarget = function(self)
+        local tpos = self:GetCurrentTargetPosition()
+        local mpos = self:GetPosition()
+        local dist = VDist2(mpos[1], mpos[3], tpos[1], tpos[3])
+        return dist
+    end,
+}
+TypeClass = TIFMissileCruise04
+
---
+++ projectiles/TIFMissileCruise05/TIFMissileCruise05_Script.lua
@@ -0,0 +1,80 @@
+#
+# Terran Land-Based Cruise Missile
+#
+local TMissileCruiseProjectile = import('/lua/terranprojectiles.lua').TMissileCruiseProjectile
+local EffectTemplate = import('/lua/EffectTemplates.lua')
+local SingleBeamProjectile = import('/lua/sim/defaultprojectiles.lua').SingleBeamProjectile
+
+TIFMissileCruise05 = Class(TMissileCruiseProjectile) {
+
+    FxTrails = EffectTemplate.TMissileExhaust01,
+    FxTrailOffset = -0.85,
+    
+    FxAirUnitHitScale = 0.65,
+    FxLandHitScale = 0.65,
+    FxNoneHitScale = 0.65,
+    FxPropHitScale = 0.65,
+    FxProjectileHitScale = 0.65,
+    FxProjectileUnderWaterHitScale = 0.65,
+    FxShieldHitScale = 0.65,
+    FxUnderWaterHitScale = 0.65,
+    FxUnitHitScale = 0.65,
+    FxWaterHitScale = 0.65,
+    FxOnKilledScale = 0.65,
+    
+    OnCreate = function(self)
+        TMissileCruiseProjectile.OnCreate(self)
+        self:SetCollisionShape('Sphere', 0, 0, 0, 2)
+        self.MoveThread = self:ForkThread(self.MovementThread)
+    end,
+
+    MovementThread = function(self)        
+        self.WaitTime = 0.1
+        self.Distance = self:GetDistanceToTarget()
+        self:SetTurnRate(8)
+        WaitSeconds(0.3)        
+        while not self:BeenDestroyed() do
+            self:SetTurnRateByDist()
+            WaitSeconds(self.WaitTime)
+        end
+    end,
+
+    SetTurnRateByDist = function(self)
+        local dist = self:GetDistanceToTarget()
+        if dist > self.Distance then
+        	self:SetTurnRate(50)
+        	WaitSeconds(3)
+        	self:SetTurnRate(8)
+        	self.Distance = self:GetDistanceToTarget()
+        end
+        if dist > 50 then        
+            #Freeze the turn rate as to prevent steep angles at long distance targets
+            WaitSeconds(2)
+            self:SetTurnRate(10)
+        elseif dist > 30 and dist <= 50 then
+						self:SetTurnRate(12)
+						WaitSeconds(1.5)
+            self:SetTurnRate(12)
+        elseif dist > 10 and dist <= 25 then
+            WaitSeconds(0.3)
+            self:SetTurnRate(50)
+				elseif dist > 0 and dist <= 10 then         
+            self:SetTurnRate(100)   
+            KillThread(self.MoveThread)         
+        end
+    end,        
+
+    GetDistanceToTarget = function(self)
+        local tpos = self:GetCurrentTargetPosition()
+        local mpos = self:GetPosition()
+        local dist = VDist2(mpos[1], mpos[3], tpos[1], tpos[3])
+        return dist
+    end,
+    
+    OnImpact = function(self, targetType, targetEntity)
+        local army = self:GetArmy()
+        SingleBeamProjectile.OnImpact(self, targetType, targetEntity)
+    end,
+}
+TypeClass = TIFMissileCruise05
+
