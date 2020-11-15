179e2d430ee9352ca697364a443b4604e9cc80fe
shalkya <PYL35@hotmail.fr>
2020-11-15 18:11:25 +0100

    add decals/tree falling to cyb tactical proj filles

--- projectiles/CIFMissileTactical03/CIFMissileTactical03_Script.lua
+++ projectiles/CIFMissileTactical03/CIFMissileTactical03_Script.lua
@@ -15,20 +15,33 @@ CIFMissileTactical03 = Class(CLOATacticalMissileProjectile) {
         self:SetCollisionShape('Sphere', 0, 0, 0, 2.0)
         self.Split = false
         self.MovementTurnLevel = 1
-        self:ForkThread( self.MovementThread )        
+        self:ForkThread( self.MovementThread )
     end,
     
     PassDamageData = function(self, damageData)
         CLOATacticalMissileProjectile.PassDamageData(self,damageData)
-        local launcherbp = self:GetLauncher():GetBlueprint()  
+        local launcherbp = self:GetLauncher():GetBlueprint()
         self.ChildDamageData = table.copy(self.DamageData)
         self.ChildDamageData.DamageAmount = launcherbp.SplitDamage.DamageAmount or 0
-        self.ChildDamageData.DamageRadius = launcherbp.SplitDamage.DamageRadius or 1   
+        self.ChildDamageData.DamageRadius = launcherbp.SplitDamage.DamageRadius or 1
     end,    
     
     OnImpact = function(self, targetType, targetEntity)
-        local army = self:GetArmy()
-        CreateLightParticle( self, -1, army, 3, 7, 'glow_03', 'ramp_fire_11' ) 
+        local army = self.Army
+        CreateLightParticle( self, -1, army, 3, 7, 'glow_03', 'ramp_fire_11' )
+        
+        if targetType ~= 'Shield' and targetType ~= 'Water' and targetType ~= 'Air' and targetType ~= 'UnitAir' and targetType ~= 'Projectile' then
+            local RandomFloat = import('/lua/utilities.lua').GetRandomFloat
+            local rotation = RandomFloat(0,2*math.pi)
+            local radius = self.DamageData.DamageRadius
+            local pos = self:GetPosition()
+            
+            DamageArea(self, pos, radius, 1, 'Force', true)
+            DamageArea(self, pos, radius, 1, 'Force', true)
+            
+            CreateDecal(pos, rotation, 'scorch_001_albedo', '', 'Albedo', radius+3, radius+3, 150, 70, army)
+        end
+        
         -- if I collide with terrain dont split
         if targetType != 'Projectile' then
             self.Split = true
@@ -38,31 +51,31 @@ CIFMissileTactical03 = Class(CLOATacticalMissileProjectile) {
     
     OnDamage = function(self, instigator, amount, vector, damageType)
         if not self.Split and (amount >= self:GetHealth()) then
-            self.Split = true        
+            self.Split = true
             local vx, vy, vz = self:GetVelocity()
             local velocity = 7
             local ChildProjectileBP = '/projectiles/CIFMissileTacticalSplit01/CIFMissileTacticalSplit01_proj.bp'
             local angle = (2*math.pi) / self.NumChildMissiles
-            local spreadMul = 0.5  -- Adjusts the width of the dispersal        
+            local spreadMul = 0.5  -- Adjusts the width of the dispersal
 
             -- Launch projectiles at semi-random angles away from split location
             for i = 0, (self.NumChildMissiles - 1) do
                 local xVec = vx + math.sin(i*angle) * spreadMul
                 local yVec = vy + math.cos(i*angle) * spreadMul
-                local zVec = vz + math.cos(i*angle) * spreadMul 
+                local zVec = vz + math.cos(i*angle) * spreadMul
                 local proj = self:CreateChildProjectile(ChildProjectileBP)
                 proj:SetVelocity(xVec,yVec,zVec)
                 proj:SetVelocity(velocity)
-                proj:PassDamageData(self.ChildDamageData)                        
+                proj:PassDamageData(self.ChildDamageData)
             end
         end
         CLOATacticalMissileProjectile.OnDamage(self, instigator, amount, vector, damageType)
-    end,      
+    end,
 
-    MovementThread = function(self)        
+    MovementThread = function(self)
         self.WaitTime = 0.1
         self:SetTurnRate(8)
-        WaitSeconds(0.3)        
+        WaitSeconds(0.3)
         while not self:BeenDestroyed() do
             self:SetTurnRateByDist()
             WaitSeconds(self.WaitTime)
@@ -72,23 +85,23 @@ CIFMissileTactical03 = Class(CLOATacticalMissileProjectile) {
     SetTurnRateByDist = function(self)
         local dist = self:GetDistanceToTarget()
         -- Get the nuke as close to 90 deg as possible
-        if dist > 50 then        
+        if dist > 50 then
             -- Freeze the turn rate as to prevent steep angles at long distance targets
             WaitSeconds(2)
             self:SetTurnRate(20)
         elseif dist > 128 and dist <= 213 then
-						-- Increase check intervals
-						self:SetTurnRate(30)
-						WaitSeconds(1.5)
+            -- Increase check intervals
             self:SetTurnRate(30)
-        elseif dist > 43 and dist <= 107 then
-						-- Further increase check intervals
+            WaitSeconds(1.5)
+            self:SetTurnRate(30)
+        elseif dist > 43 and dist <= 128 then
+            -- Further increase check intervals
             WaitSeconds(0.3)
             self:SetTurnRate(50)
-				elseif dist > 0 and dist <= 43 then
-						-- Further increase check intervals            
-            self:SetTurnRate(100)   
-            KillThread(self.MoveThread)         
+        elseif dist > 0 and dist <= 43 then
+            -- Further increase check intervals            
+            self:SetTurnRate(100)
+            KillThread(self.MoveThread)
         end
     end,        
 
