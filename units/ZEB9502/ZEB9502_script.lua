76bd5bfe269b816d083f867cff9e1a7d885bfc37
Petricpwnz <petrasanic@yandex.ru>
2020-11-16 22:13:17 +0200

    equalize t2 airfacs rolloffs

--- units/ZEB9502/ZEB9502_script.lua
+++ units/ZEB9502/ZEB9502_script.lua
@@ -52,6 +52,43 @@ ZEB9502 = Class(TAirFactoryUnit) {
         self.ArmSlider2:SetGoal(0, 0, 0)
         self.ArmSlider2:SetSpeed(40)
     end,
+
+    --Overwrite FinishBuildThread to speed up platform lowering rate
+
+        FinishBuildThread = function(self, unitBeingBuilt, order)
+            self:SetBusy(true)
+            self:SetBlockCommandQueue(true)
+            local bp = self:GetBlueprint()
+            local bpAnim = bp.Display.AnimationFinishBuildLand
+            if bpAnim and EntityCategoryContains(categories.LAND, unitBeingBuilt) then
+                self.RollOffAnim = CreateAnimator(self):PlayAnim(bpAnim):SetRate(10)        --Change: SetRate(4)
+                self.Trash:Add(self.RollOffAnim)
+                WaitTicks(1)
+                WaitFor(self.RollOffAnim)
+            end
+            if unitBeingBuilt and not unitBeingBuilt.Dead then
+                unitBeingBuilt:DetachFrom(true)
+            end
+            self:DetachAll(bp.Display.BuildAttachBone or 0)
+            self:DestroyBuildRotator()
+            if order != 'Upgrade' then
+                ChangeState(self, self.RollingOffState)
+            else
+                self:SetBusy(false)
+                self:SetBlockCommandQueue(false)
+            end
+        end,
+
+    --Overwrite PlayFxRollOffEnd to speed up platform raising rate
+
+        PlayFxRollOffEnd = function(self)
+            if self.RollOffAnim then
+                self.RollOffAnim:SetRate(10)                                            --Change: SetRate(-4)
+                WaitFor(self.RollOffAnim)
+                self.RollOffAnim:Destroy()
+                self.RollOffAnim = nil
+            end
+        end,
 }
 
 TypeClass = ZEB9502
--- units/ZRB9502/ZRB9502_script.lua
+++ units/ZRB9502/ZRB9502_script.lua
@@ -16,5 +16,41 @@ ZRB9502 = Class(CAirFactoryUnit) {
     UpgradeBuilderArm1 = 'Arm03_B02',
     UpgradeBuilderArm2 = 'Arm04_B02',
 
+    --Overwrite FinishBuildThread to speed up platform lowering rate
+
+        FinishBuildThread = function(self, unitBeingBuilt, order)
+            self:SetBusy(true)
+            self:SetBlockCommandQueue(true)
+            local bp = self:GetBlueprint()
+            local bpAnim = bp.Display.AnimationFinishBuildLand
+            if bpAnim and EntityCategoryContains(categories.LAND, unitBeingBuilt) then
+                self.RollOffAnim = CreateAnimator(self):PlayAnim(bpAnim):SetRate(40)        --Change: SetRate(4)
+                self.Trash:Add(self.RollOffAnim)
+                WaitTicks(1)
+                WaitFor(self.RollOffAnim)
+            end
+            if unitBeingBuilt and not unitBeingBuilt.Dead then
+                unitBeingBuilt:DetachFrom(true)
+            end
+            self:DetachAll(bp.Display.BuildAttachBone or 0)
+            self:DestroyBuildRotator()
+            if order != 'Upgrade' then
+                ChangeState(self, self.RollingOffState)
+            else
+                self:SetBusy(false)
+                self:SetBlockCommandQueue(false)
+            end
+        end,
+
+    --Overwrite PlayFxRollOffEnd to speed up platform raising rate
+
+        PlayFxRollOffEnd = function(self)
+            if self.RollOffAnim then
+                self.RollOffAnim:SetRate(40)                                            --Change: SetRate(-4)
+                WaitFor(self.RollOffAnim)
+                self.RollOffAnim:Destroy()
+                self.RollOffAnim = nil
+            end
+        end,
 }
 TypeClass = ZRB9502
