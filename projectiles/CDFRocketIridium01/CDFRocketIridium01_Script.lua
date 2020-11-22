58a85d8c3190e736a10840eede3ac22165b311e0
shalkya <PYL35@hotmail.fr>
2020-11-22 12:33:30 +0100

    add T2 cybran gs projectile file

---
+++ projectiles/CDFRocketIridium01/CDFRocketIridium01_Script.lua
@@ -0,0 +1,7 @@
+#
+# Cybran Anti Air Missile
+#
+local CIridiumRocketProjectile = import('/lua/cybranprojectiles.lua').CIridiumRocketProjectile
+CDFRocketIridium01 = Class(CIridiumRocketProjectile) {}
+
+TypeClass = CDFRocketIridium01
