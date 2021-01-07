49d4eddd80fdf8d6fe7ced967d254e66626b231e
TurinturambarFAF <41325352+TurinturambarFAF@users.noreply.github.com>
2021-01-07 13:20:52 +0100

    update UEF SCU rebalance (#3151)
    * update UI
    
    updates SCU upgrade and preset descriptions and naming
    
    * revert granade launcher upgrade
    
    removes the granade launcher upgrade and replaces it with the former upgrade (increased area of effect).
    removes range increase on the AoE upgrade.
    
    * adjust gun upgrade range and nerf shield
    
    adjust gun upgrade range and nerf shield cost and strenght
    
    * removing granade launcher and updating presets
    
    last part of removal of granade launcher upgrade (reverting to AoE upgrade) and adding simple combat preset
    
    * fix selection priority
    
    fix selection priority

--- units/UEL0301/UEL0301_script.lua
+++ units/UEL0301/UEL0301_script.lua
@@ -10,7 +10,6 @@ local EffectUtil = import('/lua/EffectUtilities.lua')
 local CommandUnit = import('/lua/defaultunits.lua').CommandUnit
 local TWeapons = import('/lua/terranweapons.lua')
 local TDFHeavyPlasmaCannonWeapon = TWeapons.TDFHeavyPlasmaCannonWeapon
-local TIFFragLauncherWeapon = TWeapons.TDFFragmentationGrenadeLauncherWeapon
 local SCUDeathWeapon = import('/lua/sim/defaultweapons.lua').SCUDeathWeapon
 
 UEL0301 = Class(CommandUnit) {
@@ -25,12 +24,6 @@ UEL0301 = Class(CommandUnit) {
     },
 
     Weapons = {
-		Grenade = Class(TIFFragLauncherWeapon) {
-            OnCreate = function(self)
-                TIFFragLauncherWeapon.OnCreate(self)
-                self:SetWeaponEnabled(false)
-            end,
-        },
         RightHeavyPlasmaCannon = Class(TDFHeavyPlasmaCannonWeapon) {},
         DeathWeapon = Class(SCUDeathWeapon) {},
     },
@@ -40,7 +33,6 @@ UEL0301 = Class(CommandUnit) {
         self:SetCapturable(false)
         self:HideBone('Jetpack', true)
         self:HideBone('SAM', true)
-		self:GetWeaponByLabel('Grenade').NeedsUpgrade = true
         self:SetupBuildBones()
 		self.HasLeftPod = false
         self.HasRightPod = false
@@ -233,17 +225,17 @@ UEL0301 = Class(CommandUnit) {
         elseif enh =='AdvancedCoolingUpgradeRemove' then
             local wep = self:GetWeaponByLabel('RightHeavyPlasmaCannon')
             wep:ChangeRateOfFire(self:GetBlueprint().Weapon[1].RateOfFire or 1)
-        --Granade Launcher
+        --Gun AoE
 		elseif enh =='HighExplosiveOrdnance' then
-			self:SetWeaponEnabledByLabel('Grenade', true)
-            local wep = self:GetWeaponByLabel('Grenade')
-            wep:ChangeMaxRadius(bp.NewMaxRadius or 25)
+            local wep = self:GetWeaponByLabel('RightHeavyPlasmaCannon')
+            wep:AddDamageRadiusMod(bp.NewDamageRadius)
+            wep:ChangeMaxRadius(bp.NewMaxRadius or 35)
         elseif enh =='HighExplosiveOrdnanceRemove' then
-            local wep = self:GetWeaponByLabel('Grenade')
-            wep:ChangeMaxRadius(self:GetBlueprint().Weapon[1].MaxRadius or 25)
-			self:SetWeaponEnabledByLabel('Grenade', false)
-        end
-    end,
+            local wep = self:GetWeaponByLabel('RightHeavyPlasmaCannon')
+            wep:AddDamageRadiusMod(bp.NewDamageRadius)
+			wep:ChangeMaxRadius(bp.NewMaxRadius or 35)
+		end
+	end,
 
     OnIntelEnabled = function(self)
         CommandUnit.OnIntelEnabled(self)
