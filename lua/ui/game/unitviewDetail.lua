b4a25d77e2689cde5b891b7f89981310e236cc00
Uveso <Archivar@Zwergenteam.de>
2021-01-13 04:57:43 +0100

    Fix Tooltip for Experimental Transporter
    Some mods provide Experimental Trasnporter and the ability function only checks for Tech1-3 units.
    Experimental Transporters will trigger an error.

--- lua/ui/game/unitviewDetail.lua
+++ lua/ui/game/unitviewDetail.lua
@@ -375,6 +375,9 @@ GetAbilityDesc = {
         if bp.CategoriesHash.TECH3 then
             return text..'≈28'
         end
+        if bp.CategoriesHash.EXPERIMENTAL then
+            return text..'≈28'
+        end
     end,
     ability_airstaging = function(bp)
         return LOCF('<LOC uvd_RepairRate>', bp.Transport.RepairRate)..', '
