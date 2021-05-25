3828e48eb038db376b46796dc39fb87da9540a7b
keyser <PYL35@hotmail.fr>
2021-05-25 10:19:42 +0200

    fix projectiles T1 arty

--- projectiles/AIFMortar01/AIFMortar01_script.lua
+++ projectiles/AIFMortar01/AIFMortar01_script.lua
@@ -6,17 +6,17 @@ local EffectTemplate = import('/lua/EffectTemplates.lua')
 
 AIFMortar01 = Class(AArtilleryProjectile) {
     OnImpact = function(self, targetType, targetEntity)
+        local pos = self:GetPosition()
+        local radius = self.DamageData.DamageRadius
         if targetType ~= 'Shield' and targetType ~= 'Water' and targetType ~= 'Air' and targetType ~= 'UnitAir' and targetType ~= 'Projectile' then
             local RandomFloat = import('/lua/utilities.lua').GetRandomFloat
             local rotation = RandomFloat(0,2*math.pi)
-            local pos = self:GetPosition()
-            local radius = self.DamageData.DamageRadius
             local army = self.Army
             
-            DamageArea( self, pos, radius, 1, 'Force', true )
-            DamageArea( self, pos, radius, 1, 'Force', true )
             CreateDecal(pos, rotation, 'crater_radial01_albedo', '', 'Albedo', radius, radius, 100, 10, army)
         end
+        DamageArea( self, pos, radius, 1, 'Force', false )
+        DamageArea( self, pos, radius, 1, 'Force', false )
         
         AArtilleryProjectile.OnImpact(self, targetType, targetEntity)
     end,
@@ -27,4 +27,3 @@ AIFMortar01 = Class(AArtilleryProjectile) {
 }
 
 TypeClass = AIFMortar01
-
--- projectiles/SIFThunthoArtilleryShell02/SIFThunthoArtilleryShell02_script.lua
+++ projectiles/SIFThunthoArtilleryShell02/SIFThunthoArtilleryShell02_script.lua
@@ -12,17 +12,18 @@ local SThunthoArtilleryShell2 = import('/lua/seraphimprojectiles.lua').SThunthoA
 
 SIFThunthoArtilleryShell02 = Class(SThunthoArtilleryShell2) {
     OnImpact = function(self, targetType, targetEntity) 
+        local pos = self:GetPosition()
+        local radius = self.DamageData.DamageRadius
         if targetType ~= 'Shield' and targetType ~= 'Water' and targetType ~= 'Air' and targetType ~= 'UnitAir' and targetType ~= 'Projectile' then
             local RandomFloat = import('/lua/utilities.lua').GetRandomFloat
             local rotation = RandomFloat(0,2*math.pi)
-            local pos = self:GetPosition()
-            local radius = self.DamageData.DamageRadius
             local army = self.Army
             
-            DamageArea( self, pos, radius, 1, 'Force', true )
-            DamageArea( self, pos, radius, 1, 'Force', true )
             CreateDecal(pos, rotation, 'crater_radial01_albedo', '', 'Albedo', radius-1, radius-1, 100, 10, army)
         end
+        
+        DamageArea( self, pos, radius, 1, 'Force', false )
+        DamageArea( self, pos, radius, 1, 'Force', false )
         
         SThunthoArtilleryShell2.OnImpact(self, targetType, targetEntity)
     end,
