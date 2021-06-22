c28db3fdd8aff2cc80d0a06d228ef3ef2b255670
Uveso <Archivar@Zwergenteam.de>
2021-06-22 08:29:37 +0200

    cleanup

--- lua/ui/lobby/lobby.lua
+++ lua/ui/lobby/lobby.lua
@@ -3017,14 +3017,14 @@ function CreateUI(maxPlayers)
         Prefs.SetToCurrentProfile('LobbyHideDefaultOptions', tostring(checked))
     end
 
-	-- curated Maps
-	GUI.curatedmapsButton = UIUtil.CreateButtonWithDropshadow(GUI.panel, '/Button/medium/', "<LOC lobui_0433>Curated Maps")
-	Tooltip.AddButtonTooltip(GUI.curatedmapsButton, 'lob_curated_maps')
-	LayoutHelpers.AtBottomIn(GUI.curatedmapsButton, GUI.optionsPanel, -51)
+    -- curated Maps
+    GUI.curatedmapsButton = UIUtil.CreateButtonWithDropshadow(GUI.panel, '/Button/medium/', "<LOC lobui_0433>Curated Maps")
+    Tooltip.AddButtonTooltip(GUI.curatedmapsButton, 'lob_curated_maps')
+    LayoutHelpers.AtBottomIn(GUI.curatedmapsButton, GUI.optionsPanel, -51)
     LayoutHelpers.AtHorizontalCenterIn(GUI.curatedmapsButton, GUI.optionsPanel, -55)
-	GUI.curatedmapsButton.OnClick = function()
-		OpenURL('http://forums.faforever.com/viewtopic.php?f=2&t=17820')
-	end
+    GUI.curatedmapsButton.OnClick = function()
+        OpenURL('http://forums.faforever.com/viewtopic.php?f=2&t=17820')
+    end
 
     -- A buton that, for the host, is "game options", but for everyone else shows a ready-only mod
     -- manager.
