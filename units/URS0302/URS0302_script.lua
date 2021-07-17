5132c07531b6f991595c22a92f5dba6c01c57c3c
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-07-17 10:01:46 +0200

    Small fix for the zapper

--- units/URS0302/URS0302_script.lua
+++ units/URS0302/URS0302_script.lua
@@ -12,7 +12,7 @@ local CybranWeaponsFile = import('/lua/cybranweapons.lua')
 local CAAAutocannon = CybranWeaponsFile.CAAAutocannon
 local CDFProtonCannonWeapon = CybranWeaponsFile.CDFProtonCannonWeapon
 local CANNaniteTorpedoWeapon = CybranWeaponsFile.CANNaniteTorpedoWeapon
-local CAMZapperWeapon02 = CybranWeaponsFile.CAMZapperWeapon02
+local CAMZapperWeapon03 = CybranWeaponsFile.CAMZapperWeapon03
        
 URS0302 = Class(CSeaUnit) {
     Weapons = {
