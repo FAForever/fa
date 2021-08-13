3142d229c06231b2cbaef2587d4ff08c0ff49dc1
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-08-14 01:15:33 +0200

    Move content from schook/userinit to userinit

--- lua/userInit.lua
+++ lua/userInit.lua
@@ -26,3 +26,17 @@ FrontEndData = {}
 -- Prefetch user side data
 Prefetcher = CreatePrefetchSet()
 
+function PrintText(textData)
+    if textData then
+        local data = textData
+        if type(textData) == 'string' then
+            data = {text = textData, size = 14, color = 'ffffffff', duration = 5, location = 'center'}
+        end
+        import('/lua/ui/game/textdisplay.lua').PrintToScreen(data)
+    end
+end
+
+local replayID = import('/lua/ui/uiutil.lua').GetReplayId()
+if replayID then
+    LOG("REPLAY ID: " .. replayID)
+end
