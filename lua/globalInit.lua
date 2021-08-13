a9ec110a9905b7d23f1a95b01727aa6a1e2220f3
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-08-14 01:05:18 +0200

    Move content of schook/globalinit.lua to globalinit.lua

--- lua/globalInit.lua
+++ lua/globalInit.lua
@@ -32,3 +32,27 @@ for name,cclass in moho do
     --SPEW('C->lua ',name)
     ConvertCClassToLuaClass(cclass)
 end
+
+-- Flag initial loading of blueprints
+InitialRegistration = true
+
+-- Load blueprint systems
+doscript '/lua/system/BuffBlueprints.lua'
+
+-- Load buff definitions
+import('/lua/sim/BuffDefinitions.lua')
+
+-- Load Platoon Template systems
+doscript '/lua/system/GlobalPlatoonTemplate.lua'
+
+-- Load Builder system
+doscript '/lua/system/GlobalBuilderTemplate.lua'
+
+-- Load Builder Group systems
+doscript '/lua/system/GlobalBuilderGroup.lua'
+
+-- Load Global Base Templates
+doscript '/lua/system/GlobalBaseTemplate.lua'
+
+InitialRegistration = false
+
