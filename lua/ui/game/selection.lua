fa70365218880678351c19677b6366ce4097c73f
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-08-14 00:56:16 +0200

    Move schook definitions to the actual file

--- lua/ui/game/selection.lua
+++ lua/ui/game/selection.lua
@@ -188,3 +188,22 @@ end
 function ResetSelectionSets(new_sets)
     selectionSets = new_sets
 end
+
+-- from schook selection.lua
+
+local hidden_select = false
+function IsHidden()
+    return hidden_select == true
+end
+
+function Hidden(callback)
+    local CM = import('/lua/ui/game/commandmode.lua')
+    local current_command = CM.GetCommandMode()
+    local old_selection = GetSelectedUnits() or {}
+
+    hidden_select = true
+    callback()
+    SelectUnits(old_selection)
+    CM.StartCommandMode(current_command[1], current_command[2])
+    hidden_select = false
+end
