4a8134bc6a7e358dbdcb84e8439fb851cfbd3e66
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-08-27 17:10:26 +0200

    Add UEF t2 cruiser script file

---
+++ units/UES0202/UES0202_script.lua
@@ -0,0 +1,101 @@
+#****************************************************************************
+#**
+#**  File     :  /cdimage/units/UES0202/UES0202_script.lua
+#**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
+#**
+#**  Summary  :  UEF Cruiser Script
+#**
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+local EffectTemplate = import('/lua/EffectTemplates.lua')
+local TSeaUnit = import('/lua/terranunits.lua').TSeaUnit
+local WeaponFile = import('/lua/terranweapons.lua')
+local TSAMLauncher = WeaponFile.TSAMLauncher
+local TDFGaussCannonWeapon = WeaponFile.TDFGaussCannonWeapon
+local TAMPhalanxWeapon = WeaponFile.TAMPhalanxWeapon
+local TIFCruiseMissileLauncher = WeaponFile.TIFCruiseMissileLauncher
+
+UES0202 = Class(TSeaUnit) {
+    DestructionTicks = 200,
+
+    Weapons = {
+        FrontTurret01 = Class(TDFGaussCannonWeapon) {},
+        BackTurret02 = Class(TSAMLauncher) {
+            FxMuzzleFlash = EffectTemplate.TAAMissileLaunchNoBackSmoke,
+        },
+        PhalanxGun01 = Class(TAMPhalanxWeapon) {
+            PlayFxWeaponUnpackSequence = function(self)
+                if not self.SpinManip then 
+                    self.SpinManip = CreateRotator(self.unit, 'Center_Turret_Barrel', 'z', nil, 270, 180, 60)
+                    self.SpinManip:SetPrecedence(10)
+                    self.unit.Trash:Add(self.SpinManip)
+                end
+                if self.SpinManip then
+                    self.SpinManip:SetTargetSpeed(270)
+                end
+                TAMPhalanxWeapon.PlayFxWeaponUnpackSequence(self)
+            end,
+
+            PlayFxWeaponPackSequence = function(self)
+                if self.SpinManip then
+                    self.SpinManip:SetTargetSpeed(0)
+                end
+                TAMPhalanxWeapon.PlayFxWeaponPackSequence(self)
+            end,
+        },
+        
+        CruiseMissile = Class(TIFCruiseMissileLauncher) {
+                CurrentRack = 1,
+                
+                #taken out because all this waiting causes broken rate of fire clock issues
+                --PlayFxMuzzleSequence = function(self, muzzle)
+                    --local bp = self:GetBlueprint()
+                    --self.Rotator = CreateRotator(self.unit, bp.RackBones[self.CurrentRack].RackBone, 'y', nil, 90, 90, 90)
+                    --muzzle = bp.RackBones[self.CurrentRack].MuzzleBones[1]
+                    --self.Rotator:SetGoal(90)
+                    --TIFCruiseMissileLauncher.PlayFxMuzzleSequence(self, muzzle)
+                    --WaitFor(self.Rotator)
+                    --WaitSeconds(1)
+                --end,
+                
+                CreateProjectileAtMuzzle = function(self, muzzle)
+                    muzzle = self:GetBlueprint().RackBones[self.CurrentRack].MuzzleBones[1]
+                    if self.CurrentRack >= 8 then
+                        self.CurrentRack = 1
+                    else
+                        self.CurrentRack = self.CurrentRack + 1
+                    end
+                    TIFCruiseMissileLauncher.CreateProjectileAtMuzzle(self, muzzle)
+                end,
+                
+                #taken out because all this waiting causes broken rate of fire clock issues
+                --PlayFxRackReloadSequence = function(self)
+                    --WaitSeconds(1)
+                    --self.Rotator:SetGoal(0)
+                    --WaitFor(self.Rotator)
+                    --self.Rotator:Destroy()
+                    --self.Rotator = nil
+                --end,
+            },
+    },
+
+    RadarThread = function(self)
+        local spinner = CreateRotator(self, 'Spinner04', 'x', nil, 0, 30, -45)
+        while true do
+            WaitFor(spinner)
+            spinner:SetTargetSpeed(-45)
+            WaitFor(spinner)
+            spinner:SetTargetSpeed(45)
+        end
+    end,
+
+    OnStopBeingBuilt = function(self, builder,layer)
+        TSeaUnit.OnStopBeingBuilt(self, builder,layer)
+        self:ForkThread(self.RadarThread)
+        self.Trash:Add(CreateRotator(self, 'Spinner01', 'y', nil, 45, 0, 0))
+        self.Trash:Add(CreateRotator(self, 'Spinner03', 'y', nil, -30, 0, 0))
+    end,
+
+}
+
+TypeClass = UES0202
