6721dcfaa07be415db0f893900fb56c0e88da417
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-09-15 23:22:29 +0200

    Fix the LOD on the Aeon temple for Alpine Valley

---
+++ units/XAC2201/XAC2201_script.lua
@@ -0,0 +1,15 @@
+#****************************************************************************
+#** 
+#**  File     :  /cdimage/units/XAC2201/XAC2201_script.lua 
+#** 
+#** 
+#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+#****************************************************************************
+local ACivilianStructureUnit = import('/lua/aeonunits.lua').ACivilianStructureUnit
+
+XAC2201 = Class(ACivilianStructureUnit) {
+}
+
+
+TypeClass = XAC2201
+
