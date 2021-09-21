f7faf2c5ab6fa96c42fdfc9f74304eaf720c1b1f
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2021-09-21 20:43:43 +0200

    Replace '#' -> '--'

--- lua/sim/DefaultDamage.lua
+++ lua/sim/DefaultDamage.lua
@@ -1,12 +1,12 @@
-#****************************************************************************
-#**
-#**  File     :  /lua/sim/defaultdamage.lua
-#**  Author(s): John Comes
-#**
-#**  Summary  : A common way to do damage over than direct damage, ie: Dots, area dots, etc.
-#**
-#**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+--****************************************************************************
+--**
+--**  File     :  /lua/sim/defaultdamage.lua
+--**  Author(s): John Comes
+--**
+--**  Summary  : A common way to do damage over than direct damage, ie: Dots, area dots, etc.
+--**
+--**  Copyright ï¿½ 2005 Gas Powered Games, Inc.  All rights reserved.
+--****************************************************************************
 
 UnitDoTThread = function(instigator, unit, pulses, pulseTime, damage, damType, friendly)
     for i = 1, pulses do
@@ -27,16 +27,16 @@ AreaDoTThread = function(instigator, position, pulses, pulseTime, radius, damage
 end
 
 
-# SCALABLE RADIUS AREA DOT
-# - Allows for a scalable damage radius that begins with DamageStartRadius and ends
-# - with DamageEndRadius, interpolates between based on frequency and duration.
+-- SCALABLE RADIUS AREA DOT
+-- - Allows for a scalable damage radius that begins with DamageStartRadius and ends
+-- - with DamageEndRadius, interpolates between based on frequency and duration.
 
 function ScalableRadiusAreaDoT(entity)
     local spec = entity.Spec.Data
 
-    # FIX ME
-    # Change this to get position from the entity, once we have the tech to set the entity's position
-    # local position = entity:GetPosition()
+    -- FIX ME
+    -- Change this to get position from the entity, once we have the tech to set the entity's position
+    -- local position = entity:GetPosition()
     local position = entity.Spec.Position
     local radius = spec.StartRadius or 0
     local freq = spec.Frequency or 1
