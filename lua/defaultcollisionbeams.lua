1be61f320099e0a72b7d4540580ed82877125b63
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2021-09-24 15:06:34 +0200

    Fix copyright symbol

--- lua/defaultcollisionbeams.lua
+++ lua/defaultcollisionbeams.lua
@@ -5,7 +5,7 @@
 --
 --  Summary  :  Default definitions collision beams
 --
---  Copyright � 2005 Gas Powered Games, Inc.  All rights reserved.
+--  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
 ------------------------------------------------------------
 
 local CollisionBeam = import('/lua/sim/CollisionBeam.lua').CollisionBeam
