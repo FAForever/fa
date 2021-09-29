4f94bd89b7d8be3967b2c08d4e57223c615d8dea
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-09-29 18:31:28 +0200

    Adjust UEF Destroyer hierarchy

--- projectiles/TDFGauss04/TDFGauss04_script.lua
+++ projectiles/TDFGauss04/TDFGauss04_script.lua
@@ -1,7 +1,7 @@
 #
 # Terran Gauss Cannon Projectile
 #
-local TDFGaussCannonProjectile = import('/lua/terranprojectiles.lua').TDFLandGaussCannonProjectile
+local TDFGaussCannonProjectile = import('/lua/terranprojectiles.lua').TDFGaussCannonProjectile
 TDFGauss04 = Class(TDFGaussCannonProjectile) {
     
     OnCreate = function(self, inWater)
