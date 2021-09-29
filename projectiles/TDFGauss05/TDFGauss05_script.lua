da961e815a088a175e87327777bf3b87378f4d68
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-09-29 18:58:30 +0200

    Give Destroyer its own projectile for a slightly larger scale

---
+++ projectiles/TDFGauss05/TDFGauss05_script.lua
@@ -0,0 +1,25 @@
+#
+# Terran Gauss Cannon Projectile
+#
+local TDFGaussCannonProjectile = import('/lua/terranprojectiles.lua').TDFGaussCannonProjectile
+TDFGauss04 = Class(TDFGaussCannonProjectile) {
+    
+    FxUnitHitScale = 1.6,
+    FxLandHitScale = 1.6,
+
+    OnCreate = function(self, inWater)
+        TDFGaussCannonProjectile.OnCreate(self, inWater)
+        if not inWater then
+            self:SetDestroyOnWater(true)
+        else
+            self:ForkThread(self.DestroyOnWaterThread)
+        end
+    end,
+    
+    DestroyOnWaterThread = function(self)
+        WaitSeconds(0.2)
+        self:SetDestroyOnWater(true)
+    end,
+}
+TypeClass = TDFGauss04
+
