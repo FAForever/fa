03001c99c06bfea8452002fffd71a0c58d60e3e0
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-09-29 14:40:21 +0200

    Fix UEF Triad

--- projectiles/TDFGauss02/TDFGauss02_script.lua
+++ projectiles/TDFGauss02/TDFGauss02_script.lua
@@ -2,7 +2,7 @@
 -- Terran Gauss Cannon Projectile : UEB2301 (uef T2 pd)
 --
 
-local TDFGaussCannonProjectile = import('/lua/terranprojectiles.lua').TDFLandGaussCannonProjectile
+local TDFGaussCannonProjectile = import('/lua/terranprojectiles.lua').TDFGaussCannonProjectile
 
 TDFGauss01 = Class(TDFGaussCannonProjectile) {
     FxTrails = {'/effects/emitters/gauss_cannon_munition_trail_03_emit.bp',},
