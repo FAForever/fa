47ab308b76fdf9219c9756f8575a1575fd085c18
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-10-04 22:45:29 +0200

    Add comment about shared space

--- lua/shared/overcharge.lua
+++ lua/shared/overcharge.lua
@@ -1,4 +1,8 @@
 
+-- All functions in this file (inside /lua/shared) should be:
+-- - pure: they should only use the arguments provided, do not touch any global state.
+-- - sim / ui proof: they should work for both sim code and ui code.
+
 --- Formula to compute the damage of an overcharge.
 EnergyAsDamage = function(energy)
     return 0.25 * energy
