90fcbfbca4fe778c30447204efce07401e5e4ca1
Tagada14 <jan@buszka.eu>
2021-10-05 19:03:33 +0200

    Adjust the AoE and Damage of strats

--- units/UEA0304/UEA0304_Script.lua
+++ units/UEA0304/UEA0304_Script.lua
@@ -15,7 +15,7 @@ UEA0304 = Class(TAirUnit) {
         LinkedRailGun1 = Class(TAirToAirLinkedRailgun) {},
         LinkedRailGun2 = Class(TAirToAirLinkedRailgun) {},
     },
-
+    
     OnDamage = function(self, instigator, amount, vector, damageType)
         if instigator and instigator:GetBlueprint().CategoriesHash.STRATEGICBOMBER and instigator.Army == self.Army then
             return
