28ff3c61f56c41fb885d7e9a784c6f0681d049e6
Tagada14 <jan@buszka.eu>
2021-10-10 18:08:41 +0200

    Add comments and replace #'s with --

--- projectiles/TDFGauss03/TDFGauss03_script.lua
+++ projectiles/TDFGauss03/TDFGauss03_script.lua
@@ -1,6 +1,6 @@
-#
-# Terran Gauss Cannon Projectile
-#
+--
+-- Terran Gauss Cannon Projectile (UES0302) UEF Battleship
+--
 local TDFGaussCannonProjectile = import('/lua/terranprojectiles.lua').TDFShipGaussCannonProjectile
 TDFGauss03 = Class(TDFGaussCannonProjectile) {
     FxTrails = {'/effects/emitters/gauss_cannon_munition_trail_03_emit.bp',},
