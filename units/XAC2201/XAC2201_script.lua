f716110d7366b2f33fb01b545ad007b1d6c701e7
Jip <w.b.wijnia@gmail.com>
2021-11-03 19:03:08 +0100

    Various small fixes for units (#3420)
    * Fix barrel of Cybran t1 artillery
    
    Now only the barrel instead of the entire arm moves when firing.
    
    * Fix the LOD on the Aeon temple for Alpine Valley
    
    * Add adjusted t1 cybran artillery animation
    
    * Remove files that were supposed to be stashed
    
    * Add proper rackbone for new animation
    
    * Adjust zapper effect size
    
    * Add files from Madmax
    
    * Refactor '#' -> '--'
    
    * Rename flash -> shell
    
    * Adjust shell emitter for graphics and make it rotate
    
    * Refactor '#' -> '--'
    
    * Refactor for performance

---
+++ units/XAC2201/XAC2201_script.lua
@@ -0,0 +1,10 @@
+--****************************************************************************
+--** 
+--**  File     :  /cdimage/units/XAC2201/XAC2201_script.lua 
+--** 
+--** 
+--**  Copyright � 2005 Gas Powered Games, Inc.  All rights reserved.
+--****************************************************************************
+
+XAC2201 = Class(import('/lua/aeonunits.lua').ACivilianStructureUnit) { }
+TypeClass = XAC2201
