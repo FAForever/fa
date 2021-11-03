27625974692d54d0ef0310c5e6478bdcc259fc5a
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-11-03 18:48:09 +0100

    Rename flash -> shell

--- units/URL0103/URL0103_Script.lua
+++ units/URL0103/URL0103_Script.lua
@@ -21,16 +21,13 @@ URL0103 = Class(CWalkingLandUnit) {
                 '/effects/emitters/cybran_artillery_muzzle_flash_01_emit.bp',
                 '/effects/emitters/cybran_artillery_muzzle_flash_02_emit.bp',
                 '/effects/emitters/cannon_muzzle_smoke_02_emit.bp',
-				'/effects/emitters/cybran_artillery_muzzle_flash_03_emit.bp',
+				'/effects/emitters/cybran_artillery_muzzle_shell_01_emit.bp',
 				'/effects/emitters/cannon_muzzle_smoke_13_emit.bp',
             },
             FxMuzzleFlashScale = 0.5,
 			
         },
     },
-    OnStopBeingBuilt = function(self,builder,layer)
-        CWalkingLandUnit.OnStopBeingBuilt(self,builder,layer)
-    end,
 }
 
 TypeClass = URL0103
