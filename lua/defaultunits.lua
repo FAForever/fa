5d914b4b0ec540d3acdf6834fe66a2437dffa67f
Jip <w.b.wijnia@gmail.com>
2021-11-06 21:05:53 +0100

    Fix AI detection and iMAP threat bloating (#3512)
    * Fix the ai detection and remove ai threat code
    
    * Add check for map type and map settings
    
    * Adjust information message for devs

--- lua/defaultunits.lua
+++ lua/defaultunits.lua
@@ -1545,52 +1545,7 @@ MobileUnit = Class(Unit) {
     end,
 
     OnKilled = function(self, instigator, type, overkillRatio)
-
-        -- only assign threat values when we have AIs that will make use of them.
-        if GameHasAIs then 
-            -- Add unit's threat to our influence map
-            local threat = 5
-            local decay = 0.1
-            local currentLayer = self.Layer
-            if instigator then
-                local unit = false
-                if IsUnit(instigator) then
-                    unit = instigator
-                elseif IsProjectile(instigator) or IsCollisionBeam(instigator) then
-                    unit = instigator.unit
-                end
-
-                if unit then
-                    local unitPos = unit:GetCachePosition()
-                    if EntityCategoryContains(categories.STRUCTURE, unit) then
-                        decay = 0.01
-                    end
-
-                    if unitPos then
-                        if currentLayer == 'Sub' then
-                            threat = self:GetAIBrain():GetThreatAtPosition(unitPos, 0, true, 'AntiSub')
-                        elseif currentLayer == 'Air' then
-                            threat = self:GetAIBrain():GetThreatAtPosition(unitPos, 0, true, 'AntiAir')
-                        else
-                            threat = self:GetAIBrain():GetThreatAtPosition(unitPos, 0, true, 'AntiSurface')
-                        end
-                        threat = threat / 2
-                    end
-                end
-            end
-
-            if currentLayer == 'Sub' then
-                self:GetAIBrain():AssignThreatAtPosition(self:GetPosition(), threat, decay * 10, 'AntiSub')
-            elseif currentLayer == 'Air' then
-                self:GetAIBrain():AssignThreatAtPosition(self:GetPosition(), threat, decay, 'AntiAir')
-            elseif currentLayer == 'Water' then
-                self:GetAIBrain():AssignThreatAtPosition(self:GetPosition(), threat, decay * 10, 'AntiSurface')
-            else
-                self:GetAIBrain():AssignThreatAtPosition(self:GetPosition(), threat, decay, 'AntiSurface')
-            end
-        end
-
-        -- This unit was in a transport
+        -- This unit was in a transport and should create a wreck on crash
         if self.killedInTransport then
             self.killedInTransport = false
         else
