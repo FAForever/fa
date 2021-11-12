f71aa2ae8167916cea297023b6af482773c05896
CheeseBerry <90977374+ChessBerry@users.noreply.github.com>
2021-11-13 00:02:47 +0100

    Excluded editing SBOZhanaseeBomb01_script.lua for now
    because I just don't understand the triple chained colon invoke syntax,
    and neither does my editor.

--- projectiles/SBOZhanaseeBomb01/SBOZhanaseeBomb01_script.lua
+++ projectiles/SBOZhanaseeBomb01/SBOZhanaseeBomb01_script.lua
@@ -1,13 +1,3 @@
--- Automatically upvalued moho functions for performance
-local GlobalMethods = _G
-local GlobalMethodsCreateDecal = GlobalMethods.CreateDecal
-local GlobalMethodsCreateLightParticle = GlobalMethods.CreateLightParticle
-local GlobalMethodsDamageArea = GlobalMethods.DamageArea
-
-local ProjectileMethods = _G.moho.projectile_methods
-local ProjectileMethodsSetVelocity = ProjectileMethods.SetVelocity
--- End of automatically upvalued moho functions
-
 -------------------------------------------------------------------------------
 --
 --  File     :  /data/projectiles/SBOZhanaseeBomb/SBOZhanaseeBomb01_script.lua
@@ -28,21 +18,19 @@ SBOZhanaseeBombProjectile01 = Class(SZhanaseeBombProjectile)({
         local radius = self.DamageData.DamageRadius
         local FriendlyFire = self.DamageData.DamageFriendly and radius ~= 0
 
-        GlobalMethodsDamageArea(self, pos, radius, 1, 'Force', FriendlyFire)
-        GlobalMethodsDamageArea(self, pos, radius, 1, 'Force', FriendlyFire)
+        DamageArea(self, pos, radius, 1, 'Force', FriendlyFire)
+        DamageArea(self, pos, radius, 1, 'Force', FriendlyFire)
 
         self.DamageData.DamageAmount = self.DamageData.DamageAmount - 2
 
-        GlobalMethodsCreateLightParticle(self, -1, army, 26, 5, 'sparkle_white_add_08', 'ramp_white_24')
-        self:CreateProjectile('/effects/entities/SBOZhanaseeBombEffect01/SBOZhanaseeBombEffect01_proj.bp', 0, 0, 0, 0, 10.0, 0):SetCollision(false)
+        CreateLightParticle(self, -1, army, 26, 5, 'sparkle_white_add_08', 'ramp_white_24')
 
         -- One initial projectile following same directional path as the original
-        ProjectileMethodsSetVelocity(CreateProjectile("/effects/entities/SBOZhanaseeBombEffect01/SBOZhanaseeBombEffect01_proj.bp", 0, 0, 0, 0, 10.0, 0), 0, 10.0, 0)
-        self:CreateProjectile('/effects/entities/SBOZhanaseeBombEffect02/SBOZhanaseeBombEffect02_proj.bp', 0, 0, 0, 0, 0.05, 0):SetCollision(false)
-        ProjectileMethodsSetVelocity(CreateProjectile("/effects/entities/SBOZhanaseeBombEffect02/SBOZhanaseeBombEffect02_proj.bp", 0, 0, 0, 0, 0.05, 0), 0, 0.05, 0)
+        self:CreateProjectile('/effects/entities/SBOZhanaseeBombEffect01/SBOZhanaseeBombEffect01_proj.bp', 0, 0, 0, 0, 10.0, 0):SetCollision(false):SetVelocity(0, 10.0, 0)
+        self:CreateProjectile('/effects/entities/SBOZhanaseeBombEffect02/SBOZhanaseeBombEffect02_proj.bp', 0, 0, 0, 0, 0.05, 0):SetCollision(false):SetVelocity(0, 0.05, 0)
 
         if targetType ~= 'Shield' and targetType ~= 'Water' and targetType ~= 'Air' and targetType ~= 'UnitAir' and targetType ~= 'Projectile' then
-            GlobalMethodsCreateDecal(pos, RandomFloat(0.0, 6.28), 'Scorch_012_albedo', '', 'Albedo', 40, 40, 300, 200, army)
+            CreateDecal(pos, RandomFloat(0.0, 6.28), 'Scorch_012_albedo', '', 'Albedo', 40, 40, 300, 200, army)
         end
 
         SZhanaseeBombProjectile.OnImpact(self, targetType, targetEntity)
