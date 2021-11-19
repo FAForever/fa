239e6faa9ad837e19c7fd5d78223c027dee24903
Tagada14 <44676678+Tagada14@users.noreply.github.com>
2021-11-19 15:39:00 +0100

    Change the visual effects of UEF Gauss projectiles (#3480)
    * Add base game files
    
    Add TDFGauss01 and TDFGauss02 projectile files
    
    * Add comments and replace #'s with --
    
    * Fix typos
    
    * Add base game files
    
    * Add base game files
    
    * Rename all the files and adjust values
    
    * Remove unused original files
    
    * Create new projectile class and Emitter Class
    
    * Update TDFGauss03_script.lua
    
    * Code refactor '#' -> '--'
    
    * Add comment
    
    * Add base game files and rename them
    
    * Create new projectile and emitter class
    
    Create new projectile and emitter class, rename the old projectile and emitter class, change the references for both the old and new class.
    
    * Update the emitter files

--- projectiles/TDFGauss01/TDFGauss01_script.lua
+++ projectiles/TDFGauss01/TDFGauss01_script.lua
@@ -1,12 +1,9 @@
 --
 -- Terran Gauss Cannon Projectile
 --
-local TDFGaussCannonProjectile = import('/lua/terranprojectiles.lua').TDFMediumShipGaussCannonProjectile
-TDFGauss05 = Class(TDFGaussCannonProjectile) {
+local TDFGaussCannonProjectile = import('/lua/terranprojectiles.lua').TDFGaussCannonProjectile
+TDFGauss01 = Class(TDFGaussCannonProjectile) {
     
-    FxUnitHitScale = 1.6,
-    FxLandHitScale = 1.6,
-
     OnCreate = function(self, inWater)
         TDFGaussCannonProjectile.OnCreate(self, inWater)
         if not inWater then
@@ -21,5 +18,5 @@ TDFGauss05 = Class(TDFGaussCannonProjectile) {
         self:SetDestroyOnWater(true)
     end,
 }
-TypeClass = TDFGauss05
+TypeClass = TDFGauss01
 
--- projectiles/TDFGauss02/TDFGauss02_script.lua
+++ projectiles/TDFGauss02/TDFGauss02_script.lua
@@ -2,9 +2,9 @@
 -- Terran Gauss Cannon Projectile : UEB2301 (uef T2 pd)
 --
 
-local TDFGaussCannonProjectile = import('/lua/terranprojectiles.lua').TDFGaussCannonProjectile
+local TDFGaussCannonProjectile = import('/lua/terranprojectiles.lua').TDFMediumLandGaussCannonProjectile
 
-TDFGauss01 = Class(TDFGaussCannonProjectile) {
+TDFGauss02 = Class(TDFGaussCannonProjectile) {
     FxTrails = {'/effects/emitters/gauss_cannon_munition_trail_03_emit.bp',},
 }
-TypeClass = TDFGauss01
+TypeClass = TDFGauss02
