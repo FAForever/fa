3899a753a0ecfc9f0555910635c63f86ec463297
4z0t <gastec139@gmail.com>
2021-12-18 14:29:59 +0300

    add falling snowfalkes in lobby

--- lua/ui/lobby/lobby.lua
+++ lua/ui/lobby/lobby.lua
@@ -3788,6 +3788,70 @@ function CreateUI(maxPlayers)
             WaitSeconds(1)
         end
     end)
+    CreateSnowFlakes ()
+end
+
+local snowFlakePath = "/textures/ui/events/snow/snowflake.dds"
+local snowFlakeWidth = 10
+local snowFlakeHeight = 10
+local snowFlakeCount = 500
+local Math_Random = math.random
+local Math_Cos = math.cos
+local Math_Sin = math.sin
+local Math_Floor = math.floor
+
+function CreateSnowFlakes()
+    local parent = GUI
+    for i = 1, snowFlakeCount do
+        CreateSnowFlake(parent, 100, math.random() * 2, math.random(parent.Width()),
+            math.random(parent.Height()))
+    end
+end
+
+function CreateSnowFlake(parent, speed, scale, xPos, yPos)
+    local snowFlake = Bitmap(parent, snowFlakePath)
+    snowFlake.parent = parent
+    -- local snowFlake = Bitmap(self.ClientGroup)
+    -- snowFlake:SetSolidColor('ffffffff')
+    LayoutHelpers.AtLeftTopIn(snowFlake, snowFlake.parent, xPos, yPos)
+    LayoutHelpers.SetDimensions(snowFlake, snowFlakeWidth * scale, snowFlakeHeight * scale)
+    snowFlake:SetAlpha(math.random())
+    snowFlake.speed = speed;
+    snowFlake.xPos = xPos;
+    snowFlake.yPos = yPos;
+    snowFlake.scale = scale
+
+    snowFlake.counter = math.random() * 10;
+    snowFlake.sign = math.random() < 0.5 and 1 or -1
+    snowFlake:DisableHitTest()
+    snowFlake:SetNeedsFrameUpdate(true)
+    snowFlake.OnFrame = function(control, delta)
+        control.counter = control.counter + control.speed / 5000;
+        local counterCos = Math_Cos(control.counter)
+        local counterSin = Math_Sin(control.counter)
+        control.xPos = control.xPos + control.sign * control.speed * counterCos / 200;
+        control.yPos = control.yPos + counterSin / 100 + control.speed / 100;
+        control.scale = control.scale + counterCos / 100;
+
+        LayoutHelpers.AtLeftTopIn(control, control.parent, Math_Floor(control.xPos), Math_Floor(control.yPos))
+        if control.sign > 0 then
+            LayoutHelpers.SetDimensions(snowFlake, snowFlakeWidth * control.scale,
+                snowFlakeHeight * control.scale * counterCos)
+        else
+            LayoutHelpers.SetDimensions(snowFlake, snowFlakeWidth * control.scale * counterSin,
+                snowFlakeHeight * control.scale)
+        end
+
+        control:Show()
+
+        if (control.Left() < control.parent.Left() or control.Right() > control.parent.Right()) then
+            control:Hide()
+        end
+        if (control.yPos > control.parent.Height()) then
+            control.yPos = -10
+            control.xPos = Math_Random(control.parent.Width())
+        end
+    end
 end
 
 function setupChatEdit(chatPanel)
