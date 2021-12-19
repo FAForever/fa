fab2c6c4c5d5cbd4b76d5c2b0fb1d711b03eaf79
Sean Wheeldon <the.balthazar@gmail.com>
2021-12-19 10:53:32 +0000

    Spawn menu moderate overhaul (#3602)
    * Update createunit.lua
    
    Spawn menu overhaul.
    
    * Add files via upload
    
    Prop spawn dialogue
    
    * Update SimCallbacks.lua
    
    Added BoxFormationSpawn
    
    * Update options.lua
    
    Spawn menu options
    
    * Update createprop.lua
    
    Fixed props not importing prefs.
    
    * Update SimCallbacks.lua
    
    Added tarmacs for spawned structures and option for orientation input.
    
    * Update createunit.lua
    
    Added orientation input, changed created button alignment, and added a helper function to reduce copy-paste
    
    * Add guard for dummy units
    
    * Update createunit.lua
    
    Made spawn on-release instead of on click, and added right-click to break condition to match what command mode displays.

--- lua/options/options.lua
+++ lua/options/options.lua
@@ -770,6 +770,88 @@ options = {
                     },
                 },
             },
+            {
+                title = "<LOC spawnmenu001>Spawn Menu: Team Columns",
+                key = 'spawn_menu_team_columns',
+                type = 'slider',
+                default = 4,
+                custom = {
+                    min = 1,
+                    max = 10,
+                    inc = 1,
+                },
+            },
+            {
+                title = "<LOC spawnmenu002>Spawn Menu: Filter Columns",
+                key = 'spawn_menu_filter_columns',
+                type = 'slider',
+                default = 6,
+                custom = {
+                    min = 1,
+                    max = 15,
+                    inc = 1,
+                },
+            },
+            {
+                title = "<LOC spawnmenu003>Spawn Menu: Split Sources",
+                key = 'spawn_menu_split_sources',
+                type = 'toggle',
+                default = 0,
+                custom = {
+                    states = {
+                        {text = "<LOC _Off>", key = 0 },
+                        {text = "<LOC _On>", key = 1 },
+                    },
+                },
+            },
+            {
+                title = "<LOC spawnmenu004>Spawn Menu: Type Filter Mode",
+                key = 'spawn_menu_type_filter_mode',
+                type = 'toggle',
+                default = 0,
+                custom = {
+                    states = {
+                        {text = "<LOC spawnmenu004phs>Physics Motion Type", key = 0 },
+                        {text = "<LOC spawnmenu004cat>Blueprint Category", key = 1 },
+                    },
+                },
+            },
+            {
+                title = "<LOC spawnmenu005>Spawn Menu: Include No-tech Filter",
+                key = 'spawn_menu_notech_filter',
+                type = 'toggle',
+                default = 1,
+                custom = {
+                    states = {
+                        {text = "<LOC _Off>", key = 0 },
+                        {text = "<LOC _On>", key = 1 },
+                    },
+                },
+            },
+            {
+                title = "<LOC spawnmenu006>Spawn Menu: Include ACU/Paragon Filter",
+                key = 'spawn_menu_paragon_filter',
+                type = 'toggle',
+                default = 0,
+                custom = {
+                    states = {
+                        {text = "<LOC _Off>", key = 0 },
+                        {text = "<LOC _On>", key = 1 },
+                    },
+                },
+            },
+            {
+                title = "<LOC spawnmenu007>Spawn Menu: Filter By Menu Sort",
+                key = 'spawn_menu_filter_menu_sort',
+                type = 'toggle',
+                default = 1,
+                custom = {
+                    states = {
+                        {text = "<LOC _Off>", key = 0 },
+                        {text = "<LOC _On>", key = 1 },
+                    },
+                },
+            },
         },
     },
     video = {
