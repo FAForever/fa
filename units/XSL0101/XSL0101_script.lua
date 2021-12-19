e32c8442aeaee03926c5454d9d7590ea679a254b
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-12-19 09:58:19 +0100

    Always check if unit died after waiting

--- units/XSL0101/XSL0101_script.lua
+++ units/XSL0101/XSL0101_script.lua
@@ -1,7 +1,7 @@
 -----------------------------------------------------------------
 -- File     :  /cdimage/units/XSL0101/XSL0101_script.lua
 -- Summary  :  Seraphim Land Scout Script
--- Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+-- Copyright ï¿½ 2007 Gas Powered Games, Inc.  All rights reserved.
 -----------------------------------------------------------------
 
 local SWalkingLandUnit = import('/lua/seraphimunits.lua').SWalkingLandUnit
@@ -68,7 +68,7 @@ XSL0101 = Class(SWalkingLandUnit) {
         if not self.Dead and self:GetFractionComplete() == 1 and self.Sync.LowPriority then
             self.WaitingForCloak = true
             WaitSeconds(self:GetBlueprint().Intel.StealthWaitTime)
-            if self:IsMoving() or self:IsUnitState("Attacking") then
+            if self.Dead or self:IsMoving() or self:IsUnitState("Attacking") then
                 self.WaitingForCloak = false
                 return
             end
