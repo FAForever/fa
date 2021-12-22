525ac0a6438e1da39be53307ac047d7acd97dbd4
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-12-22 08:42:51 +0100

    Add a separate exception for kyros lobby

--- init_fafdevelop.lua
+++ init_fafdevelop.lua
@@ -88,6 +88,7 @@ allowedAssetsScd = LowerHashTable(allowedAssetsScd)
 
 -- typical backwards compatible packages
 local allowedAssetsNxt = { }
+allowedAssetsNxt["kyros.nxt"] = true
 allowedAssetsNxt["texturepack.nxt"] = true
 allowedAssetsNxt["advanced strategic icons.nxt"] = true
 allowedAssetsNxt["advanced_strategic_icons.nxt"] = true
