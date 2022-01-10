3a4c7ec62ab2e5196b0652b4f675762d1105f268
BlackYps <jonas-fb@gmx.de>
2022-01-10 22:39:03 +0100

    Change colors again

--- lua/GameColors.lua
+++ lua/GameColors.lua
@@ -7,18 +7,18 @@ GameColors = {
     "FFe80a0a",      -- Cybran red
     "FF2929e1",      -- UEF blue
     "ff901427",      -- dark red
-    "ff66ffcc",      -- aqua
+    "ff9161ff",      -- purple
     "FFFF873E",      -- Nomads orange
-    "ff40bf40",      -- mid green
+    "ff66ffcc",      -- aqua
     "fffafa00",      -- new yellow
     "ffff88ff",      -- pink
     "ffff32ff",      -- new fuschia
-    "ff9161ff",      -- purple
     "FF5F01A7",      -- dark purple
     "ffa79602",      -- Sera golden
     "ffb76518",      -- new brown
     "FF2F4F4F",      -- olive (dark green)
     "ff2e8b57",      -- new green
+    "ff40bf40",      -- mid green
     "ff9fd802",      -- Order Green
     "ff616d7e",      -- grey
     "ffffffff",      -- white
@@ -29,18 +29,18 @@ GameColors = {
     "FFe80a0a",      -- Cybran red
     "FF2929e1",      -- UEF blue
     "ff901427",      -- dark red
-    "ff66ffcc",      -- aqua
+    "ff9161ff",      -- purple
     "FFFF873E",      -- Nomads orange
-    "ff40bf40",      -- mid green
+    "ff66ffcc",      -- aqua
     "fffafa00",      -- new yellow
     "ffff88ff",      -- pink
     "ffff32ff",      -- new fuschia
-    "ff9161ff",      -- purple
     "FF5F01A7",      -- dark purple
     "ffa79602",      -- Sera golden
     "ffb76518",      -- new brown
     "FF2F4F4F",      -- olive (dark green)
     "ff2e8b57",      -- new green
+    "ff40bf40",      -- mid green
     "ff9fd802",      -- Order Green
     "ff616d7e",      -- grey
     "ffffffff",      -- white
