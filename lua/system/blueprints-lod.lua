b33e1ef20c104b490f1428147d82ad2f9a16d091
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2022-02-13 18:30:03 +0100

    Tweaked into a curve

--- lua/system/blueprints-lod.lua
+++ lua/system/blueprints-lod.lua
@@ -1,13 +1,6 @@
 
 local MathSqrt = math.sqrt
 
-local LODFactors = {
-    [1] = { 1.0 },
-    [2] = { 0.3, 1.0 },
-    [3] = { 0.1, 0.3, 1.0 },
-    [4] = { 0.1, 0.3, 0.6, 1.0 },
-}
-
 local function CalculateLODOfProp(prop)
     local sx = prop.SizeX or 1 
     local sy = prop.SizeY or 1
@@ -16,28 +9,36 @@ local function CalculateLODOfProp(prop)
     -- give more emphasis to the x / z value as that is easier to see in the average camera angle
     local weighted = 0.40 * sx + 0.2 * sy + 0.4 * sz 
 
-    -- https://www.desmos.com/calculator/amw5fi5569
     -- 1 -> ~ 330
     -- 2 -> ~ 470
     -- 3 -> ~ 580
     -- 4 -> ~ 670
     -- 5 -> ~ 750
     -- 6 -> ~ 820
+    -- https://www.desmos.com/calculator/amw5fi5569 (1.5 * sqrt(100 * 500 * x))
     local lod = 1.5 * MathSqrt(100 * 500 * weighted)
     
     if prop.Display and prop.Display.Mesh and prop.Display.Mesh.LODs then
 
-        local factors = LODFactors[table.getn(prop.Display.Mesh.LODs)]
+        -- used for scaling the LODs
+        local n = table.getn(prop.Display.Mesh.LODs)
 
-        -- find order of LODs 
+        -- read LODs in order
         for k = 1, table.getn(prop.Display.Mesh.LODs) do 
             local data = prop.Display.Mesh.LODs[k]
 
+            -- 1/1 -> 1/1
+            -- 1/2 -> 1/4
+            -- 1/3 -> 1/9
+            -- 1/4 -> 1/16
+            -- https://www.desmos.com/calculator/keue6viu3b (x * x)
+            local factor = (k / n) * (k / n)
+
             -- if prop.ScriptClass and prop.ScriptClass == "TreeGroup" then 
-            --     LOG("Mapping ( " .. tostring(weighted) .. "): " .. tostring(data.LODCutoff) .. " -> " .. tostring(factors[k] * lod))
+            --     LOG("Mapping ( " .. tostring(weighted) .. "): " .. tostring(data.LODCutoff) .. " -> " .. tostring(factor * lod))
             -- end
 
-            data.LODCutoff = factors[k] * lod 
+            data.LODCutoff = factor * lod + 10
         end
     end
 end
