3b2186b827a91ecfbd164daa2240bf54b096c352
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2022-02-27 16:25:46 +0100

    Remove debug windows from default keymaps

--- lua/keymap/defaultKeyMap.lua
+++ lua/keymap/defaultKeyMap.lua
@@ -32,8 +32,6 @@ defaultKeyMap = {
     ['F10']                 = 'toggle_main_menu',
     ['F11']                 = 'toggle_disconnect_screen',
     ['F12']                 = 'show_objective_screen',
-    ['K']                   = 'toggle_markers_screen',
-    ['Y']                   = 'toggle_ai_screen',
 
     ['1']                   = 'group1',
     ['2']                   = 'group2',
