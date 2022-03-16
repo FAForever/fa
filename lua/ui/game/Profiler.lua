ac63bf5469f7bca992372cdf01003af66cd82e05
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2022-03-16 20:55:25 +0100

    Fix missing import

--- lua/ui/game/Profiler.lua
+++ lua/ui/game/Profiler.lua
@@ -2,6 +2,7 @@ local UIUtil = import('/lua/ui/uiutil.lua')
 local LayoutHelpers = import('/lua/maui/layouthelpers.lua')
 local Group = import('/lua/maui/group.lua').Group
 local Bitmap = import('/lua/maui/bitmap.lua').Bitmap
+local Window = import('/lua/maui/window.lua').Window
 local GameMain = import('/lua/ui/game/gamemain.lua')
 local Text = import('/lua/maui/text.lua').Text
 local Edit = import('/lua/maui/edit.lua').Edit
