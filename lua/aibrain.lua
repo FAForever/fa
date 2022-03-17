e02a5c280d60bf22793189d9d2bca580bb8f4765
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2022-03-17 13:34:33 +0100

    Guarantee that each army has some mass and energy storage

--- lua/aibrain.lua
+++ lua/aibrain.lua
@@ -208,6 +208,10 @@ AIBrain = Class(moho.aibrain_methods) {
 
     CreateBrainShared = function(self, planName)
 
+        -- make sure there is always some storage
+        self:GiveStorage('Mass', 100)
+        self:GiveStorage('Energy', 100)
+
         -- make sure the army stats exist
         self:SetArmyStat('Economy_Ratio_Mass', 1.0)
         self:SetArmyStat('Economy_Ratio_Energy', 1.0)
