da8beaa124c84cea81cd7e36adb93d73488a61bc
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2022-03-26 23:02:44 +0100

    Add latest version of the test

--- tests/test_class.lua
+++ tests/test_class.lua
@@ -1,6 +1,10 @@
 -- Test framework
 local lust = require "lust"
 
+table.empty = function(t)
+  return next(t) == nil 
+end
+
 -- Functions are imported to the global scope...
 require "../lua/system/class.lua"
 
@@ -21,31 +25,152 @@ local unit_methods = {
   GetCurrentLayer = function() end,
 }
 
-local Unit = Class(entity_methods, unit_methods)
+local Exclusions = { 
+  __index = true,
+  n = true,
+}
+
+-- convert the 'c class', we boldy assume this works :)
+ConvertCClassToLuaSimplifiedClass(entity_methods)
+ConvertCClassToLuaSimplifiedClass(unit_methods)
 
 lust.describe(
   "Test class system", 
   function()
+
     lust.describe(
       "Basic class tests", 
       function()
+
         lust.it(
           "Basic inheritance", 
           function()
 
-            local Entity = Class(entity_methods) {
+            local Specs = {
               OnCreate = function() end,
               OnDestroy = function() end,
             }
 
+            -- create an entity class
+            local Entity = Class (entity_methods) (Specs)
+
+            -- make an instance
             local instance = Entity()
 
-            for k, v in Entity do 
-              lust.expect(instance[k]).to.equal(Entity[k])
+            -- confirm the functions are inherited properly from the specifications
+            for k, v in Specs do 
+              if not Exclusions[k] then 
+                lust.expect(tostring(instance[k])).to.equal(tostring(Entity[k]))
+              end
             end
 
+            -- confirm the functions are inherited properly from the entity methods
             for k, v in entity_methods do 
-              lust.expect(instance[k]).to.equal(entity_methods[k])
+              if not Exclusions[k] then 
+                lust.expect(tostring(instance[k])).to.equal(tostring(entity_methods[k]))
+              end
+            end
+
+            -- check the meta table
+            lust.expect(tostring(getmetatable(instance))).to.equal(tostring(Entity))
+          end
+        )
+
+        lust.it(
+          "Basic dual inheritance", 
+          function()
+
+            local Specs = {
+              OnCreate = function() end,
+              OnDestroy = function() end,
+            }
+
+            -- create an entity class
+            local Entity = Class (entity_methods, unit_methods) (Specs)
+
+            -- make an instance
+            local instance = Entity()
+
+            -- confirm the functions are inherited properly from the specifications
+            for k, v in Specs do 
+              if not Exclusions[k] then 
+                lust.expect(tostring(instance[k])).to.equal(tostring(Entity[k]))
+              end
+            end
+
+            -- confirm the functions are inherited properly from the entity methods
+            for k, v in entity_methods do 
+              if not Exclusions[k] then 
+                lust.expect(tostring(instance[k])).to.equal(tostring(entity_methods[k]))
+              end
+            end
+
+            -- confirm the functions are inherited properly from the entity methods
+            for k, v in unit_methods do 
+              if not Exclusions[k] then 
+                lust.expect(tostring(instance[k])).to.equal(tostring(unit_methods[k]))
+              end
+            end
+
+            -- check the meta table
+            lust.expect(tostring(getmetatable(instance))).to.equal(tostring(Entity))
+          end
+        )
+
+        lust.it(
+          "Basic override", 
+          function()
+
+            local Specs = {
+              GetPosition = function() end,
+              OnCreate = function() end,
+              OnDestroy = function() end,
+            }
+
+            -- create an entity class
+            local Entity = Class (entity_methods) (Specs)
+
+            -- make an instance
+            local instance = Entity()
+
+            -- confirm the specifications are inherited properly
+            for k, v in Specs do 
+              if not Exclusions[k] then 
+                lust.expect(tostring(instance[k])).to.equal(tostring(Entity[k]))
+              end
+            end
+
+            -- confirm that one function is overridden by the specificiation
+            lust.expect(tostring(instance.GetPosition)).to_not.equal(tostring(entity_methods.GetPosition))
+          end
+        )
+
+        lust.it(
+          "Basic self value", 
+          function()
+
+            local Specs = {
+              OnCreate = function() end,
+              OnDestroy = function() end,
+            }
+
+            -- create an entity class
+            local Entity = Class (entity_methods) (Specs)
+
+            -- make two instances with their own data set
+            local instanceA = Entity()
+            instanceA.Bob = true 
+            instanceA.Charlie = true 
+            instanceA.Delta = true 
+
+            local instanceB = Entity()
+            instanceB.Bob = false 
+            instanceB.Charlie = false 
+            instanceB.Delta = false 
+
+            -- confirm the changes of each instance are local
+            for k, v in instanceA do 
+                lust.expect(instanceA[k]).to_not.equal(instanceB[k])
             end
           end
         )
