647eeac213304a091a0eeb0dccd38899426c9a6e
4z0t <gastec139@gmail.com>
2022-03-26 14:09:54 +0300

    upvalue calls

--- lua/UTF.lua
+++ lua/UTF.lua
@@ -1,32 +1,45 @@
+local StringChar = string.char
+local MathMod = math.mod
+local MathFloor = math.floor
+local StringLen = string.len
+local TableDeepcopy = table.deepcopy
+local unpack = unpack
+local STR_xtoi = STR_xtoi
+local STR_itox = STR_itox
+local STR_Utf8SubString = STR_Utf8SubString
+local TableInsert = table.insert
+local StringSub = string.sub
+local StringByte = string.byte
+
 function UTF(unicode)
     if unicode <= 0x7F then
-        return string.char(unicode)
+        return StringChar(unicode)
     end
 
     if (unicode <= 0x7FF) then
-        local Byte0 = 0xC0 + math.floor(unicode / 0x40);
-        local Byte1 = 0x80 + math.mod(unicode, 0x40);
-        return string.char(Byte0, Byte1);
+        local Byte0 = 0xC0 + MathFloor(unicode / 0x40);
+        local Byte1 = 0x80 + MathMod(unicode, 0x40);
+        return StringChar(Byte0, Byte1);
     end
 
     if (unicode <= 0xFFFF) then
-        local Byte0 = 0xE0 + math.floor(unicode / 0x1000);
-        local Byte1 = 0x80 + math.mod(math.floor(unicode / 0x40), 0x40);
-        local Byte2 = 0x80 + math.mod(unicode, 0x40);
-        return string.char(Byte0, Byte1, Byte2);
+        local Byte0 = 0xE0 + MathFloor(unicode / 0x1000);
+        local Byte1 = 0x80 + MathMod(MathFloor(unicode / 0x40), 0x40);
+        local Byte2 = 0x80 + MathMod(unicode, 0x40);
+        return StringChar(Byte0, Byte1, Byte2);
     end
 
     if (unicode <= 0x10FFFF) then
         local code = unicode
-        local Byte3 = 0x80 + math.mod(code, 0x40);
-        code = math.floor(code / 0x40)
-        local Byte2 = 0x80 + math.mod(code, 0x40);
-        code = math.floor(code / 0x40)
-        local Byte1 = 0x80 + math.mod(code, 0x40);
-        code = math.floor(code / 0x40)
+        local Byte3 = 0x80 + MathMod(code, 0x40);
+        code = MathFloor(code / 0x40)
+        local Byte2 = 0x80 + MathMod(code, 0x40);
+        code = MathFloor(code / 0x40)
+        local Byte1 = 0x80 + MathMod(code, 0x40);
+        code = MathFloor(code / 0x40)
         local Byte0 = 0xF0 + code;
 
-        return string.char(Byte0, Byte1, Byte2, Byte3);
+        return StringChar(Byte0, Byte1, Byte2, Byte3);
     end
 
     WARN('Unicode cannot be greater than U+10FFFF! (UTF(' .. unicode .. '))')
@@ -76,16 +89,16 @@ end
 --- @return string
 function EscapeString(str)
     local function norm(s)
-        if string.len(s) == 1 then
+        if StringLen(s) == 1 then
             return '0' .. s
         end
         return s
     end
     local function sym_to_unicode(sym)
-        local symlen = string.len(sym)
+        local symlen = StringLen(sym)
         local s = '\\u'
         for i = 1, symlen do
-            s = s .. norm(STR_itox(string.byte(sym, i)))
+            s = s .. norm(STR_itox(StringByte(sym, i)))
         end
         return s
     end
@@ -104,17 +117,13 @@ end
 --- @param str string
 --- @return string
 function UnescapeString(str)
-    -- local function unicode_to_sym(unicode)
-    --     local unilen = string.len(unicode)
-    --     return UTF(STR_xtoi(string.sub(unicode, 3, unilen)))
-    -- end
     local function unicode_to_sym(unicode)
         local bytes = {}
-        local unilen = string.len(unicode)
+        local unilen = StringLen(unicode)
         for i = 3, unilen, 2 do
-            table.insert(bytes, STR_xtoi(string.sub(unicode, i, i + 1)))
+            TableInsert(bytes, STR_xtoi(StringSub(unicode, i, i + 1)))
         end
-        return string.char(unpack(bytes))
+        return StringChar(unpack(bytes))
     end
     local result = ''
     for s in string.gfind(str, "\\u%x+") do
@@ -133,7 +142,7 @@ function UnescapeTable(t, doNotCopy)
         return
     end
     if not doNotCopy then
-        t = table.deepcopy(t)
+        t = TableDeepcopy(t)
     end
     for k, v in t do
         if type(v) == 'string' then
@@ -154,7 +163,7 @@ function EscapeTable(t, doNotCopy)
         return
     end
     if not doNotCopy then
-        t = table.deepcopy(t)
+        t = TableDeepcopy(t)
     end
     for k, v in t do
         if type(v) == 'string' then
