2d5bdedbbb075df15e501570639ca7f677a2f3e3
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2022-04-26 21:39:22 +0200

    Improve performance of entity allocations

--- lua/sim/Entity.lua
+++ lua/sim/Entity.lua
@@ -1,13 +1,19 @@
-#****************************************************************************
-#**  File     :  /lua/sim/Entity.lua
-#**  Summary  : The Entity lua module
-#**
-#**  Copyright © 2008 Gas Powered Games, Inc.  All rights reserved.
-#****************************************************************************
+--****************************************************************************
+--**  File     :  /lua/sim/Entity.lua
+--**  Summary  : The Entity lua module
+--**
+--**  Copyright © 2008 Gas Powered Games, Inc.  All rights reserved.
+--****************************************************************************
+
+-- we can't scope this as an upvalue because this file is touched upon by UI code.
+-- - unitview.lua -> import of game.lua (ui file)
+-- - game.lua -> import of BuffField.lua (sim file)
+-- - BuffField.lua -> import of Entity (sim file)
+-- local _c_CreateEntity = _c_CreateEntity 
 
 Entity = Class(moho.entity_methods) {
 
-    --- Called when the entity is created
+    --- Called when the entity is allocated
     -- @param self The entity itself
     -- @param spec The specifications of the entity
     -- -  if 'spec.Owner' is set to a unit then the visibility of the entity matches that of the unit 
@@ -16,16 +22,10 @@ Entity = Class(moho.entity_methods) {
     end,
 
     __post_init = function(self, spec)
-        self:OnCreate(spec)
+        self.OnCreate(self, spec)
     end,
 
-    OnCreate = function(self, spec)
-        self.Spec = spec
-        self.EntityId = self:GetEntityId()
-        self.Army = self:GetArmy()
-    end,
-
-    OnDestroy = function(self)
-
-    end,
+    -- kept for backwards compatibility with mods
+    OnCreate = function(self, spec) end,
+    OnDestroy = function(self) end,
 }
