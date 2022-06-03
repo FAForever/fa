48b6634fcf8a5b347319020bcf97d54eb5d941fc
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2022-06-03 19:38:45 +0200

    Adjust description slightly

--- lua/ui/help/tooltips.lua
+++ lua/ui/help/tooltips.lua
@@ -891,7 +891,7 @@ Tooltips = {
     },
     Lobby_patchnotes = {
         title = "<LOC tooltipui2000>Patchnotes",
-        description = "<LOC tooltipui2001>List of current and Previous Game/Balance Patchnotes",
+        description = "<LOC tooltipui2001>Access to all previous patch notes made by the community",
     },
     Lobby_Load = {
         title = "<LOC tooltipui0171>Load",
