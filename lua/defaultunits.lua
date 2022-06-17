35567e36d8bba97c3e4f99392656ddb1006d6051
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2022-06-17 17:46:13 +0200

    Fix rolloff points for factories

--- lua/defaultunits.lua
+++ lua/defaultunits.lua
@@ -1401,6 +1401,77 @@ SeaFactoryUnit = Class(FactoryUnit) {
             self.UnitBeingBuilt:Destroy()
         end
     end,
+
+    CalculateRollOffPoint = function(self)
+
+        local px, py, pz = self:GetPositionXYZ()
+        LOG(string.format("Origin point: (%d, %d, %d)", px, py, pz))
+
+        -- retrieve roll off points
+        local bp = self.Blueprint.Physics.RollOffPoints
+        if not bp then 
+            return 0, px, py, pz 
+        end
+
+        -- retrieve rally point
+        local rallyPoint = self:GetRallyPoint()
+        if not rallyPoint then 
+            return 0, px, py, pz
+        end
+
+        local correctedRolloffX = self.Blueprint.Physics.CorrectNavalRollOffPoints or false 
+        LOG(correctedRolloffX)
+        if correctedRolloffX then 
+            local bone = (self:IsValidBone('Attachpoint') and 'Attachpoint') or (self:IsValidBone('Attachpoint01') and 'Attachpoint01')
+            local bx, by, bz = self:GetPositionXYZ(bone)
+            LOG(string.format("Attachment point: (%d, %d, %d)", bx, by, bz))
+            correctedRolloffX = bx - px
+        end
+
+        local modz = 1.0 + 0.1 * self.UnitBeingBuilt.Blueprint.SizeZ
+
+        -- find the nearest roll off point
+        local bpKey = 1
+        local distance, lowest = nil
+        for k, rolloffPoint in bp do
+
+
+
+
+
+            local ropx = correctedRolloffX
+            local ropz = modz * rolloffPoint.Z
+
+            LOG(ropx)
+            LOG(ropz) 
+
+            if correctedRolloffX then 
+                LOG(string.format("%s: %d -> %d", self.Blueprint.BlueprintId, rolloffPoint.X, correctedRolloffX or 0 ))
+            end
+
+            distance = VDist2(rallyPoint[1], rallyPoint[3], (ropx) + px, ropz + pz)
+            if not lowest or distance < lowest then
+                bpKey = k
+                lowest = distance
+            end
+        end
+
+        local fx, fy, fz, spin
+        local bpP = bp[bpKey]
+        local unitBP = self.UnitBeingBuilt.Blueprint.Display.ForcedBuildSpin
+        if unitBP then
+            spin = unitBP
+        else
+            spin = bpP.UnitSpin
+        end
+
+        fx = correctedRolloffX + px
+        fy = bpP.Y + py
+        fz = modz * bpP.Z + pz
+
+        return spin, fx, fy, fz
+    end,
+
 }
 
 -- SHIELD STRCUTURE UNITS
