FAForever/fa

commit b567fc2bf660897fda035b6e2ef7cd538c155808

Re-implement game results (#4016)
Implements the processing of the game results from scratch.

lua/sim/MatchState.lua
@@ -1,0 +1,209 @@
+
+local Conditions = {
+    demoralization = categories.COMMAND,
+    domination = categories.STRUCTURE + categories.ENGINEER - categories.WALL,
+    eradication = categories.ALLUNITS - categories.WALL,
+}
+
+--- Ends the game, processing all events including sending score and statistics to the UI
+function CallEndGame()
+    ForkThread(function()
+        WaitSeconds(2.9)
+        for _, v in GameOverListeners do
+            v()
+        end
+        Sync.GameEnded = true
+        WaitSeconds(0.1)
+        EndGame()
+    end)
+end
+
+--- Finds and collectors the brains that are defeated
+---@param aliveBrains AIBrain[] # Table of brains that are relevant to check for defeat
+---@param condition Categories  # Categories to check for units that are required to remain in the game
+---@param delay number          # Delay between each brain to spread the load over various ticks
+---@return AIBrain[]            # Table of brains that are considered defeated, can be empty
+local function CollectDefeatedBrains(aliveBrains, condition, delay)
+    local defeatedBrains = { }
+    for k, brain in aliveBrains do
+        local criticalUnits = brain:GetListOfUnits(condition)
+        if criticalUnits then
+            -- critical units found, make sure they all exist properly
+            local oneCriticalUnitAlive = false
+            for _, unit in criticalUnits do
+                if (not IsDestroyed(unit)) and (unit:GetFractionComplete() == 1) then
+                    oneCriticalUnitAlive = true
+                    break
+                end
+            end
+
+            -- no critical units alive or finished, brain is defeated
+            if not oneCriticalUnitAlive then 
+                defeatedBrains[k] = brain
+            end
+
+        -- no critical units found, brain is defeated
+        else 
+            defeatedBrains[k] = brain
+        end
+
+        WaitTicks(delay)
+    end
+
+    return defeatedBrains
+end
+
+--- Determines observer behavior after an army is defeated
+---@param armyIndex number
+function ObserverAfterDeath(armyIndex)
+    if not ScenarioInfo.Options.AllowObservers then return end
+    local humans = {}
+    local humanIndex = 0
+    for i, data in ArmyBrains do
+        if data.BrainType == 'Human' then
+            if IsAlly(armyIndex, i) then
+                if not ArmyIsOutOfGame(i) then
+                    return
+                end
+                table.insert(humans, humanIndex)
+            end
+            humanIndex = humanIndex + 1
+        end
+    end
+
+    for _, index in humans do
+        for i in ArmyBrains do
+            SetCommandSource(i - 1, index, false)
+        end
+    end
+end
+
+--- Continiously scans the game for brains being defeated or changes in alliances that can cause the game to end
+local function MatchStateThread()
+
+    -- determine game conditions
+    local condition = Conditions[ScenarioInfo.Options.Victory]
+    if not condition and not (ScenarioInfo.Options.Victory == 'sandbox') then
+        SPEW("Unknown victory condition supplied: " .. ScenarioInfo.Options.Victory .. ", victory condition defaults to sandbox.")
+        return
+    end
+
+    -- consider all non-civilian brains to be alive and kicking
+    local aliveBrains = { }
+    for _, brain in ArmyBrains do
+        local index = brain:GetArmyIndex()
+        if not ArmyIsCivilian(index) then
+            aliveBrains[index] = brain
+        end
+    end
+
+    -- keep scanning the gamestate for changes in alliances and brain state
+    while true do
+        -- check for defeat
+        local defeatedBrains = CollectDefeatedBrains(aliveBrains, condition, 4)
+        local defeatedBrainsCount = table.getsize(defeatedBrains)
+        if defeatedBrainsCount > 0 then
+
+            -- take into account cascading effects
+            local lastDefeatedBrainsCount
+            repeat 
+                WaitTicks(4)
+                
+                lastDefeatedBrainsCount = defeatedBrainsCount
+
+                -- re-compute the defeated brains until it no longer increases
+                defeatedBrains = CollectDefeatedBrains(aliveBrains, condition, 1)
+                defeatedBrainsCount = table.getsize(defeatedBrains)
+            until defeatedBrainsCount == lastDefeatedBrainsCount
+
+            -- call them out as being defeated and exclude them
+            for k, brain in defeatedBrains do
+                -- take the army out of the game, adjust command sources
+                SetArmyOutOfGame(k)
+                ObserverAfterDeath(k)
+
+                -- process on defeat logic of brain
+                brain:OnDefeat()
+
+                -- communicate to the server that this brain has been defeated
+                table.insert(Sync.GameResult, { k, "defeat -10" })
+
+                -- stop considering it a brain that is still alive
+                aliveBrains[k] = nil
+            end
+        end
+
+        -- loop through the brains that are still alive to check for alliance differences
+
+        if table.getsize(aliveBrains) > 0 then 
+
+            -- check for draw
+            local draw = true
+            for _, brain in aliveBrains do
+                draw = draw and brain.OfferingDraw
+            end
+
+            if draw then
+                for k, brain in aliveBrains do
+                    -- take the army out of the game, adjust command sources
+                    SetArmyOutOfGame(k)
+                    ObserverAfterDeath(k)
+
+                    -- process on draw logic of brain
+                    brain:OnDraw()
+
+                    -- communicate to the server that this brain has been defeated
+                    table.insert(Sync.GameResult, { k, "draw 0" })
+
+                    -- stop considering it a brain that is still alive
+                    aliveBrains[k] = nil
+                end
+
+                CallEndGame()
+                break
+            end
+
+            -- check for win
+            local win = true 
+            for _, brain in aliveBrains do
+                win = win and brain.RequestingAlliedVictory
+            end
+
+            for k, _ in aliveBrains do
+                for l, _ in aliveBrains do
+                    win = win and IsAlly(k, l)
+                end
+            end
+
+            if win then 
+                for k, brain in aliveBrains do
+                    -- take the army out of the game, adjust command sources
+                    SetArmyOutOfGame(k)
+                    ObserverAfterDeath(k)
+
+                    -- process on draw logic of brain
+                    brain:OnVictory()
+
+                    -- communicate to the server that this brain has been defeated
+                    table.insert(Sync.GameResult, { k, "victory 10" })
+
+                    -- stop considering it a brain that is still alive
+                    aliveBrains[k] = nil
+                end
+
+                CallEndGame()
+                break
+            end
+
+        -- apparently no undefeated brains are left
+        else
+            CallEndGame()
+            break
+        end
+
+        WaitTicks(10)
+    end
+
+end
+
+ForkThread(MatchStateThread)

lua/ScenarioFramework.lua
@@ -37,7 +37,7 @@ function EndOperation(_success, _allPrimary, _allSecondary, _allBonus)
         _opData = import(opFile)
     end
 
-    import('/lua/victory.lua').CallEndGame() -- We need this here to populate the score screen
+    import('/lua/sim/matchstate.lua').CallEndGame() -- We need this here to populate the score screen
 
     ForkThread(function()
         WaitSeconds(3) -- Wait for the stats to be synced

lua/UserSync.lua
@@ -104,28 +104,6 @@ function OnSync()
         UpdateReclaim(Sync.Reclaim)
     end
 
-    if Sync.Teamkill and not SessionIsReplay() then
-        local armies, clients = GetArmiesTable().armiesTable, GetSessionClients()
-        local victim, instigator = Sync.Teamkill.victim, Sync.Teamkill.instigator
-        local data = {time=Sync.Teamkill.killTime, victim={}, instigator={}}
-
-        for k, army in {victim=victim, instigator=instigator} do
-            data[k].name = armies[army] and armies[army].nickname or "-"
-            data[k].id = clients[army] and clients[army].uid or 0
-        end
-
-        GpgNetSend('TeamkillHappened', data.time, data.victim.id, data.victim.name,  data.instigator.id, data.instigator.name)
-        WARN(string.format("TEAMKILL: %s KILLED BY %s, TIME: %s", data.victim.name, data.instigator.name, data.time))
-
-        if GetFocusArmy() == victim then
-            import('/lua/ui/dialogs/teamkill.lua').CreateDialog(data)
-        end
-    end
-
-    if Sync.EnforceRating then
-        GpgNetSend('EnforceRating')
-    end
-
     if Sync.EnhanceMessage and not table.empty(Sync.EnhanceMessage) then
         for _, messageTable in Sync.EnhanceMessage do
             sendEnhancementMessage(messageTable)
@@ -138,10 +116,6 @@ function OnSync()
 
     if Sync.StartPositions then
         import('/lua/ui/game/worldview.lua').MarkStartPositions(Sync.StartPositions)
-    end
-
-    if Sync.GameEnded then
-        GpgNetSend('GameEnded')
     end
 
     if Sync.LobbyOptions then 
@@ -234,33 +208,6 @@ function OnSync()
 
     if not table.empty(Sync.Score) then
         import('/lua/ui/game/score.lua').currentScores = Sync.Score
-    end
-
-    if not table.empty(Sync.ScoreAccum) then
-        import('/lua/ui/dialogs/hotstats.lua').scoreData = Sync.ScoreAccum
-    end
-
-
-    for _, gameResult in Sync.GameResult do
-        local armyIndex, result = unpack(gameResult)
-
-        -- Only send defeat results if the player has not been defeated yet
-        if not hasSeenResult or not utils.StringStartsWith(result, "defeat") then
-            LOG(string.format('Sending game result: %i %s', armyIndex, result))
-            GpgNetSend('GameResult', armyIndex, result)
-            import('/lua/ui/game/gameresult.lua').DoGameResult(armyIndex, result)
-
-            if armyIndex == GetFocusArmy() then
-                hasSeenResult = true
-            end
-        end
-    end
-
-    if Sync.StatsToSend then
-        local json = import('/lua/system/dkson.lua').json.encode({ stats = Sync.StatsToSend })
-        LOG('Sending stats: '..json)
-        GpgNetSend('JsonStats', json)
-        Sync.StatsToSend = nil
     end
 
     if Sync.PausedBy then
@@ -423,5 +370,68 @@ function OnSync()
 
     if Sync.ChangeCameraZoom != nil then
         import('/lua/ui/game/gamemain.lua').SimChangeCameraZoom(Sync.ChangeCameraZoom)
+    end
+
+    if not table.empty(Sync.ScoreAccum) then
+        LOG("Score data received!")
+        import('/lua/ui/dialogs/hotstats.lua').scoreData = Sync.ScoreAccum
+    end
+
+    -- Game <-> server communications
+
+    -- Adjusting the behavior of this part of the sync is strictly forbidden and is considered
+    -- game manipulation and / or rating manipulation. See also the in-game rules:
+    -- - https://www.faforever.com/rules
+
+    --- Processes game results to adjust UI capabilities
+    for _, gameResult in Sync.GameResult do
+        local armyIndex, result = unpack(gameResult)
+        import('/lua/ui/game/gameresult.lua').DoGameResult(armyIndex, result)
+    end
+
+    if not SessionIsReplay() then
+
+        --- Sends the defeat / victory / draw game results over to the server
+        for _, gameResult in Sync.GameResult do
+            local armyIndex, result = unpack(gameResult)
+            SPEW(string.format("(%s) Sending game result: %s %s", tostring(GameTick()), armyIndex, result))
+            GpgNetSend('GameResult', armyIndex, result)
+        end
+
+        --- Sends the (unit) statistics over to the server
+        if Sync.StatsToSend then
+            local json = import('/lua/system/dkson.lua').json.encode({ stats = Sync.StatsToSend })
+            GpgNetSend('JsonStats', json)
+            Sync.StatsToSend = nil
+        end
+
+        --- Sends potential team kill events to the server
+        if Sync.Teamkill then
+            local armies, clients = GetArmiesTable().armiesTable, GetSessionClients()
+            local victim, instigator = Sync.Teamkill.victim, Sync.Teamkill.instigator
+            local data = {time=Sync.Teamkill.killTime, victim={}, instigator={}}
+
+            for k, army in {victim=victim, instigator=instigator} do
+                data[k].name = armies[army] and armies[army].nickname or "-"
+                data[k].id = clients[army] and clients[army].uid or 0
+            end
+
+            GpgNetSend('TeamkillHappened', data.time, data.victim.id, data.victim.name,  data.instigator.id, data.instigator.name)
+            WARN(string.format("TEAMKILL: %s KILLED BY %s, TIME: %s", data.victim.name, data.instigator.name, data.time))
+
+            if GetFocusArmy() == victim then
+                import('/lua/ui/dialogs/teamkill.lua').CreateDialog(data)
+            end
+        end
+
+        --- Informs the server to enforce the rating of the game
+        if Sync.EnforceRating then
+            GpgNetSend('EnforceRating')
+        end
+
+        --- Informs the server that the game has ended
+        if Sync.GameEnded then
+            GpgNetSend('GameEnded')
+        end
     end
 end

lua/aibrain.lua
@@ -35,12 +35,6 @@ local BrainGetUnitsAroundPoint = moho.aibrain_methods.GetUnitsAroundPoint
 local BrainGetListOfUnits = moho.aibrain_methods.GetListOfUnits
 local CategoriesDummyUnit = categories.DUMMYUNIT
 
-local observer = false
-local Points = {
-    defeat = -10,
-    draw = 0,
-    victory = 10
-}
 
 local CoroutineYield = coroutine.yield
 
@@ -48,6 +42,7 @@ local CoroutineYield = coroutine.yield
 ---@alias HqTech "TECH2"|"TECH3"
 ---@alias HqLayer "LAND"|"AIR"|"NAVY"
 ---@alias HqFaction "AEON"|"UEF"|"SERAPHIM"|"CYBRAN"|"NOMADS"
+---@alias BrainState "Defeat" | "Victory" | "InProgress" | "Draw"
 
 ---@class AIBrain: moho.aibrain_methods
 ---@field Trash TrashBag
@@ -91,7 +86,11 @@ local CoroutineYield = coroutine.yield
 ---@field EnergyExcessThread thread
 ---@field IntelData table<string, number>|nil
 ---@field targetoveride boolean
+---@field Status BrainState
 AIBrain = Class(moho.aibrain_methods) {
+
+    -- The state of the brain in the match
+    Status = 'InProgress',
 
     --- HUMAN BRAIN FUNCTIONS HANDLED HERE
     ---@param self AIBrain
@@ -736,30 +735,19 @@ AIBrain = Class(moho.aibrain_methods) {
     end,
 
     ---@param self AIBrain
+    ---@deprecated 
     ReportScore = function(self)
-        local kills = self:GetArmyStat('Enemies_Commanders_Destroyed', 0).Value
-        local score = Points[self.Result] or 0 + kills
-        table.insert(Sync.GameResult, {self:GetArmyIndex(), string.format("%s %i", self.Result or 'score', score)})
     end,
 
     ---@param self AIBrain
     ---@param result AIResult
+    ---@deprecated
     SetResult = function(self, result)
-        if self.Result then return end
-        if not Points[result] then
-            WARN("brain:SetResult() " .. result .. " not a valid result")
-            return
-        end
-
-        self.Result = result
-        self:ReportScore()
     end,
 
     ---@param self AIBrain
     OnDefeat = function(self)
-        self:SetResult("defeat")
-
-        SetArmyOutOfGame(self:GetArmyIndex())
+        self.Status = 'Defeat'
 
         import('/lua/SimUtils.lua').UpdateUnitCap(self:GetArmyIndex())
         import('/lua/SimPing.lua').OnArmyDefeat(self:GetArmyIndex())
@@ -1009,17 +997,17 @@ AIBrain = Class(moho.aibrain_methods) {
 
     ---@param self AIBrain
     OnVictory = function(self)
-        self:SetResult("victory")
+        self.Status = 'Victory'
     end,
 
     ---@param self AIBrain
     OnDraw = function(self)
-        self:SetResult("draw")
+        self.Status = 'Draw'
     end,
 
     ---@param self AIBrain
     IsDefeated = function(self)
-        return self.Result == "defeat"
+        return self.Status == "Defeat"
     end,
 
     ---@param self AIBrain

lua/defaultunits.lua
@@ -2477,10 +2477,6 @@ ACUUnit = Class(CommandUnit) {
             if IsAlly(self.Army, instigator.Army) and not ((type == 'DeathExplosion' or type == 'Nuke' or type == 'Deathnuke') and not instigator.SelfDestructed) then
                 WARN('Teamkill detected')
                 Sync.Teamkill = {killTime = GetGameTimeSeconds(), instigator = instigator.Army, victim = self.Army}
-            else
-                ForkThread(function()
-                    instigatorBrain:ReportScore()
-                end)
             end
         end
         ArmyBrains[self.Army].CommanderKilledBy = (instigator or self).Army

lua/sim/score.lua
@@ -2,11 +2,11 @@ historyInterval = 10
 scoreInterval = 1
 alliesScore = true
 
+local GameIsOver = false
 local ArmyScore = {}
 local scoreOption = ScenarioInfo.Options.Score or "no"
 scoreData = {interval = historyInterval, current = ArmyScore, history = {}}
 
-local victory = import('/lua/victory.lua')
 -- Some of these values pre-existed and are used in other places, that's why their naming is not consistent
 local categoriesToCollect = {
     land = categories.LAND,
@@ -45,7 +45,7 @@ function CalculateBrainScore(brain)
 end
 
 local function ScoreResourcesThread()
-    while not victory.gameOver do
+    while not GameIsOver do
         WaitSeconds(1)
         for index, brain in ArmyBrains do
             if ArmyIsCivilian(index) then continue end
@@ -87,7 +87,7 @@ local function ScoreResourcesThread()
 end
 
 local function ScoreHistoryThread()
-    while not victory.gameOver do
+    while not GameIsOver do 
         WaitSeconds(scoreData.interval)
         local data = {}
         for index, brain in ArmyBrains do
@@ -185,7 +185,7 @@ local function ScoreThread()
     local lastConsumedEnergy = 0
     local estimatedTicksSinceLastUpdate = 0
 
-    while not victory.gameOver do
+    while not GameIsOver do
         local updInterval = scoreInterval / table.getsize(ArmyBrains)
         for index, brain in ArmyBrains do
             local CurTime = GetGameTimeSeconds()
@@ -264,9 +264,8 @@ end
 function init()
     ForkThread(ScoreThread)
     table.insert(GameOverListeners, function()
+        GameIsOver = true
         Sync.ScoreAccum = scoreData
-        if victory.gameOver then
-            Sync.StatsToSend = ArmyScore
-        end
+        Sync.StatsToSend = ArmyScore
     end)
 end

lua/simInit.lua
@@ -354,7 +354,7 @@ function BeginSession()
     import('/lua/sim/score.lua').init()
 
     --start watching for victory conditions
-    ForkThread(import('/lua/victory.lua').CheckVictory, ScenarioInfo)
+    import('/lua/sim/matchstate.lua')
 end
 
 function GameTimeLogger()

lua/ui/game/gameresult.lua
@@ -23,15 +23,9 @@ local MyArmyResultStrings = {
     replay = "<LOC GAMERESULT_0003>Replay Finished.",
 }
 
-function OnReplayEnd()
-    import('/lua/ui/game/tabs.lua').TabAnnouncement('main', LOC(MyArmyResultStrings.replay))
-    import('/lua/ui/game/tabs.lua').AddModeText("<LOC _Score>", function() import('/lua/ui/dialogs/score.lua').CreateDialog(true) end)
-end
-
 local announced = {}
 
 function DoGameResult(armyIndex, result)
-    LOG("GAMERESULT : ", result)
     local condPos = string.find(result, " ")
     if condPos ~= 0 then
         result = string.sub(result, 1, condPos - 1)

commit 279eaab05aad3f42f915d027b6eea8377bfd2ed4

Fix minor issues with the release (#4033)
Reduces the amount of tree fire at the start of the game and fixes a logic typo introduced by #4016

effects/Entities/UnitTeleport01/UnitTeleport01_script.lua
@@ -75,7 +75,6 @@ UnitTeleportEffect01 = Class(NullShell) {
         
         -- light some trees on fire
         DamageRing(self, pos, 1, 16, 1, 'TreeFire', false, false)
-        DamageRing(self, pos, 1, 16, 1, 'TreeFire', false, false)
 
 		-- knockdown trees
         for k = 1, 2 do 

lua/proptree.lua
@@ -121,19 +121,16 @@ Tree = Class(Prop) {
     --- Contains all the falling logic
     FallThread = function(self, dx, dy, dz, depth)
 
-        -- prevent collisions
-        self:SetCollisionShape('None')
-
         -- make it fall down
         local motor = self:FallDown()
         motor:Whack(dx, dy, dz, depth, true)
 
         -- no longer be able to catch fire after a while
-        WaitTicks(150)
+        WaitTicks(150 + Random(0, 50))
         self.NoBurn = true 
 
         -- make it sink after a while
-        WaitTicks(150)
+        WaitTicks(150 + Random(0, 50))
         self:SinkAway(-.1)
 
         -- get rid of it when it is completely below the terrain
@@ -190,7 +187,7 @@ Tree = Class(Prop) {
         self.PlayPropAmbientSound(self, 'BurnLoop')
 
         -- wait a bit before we change to a scorched tree
-        WaitTicks(50)
+        WaitTicks(50 + Random(0, 10))
         EntitySetMesh(self, self.Blueprint.Display.MeshBlueprintWrecked)
 
         -- more fire effects
@@ -210,7 +207,7 @@ Tree = Class(Prop) {
         end
 
         -- wait a bit before we make a scorch mark
-        WaitTicks(50)
+        WaitTicks(50 + Random(0, 10))
         CreateScorchMarkSplat( self, 0.5, -1 )
 
         -- try and spread the fire

lua/sim/MatchState.lua
@@ -83,8 +83,12 @@ local function MatchStateThread()
 
     -- determine game conditions
     local condition = Conditions[ScenarioInfo.Options.Victory]
-    if not condition and not (ScenarioInfo.Options.Victory == 'sandbox') then
-        SPEW("Unknown victory condition supplied: " .. ScenarioInfo.Options.Victory .. ", victory condition defaults to sandbox.")
+    
+    if not condition then
+        if ScenarioInfo.Options.Victory ~= 'sandbox' then
+            SPEW("Unknown victory condition supplied: " .. ScenarioInfo.Options.Victory .. ", victory condition defaults to sandbox.")
+        end
+        
         return
     end
 