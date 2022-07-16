3f4ca84803fb64e5e3474e9ea3712dabb319574a
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2022-07-16 21:25:36 +0200

    Add comments about unusued class on the Lua end

--- engine/Sim/CAiNavigatorImpl.lua
+++ engine/Sim/CAiNavigatorImpl.lua
@@ -1,5 +1,5 @@
 ---@declare-global
----@class moho.navigator_methods
+---@class moho.navigator_methods This particular class is not made approachable in Lua, and it appears to be unfinished implementation-wise. This is therefore merely a dummy class
 local CNavigator = {}
 
 ---
