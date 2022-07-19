f824763ce6084851a2d01184a1ff7e774d8e785d
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2022-07-19 23:13:38 +0200

    Fix issues with tracking AA that have a damage radius > 0

--- lua/sim/Unit.lua
+++ lua/sim/Unit.lua
@@ -1481,20 +1481,20 @@ Unit = Class(moho.unit_methods) {
         end
 
         -- check for exclusions from projectile perspective
-        for k = 1, other.Blueprint.DoNotCollideListCount do 
-            if self.Cache.HashedCats[other.Blueprint.DoNotCollideList[k]] then 
+        for k = 1, other.Blueprint.DoNotCollideListCount do
+            if self.Cache.HashedCats[other.Blueprint.DoNotCollideList[k]] then
                 return false 
             end
         end
 
         -- check for exclusions from unit perspective
         for k = 1, self.Cache.DoNotCollideCatsCount do 
-            if other.Blueprint.CategoriesHash[self.Cache.DoNotCollideCats[k]] then 
-                return false 
+            if other.Blueprint.CategoriesHash[self.Cache.DoNotCollideCats[k]] then
+                return false
             end
         end
 
-        return true 
+        return true
     end,
 
     --- Called when a unit collides with a collision beam to check if the collision is valid
