bf1ccbce1cdc4a8dbb26618d7dd33602d3010422
4z0t <gastec139@gmail.com>
2022-08-02 10:53:33 +0300

    fix

--- lua/keymap/selectedinfo.lua
+++ lua/keymap/selectedinfo.lua
@@ -2,14 +2,14 @@ local Prefs = import('/lua/user/prefs.lua')
 local options = Prefs.GetFromCurrentProfile('options')
 
 local selectionOverlay = {
-        key = 'selection',
-        Label = "<LOC map_options_0006>Selection",
-        Pref = 'range_RenderSelected',
-        Type = 3,
-        Tooltip = "overlay_selection",
+    key = 'selection',
+    Label = "<LOC map_options_0006>Selection",
+    Pref = 'range_RenderSelected',
+    Type = 3,
+    Tooltip = "overlay_selection",
 }
 
-function GetUnitRolloverInfo(unit)
+function GetUnitRolloverInfo(unit, skipFocus)
     local info = {}
 
     info.blueprintId = unit:GetBlueprint().BlueprintId
@@ -31,8 +31,18 @@ function GetUnitRolloverInfo(unit)
     info.shieldRatio = unit:GetShieldRatio()
     info.workProgress = unit:GetWorkProgress()
 
-    if unit:GetFocus() then
-        info.focus = GetUnitRolloverInfo(unit:GetFocus())
+    local focus = unit:GetFocus()
+    if focus and not skipFocus then
+        _visited = { [unit:GetEntityId()] = true }
+        while focus do
+            if _visited[focus:GetEntityId()] then
+                info.focus = GetUnitRolloverInfo(unit:GetFocus(), true)
+                break
+            end
+            _visited[focus:GetEntityId()] = true
+            info.focus = focus
+            focus = focus:GetFocus()
+        end
     end
 
     local killStat = unit:GetStat('KILLS')
@@ -52,4 +62,3 @@ function GetUnitRolloverInfo(unit)
 
     return info
 end
-
