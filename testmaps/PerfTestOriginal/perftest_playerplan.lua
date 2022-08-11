38ed51d8eb782654a732fc1793bb23cad77725c6
Rowey <row.josh02@gmail.com>
2022-08-11 21:39:08 +0100

    Fix Copyright Symbols

--- testmaps/PerfTestOriginal/perftest_playerplan.lua
+++ testmaps/PerfTestOriginal/perftest_playerplan.lua
@@ -5,7 +5,7 @@
 --#**
 --#**  Summary  :
 --#**
---#**  Copyright � 2006 Gas Powered Games, Inc.  All rights reserved.
+--#**  Copyright © 2006 Gas Powered Games, Inc.  All rights reserved.
 --#****************************************************************************
 
 function EvaluatePlan(brain)
--- testmaps/PerfTestOriginal/perftest_playerplanlist.lua
+++ testmaps/PerfTestOriginal/perftest_playerplanlist.lua
@@ -5,7 +5,7 @@
 --#**
 --#**  Summary  : AIBrain Strategic Plans Definitions
 --#**
---#**  Copyright � 2006 Gas Powered Games, Inc.  All rights reserved.
+--#**  Copyright © 2006 Gas Powered Games, Inc.  All rights reserved.
 --#****************************************************************************
 
 AIPlansList =
