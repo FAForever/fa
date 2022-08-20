9c189e65248ef1e403821e4b803eecbf1f86c060
Jip <w.b.wijnia@gmail.com>
2022-08-20 12:35:36 +0200

    Check for unit

--- lua/SimHooks.lua
+++ lua/SimHooks.lua
@@ -74,7 +74,7 @@ do
     -- do not allow command units to be given
     local oldChangeUnitArmy = _G.ChangeUnitArmy
     _G.ChangeUnitArmy = function(unit, army)
-        if unit.Blueprint.CategoriesHash["COMMAND"] then 
+        if unit and unit.Blueprint.CategoriesHash["COMMAND"] then
             return nil
         end
 
