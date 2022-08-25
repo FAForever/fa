37bcdbca32049042af2bcbb0f12b305d62865bd0
Rowey <row.josh02@gmail.com>
2022-08-25 21:28:12 +0100

    change Unit back to  StructerUnit

--- lua/sim/AdjacencyBuffFunctions.lua
+++ lua/sim/AdjacencyBuffFunctions.lua
@@ -12,22 +12,22 @@
 local TableGetn = table.getn
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 DefaultBuffRemove = function(buff, unit, instigator)
     unit:DestroyAdjacentEffects(instigator)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 DefaultBuffAffect = function(buff, unit, instigator)
     unit:CreateAdjacentEffect(instigator)
 end
 
 -- Energy Build Bonus - Energy Active Consumption
 ---@param buff BlueprintBuff
----@param unit Unit
+---@param unit StructureUnit
 ---@return boolean
 EnergyBuildBuffCheck = function(buff, unit)
     local bp = unit:GetBlueprint()
@@ -41,22 +41,22 @@ EnergyBuildBuffCheck = function(buff, unit)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 EnergyBuildBuffRemove = function(buff, unit, instigator)
     DefaultBuffRemove(buff, unit, instigator)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 EnergyBuildBuffAffect = function(buff, unit, instigator)
     DefaultBuffAffect(buff, unit, instigator)
 end
 
 -- Mass Build Bonus - Mass Active Consumption
 ---@param buff BlueprintBuff
----@param unit Unit
+---@param unit StructureUnit
 ---@return boolean
 MassBuildBuffCheck = function(buff, unit)
     local bp = unit:GetBlueprint()
@@ -70,22 +70,22 @@ MassBuildBuffCheck = function(buff, unit)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 MassBuildBuffRemove = function(buff, unit, instigator)
     DefaultBuffRemove(buff, unit, instigator)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 MassBuildBuffAffect = function(buff, unit, instigator)
     DefaultBuffAffect(buff, unit, instigator)
 end
 
 -- Energy Maintenance Bonus
 ---@param buff BlueprintBuff
----@param unit Unit
+---@param unit StructureUnit
 ---@return boolean
 EnergyMaintenanceBuffCheck = function(buff, unit)
     local bp = unit:GetBlueprint()
@@ -96,21 +96,21 @@ EnergyMaintenanceBuffCheck = function(buff, unit)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 EnergyMaintenanceBuffRemove = function(buff, unit, instigator)
     DefaultBuffRemove(buff, unit, instigator)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 EnergyMaintenanceBuffAffect = function(buff, unit, instigator)
     DefaultBuffAffect(buff, unit, instigator)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
+---@param unit StructureUnit
 ---@return boolean
 MassActiveBuffCheck = function(buff, unit)
     local bp = unit:GetBlueprint()
@@ -127,7 +127,7 @@ EnergyActiveBuffCheck = MassActiveBuffCheck
 
 -- Energy Weapon Bonus
 ---@param buff BlueprintBuff
----@param unit Unit
+---@param unit StructureUnit
 ---@return boolean
 EnergyWeaponBuffCheck = function(buff, unit)
     for i = 1, unit:GetWeaponCount() do
@@ -140,22 +140,22 @@ EnergyWeaponBuffCheck = function(buff, unit)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 EnergyWeaponBuffRemove = function(buff, unit, instigator)
     DefaultBuffRemove(buff, unit, instigator)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 EnergyWeaponBuffAffect = function(buff, unit, instigator)
     DefaultBuffAffect(buff, unit, instigator)
 end
 
 -- Weapon Rate of Fire
 ---@param buff BlueprintBuff
----@param unit Unit
+---@param unit StructureUnit
 ---@return boolean
 RateOfFireBuffCheck = function(buff, unit)
     if unit:GetWeaponCount() > 0 then
@@ -165,22 +165,22 @@ RateOfFireBuffCheck = function(buff, unit)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 RateOfFireBuffRemove = function(buff, unit, instigator)
     DefaultBuffRemove(buff, unit, instigator)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 RateOfFireBuffAffect = function(buff, unit, instigator)
     DefaultBuffAffect(buff, unit, instigator)
 end
 
 -- Energy Production
 ---@param buff BlueprintBuff
----@param unit Unit
+---@param unit StructureUnit
 ---@return boolean
 EnergyProductionBuffCheck = function(buff, unit)
     local bp = unit:GetBlueprint()
@@ -191,22 +191,22 @@ EnergyProductionBuffCheck = function(buff, unit)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 EnergyProductionBuffRemove = function(buff, unit, instigator)
     DefaultBuffRemove(buff, unit, instigator)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 EnergyProductionBuffAffect = function(buff, unit, instigator)
     DefaultBuffAffect(buff, unit, instigator)
 end
 
 -- Mass Production
 ---@param buff BlueprintBuff
----@param unit Unit
+---@param unit StructureUnit
 ---@return boolean
 MassProductionBuffCheck = function(buff, unit)
     local bp = unit:GetBlueprint()
@@ -217,15 +217,15 @@ MassProductionBuffCheck = function(buff, unit)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 MassProductionBuffRemove = function(buff, unit, instigator)
     DefaultBuffRemove(buff, unit, instigator)
 end
 
 ---@param buff BlueprintBuff
----@param unit Unit
----@param instigator Unit
+---@param unit StructureUnit
+---@param instigator StructureUnit
 MassProductionBuffAffect = function(buff, unit, instigator)
     DefaultBuffAffect(buff, unit, instigator)
 end
