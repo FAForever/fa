b9550f7dafe3cfe2a9f7d0d2a03f222e0bfc189e
Emperor-Penguin1 <randompenguin27@gmail>
2022-09-13 12:52:27 -0400

    Add Commas

--- lua/ui/help/loadingtips.lua
+++ lua/ui/help/loadingtips.lua
@@ -42,57 +42,57 @@ Tips = {
     "<LOC loadingtip_0040>Stealth only hides units from radar, cloak only from vision",
     "<LOC loadingtip_0041>Veterancy is based on the mass value of the killed unit and how much damage a unit dealt to it",
     "<LOC loadingtip_0042>To kill with a single overcharge, you need 1 energy storage for T1 units, up to 2 for T2, and up to 6 for T3",
-    "<LOC loadingtip_0043>To see stealthed underwater units you need water vision or omni"
-    "<LOC loadingtip_0044>Keep building available mexes throughout the map/game with your engineers"
-    "<LOC loadingtip_0045>Use military units (especially tanks on most maps) to take mexes/territory and counter the enemy"
-    "<LOC loadingtip_0046>You should generally use your ACU to secure groups of mexes and engage in combat in the early game"
-    "<LOC loadingtip_0047>Don't be too reckless with your ACU (it can be very useful in combat without being suicidal)"
-    "<LOC loadingtip_0048>Keep scouting throughout the game (most people don't scout enough)"
-    "<LOC loadingtip_0049>Pay enough attention to what you scout"
-    "<LOC loadingtip_0050>It is generally better to take battles you will efficiently win and avoid battles you will lose"
-    "<LOC loadingtip_0051>Taking the right battles and avoiding the wrong ones is generally especially important in air"
-    "<LOC loadingtip_0052>Don't play too zoomed in - there are more important things than perferct adjacency and base layout"
-    "<LOC loadingtip_0053>Generally, you should focus on macro more than micro, but still micro some"
-    "<LOC loadingtip_0054>Manual reclaim is generally not considered a good use of your time after the early game"
-    "<LOC loadingtip_0055>Generally try to avoid having idle units without a good reason, especially in the early game"
-    "<LOC loadingtip_0056>Setting up good hotkeys can help you save time to focus more on the game and less on your mouse"
-    "<LOC loadingtip_0057>Focus on doing the best you can and learning what you can do better instead of blaming others"
-    "<LOC loadingtip_0058>If scout enough, you should know what the enemy makes and can generally try to counter it"
-    "<LOC loadingtip_0059>If the enemy makes a bunch of land units, minimal AA and no planes, bombers could counter them"
-    "<LOC loadingtip_0060>If the enemy makes a bunch of PD, you can often go around it, outrange it, or out eco them"
-    "<LOC loadingtip_0061>If the enemy spams T1/T2 air, you can generally make interceptors and mobile flak (T2 AA)"
-    "<LOC loadingtip_0062>If the enemy eco's a lot more than you, you can generally attack them and take winning battles"
-    "<LOC loadingtip_0063>If the enemy turtles hard with minimal map control, you can generally out eco them and win later"
-    "<LOC loadingtip_0064>If the enemy acts super aggressively, you can try to bait them into battles that are good for you"
-    "<LOC loadingtip_0065>Scout, and try to counter what the enemy builds, how the enemy plays, and where the enemy goes"
-    "<LOC loadingtip_0066>You should generally have your engineers use attack move orders for reclaiming"
-    "<LOC loadingtip_0067>Manual reclaim can be useful during the very early game if you have nothing better to do"
-    "<LOC loadingtip_0068>Patrol is generally worse for reclaiming than attack move is"
-    "<LOC loadingtip_0069>Use T1 engineers with attack move orders to get the reclaim from dead unit wrecks from battles"
-    "<LOC loadingtip_0070>Fighting battles where you can get the reclaim and your enemies can't matters a lot"
-    "<LOC loadingtip_0071>A wreck from a dead unit can yield up to about 81% of the unit's mass cost"
-    "<LOC loadingtip_0072>It can be worthwhile to kill/reclaim your old low-tech units to have additional nass"
-    "<LOC loadingtip_0073>Think about value and opportunity costs (ie: would you rather have ~16 T1 tanks or 1 T2 mex?)"
-    "<LOC loadingtip_0074>Low tech units often do more damage per mass but have less health per mass compared to high tech"
-    "<LOC loadingtip_0075>Upgrading a factory costs mass that could've been put into units, so don't upgrade too early"
-    "<LOC loadingtip_0076>In team games, be a team player"
-    "<LOC loadingtip_0077>If you need help or expect to lose your lane soon, tell your team"
-    "<LOC loadingtip_0078>If you don't know what to do next, ask your team what to do"
-    "<LOC loadingtip_0079>If you have extra mass and can't spend it fast enough, you can gift it to a teammate who can"
-    "<LOC loadingtip_0080>If your opponent is attacking your ally, help your ally or be doing something worth the cost"
-    "<LOC loadingtip_0081>If you notice an enemy T4/nuke/etc that your team hasn't noticed, ping/mark it and say something"
-    "<LOC loadingtip_0082>Try to coordinate your units with your allies' units when it seems worthwhile"
-    "<LOC loadingtip_0083>You may want to try UI mods like NoShake, Advanced Target Priorities 1.1, Rheclaim, etc"
-    "<LOC loadingtip_0085>You can search on the vault for numerous UI mods to try, like HotBuild Overhaul or icon mods"
-    "<LOC loadingtip_0086>Watch replays of your games to learn what different players did well/poorly to help you improve"
-    "<LOC loadingtip_0087>You can ask for advice in the gameplay-and-training channel on the FAF Discord"
-    "<LOC loadingtip_0088>The FAF wiki has useful guides that can teach you how to get better at FAF"
-    "<LOC loadingtip_0089>Scale your economy (mass, energy, & build power) over time"
-    "<LOC loadingtip_0090>Spamming units is often worthwhile, but don't forget to transition to higher tech units"
-    "<LOC loadingtip_0091>You can generally spam interceptors against t1 and t2 air, and spam ASF against t3 air"
-    "<LOC loadingtip_0092>Avoid stalling energy (don't get to 0 energy storage with a negative net energy income)"
-    "<LOC loadingtip_0093>Spend your mass (it's generally inefficient to keep a lot of mass in storage instead of using it)"
-    "<LOC loadingtip_0094>Avoid building too much energy (put your resources into things that will be the most useful)"
-    "<LOC loadingtip_0095>If you are stalling mass, it might be a good idea to have more of your engineers reclaiming"
-    "<LOC loadingtip_0096>It's generally a good idea to expand around the map early on to get mexes, reclaim and map control"
+    "<LOC loadingtip_0043>To see stealthed underwater units you need water vision or omni",
+    "<LOC loadingtip_0044>Keep building available mexes throughout the map/game with your engineers",
+    "<LOC loadingtip_0045>Use military units (especially tanks on most maps) to take mexes/territory and counter the enemy",
+    "<LOC loadingtip_0046>You should generally use your ACU to secure groups of mexes and engage in combat in the early game",
+    "<LOC loadingtip_0047>Don't be too reckless with your ACU (it can be very useful in combat without being suicidal)",
+    "<LOC loadingtip_0048>Keep scouting throughout the game (most people don't scout enough)",
+    "<LOC loadingtip_0049>Pay enough attention to what you scout",
+    "<LOC loadingtip_0050>It is generally better to take battles you will efficiently win and avoid battles you will lose",
+    "<LOC loadingtip_0051>Taking the right battles and avoiding the wrong ones is generally especially important in air",
+    "<LOC loadingtip_0052>Don't play too zoomed in - there are more important things than perferct adjacency and base layout",
+    "<LOC loadingtip_0053>Generally, you should focus on macro more than micro, but still micro some",
+    "<LOC loadingtip_0054>Manual reclaim is generally not considered a good use of your time after the early game",
+    "<LOC loadingtip_0055>Generally try to avoid having idle units without a good reason, especially in the early game",
+    "<LOC loadingtip_0056>Setting up good hotkeys can help you save time to focus more on the game and less on your mouse",
+    "<LOC loadingtip_0057>Focus on doing the best you can and learning what you can do better instead of blaming others",
+    "<LOC loadingtip_0058>If scout enough, you should know what the enemy makes and can generally try to counter it",
+    "<LOC loadingtip_0059>If the enemy makes a bunch of land units, minimal AA and no planes, bombers could counter them",
+    "<LOC loadingtip_0060>If the enemy makes a bunch of PD, you can often go around it, outrange it, or out eco them",
+    "<LOC loadingtip_0061>If the enemy spams T1/T2 air, you can generally make interceptors and mobile flak (T2 AA)",
+    "<LOC loadingtip_0062>If the enemy eco's a lot more than you, you can generally attack them and take winning battles",
+    "<LOC loadingtip_0063>If the enemy turtles hard with minimal map control, you can generally out eco them and win later",
+    "<LOC loadingtip_0064>If the enemy acts super aggressively, you can try to bait them into battles that are good for you",
+    "<LOC loadingtip_0065>Scout, and try to counter what the enemy builds, how the enemy plays, and where the enemy goes",
+    "<LOC loadingtip_0066>You should generally have your engineers use attack move orders for reclaiming",
+    "<LOC loadingtip_0067>Manual reclaim can be useful during the very early game if you have nothing better to do",
+    "<LOC loadingtip_0068>Patrol is generally worse for reclaiming than attack move is",
+    "<LOC loadingtip_0069>Use T1 engineers with attack move orders to get the reclaim from dead unit wrecks from battles",
+    "<LOC loadingtip_0070>Fighting battles where you can get the reclaim and your enemies can't matters a lot",
+    "<LOC loadingtip_0071>A wreck from a dead unit can yield up to about 81% of the unit's mass cost",
+    "<LOC loadingtip_0072>It can be worthwhile to kill/reclaim your old low-tech units to have additional nass",
+    "<LOC loadingtip_0073>Think about value and opportunity costs (ie: would you rather have ~16 T1 tanks or 1 T2 mex?)",
+    "<LOC loadingtip_0074>Low tech units often do more damage per mass but have less health per mass compared to high tech",
+    "<LOC loadingtip_0075>Upgrading a factory costs mass that could've been put into units, so don't upgrade too early",
+    "<LOC loadingtip_0076>In team games, be a team player",
+    "<LOC loadingtip_0077>If you need help or expect to lose your lane soon, tell your team",
+    "<LOC loadingtip_0078>If you don't know what to do next, ask your team what to do",
+    "<LOC loadingtip_0079>If you have extra mass and can't spend it fast enough, you can gift it to a teammate who can",
+    "<LOC loadingtip_0080>If your opponent is attacking your ally, help your ally or be doing something worth the cost",
+    "<LOC loadingtip_0081>If you notice an enemy T4/nuke/etc that your team hasn't noticed, ping/mark it and say something",
+    "<LOC loadingtip_0082>Try to coordinate your units with your allies' units when it seems worthwhile",
+    "<LOC loadingtip_0083>You may want to try UI mods like NoShake, Advanced Target Priorities 1.1, Rheclaim, etc",
+    "<LOC loadingtip_0085>You can search on the vault for numerous UI mods to try, like HotBuild Overhaul or icon mods",
+    "<LOC loadingtip_0086>Watch replays of your games to learn what different players did well/poorly to help you improve",
+    "<LOC loadingtip_0087>You can ask for advice in the gameplay-and-training channel on the FAF Discord",
+    "<LOC loadingtip_0088>The FAF wiki has useful guides that can teach you how to get better at FAF",
+    "<LOC loadingtip_0089>Scale your economy (mass, energy, & build power) over time",
+    "<LOC loadingtip_0090>Spamming units is often worthwhile, but don't forget to transition to higher tech units",
+    "<LOC loadingtip_0091>You can generally spam interceptors against t1 and t2 air, and spam ASF against t3 air",
+    "<LOC loadingtip_0092>Avoid stalling energy (don't get to 0 energy storage with a negative net energy income)",
+    "<LOC loadingtip_0093>Spend your mass (it's generally inefficient to keep a lot of mass in storage instead of using it)",
+    "<LOC loadingtip_0094>Avoid building too much energy (put your resources into things that will be the most useful)",
+    "<LOC loadingtip_0095>If you are stalling mass, it might be a good idea to have more of your engineers reclaiming",
+    "<LOC loadingtip_0096>It's generally a good idea to expand around the map early on to get mexes, reclaim and map control",
 }
