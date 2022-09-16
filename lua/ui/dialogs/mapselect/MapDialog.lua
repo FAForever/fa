8d083e7e63af69bf48a3ed1dbb22804216dd78e5
Jip <w.b.wijnia@gmail.com>
2022-09-16 22:38:05 +0200

    Define UI classes

---
+++ lua/ui/dialogs/mapselect/MapDialog.lua
@@ -0,0 +1,14 @@
+
+local LayoutHelpers = import('/lua/maui/layouthelpers.lua')
+local UIUtil = import('/lua/ui/uiutil.lua')
+
+local Group = import('/lua/maui/group.lua').Group
+
+---@class MapDialog : Group
+MapDialog = Class(Group) {
+
+    __init = function(self, parent)
+
+    end,
+
+}
---
+++ lua/ui/dialogs/mapselect/MapPreview.lua
@@ -0,0 +1,14 @@
+
+local LayoutHelpers = import('/lua/maui/layouthelpers.lua')
+local UIUtil = import('/lua/ui/uiutil.lua')
+
+local Group = import('/lua/maui/group.lua').Group
+
+---@class MapPreview : Group
+MapPreview = Class(Group) {
+
+    __init = function(self, parent)
+
+    end,
+
+}
