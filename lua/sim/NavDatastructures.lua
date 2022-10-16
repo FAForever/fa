d4dcdbeeab5ff653a4e739ef1c9774f67bfe55c0
Jip <w.b.wijnia@gmail.com>
2022-10-16 17:17:31 +0200

    Remove semicolons

--- lua/sim/NavDatastructures.lua
+++ lua/sim/NavDatastructures.lua
@@ -45,37 +45,37 @@ NavPathToHeap = ClassSimple {
     ExtractMin = function(self)
         -- if the Heap is empty, we got nothing to return!
         if self.HeapSize == 0 then
-            return nil;
+            return nil
         end
 
         -- keep a reference to the top value.
-        local value = self.Heap[1];
+        local value = self.Heap[1]
 
         -- put our highest value at the top.
-        self.Heap[1] = self.Heap[self.HeapSize];
-        self.Heap[self.HeapSize] = nil;
-        self.HeapSize = self.HeapSize - 1;
+        self.Heap[1] = self.Heap[self.HeapSize]
+        self.Heap[self.HeapSize] = nil
+        self.HeapSize = self.HeapSize - 1
 
         -- fix its position.
-        self:Heapify(1);
+        self:Heapify(1)
 
-        return value;
+        return value
     end,
 
     Heapify = function(self, index)
         -- find the left / right child and the parent.
-        local parent = index;
-        local left = self:ToLeftChild(index);
-        local right = self:ToRightChild(index);
+        local parent = index
+        local left = self:ToLeftChild(index)
+        local right = self:ToRightChild(index)
 
         -- find the best of the two, we assume the left child is.
-        local min = left;
+        local min = left
 
         -- if there is a right child, then there always has to be a left child. Hence, we can now assume there's both a right and a left child.
         -- compare the two: if right is smaller, then assign min = right. Else, keep min on left.
         if self.Heap[right] then
             if (self.compare(self.Heap[right])) < (self.compare(self.Heap[left])) then
-                min = right;
+                min = right
             end
         end
 
@@ -84,55 +84,55 @@ NavPathToHeap = ClassSimple {
         if self.Heap[min] then
             if (self.compare(self.Heap[min])) < (self.compare(self.Heap[parent])) then
                 -- swap the two values.
-                self:Swap(parent, min);
+                self:Swap(parent, min)
 
                 -- check if the (parent) value that is now at the child position is at the correct position within the Heap.
-                self:Heapify(min);
+                self:Heapify(min)
             end
         end
     end,
 
     Rootify = function(self, index)
-        local parent = self:ToParent(index);
-        local current = index;
+        local parent = self:ToParent(index)
+        local current = index
     
         if (current == 1) or self.compare(self.Heap[parent]) < self.compare(self.Heap[current]) then
-            return;
+            return
         end
     
-        self:Swap(parent, current);
-        self:Rootify(parent);
+        self:Swap(parent, current)
+        self:Rootify(parent)
     end,
 
     Swap = function(self, a, b)
-        local l = self.Heap[a];
-        self.Heap[a] = self.Heap[b];
-        self.Heap[b] = l;
+        local l = self.Heap[a]
+        self.Heap[a] = self.Heap[b]
+        self.Heap[b] = l
     end,
 
     Insert = function(self, element)
         if element then
-            self.HeapSize = self.HeapSize + 1;
-            self.Heap[self.HeapSize] = element;
-            self:Rootify(self.HeapSize);
+            self.HeapSize = self.HeapSize + 1
+            self.Heap[self.HeapSize] = element
+            self:Rootify(self.HeapSize)
         else
-            WARN("given object to Heap was nil!");
+            WARN("given object to Heap was nil!")
         end
     end,
 
     ToParent = function(self, index)
         -- index / 2
-        return index >> 1;
+        return index >> 1
     end,
 
     ToRightChild = function(self, index)
         -- 2 * index + 1
-        return (index << 1) | 1;
+        return (index << 1) | 1
     end,
 
     ToLeftChild = function (self, index)
         -- 2 * index
-        return index << 1;
+        return index << 1
     end,
 }
 
