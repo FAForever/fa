5fcfabaf4de88bce18cb7c8154acc335f57750cf
Jip <w.b.wijnia@gmail.com>
2022-10-16 17:28:34 +0200

    Improve annotations on heap, remove bit-wise shifts and or

--- lua/sim/NavDatastructures.lua
+++ lua/sim/NavDatastructures.lua
@@ -33,15 +33,20 @@
 ---@field HeapSize number
 NavPathToHeap = ClassSimple {
 
+    ---@param self NavPathToHeap
     __init = function(self)
         self.Heap = { }
         self.HeapSize = 0
     end,
 
+    ---@param self NavPathToHeap
+    ---@return boolean
     IsEmpty = function(self)
         return self.HeapSize == 0
     end,
 
+    ---@param self NavPathToHeap
+    ---@return NavPathToNode?
     ExtractMin = function(self)
         -- if the Heap is empty, we got nothing to return!
         if self.HeapSize == 0 then
@@ -62,6 +67,8 @@ NavPathToHeap = ClassSimple {
         return value
     end,
 
+    ---@param self NavPathToHeap
+    ---@param index number
     Heapify = function(self, index)
         -- find the left / right child and the parent.
         local parent = index
@@ -74,7 +81,7 @@ NavPathToHeap = ClassSimple {
         -- if there is a right child, then there always has to be a left child. Hence, we can now assume there's both a right and a left child.
         -- compare the two: if right is smaller, then assign min = right. Else, keep min on left.
         if self.Heap[right] then
-            if (self.compare(self.Heap[right])) < (self.compare(self.Heap[left])) then
+            if (self.Heap[right].AcquiredCosts + self.Heap[right].ExpectedCosts) < (self.Heap[left].AcquiredCosts + self.Heap[left].ExpectedCosts) then
                 min = right
             end
         end
@@ -82,7 +89,7 @@ NavPathToHeap = ClassSimple {
         -- if there is a child, compare the lowest child (is given due to code above) with our parent. 
         -- If it's smaller, switch the parent and the lowest child.
         if self.Heap[min] then
-            if (self.compare(self.Heap[min])) < (self.compare(self.Heap[parent])) then
+            if (self.Heap[min].AcquiredCosts + self.Heap[min].ExpectedCosts ) < (self.Heap[parent].AcquiredCosts + self.Heap[parent].ExpectedCosts ) then
                 -- swap the two values.
                 self:Swap(parent, min)
 
@@ -92,24 +99,35 @@ NavPathToHeap = ClassSimple {
         end
     end,
 
+    ---@param self NavPathToHeap
+    ---@param index number
     Rootify = function(self, index)
         local parent = self:ToParent(index)
-        local current = index
     
-        if (current == 1) or self.compare(self.Heap[parent]) < self.compare(self.Heap[current]) then
+        if  -- if we're the root
+            (index == 1) or
+
+            -- or we're at the correct place, cost-wise
+            self.Heap[parent].AcquiredCosts + self.Heap[parent].ExpectedCosts < self.Heap[index].AcquiredCosts + self.Heap[index].ExpectedCosts 
+        then
             return
         end
     
-        self:Swap(parent, current)
+        self:Swap(parent, index)
         self:Rootify(parent)
     end,
 
+    ---@param self NavPathToHeap
+    ---@param a number
+    ---@param b number
     Swap = function(self, a, b)
         local l = self.Heap[a]
         self.Heap[a] = self.Heap[b]
         self.Heap[b] = l
     end,
 
+    ---@param self NavPathToHeap
+    ---@param element NavPathToNode
     Insert = function(self, element)
         if element then
             self.HeapSize = self.HeapSize + 1
@@ -120,19 +138,28 @@ NavPathToHeap = ClassSimple {
         end
     end,
 
+    ---@param self NavPathToHeap
+    ---@param index number
+    ---@return number
     ToParent = function(self, index)
         -- index / 2
-        return index >> 1
+        return 0.5 * index
     end,
 
+    ---@param self NavPathToHeap
+    ---@param index number
+    ---@return number
     ToRightChild = function(self, index)
         -- 2 * index + 1
-        return (index << 1) | 1
+        return 2 * index + 1
     end,
 
+    ---@param self NavPathToHeap
+    ---@param index number
+    ---@return number
     ToLeftChild = function (self, index)
         -- 2 * index
-        return index << 1
+        return 2 * index
     end,
 }
 
