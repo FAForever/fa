c10de325e157ae1f170a9547755e62213aafa0a0
G C <37224614+Hdt80bro@users.noreply.github.com>
2022-10-26 23:16:59 -0700

    Pull color testing into separate PR

--- tests/test_class.lua
+++ tests/test_class.lua
@@ -1,8 +1,8 @@
 -- Test framework
-local lust = require "lust"
+local luft = require "luft"
 
 table.empty = function(t)
-  return next(t) == nil 
+    return next(t) == nil
 end
 
 -- Functions are imported to the global scope...
@@ -10,45 +10,36 @@ require "../lua/system/class.lua"
 
 -- Test data
 local entity_methods = {
-  GetPosition = function() end,
-  GetBlueprint = function() end,
-  GetOrientation = function() end,
-  Destroy = function() end,
-  SetMesh = function() end,
-  GetArmy = function() end,
+    GetPosition = function() end;
+    GetBlueprint = function() end;
+    GetOrientation = function() end;
+    Destroy = function() end;
+    SetMesh = function() end;
+    GetArmy = function() end;
 }
 
 local unit_methods = {
-  RevertElevation = function() end,
-  ShowBone = function() end,
-  HideBone = function() end,
-  GetCurrentLayer = function() end,
+    RevertElevation = function() end;
+    ShowBone = function() end;
+    HideBone = function() end;
+    GetCurrentLayer = function() end;
 }
 
-local Exclusions = { 
-  __index = true,
-  n = true,
+local Exclusions = {
+    __index = true,
+    n = true,
 }
 
 -- convert the 'c class', we boldy assume this works :)
 ConvertCClassToLuaSimplifiedClass(entity_methods)
 ConvertCClassToLuaSimplifiedClass(unit_methods)
 
-lust.describe(
-  "Test class system", 
-  function()
-
-    lust.describe(
-      "Basic class tests", 
-      function()
-
-        lust.it(
-          "Basic inheritance", 
-          function()
-
+luft.describe("Test class system", function()
+    luft.describe("Basic class tests", function()
+        luft.test("Basic inheritance", function()
             local Specs = {
-              OnCreate = function() end,
-              OnDestroy = function() end,
+                OnCreate = function() end;
+                OnDestroy = function() end;
             }
 
             -- create an entity class
@@ -58,126 +49,111 @@ lust.describe(
             local instance = Entity()
 
             -- confirm the functions are inherited properly from the specifications
-            for k, v in Specs do 
-              if not Exclusions[k] then 
-                lust.expect(tostring(instance[k])).to.equal(tostring(Entity[k]))
-              end
+            for k in pairs(Specs) do
+                if not Exclusions[k] then
+                    luft.expect(tostring(instance[k])).to.equal(tostring(Entity[k]))
+                end
             end
 
             -- confirm the functions are inherited properly from the entity methods
-            for k, v in entity_methods do 
-              if not Exclusions[k] then 
-                lust.expect(tostring(instance[k])).to.equal(tostring(entity_methods[k]))
-              end
+            for k in pairs(entity_methods) do
+                if not Exclusions[k] then
+                    luft.expect(tostring(instance[k])).to.equal(tostring(entity_methods[k]))
+                end
             end
 
             -- check the meta table
-            lust.expect(tostring(getmetatable(instance))).to.equal(tostring(Entity))
-          end
-        )
+            luft.expect(tostring(getmetatable(instance))).to.equal(tostring(Entity))
+        end)
 
-        lust.it(
-          "Basic dual inheritance", 
-          function()
-
+        luft.test("Basic dual inheritance", function()
             local Specs = {
-              OnCreate = function() end,
-              OnDestroy = function() end,
+                OnCreate = function() end;
+                OnDestroy = function() end;
             }
 
             -- create an entity class
-            local Entity = Class (entity_methods, unit_methods) (Specs)
+            local Entity = Class(entity_methods, unit_methods)(Specs)
 
             -- make an instance
             local instance = Entity()
 
             -- confirm the functions are inherited properly from the specifications
-            for k, v in Specs do 
-              if not Exclusions[k] then 
-                lust.expect(tostring(instance[k])).to.equal(tostring(Entity[k]))
-              end
+            for k in pairs(Specs) do
+                if not Exclusions[k] then
+                    luft.expect(tostring(instance[k])).to.equal(tostring(Entity[k]))
+                end
             end
 
             -- confirm the functions are inherited properly from the entity methods
-            for k, v in entity_methods do 
-              if not Exclusions[k] then 
-                lust.expect(tostring(instance[k])).to.equal(tostring(entity_methods[k]))
-              end
+            for k in pairs(entity_methods) do
+                if not Exclusions[k] then
+                    luft.expect(tostring(instance[k])).to.equal(tostring(entity_methods[k]))
+                end
             end
 
             -- confirm the functions are inherited properly from the entity methods
-            for k, v in unit_methods do 
-              if not Exclusions[k] then 
-                lust.expect(tostring(instance[k])).to.equal(tostring(unit_methods[k]))
-              end
+            for k in pairs(unit_methods) do
+                if not Exclusions[k] then
+                    luft.expect(tostring(instance[k])).to.equal(tostring(unit_methods[k]))
+                end
             end
 
             -- check the meta table
-            lust.expect(tostring(getmetatable(instance))).to.equal(tostring(Entity))
-          end
-        )
+            luft.expect(tostring(getmetatable(instance))).to.equal(tostring(Entity))
+        end)
 
-        lust.it(
-          "Basic override", 
-          function()
-
+        luft.test("Basic override", function()
             local Specs = {
-              GetPosition = function() end,
-              OnCreate = function() end,
-              OnDestroy = function() end,
+                GetPosition = function() end;
+                OnCreate = function() end;
+                OnDestroy = function() end;
             }
 
             -- create an entity class
-            local Entity = Class (entity_methods) (Specs)
+            local Entity = Class(entity_methods)(Specs)
 
             -- make an instance
             local instance = Entity()
 
             -- confirm the specifications are inherited properly
-            for k, v in Specs do 
-              if not Exclusions[k] then 
-                lust.expect(tostring(instance[k])).to.equal(tostring(Entity[k]))
-              end
+            for k in pairs(Specs) do
+                if not Exclusions[k] then
+                    luft.expect(tostring(instance[k])).to.equal(tostring(Entity[k]))
+                end
             end
 
             -- confirm that one function is overridden by the specificiation
-            lust.expect(tostring(instance.GetPosition)).to_not.equal(tostring(entity_methods.GetPosition))
-          end
-        )
+            luft.expect(tostring(instance.GetPosition)).to_not.equal(tostring(entity_methods.GetPosition))
+        end)
 
-        lust.it(
-          "Basic self value", 
-          function()
-
+        luft.test("Basic self value", function()
             local Specs = {
-              OnCreate = function() end,
-              OnDestroy = function() end,
+                OnCreate = function() end;
+                OnDestroy = function() end;
             }
 
             -- create an entity class
-            local Entity = Class (entity_methods) (Specs)
+            local Entity = Class(entity_methods)(Specs)
 
             -- make two instances with their own data set
             local instanceA = Entity()
-            instanceA.Bob = true 
-            instanceA.Charlie = true 
-            instanceA.Delta = true 
+            instanceA.Bob = true
+            instanceA.Charlie = true
+            instanceA.Delta = true
 
             local instanceB = Entity()
-            instanceB.Bob = false 
-            instanceB.Charlie = false 
-            instanceB.Delta = false 
+            instanceB.Bob = false
+            instanceB.Charlie = false
+            instanceB.Delta = false
 
             -- confirm the changes of each instance are local
-            for k, v in instanceA do 
-                lust.expect(instanceA[k]).to_not.equal(instanceB[k])
+            for k in pairs(instanceA) do
+                luft.expect(instanceA[k]).to_not.equal(instanceB[k])
             end
-          end
-        )
-      end
-    )
-  end
-)
+        end)
+    end)
+end)
 
 -- Make sure to call finish so that any errors will fail the CI!
-lust.finish()
+luft.finish()
--- tests/test_stringmatch.lua
+++ tests/test_stringmatch.lua
@@ -1,85 +1,68 @@
-local lust = require"lust"
+local luft = require "luft"
 
-require"../lua/system/utils.lua"
+require "../lua/system/utils.lua"
 
-lust.describe("test string.match and string.gmatch", function()
-    local test='The quick brown FOX745 JUMPS over the lazy doge.'
+luft.describe("test string.match and string.gmatch", function()
+    local test = "The quick brown FOX745 JUMPS over the lazy doge."
 
-    lust.it('matches exact string start', function()
-        lust.expect(test:match'^The').to.equal'The'
-        lust.expect(test:match'^quick').to.equal(nil)
+    luft.test("matches exact string start", function()
+        luft.expect(test:match("^The")).to.equal "The"
+        luft.expect(test:match("^quick")).to.be_nil()
     end)
 
-    lust.it("matches exact offset string start", function()
-        lust.expect(test:match('^quick',5)).to.equal'quick'
+    luft.test("matches exact offset string start", function()
+        luft.expect(test:match("^quick", 5)).to.equal "quick"
     end)
 
-    lust.it("matches end of string capture", function()
-        lust.expect(test:match'd(og)e%.$').to.equal'og'
-        lust.expect(test:match'lazy$').to.equal(nil)
+    luft.test("matches end of string capture", function()
+        luft.expect(test:match("d(og)e%.$")).to.equal "og"
+        luft.expect(test:match("lazy$")).to.be_nil()
     end)
 
-    lust.it("matches regex captures", function()
-        lust.expect(test:match'%d').to.equal'7'
-        lust.expect(test:match'%u+%d+').to.equal'FOX745'
+    luft.test("matches regex captures", function()
+        luft.expect(test:match("%d")).to.equal "7"
+        luft.expect(test:match("%u+%d+")).to.equal "FOX745"
     end)
 
-    lust.it("matches multiple captures", function()
-        local a,b,c = test:match'(%u+) (%l+)'
-        lust.expect(a).to.equal'JUMPS'
-        lust.expect(b).to.equal'over'
-        lust.expect(c).to.equal(nil)
+    luft.test("matches multiple captures", function()
+        luft.expect(test:match("(%u+) (%l+)")).to.equal("JUMPS", "over")
     end)
 
-    local loctest = '<LOC arbitrary_loc_tag>The quick brown test string.'
-    local balancetest = '< < > ><> <.>'
+    local loctest = "<LOC arbitrary_loc_tag>The quick brown test string."
+    local balancetest = "< < > ><> <.>"
 
-    lust.it("matches balanced regex", function()
-        lust.expect(loctest:match'%b<>').to.equal'<LOC arbitrary_loc_tag>'
-        lust.expect(balancetest:match'%b<>').to.equal'< < > >'
-        lust.expect(balancetest:match'%b><').to.equal'><'
+    luft.test("matches balanced regex", function()
+        luft.expect(loctest:match("%b<>")).to.equal "<LOC arbitrary_loc_tag>"
+        luft.expect(balancetest:match("%b<>")).to.equal "< < > >"
+        luft.expect(balancetest:match("%b><")).to.equal "><"
     end)
 
-    lust.it("matches loc tag", function()
-        lust.expect(loctest:match'<LOC ([^>]+)>').to.equal'arbitrary_loc_tag'
-        lust.expect(loctest:match'<LOC [^>]+>(.*)').to.equal'The quick brown test string.'
-        local a,b,c = loctest:match'<LOC ([^>]+)>(.*)'
-        lust.expect(a).to.equal'arbitrary_loc_tag'
-        lust.expect(b).to.equal'The quick brown test string.'
-        lust.expect(c).to.equal(nil)
+    luft.test("matches loc tag", function()
+        luft.expect(loctest:match("<LOC ([^>]+)>")).to.equal "arbitrary_loc_tag"
+        luft.expect(loctest:match("<LOC [^>]+>(.*)")).to.equal "The quick brown test string."
+        luft.expect(loctest:match("<LOC ([^>]+)>(.*)"))
+            .to.equal("arbitrary_loc_tag", "The quick brown test string.")
     end)
 
-    lust.it("gmatch iterates over word pairs correctly", function()
-        local iterator = test:gmatch'[^ ]+ [^ ]+'
-        lust.expect(iterator).to.be.a'function'
-        lust.expect(iterator()).to.equal'The quick'
-        lust.expect(iterator()).to.equal'brown FOX745'
-        lust.expect(iterator()).to.equal'JUMPS over'
-        lust.expect(iterator()).to.equal'the lazy'
-        lust.expect(iterator()).to.equal(nil)
+    luft.test("gmatch iterates over word pairs correctly", function()
+        local iterator = test:gmatch("[^ ]+ [^ ]+")
+        luft.expect(iterator).to.be.a "function"
+        luft.expect(iterator()).to.equal "The quick"
+        luft.expect(iterator()).to.equal "brown FOX745"
+        luft.expect(iterator()).to.equal "JUMPS over"
+        luft.expect(iterator()).to.equal "the lazy"
+        luft.expect(iterator()).to.be_nil()
     end)
 
-    lust.it("gmatch correctly returns multiple arguments", function()
-        local iterator = test:gmatch'([^ ]+) ([^ ]+)'
-        lust.expect(iterator).to.be.a'function'
-        local a,b,c = iterator()
-        lust.expect(a).to.equal'The'
-        lust.expect(b).to.equal'quick'
-        lust.expect(c).to.equal(nil)
-        a,b,c = iterator()
-        lust.expect(a).to.equal'brown'
-        lust.expect(b).to.equal'FOX745'
-        lust.expect(c).to.equal(nil)
-        a,b,c = iterator()
-        lust.expect(a).to.equal'JUMPS'
-        lust.expect(b).to.equal'over'
-        lust.expect(c).to.equal(nil)
-        a,b,c = iterator()
-        lust.expect(a).to.equal'the'
-        lust.expect(b).to.equal'lazy'
-        lust.expect(c).to.equal(nil)
-        lust.expect(iterator()).to.equal(nil)
+    luft.test("gmatch correctly returns multiple arguments", function()
+        local iterator = test:gmatch("([^ ]+) ([^ ]+)")
+        luft.expect(iterator).to.be.a "function"
+        luft.expect(iterator()).to.equal("The", "quick")
+        luft.expect(iterator()).to.equal("brown", "FOX745")
+        luft.expect(iterator()).to.equal("JUMPS", "over")
+        luft.expect(iterator()).to.equal("the", "lazy")
+        luft.expect(iterator()).to.be_nil()
     end)
 end)
 
-lust.finish()
+luft.finish()
