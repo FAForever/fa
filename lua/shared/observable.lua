ac256d513175eb86e95bdb21a108fcaf8421ee99
4z0t <gastec139@gmail.com>
2022-11-14 20:36:58 +0300

    annotate observable

--- lua/shared/observable.lua
+++ lua/shared/observable.lua
@@ -1,4 +1,3 @@
-
 -- implementation of the observable pattern as described on:
 --  - https://en.wikipedia.org/wiki/Observer_pattern
 
@@ -6,28 +5,27 @@
 local TableInsert = table.insert
 
 -- setup for a basic meta table
+---@class Observable
+---@field Listeners function[]
 local ObservableMeta = {}
 ObservableMeta.__index = ObservableMeta
 
 --- Adds an observer that is updated when the value is subject is set.
--- @param callback A function that receives the value as its first argument.
+---@param callback function function that receives the value as its first argument.
 function ObservableMeta:AddObserver(callback)
     TableInsert(self.Listeners, callback)
 end
 
---- Sets the value of the subject and notifies all observers with the updated value.
+---Sets the value of the subject and notifies all observers with the updated value.
+---@param value any
 function ObservableMeta:Set(value)
-    for k, callback in self.Listeners do 
+    for _, callback in self.Listeners do
         callback(value)
     end
 end
 
 --- Constructs an observable as described by the observable pattern
+---@return Observable
 function Create()
-    local observable = {}
-    setmetatable(observable, ObservableMeta)
-
-    observable.Listeners = { }
-
-    return observable
+    return setmetatable({ Listeners = {} }, ObservableMeta)
 end
