b612f9068bd8a5960fcc4671b9a48d139be62413
main <creativename1111@gmail.com>
2022-11-20 18:10:15 -0500

    Fix conflict?

--- units/URA0304/URA0304_Script.lua
+++ units/URA0304/URA0304_Script.lua
@@ -2,7 +2,7 @@
 --  File     :  /cdimage/units/URA0304/URA0304_script.lua
 --  Author(s):  John Comes, David Tomandl
 --  Summary  :  Cybran Strategic Bomber Script
---  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+--  Copyright Â© 2005 Gas Powered Games, Inc.  All rights reserved.
 -------------------------------------------------------------------
 
 local CAirUnit = import('/lua/cybranunits.lua').CAirUnit
