44b0837347ae1f02d8cb634c4d4d2b82e607eeea
4z0t <gastec139@gmail.com>
2022-12-03 16:40:02 +0300

    add lazyimport

--- lua/system/import.lua
+++ lua/system/import.lua
@@ -35,63 +35,25 @@ __module_metatable = {
 -- these values can be adjusted by hooking into this file
 local informDevOfLoad = false
 
---- The global import function used to keep track of modules
----@param name FileName path to the module to load
----@return Module
-function import(name)
-    local modules = __modules -- global to local
 
-    -- attempt to find the module without lowering the string
-    local existing = modules[name]
-    if existing then
-        return existing
-    end
+local function LoadModule(module)
+    local modules = __modules
 
-    -- caching: if it exists then we return the previous version
-    name = name:lower()
-    existing = modules[name]
-    if existing then
-        return existing
-    end
+    local moduleinfo = module.__moduleinfo
+    local name = moduleinfo.name
 
     -- inform the devs that we're loading this module for the first time
     if informDevOfLoad then
         SPEW("Loading module '", name, "'")
     end
 
-    ---@type ModuleInfo
-    local moduleinfo = {
-        name = name,
-        used_by = {},
-        track_imports = true,
-    }
-    -- make any old data available to the new one while it reloads
-    local oldMod = oldModules[name]
-    if oldMod then
-        moduleinfo.old = oldMod
-    end
+     -- make any old data available to the new one while it reloads
+     local oldMod = oldModules[name]
+     if oldMod then
+         moduleinfo.old = oldMod
+     end
 
-    -- set up an environment for the new module
-    ---@type Module
-    local module
-    module = {
-        __moduleinfo = moduleinfo,
-
-        -- Define a new 'import' function customized for the module, to track import dependencies
-        import = function(name2)
-            if name2:sub(1, 1) != '/' then
-                name2 = FileCollapsePath(name .. '/../' .. name2)
-            end
-            local module2 = import(name2) -- this will use the global import
-            if __modules[name].__moduleinfo.track_imports then
-                module2.__moduleinfo.used_by[name] = true
-            end
-            return module2
-        end,
-    }
-
-    -- set the meta table so that if it can't find an index it searches in _G
-    setmetatable(module, __module_metatable)
+     setmetatable(module, __module_metatable)
 
     -- add ourselves to prevent loops
     modules[name] = module
@@ -115,13 +77,93 @@ function import(name)
         error("Error importing '" .. name .. "'", 2)
     end
 
-    -- Once we've imported successfully, stop tracking dependencies. This means that importing from
+      -- Once we've imported successfully, stop tracking dependencies. This means that importing from
     -- within a function will not create a dependency, which is usually what you want. (You can
     -- explicitly set __moduleinfo.track_imports = true to switch tracking back on.)
     moduleinfo.track_imports = false
     return module
+
 end
 
+
+local __lazyimport_metatable = {
+    __index = function(tbl, key)
+        LoadModule(tbl)
+        return tbl[key]
+    end,
+
+    __newindex = function(tbl, key, val)
+        LoadModule(tbl)
+         tbl[key]=val
+    end,
+}
+
+
+--- The global import function used to keep track of modules
+---@param name FileName path to the module to load
+---@param isLazy boolean?
+---@return Module
+function import(name, isLazy)
+    local modules = __modules -- global to local
+
+    -- attempt to find the module without lowering the string
+    local existing = modules[name]
+    if existing then
+        return existing
+    end
+
+    -- caching: if it exists then we return the previous version
+    name = name:lower()
+    existing = modules[name]
+    if existing then
+        return existing
+    end
+    ---@type ModuleInfo
+    local moduleinfo = {
+        name = name,
+        used_by = {},
+        track_imports = true,
+    }
+    -- set up an environment for the new module
+    ---@type Module
+    local module
+    module = {
+        __moduleinfo = moduleinfo,
+        lazyimport = function(name2)
+            if name2:sub(1, 1) != '/' then
+                name2 = FileCollapsePath(name .. '/../' .. name2)
+            end
+            local module2 = import(name2, true) -- this will use the global import
+            if __modules[name].__moduleinfo.track_imports then
+                module2.__moduleinfo.used_by[name] = true
+            end
+            return module2
+        end,
+        -- Define a new 'import' function customized for the module, to track import dependencies
+        import = function(name2)
+            if name2:sub(1, 1) != '/' then
+                name2 = FileCollapsePath(name .. '/../' .. name2)
+            end
+            local module2 = import(name2) -- this will use the global import
+            if __modules[name].__moduleinfo.track_imports then
+                module2.__moduleinfo.used_by[name] = true
+            end
+            return module2
+        end,
+    }
+    if isLazy then
+        setmetatable(module, __lazyimport_metatable)
+    else
+        LoadModule(module)
+    end
+    
+    return module
+end
+
+
+function lazyimport(name)
+    return import(name, true)
+end
 
 -- Clear out a module from the table of loaded modules, so that on the next import attempt it will
 -- get reloaded from scratch
