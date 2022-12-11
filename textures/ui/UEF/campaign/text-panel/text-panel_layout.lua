00ce702f789dd357ba60293810abf3d65220e2bb
speed2CZ <odstrcilpetr@seznam.cz>
2022-12-11 11:44:03 +0100

    Add vanilla briefings textures

---
+++ textures/ui/UEF/campaign/text-panel/text-panel_layout.lua
@@ -0,0 +1,11 @@
+layout = {
+	['text-panel_brd_lr'] = {left = 50, top = 96, width = 64, height = 32, },
+	['text-panel_brd_lm'] = {left = 40, top = 96, width = 8, height = 32, },
+	['text-panel_brd_ll'] = {left = 9, top = 96, width = 32, height = 32, },
+	['text-panel_brd_vert_r'] = {left = 50, top = 40, width = 64, height = 8, },
+	['text-panel_brd_m'] = {left = 40, top = 40, width = 8, height = 8, },
+	['text-panel_brd_vert_l'] = {left = 9, top = 40, width = 32, height = 8, },
+	['text-panel_brd_ur'] = {left = 50, top = 9, width = 64, height = 32, },
+	['text-panel_brd_horz_um'] = {left = 40, top = 9, width = 8, height = 32, },
+	['text-panel_brd_ul'] = {left = 9, top = 9, width = 32, height = 32, },
+}
