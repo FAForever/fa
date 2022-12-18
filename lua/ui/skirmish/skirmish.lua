ea22b6b735bb22e73178bee0d7e7d66b79b54a48
Ivan <v23620@gmail.com>
2022-12-18 16:11:58 +0300

    fix typo

--- lua/ui/skirmish/skirmish.lua
+++ lua/ui/skirmish/skirmish.lua
@@ -1,12 +1,12 @@
 function openOfflineLobby(mapFolderName)
     local playerName = import("/lua/user/prefs.lua").GetCurrentProfile().Name or "Unknown"
-    local scenarioFilePath = creteScenarioFilePath(mapFolderName)
+    local scenarioFilePath = createScenarioFilePath(mapFolderName)
     local lobby = import("/lua/ui/lobby/lobby.lua")
     lobby.CreateLobby('None', 0, playerName, nil, nil, GetFrame(0), function() ExitApplication() end)
     lobby.HostGame(playerName .. "'s Skirmish", scenarioFilePath, true)
 end
 
-function creteScenarioFilePath(mapFolderName)
+function createScenarioFilePath(mapFolderName)
     return "/maps/" .. mapFolderName .. "/" .. removeVersionNumber(mapFolderName) .. "_scenario.lua"
 end
 
