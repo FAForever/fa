bcfb6c77badd59866c03487baefcba9819734805
Jip <w.b.wijnia@gmail.com>
2022-12-25 21:30:35 +0100

    Prepare changelog

--- lua/version.lua
+++ lua/version.lua
@@ -1,6 +1,6 @@
-local Version = "3748"
----@alias PATCH "3748"
----@alias VERSION "1.5.3748"
+local Version = "3749"
+---@alias PATCH "3749"
+---@alias VERSION "1.5.3749"
 ---@return PATCH
 function GetVersion()
     LOG('Supreme Commander: Forged Alliance version ' .. Version)
