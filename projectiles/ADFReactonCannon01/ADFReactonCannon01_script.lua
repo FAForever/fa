461929c4a61ae76d6b23171b508f9561927792bc
Josh <row.josh02@gmail.com>
2022-12-29 18:48:48 +0000

    Refactor projectiles part 1 (#4528)
    * pass 1
    
    * mire files
    
    * pass 2
    
    * Pass 3
    
    * Fix File based on review
    
    * More File Chages
    
    Updated based on feedback from second PR
    
    * Add base function to OnEnterWater

--- projectiles/ADFReactonCannon01/ADFReactonCannon01_script.lua
+++ projectiles/ADFReactonCannon01/ADFReactonCannon01_script.lua
@@ -7,7 +7,7 @@
 local AReactonCannonProjectile = import("/lua/aeonprojectiles.lua").AReactonCannonProjectile
 ADFReactonCannon01 = Class(AReactonCannonProjectile) {
     CreateImpactEffects = function(self, army, EffectTable, EffectScale)
-        local launcher = self:GetLauncher()
+        local launcher = self.Launcher
         if launcher and launcher:HasEnhancement('StabilitySuppressant') then
             CreateLightParticle(self, -1, self.Army, 3.0, 6, 'ring_05', 'ramp_green_02')
             CreateEmitterAtEntity(self, self.Army,'/effects/emitters/oblivion_cannon_hit_11_emit.bp')
