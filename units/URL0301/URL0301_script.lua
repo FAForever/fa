2cab994a33f7ba8831108b7ce8189b204038f16b
TurinturambarFAF <konrad.schulz@posteo.net>
2023-01-23 16:47:35 +0100

    fixes

--- units/URL0301/URL0301_script.lua
+++ units/URL0301/URL0301_script.lua
@@ -136,7 +136,7 @@ URL0301 = Class(CCommandUnit) {
                     Stacks = 'ALWAYS',
                     Duration = -1,
                     Affects = {
-					    MaxHealth = {
+                        MaxHealth = {
                             Add = bp.NewHealth,
                             Mult = 1.0,
                         },
