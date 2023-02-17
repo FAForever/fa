92051570acd1d4869289f79ad5e282acae1af37f
Jip <w.b.wijnia@gmail.com>
2023-02-17 18:57:56 +0100

    Remove accidental change

--- lua/aibrain.lua
+++ lua/aibrain.lua
@@ -755,7 +755,6 @@ AIBrain = Class(moho.aibrain_methods) {
     ---@param reconType ReconTypes
     ---@param val boolean
     OnIntelChange = function(self, blip, reconType, val)
-        LOG(self.Army)
         if reconType == 'LOSNow' or reconType == 'Omni' then
             if not val then
                 local unit = blip:GetSource()
