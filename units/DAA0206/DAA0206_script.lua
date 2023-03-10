083b43dcb2e80a90eda93847f5995b5f0a7309ca
ageekhere <veggieman25@gmail.com>
2023-03-10 11:46:21 +1100

    moved Backwards Compatibility Imports to bottom
    moved Backwards Compatibility Imports to bottom

--- units/DAA0206/DAA0206_script.lua
+++ units/DAA0206/DAA0206_script.lua
@@ -4,20 +4,16 @@
 -- Summary : Aeon Guided Missile Script
 -- Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
 ------------------------------------------------------------------------------
-
 local AAirUnits = import("/lua/aeonunits.lua")
 local DefaultWeapons = import("/lua/sim/defaultweapons.lua")
--- Kept for Mod Backwards Compatablity
-local EffectTemplate = import('/lua/EffectTemplates.lua')
-local EffectUtils = import('/lua/effectutilities.lua')
-local weapon = ClassWeapon(DefaultWeapons.DefaultProjectileWeapon) {} --cached
+local weapon = ClassWeapon(DefaultWeapons.DefaultProjectileWeapon) {}
 
 local function f_OnRunOutOfFuel(self)
     self:Kill()
 end
 
 local function f_projectileFired(self)
-    local playSound = self.PlayUnitSound --maybe could be cached global?
+    local playSound = self.PlayUnitSound
     weapon.IdleState.Main = nil
     playSound(self, 'Killed')
     playSound(self, 'Destroyed')
@@ -36,3 +32,7 @@ local DAA0206 = ClassUnit(AAirUnits.AAirUnit) {
     ProjectileFired = f_projectileFired,
 }
 TypeClass = DAA0206
+
+-- Kept for Mod Backwards Compatibility 
+local EffectTemplate = import('/lua/EffectTemplates.lua')
+local EffectUtils = import('/lua/effectutilities.lua')
