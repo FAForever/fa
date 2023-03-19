8b37f4f77fa3da8840e34e66783b864c2fcd0aa9
Aaron Warner <aaronpaulwarner@gmail.com>
2023-03-20 10:42:28 +1300

    Remove Sorian Builders, basetemplates, localization strings, platoon functions, tooltips, some utility functions and behaviors.

--- lua/ui/help/tooltips.lua
+++ lua/ui/help/tooltips.lua
@@ -1169,54 +1169,6 @@ Tooltips = {
         title = "<LOC lobui_0611>Lobby Settings",
         description = "<LOC lobui_0612>Change your lobby background or chat font size.",
     },
-    aitype_sorian = {
-        title = "<LOC sorian_0001>AI: Sorian",
-        description = "<LOC sorian_0002>A balanced AI. Good for 20x20+ maps.",
-    },
-    aitype_sorianrush = {
-        title = "<LOC sorian_0003>AI: Sorian Rush",
-        description = "<LOC sorian_0004>A rush AI. Good for 10x10 or smaller maps.",
-    },
-    aitype_sorianair = {
-        title = "<LOC sorian_0005>AI: Sorian Air",
-        description = "<LOC sorian_0006>An AI that focuses on air. Good for 20x20+ maps.",
-    },
-    aitype_sorianwater = {
-        title = "<LOC sorian_0007>AI: Sorian Water",
-        description = "<LOC sorian_0008>An AI that focuses on water. Good for 20x20+ water maps.",
-    },
-    aitype_sorianturtle = {
-        title = "<LOC sorian_0009>AI: Sorian Turtle",
-        description = "<LOC sorian_0010>A turtle AI. Good for 20x20+ maps.",
-    },
-    aitype_sorianadaptive = {
-        title = "<LOC sorian_0011>AI: Sorian Adaptive",
-        description = "<LOC sorian_0012>Chooses a Sorian AI based on the map. Use this if you can\'t decide on an AI.",
-    },
-    aitype_soriancheat = {
-        title = "<LOC sorian_0013>AIx: Sorian Cheat",
-        description = "<LOC sorian_0014>A cheating balanced AI. Good for 20x20+ maps.",
-    },
-    aitype_sorianrushcheat = {
-        title = "<LOC sorian_0015>AIx: Sorian Rush Cheat",
-        description = "<LOC sorian_0016>A cheating rush AI. Good for 10x10 or smaller maps.",
-    },
-    aitype_sorianaircheat = {
-        title = "<LOC sorian_0017>AIx: Sorian Air Cheat",
-        description = "<LOC sorian_0018>A cheating AI that focuses on air. Good for 20x20+ maps.",
-    },
-    aitype_sorianwatercheat = {
-        title = "<LOC sorian_0019>AIx: Sorian Water Cheat",
-        description = "<LOC sorian_0020>A cheating AI that focuses on water. Good for 20x20+ water maps.",
-    },
-    aitype_sorianturtlecheat = {
-        title = "<LOC sorian_0021>AIx: Sorian Turtle Cheat",
-        description = "<LOC sorian_0022>A cheating turtle AI. Good for 20x20+ maps.",
-    },
-    aitype_sorianadaptivecheat = {
-        title = "<LOC sorian_0023>AIx: Sorian Adaptive Cheat",
-        description = "<LOC sorian_0024>Chooses a Sorian cheating AI based on the map. Use this if you can\'t decide on an AI.",
-    },
     num_games = {
         title = '<LOC lobui_0748>Number of Games',
         description = '<LOC lobui_0749>Number of games played in global ladder.',
