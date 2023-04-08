61c6a909a5e6df601169c8ed248cd22345376a4b
Jip <w.b.wijnia@gmail.com>
2023-04-08 19:22:19 +0200

    Fix typo in brain class

--- lua/aibrain.lua
+++ lua/aibrain.lua
@@ -378,7 +378,7 @@ local AIBrainEnergyComponent = ClassSimple {
         -- localize scope for better performance
         local pcall = pcall
         local TableSize = table.getsize
-        local CoroutineYield = CoroutineYield
+        local CoroutineYield = coroutine.yield
 
         local ok, msg
 
