f30ade39c47de26e116fb3e31189343ae4791e17
Josh <row.josh02@gmail.com>
2023-05-21 18:19:00 +0100

    Tidy up unit scripts pt. 6 (#4652)

--- units/URL0107/URL0107_script.lua
+++ units/URL0107/URL0107_script.lua
@@ -1,37 +1,35 @@
---****************************************************************************
---**
---**  File     :  /cdimage/units/URL0107/URL0107_script.lua
---**  Author(s):  John Comes, David Tomandl, Jessica St. Croix
---**
---**  Summary  :  Cybran Heavy Infantry Script
---**
---**  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
---****************************************************************************
-
+-- File     :  /cdimage/units/URL0107/URL0107_script.lua
+-- Author(s):  John Comes, David Tomandl, Jessica St. Croix
+-- Summary  :  Cybran Heavy Infantry Script
+-- Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
+-------------------------------------------------------------------
 local CWalkingLandUnit = import("/lua/cybranunits.lua").CWalkingLandUnit
 local CybranWeaponsFile = import("/lua/cybranweapons.lua")
 local CDFLaserHeavyWeapon = CybranWeaponsFile.CDFLaserHeavyWeapon
 local EffectUtil = import("/lua/effectutilities.lua")
 
+local EmptyTable = EmptyTable
 
 ---@class URL0107 : CWalkingLandUnit
+---@field BuildEffectsBag TrashBag
 URL0107 = ClassUnit(CWalkingLandUnit) {
     Weapons = {
         LaserArms = ClassWeapon(CDFLaserHeavyWeapon) {},
     },
 
+    ---@param self URL0107
     OnCreate = function(self)
         CWalkingLandUnit.OnCreate(self)
-        self.BuildEffectBones = self:GetBlueprint().General.BuildBones.BuildEffectBones
-        if self.BuildEffectBones then
-            self:SetupBuildBones()
-        end
+        self.BuildEffectBones = self.Blueprint.General.BuildBones.BuildEffectBones
     end,
 
-    CreateBuildEffects = function( self, unitBeingBuilt, order )
-       EffectUtil.CreateCybranBuildBeamsOpti( self, { }, unitBeingBuilt, self.BuildEffectsBag, false)
+    ---@param self URL0107
+    ---@param unitBeingBuilt Unit
+    ---@param order string
+    CreateBuildEffects = function(self, unitBeingBuilt, order)
+        EffectUtil.CreateCybranBuildBeamsOpti(self, EmptyTable, unitBeingBuilt, self.BuildEffectsBag, false)
     end,
-    
+
 }
 
 TypeClass = URL0107
