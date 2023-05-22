56436bae7ebf1244c5679f4303d449295ebea66f
Aaron Warner <aaronpaulwarner@gmail.com>
2023-05-22 13:18:26 +1200

    add support for conflict line usage in simple raid platoon.

--- lua/aibrains/index.lua
+++ lua/aibrains/index.lua
@@ -13,13 +13,13 @@ keyToBrain = {
     rush = import("/lua/aibrains/base-ai.lua").AIBrain,
     -- easy = import("/lua/aibrains/base-ai.lua").AIBrain,
     normal = import("/lua/aibrains/base-ai.lua").AIBrain,
-    adaptive = import("/lua/aibrains/base-ai.lua").AIBrain,
+    adaptive = import("/lua/aibrains/adaptive-ai.lua").AIBrain,
     random = import("/lua/aibrains/base-ai.lua").AIBrain,
 
     techcheat = import("/lua/aibrains/base-ai.lua").AIBrain,
     rushcheat = import("/lua/aibrains/base-ai.lua").AIBrain,
     easycheat = import("/lua/aibrains/base-ai.lua").AIBrain,
     normalcheat = import("/lua/aibrains/base-ai.lua").AIBrain,
-    adaptivecheat = import("/lua/aibrains/base-ai.lua").AIBrain,
+    adaptivecheat = import("/lua/aibrains/adaptive-ai.lua").AIBrain,
     randomcheat = import("/lua/aibrains/base-ai.lua").AIBrain,
 }
