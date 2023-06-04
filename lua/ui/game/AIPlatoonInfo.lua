48d90114550a3cbf0767ce161a500ebc2a7fc9df
Jip <w.b.wijnia@gmail.com>
2023-06-04 19:36:13 +0200

    Improve the platoon debug window

--- lua/ui/game/AIPlatoonInfo.lua
+++ lua/ui/game/AIPlatoonInfo.lua
@@ -7,7 +7,7 @@ local Group = import("/lua/maui/group.lua").Group
 local Root = false
 
 ---@class AIPlatoonInfoUI : Window
----@field Info AIPlatoonDebugInfo
+---@field Data AIPlatoonDebugInfo
 ---@field List Scrollbar
 ---@field Structures { Tech1: Button, Tech2: Button, Tech3: Button, Experimental: Button }
 ---@field First number
@@ -19,7 +19,7 @@ AIPlatoonInfoUI = ClassUI(Window) {
     ---@param self AIPlatoonInfoUI
     ---@param parent Control
     __init = function(self, parent)
-        Window.__init(self, parent, "AI Platoon information", false, false, false, true, false, "AIPlatoonInfo2", {
+        Window.__init(self, parent, "AI Platoon information", false, true, true, true, false, "AIPlatoonInfo2", {
             Left = 10,
             Top = 300,
             Right = 310,
@@ -41,8 +41,8 @@ AIPlatoonInfoUI = ClassUI(Window) {
         ---@param data AIPlatoonDebugInfo
             function(data)
                 if not IsDestroyed(self) then
-                    self.Info = data
-                    self.NumberOfElements = table.getn(data.Info.DebugMessages)
+                    self.Data = data
+                    self.NumberOfElements = table.getn(data.PlatoonInfo.DebugMessages)
                     self:CalcVisible()
                 end
             end, 'AIPlatoonInfo', 'AIPlatoonInfo.lua'
@@ -53,6 +53,8 @@ AIPlatoonInfoUI = ClassUI(Window) {
                 if Root then
                     if not IsDestroyed(self) then
                         self.Data = nil
+                        self.NumberOfElements = 0
+                        self:CalcVisible()
                     end
                 end
             end, 'FocusArmyChanged', 'AIPlatoonInfo.lua'
@@ -88,15 +90,41 @@ AIPlatoonInfoUI = ClassUI(Window) {
     Update = function(self)
     end,
 
+    ---@param self AIPlatoonInfoUI
     OnClose = function(self)
         self:Hide()
     end,
 
+    ---@param self AIPlatoonInfoUI
+    ---@param rotation number
     OnMouseWheel = function(self, rotation)
         if rotation > 0 then
             self:ScrollLines(nil, -1)
         else
             self:ScrollLines(nil, 1)
+        end
+    end,
+
+    ---@param self AIPlatoonInfoUI
+    OnConfigClick = function(self)
+        if self.Data then
+            reprsl(self.Data)
+        end
+    end,
+
+    ---@param self AIPlatoonInfoUI
+    OnPinCheck = function(self)
+        if self.Data then
+            local camera = GetCamera('WorldCamera')
+
+            local rect = Rect(
+                self.Data.Position[1] - 20,
+                self.Data.Position[3] - 20,
+                self.Data.Position[1] + 20,
+                self.Data.Position[3] + 20
+            )
+
+            camera:MoveToRegion(rect, 1)
         end
     end,
 
@@ -152,7 +180,7 @@ AIPlatoonInfoUI = ClassUI(Window) {
         for k = 1, self.NumberOfUIElements do
             local index = k + self.First
             if index <= self.NumberOfElements then
-                self.UIElements[k]:SetText(self.Info.Info.DebugMessages[index])
+                self.UIElements[k]:SetText(self.Data.PlatoonInfo.DebugMessages[index])
             else
                 self.UIElements[k]:SetText('')
             end
