8dbfa572925c7e2a0f34c7a50206af8b9acf230f
Jip <w.b.wijnia@gmail.com>
2023-06-06 17:33:31 +0200

    Sanitize LODs of units, props and tarmacs

--- lua/system/blueprints-lod.lua
+++ lua/system/blueprints-lod.lua
@@ -10,11 +10,11 @@ local function CalculateLODOfProp(prop)
     -- give more emphasis to the x / z value as that is easier to see in the average camera angle
     local weighted = 0.40 * sx + 0.2 * sy + 0.4 * sz
     if prop.ScriptClass == 'Tree' or prop.ScriptClass == 'TreeGroup' then
-        weighted = 3
+        weighted = 2.5
     end
 
     -- https://www.desmos.com/calculator (1.1 * sqrt(100 * 500 * x))
-    local lod = 1.10 * MathSqrt(100 * 500 * weighted)
+    local lod = 0.9 * MathSqrt(100 * 500 * weighted)
 
     if prop.Display and prop.Display.Mesh and prop.Display.Mesh.LODs then
         local n = table.getn(prop.Display.Mesh.LODs)
@@ -23,7 +23,7 @@ local function CalculateLODOfProp(prop)
 
             -- https://www.desmos.com/calculator (x * x)
             local factor = (k / n) * (k / n)
-            local LODCutoff = factor * lod + 50
+            local LODCutoff = factor * lod + 10
             -- LOG(string.format("(%s) / %d: %d -> %d", unit.BlueprintId, k, data.LODCutoff, LODCutoff))
             data.LODCutoff = LODCutoff
         end
@@ -40,7 +40,7 @@ local function CalculateLODOfUnit(unit)
     local weighted = 0.40 * sx + 0.2 * sy + 0.4 * sz
 
     -- https://www.desmos.com/calculator (0.7 * sqrt(100 * 500 * x))
-    local lod = 0.7 * MathSqrt(100 * 500 * weighted)
+    local lod = 0.6 * MathSqrt(100 * 500 * weighted)
 
     if unit.Display and unit.Display.Mesh and unit.Display.Mesh.LODs then
         local n = table.getn(unit.Display.Mesh.LODs)
@@ -53,7 +53,7 @@ local function CalculateLODOfUnit(unit)
 
             -- https://www.desmos.com/calculator (x * x)
             local factor = (lk / ln) * (lk / ln)
-            local LODCutoff = factor * lod + 50
+            local LODCutoff = factor * lod + 30
             -- LOG(string.format("(%s) / %d: %d -> %d", unit.BlueprintId, k, data.LODCutoff, LODCutoff))
             data.LODCutoff = LODCutoff
         end
