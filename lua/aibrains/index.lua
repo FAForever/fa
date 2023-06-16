ce85349d70a3a1689bb5535c2314e1bd7c575042
Aaron Warner <aaronpaulwarner@gmail.com>
2023-06-17 08:29:30 +1200

    change normal key to medium. Remove medium and easy aix variants as they don't exist.

--- lua/aibrains/index.lua
+++ lua/aibrains/index.lua
@@ -12,14 +12,12 @@ keyToBrain = {
     tech = import("/lua/aibrains/base-ai.lua").AIBrain,
     rush = import("/lua/aibrains/base-ai.lua").AIBrain,
     -- easy = import("/lua/aibrains/base-ai.lua").AIBrain,
-    normal = import("/lua/aibrains/base-ai.lua").AIBrain,
+    medium = import("/lua/aibrains/base-ai.lua").AIBrain,
     adaptive = import("/lua/aibrains/base-ai.lua").AIBrain,
     random = import("/lua/aibrains/base-ai.lua").AIBrain,
 
     techcheat = import("/lua/aibrains/base-ai.lua").AIBrain,
     rushcheat = import("/lua/aibrains/base-ai.lua").AIBrain,
-    easycheat = import("/lua/aibrains/base-ai.lua").AIBrain,
-    normalcheat = import("/lua/aibrains/base-ai.lua").AIBrain,
     adaptivecheat = import("/lua/aibrains/base-ai.lua").AIBrain,
     randomcheat = import("/lua/aibrains/base-ai.lua").AIBrain,
 }
