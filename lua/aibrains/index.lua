a78df3ddeecac7409107c81a63ee4654a4c15253
Aaron Warner <aaronpaulwarner@gmail.com>
2023-06-17 08:32:18 +1200

    format list better.

--- lua/aibrains/index.lua
+++ lua/aibrains/index.lua
@@ -6,16 +6,16 @@ keyToBrain = {
     default = import("/lua/aibrains/base-ai.lua").AIBrain,
     campaign = import("/lua/aibrains/campaign-ai.lua").AIBrain,
 
-    -- base AIs
+    -- base AI
     easy = import("/lua/aibrains/easy-ai.lua").AIBrain,
-
     tech = import("/lua/aibrains/base-ai.lua").AIBrain,
     rush = import("/lua/aibrains/base-ai.lua").AIBrain,
     -- easy = import("/lua/aibrains/base-ai.lua").AIBrain,
     medium = import("/lua/aibrains/base-ai.lua").AIBrain,
     adaptive = import("/lua/aibrains/base-ai.lua").AIBrain,
     random = import("/lua/aibrains/base-ai.lua").AIBrain,
-
+    
+    -- base AIX
     techcheat = import("/lua/aibrains/base-ai.lua").AIBrain,
     rushcheat = import("/lua/aibrains/base-ai.lua").AIBrain,
     adaptivecheat = import("/lua/aibrains/base-ai.lua").AIBrain,
