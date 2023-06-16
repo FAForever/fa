07cfc9f007259847386c47a9920d6818d4883a08
Aaron Warner <aaronpaulwarner@gmail.com>
2023-06-17 08:42:53 +1200

    add turtle AI to brain index list.

--- lua/aibrains/index.lua
+++ lua/aibrains/index.lua
@@ -9,14 +9,16 @@ keyToBrain = {
     -- base AI
     easy = import("/lua/aibrains/easy-ai.lua").AIBrain,
     tech = import("/lua/aibrains/base-ai.lua").AIBrain,
+    turtle = import("/lua/aibrains/base-ai.lua").AIBrain,
     rush = import("/lua/aibrains/base-ai.lua").AIBrain,
-    -- easy = import("/lua/aibrains/base-ai.lua").AIBrain,
+    --easy = import("/lua/aibrains/base-ai.lua").AIBrain,
     medium = import("/lua/aibrains/base-ai.lua").AIBrain,
     adaptive = import("/lua/aibrains/base-ai.lua").AIBrain,
     random = import("/lua/aibrains/base-ai.lua").AIBrain,
-    
+
     -- base AIX
     techcheat = import("/lua/aibrains/base-ai.lua").AIBrain,
+    turtlecheat = import("/lua/aibrains/base-ai.lua").AIBrain,
     rushcheat = import("/lua/aibrains/base-ai.lua").AIBrain,
     adaptivecheat = import("/lua/aibrains/base-ai.lua").AIBrain,
     randomcheat = import("/lua/aibrains/base-ai.lua").AIBrain,
