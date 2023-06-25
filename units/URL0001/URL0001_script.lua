9b621f2190945d3c4b986f36bb92f7c7e213d8eb
Josh <row.josh02@gmail.com>
2023-06-25 21:05:18 +0100

    Fix Cybran Back Upgrading issues (#5150)
    * Fix Upgrading issues
    
    * Fixed Nano buff not being removed

--- units/URL0001/URL0001_script.lua
+++ units/URL0001/URL0001_script.lua
@@ -185,6 +185,9 @@ URL0001 = ClassUnit(ACUUnit, CCommandUnit) {
             if Buff.HasBuff(self, 'CybranACURegenerateBonus') then
                 Buff.RemoveBuff(self, 'CybranACUCloakBonus')
             end
+            if Buff.HasBuff(self, 'CybranACURegenerateBonus') then
+                Buff.RemoveBuff(self, 'CybranACURegenerateBonus')
+            end
             if Buff.HasBuff(self, 'CybranACUStealthBonus') then
                 Buff.RemoveBuff(self, 'CybranACUStealthBonus')
             end
