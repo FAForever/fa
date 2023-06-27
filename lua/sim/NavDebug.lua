3a145eafd995cd3cd6e3c34d05fa48c96b0d0a5b
Jip <w.b.wijnia@gmail.com>
2023-06-27 09:28:27 +0200

    Setup for a separate debug window for functions of NavUtils

--- lua/sim/NavDebug.lua
+++ lua/sim/NavDebug.lua
@@ -1,4 +1,4 @@
---******************************************************************************************************
+--**********************************************************************************
 --** Copyright (c) 2022  Willem 'Jip' Wijnia
 --**
 --** Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -18,7 +18,7 @@
 --** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 --** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 --** SOFTWARE.
---******************************************************************************************************
+--**********************************************************************************
 
 local Shared = import("/lua/shared/navgenerator.lua")
 local NavGenerator = import("/lua/sim/navgenerator.lua")
