8af2ec4067e6d9508df22852c321832c1a8b0c74
G C <37224614+Hdt80bro@users.noreply.github.com>
2023-07-06 22:57:24 -0700

    Fix clock render and premature toggling

--- units/XSL0305/XSL0305_script.lua
+++ units/XSL0305/XSL0305_script.lua
@@ -18,37 +18,35 @@ XSL0305 = ClassUnit(SLandUnit) {
     Weapons = {
         -- used for both modes of operation
         MainGun = ClassWeapon(SDFSihEnergyRifleNormalMode) {
+            -- used to determine the reload time after a shot (the next shot's RoF should be used)
+            ---@param self SDFSniperShotNormalMode
+            GetWeaponRoF = function(self)
+                local baseRoF = self.NextBlueprint.RateOfFire or self.Blueprint.RateOfFire
+                return baseRoF / (self.AdjRoFMod or 1)
+            end,
+
             RackSalvoFireReadyState = State(SDFSihEnergyRifleNormalMode.RackSalvoFireReadyState) {
                 Main = function(self)
-                    -- doesn't appear to be quite the right place to set the scheduled mode
-                    local scheduledMode = self.unit.ScheduledForSniperMode
-                    if scheduledMode ~= nil then
-                        self.unit:SetSniperMode(scheduledMode)
-                        self.unit.ScheduledForSniperMode = nil
+                    -- it will keep looping through this state - we only want to check the first
+                    -- time (immediately after a shot)
+                    if self.FiredAShot then
+                        self.FiredAShot = nil
+                        local scheduledMode = self.unit.ScheduledForSniperMode
+                        if scheduledMode ~= nil then
+                            self.unit:SetSniperMode(scheduledMode)
+                            self.unit.ScheduledForSniperMode = nil
+                        end
                     end
                     SDFSihEnergyRifleNormalMode.RackSalvoFireReadyState.Main(self)
                 end,
             },
 
             RackSalvoFiringState = State(SDFSihEnergyRifleNormalMode.RackSalvoFiringState) {
-                -- The regular clock always uses the last projectile to get the next rof - this
-                -- isn't good for a weapon that can change what projectile it fires.
-                -- Unfortunately, while the underlying numbers of ticks to reach 100% is correct,
-                -- the output of GetFireClockPct() is scaled to reach it by the current rate of fire,
-                -- so it will be a little off when first changing it.
-                RenderClockThread = function(self, rof)
-                    local unit = self.unit
-                    while not self:BeenDestroyed() and not unit.Dead do
-                        local prog = self:GetFireClockPct()
-                        unit:SetWorkProgress(prog)
-
-                        WaitSeconds(0.1)
-                        if prog >= 1 then
-                            break
-                        end
-                    end
+                Main = function(self)
+                    self.FiredAShot = true
+                    SDFSihEnergyRifleNormalMode.RackSalvoFiringState.Main(self)
                 end,
-            }
+            },
         },
         -- kind of a dummy weapon that carries the data of the sniper mode for the main gun
         SniperGun = ClassWeapon(SDFSihEnergyRifleSniperMode) {},
@@ -83,65 +81,66 @@ XSL0305 = ClassUnit(SLandUnit) {
     ---@param mode boolean
     ScheduleSniperMode = function(self, mode)
         local weapon = self:GetWeaponByLabel("MainGun")
+        -- The engine needs the next rate of fire set before the projectile launches and there
+        -- doesn't seem to be any other time we get before that happens to set it.
+        local nextBp
+        if mode then
+            nextBp = self:GetWeaponByLabel("SniperGun").Blueprint
+        else
+            nextBp = weapon:GetBlueprint()
+        end
+        weapon:ChangeRateOfFire(nextBp.RateOfFire)
+        weapon.NextBlueprint = nextBp -- keep around to properly get the RoF of the next shot
+
         if weapon:GetFireClockPct() == 1 then -- idle, ready to switch now
             self:SetSniperMode(mode)
         else
             if self.ScheduledForSniperMode ~= nil and self.ScheduledForSniperMode ~= mode then
                 self.ScheduledForSniperMode = nil -- cancel a double toggle
+                weapon.NextBlueprint = nil
             else
                 self.ScheduledForSniperMode = mode
             end
         end
-
-        -- The engine needs the next rate of fire set before the projectile launches and there
-        -- doesn't seem to be a any other time we get until that happens to set it.
-        -- Interferes with the clock - oh well.
-        local bp
-        if mode then
-            bp = self:GetWeaponByLabel("SniperGun").Blueprint
-        else
-            bp = self:GetWeaponByLabel("MainGun"):GetBlueprint()
-        end
-        weapon:ChangeRateOfFire(bp.RateOfFire)
     end,
 
     ---@param self XSL0305
     ---@param mode boolean
     SetSniperMode = function(self, mode)
         local speedMult = self.Blueprint.Physics.LandSpeedMultiplier or 1 -- left for compatability
-        local label, otherLabel = "SniperGun", "MainGun"
+        local label
         if mode then
+            label = "SniperGun"
             speedMult = speedMult * (self.Blueprint.Physics.SniperModeSpeedMultiplier or 0.75)
             if not self.TrashSniperFx[1] then
-                self.TrashSniperFx:Add(
-                    CreateAttachedEmitter(self, "XSL0305", self.Army, "/effects/emitters/seraphim_being_built_ambient_01_emit.bp")
-                )
+                self.TrashSniperFx:Add(CreateAttachedEmitter(self, "XSL0305", self.Army,
+                        "/effects/emitters/seraphim_being_built_ambient_01_emit.bp"))
             end
         else
-            label, otherLabel = otherLabel, label
+            label = "MainGun"
             self.TrashSniperFx:Destroy()
         end
 
-        local activeWeapon = self:GetWeaponByLabel("MainGun")
-        local bp = self:GetWeaponByLabel(label):GetBlueprint()
-        -- a lot of the firing sequence relies on this - we'll store the current weapon blueprint
-        -- here so that works (except for the rate of fire, which won't anyway)
-        activeWeapon.Blueprint = bp
-        activeWeapon.FxMuzzleFlash = self.Weapons[label].FxMuzzleFlash
-        activeWeapon:ChangeProjectileBlueprint(bp.ProjectileId)
-        activeWeapon:ChangeDamage(bp.Damage)
-        activeWeapon.damageTableCache = false
-        activeWeapon:ChangeFiringTolerance(bp.FiringTolerance)
-        activeWeapon:ChangeMinRadius(bp.MinRadius)
-        activeWeapon:ChangeMaxRadius(bp.MaxRadius)
-        activeWeapon:ChangeRateOfFire(bp.RateOfFire)
-        activeWeapon:SetTurretYawSpeed(bp.TurretYawSpeed)
         self:SetSpeedMult(speedMult)
 
+        local weapon = self:GetWeaponByLabel("MainGun")
+        local bp = weapon.NextBlueprint or self:GetWeaponByLabel(label):GetBlueprint()
+        -- a lot of the firing sequence relies on the stored blueprint - we'll store the current
+        -- weapon blueprint so that it works
+        weapon.Blueprint = bp
+        weapon.NextBlueprint = nil
+        weapon.FxMuzzleFlash = self.Weapons[label].FxMuzzleFlash
+        weapon.damageTableCache = false
+        weapon:ChangeProjectileBlueprint(bp.ProjectileId)
+        weapon:ChangeFiringTolerance(bp.FiringTolerance)
+        weapon:ChangeMaxRadius(bp.MaxRadius)
+        weapon:ChangeRateOfFire(bp.RateOfFire)
+        weapon:SetTurretYawSpeed(bp.TurretYawSpeed)
+
         -- old dummy weapon introduced to make the sniper bot go to the correct attack distance
-        -- (orders use the first weapon in the blueprint) back when both weapons were used - since
-        -- we change the range of the main gun now, we could use that instead, but we don't for
-        -- compatability
+        -- (orders use the max range of the first weapon in the blueprint) back when both weapons
+        -- were used - since we change the range of the main gun now, we could use that instead,
+        -- but we don't for compatability
         self:GetWeaponByLabel("DummyWeapon"):ChangeMaxRadius(bp.MaxRadius)
     end,
 }
