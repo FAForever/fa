0aedc487508f3c937d4fcdb5dd623bcc3af9a043
Jip <w.b.wijnia@gmail.com>
2023-07-23 17:46:27 +0200

    Update game version

--- lua/ui/lobby/changelogData.lua
+++ lua/ui/lobby/changelogData.lua
@@ -1,4 +1,4 @@
-last_version = 3762
+last_version = 3763
 
 ---@alias PatchNotesType "Hotfix"|"Developers patch"|"Balance patch"
 
@@ -12,6 +12,48 @@ last_version = 3762
 
 ---@type PatchNotes[]
 gamePatches = {
+    {
+        version = 3763,
+        name = "Hotfix",
+        description = {
+            "See the patch notes of 3762 and 3758 for the full patch notes.",
+            "",
+            "There was a misconception with the distribution of files and as a result, wrecks would be removed in ",
+            "the release branch when you build on top of them. It was fun while it lasted",
+            "",
+            "Jip",
+            "",
+            "## Features",
+            "",
+            "- (#5262) Introduce the ability to cap additional structures",
+            "",
+            "You can now also cap the following structures:",
+            "",
+            "- Tech 3 air factories with tech 3 power generators",
+            "- Tech 3 or experimental artillery with tech 3 power generators",
+            "",
+            "To use these you need the game option 'Automate Structure Ringing' to be on 'Full Suite'",
+            "",
+            "## Bug fixes",
+            "",
+            "- (#5266) Fix wrecks being removed when constructing on top of them",
+            "",
+            "- (#5265) Fix the UI scaling of the unit cheat menu",
+            "",
+            "- (#5237) Fix the visuals and damage output of the Mercy",
+            "",
+            "The visuals can now be seen when you're zoomed out and the Mercy deals exactly 600 area damage at the ",
+            "center of the target",
+            "",
+            "- (#5191) Fix the size of the zapper on the Cybran Aircraft Carrier",
+            "",
+            "## Contributors",
+            "",
+            "- Jip (#5265, #5266, #5237)",
+            "- MadMax (#5191)",
+            "- Hussar (#5262)",
+        }
+    },
     {
         version = 3762,
         name = "Hotfix",
