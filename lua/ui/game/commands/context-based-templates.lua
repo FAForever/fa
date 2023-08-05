70cf5d0ade22055b0af83e2b14cd48e20b2c14db
Jip <w.b.wijnia@gmail.com>
2023-08-05 14:19:00 +0200

    Rename the feature and add support for custom factions

--- lua/ui/game/commands/context-based-templates.lua
+++ lua/ui/game/commands/context-based-templates.lua
@@ -5,89 +5,61 @@ local Debug = true
 local oldSpew = _G.SPEW
 local SPEW = function(msg)
     if Debug then
-        oldSpew(string.format("Generated templates: %s", tostring(msg)))
+        oldSpew(string.format("Contextual templates: %s", tostring(msg)))
     end
 end
 
 -- convert all known templates
-local Templates = import("/lua/ui/game/commands/generated-templates-data.lua")
+local RawTemplates = import("/lua/ui/game/commands/context-based-templates-data.lua")
 
----@type GenerativeBuildTemplate[]
-local PredefinedTemplates = { }
-for k, template in Templates do
+---@type ContextBasedTemplate[]
+local Templates = { }
+for k, template in RawTemplates do
     if type(template) == "table" then
         if template.TriggersOnHover or template.TriggersOnEmptySpace then
-            table.insert(PredefinedTemplates, template)
+            table.insert(Templates, template)
             LOG(string.format("Found template: %s with name %s", tostring(k), tostring(template.Name)))
         end
     end
 end
 
-SPEW(string.format("Found %d predefined templates", table.getn(PredefinedTemplates)))
+SPEW(string.format("Found %d contextual templates", table.getn(Templates)))
 
----@type GenerativeBuildTemplate[]
-local CycleTemplates = { }
+---@type ContextBasedTemplate[]
+local ContextBasedTemplates = { }
 
 ---@type BlueprintId
-local CycleTemplateId = ''
+local ContextBasedTemplateId = ''
 
 ---@type number
-local CycleTemplateStep = 1
+local ContextBasedTemplateStep = 1
 
 ---@type number
-local CycleTemplateCount = 1
+local ContextBasedTemplateCount = 1
 
 -- reset the state when command mode ends and we were trying to do something
 local CommandMode = import("/lua/ui/game/commandmode.lua")
 CommandMode.AddEndBehavior(
     function()
-        if CycleTemplateId != '' then
-            CycleTemplateId = ''
-            CycleTemplateStep = 1
+        if ContextBasedTemplateId != '' then
+            ContextBasedTemplateId = ''
+            ContextBasedTemplateStep = 1
             ClearBuildTemplates()
         end
     end,
-    'CycleTemplates'
+    'ContextBasedTemplates'
 )
 
---- Retrieves the faction prefix of the provided units
----@param units UserUnit[]
----@return 'ua' | 'ue' | 'ur' | 'xs' | nil
-local FindFactionPrefix = function(units)
-    local hasUEF = table.getn(EntityCategoryFilterDown(categories.UEF, units)) > 0
-    local hasAeon = table.getn(EntityCategoryFilterDown(categories.AEON, units)) > 0
-    local hasCybran = table.getn(EntityCategoryFilterDown(categories.CYBRAN, units)) > 0
-    local hasSeraphim = table.getn(EntityCategoryFilterDown(categories.SERAPHIM, units)) > 0
-
-    if hasUEF and not (hasAeon or hasCybran or hasSeraphim) then
-        return 'ue'
-    end
-
-    if hasAeon and not (hasUEF or hasCybran or hasSeraphim) then
-        return 'ua'
-    end
-
-    if hasCybran and not (hasUEF or hasAeon or hasSeraphim) then
-        return 'ur'
-    end
-
-    if hasSeraphim and not (hasUEF or hasAeon or hasCybran) then
-        return 'xs'
-    end
-
-    return nil
-end
-
 --- Validates the template in-place, returns whether the process succeeded
----@param generativeTemplate GenerativeBuildTemplate
+---@param template ContextBasedTemplate
 ---@param buildableUnits table<BlueprintId, boolean>
 ---@param prefix 'ua' | 'ue' | 'ur' | 'xs'
 ---@return boolean
-local function ValidateTemplate(generativeTemplate, buildableUnits, prefix)
+local function ValidateTemplate(template, buildableUnits, prefix)
     local allUnitsExist = true
     local allUnitsBuildable = true
-    for l = 3, table.getn(generativeTemplate.TemplateData) do
-        local templateUnit = generativeTemplate.TemplateData[l]
+    for l = 3, table.getn(template.TemplateData) do
+        local templateUnit = template.TemplateData[l]
         local templateUnitBlueprintId = prefix .. templateUnit[1]:sub(3)
         local templateUnitBlueprint = __blueprints[templateUnitBlueprintId]
         if templateUnitBlueprint then
@@ -104,8 +76,8 @@ local function ValidateTemplate(generativeTemplate, buildableUnits, prefix)
     return allUnitsExist and allUnitsBuildable
 end
 
----@param a GenerativeBuildTemplate
----@param b GenerativeBuildTemplate
+---@param a ContextBasedTemplate
+---@param b ContextBasedTemplate
 local function SortTemplates(a, b)
     return a.Name < b.Name
 end
@@ -122,8 +94,7 @@ Cycle = function()
     end
 
     local selectedUnits = GetSelectedUnits()
-    if selectedUnits then
-        local selectedUnitCount = table.getn(selectedUnits)
+    if selectedUnits and not table.empty(selectedUnits) then
         local _, _, buildableCategories = GetUnitCommandData(selectedUnits)
         local buildableUnits = table.hash(EntityCategoryGetUnitList(buildableCategories))
 
@@ -133,56 +104,53 @@ Cycle = function()
             return
         end
 
-        -- sanity check for only engineers of the same faction
-        local prefix = FindFactionPrefix(selectedUnits)
-        if not prefix then
-            print("No templates for " .. prefix)
-            return
-        end
-
         -- reset when hovering over a new unit type
-        if CycleTemplateId != blueprintId then
-            CycleTemplateStep = 1
+        if ContextBasedTemplateId != blueprintId then
+            ContextBasedTemplateStep = 1
             SPEW("Reset by blueprint id!")
         end
-        CycleTemplateId = blueprintId
+        ContextBasedTemplateId = blueprintId
 
         -- gather all templates that are applicable. We need to do this each time because the
         -- selection may have changed
-        CycleTemplates = { }
-        CycleTemplateCount = 0
-        for k = 1, table.getn(PredefinedTemplates) do
+
+        -- compute blueprint prefix
+        local prefix = selectedUnits[1]:GetBlueprint().BlueprintId:sub(1, 2)
+
+        ContextBasedTemplates = { }
+        ContextBasedTemplateCount = 0
+        for k = 1, table.getn(Templates) do
             -- check if we can build the template
-            local generativeTemplate = PredefinedTemplates[k]
-            local valid = ValidateTemplate(generativeTemplate, buildableUnits, prefix)
+            local template = Templates[k]
+            local valid = ValidateTemplate(template, buildableUnits, prefix)
             if valid then
                 if  -- check of template meets contextual conditions
-                    (userUnit and generativeTemplate.TriggersOnHover and EntityCategoryContains(generativeTemplate.TriggersOnHover, userUnit)) or
-                    (not userUnit and generativeTemplate.TriggersOnEmptySpace)
+                    (userUnit and template.TriggersOnHover and EntityCategoryContains(template.TriggersOnHover, userUnit)) or
+                    (not userUnit and template.TriggersOnEmptySpace)
                 then
-                    table.insert(CycleTemplates, generativeTemplate)
-                    CycleTemplateCount = CycleTemplateCount + 1
+                    table.insert(ContextBasedTemplates, template)
+                    ContextBasedTemplateCount = ContextBasedTemplateCount + 1
                 end
             end
         end
 
         -- inform the user and bail out
-        if CycleTemplateCount == 0 then
+        if ContextBasedTemplateCount == 0 then
             print("No templates available")
             return
         end
 
         -- sort the templates on some criteria to make order consistent
-        table.sort(CycleTemplates, SortTemplates)
+        table.sort(ContextBasedTemplates, SortTemplates)
 
         -- reset when exceeding number of templates
-        local count = table.getn(CycleTemplates)
-        if CycleTemplateStep > count then
-            CycleTemplateStep = 1
+        local count = table.getn(ContextBasedTemplates)
+        if ContextBasedTemplateStep > count then
+            ContextBasedTemplateStep = 1
             SPEW("Reset by count!")
         end
 
-        local template = CycleTemplates[CycleTemplateStep]
+        local template = ContextBasedTemplates[ContextBasedTemplateStep]
         if template then
             -- start the template command mode
             import("/lua/ui/game/commandmode.lua").SetIgnoreSelection(true)
@@ -191,10 +159,10 @@ Cycle = function()
             SetActiveBuildTemplate(template.TemplateData)
 
             -- tell the user the name of the template
-            print(string.format("(%d/%d) %s", CycleTemplateStep, CycleTemplateCount, tostring(template.Name)))
+            print(string.format("(%d/%d) %s", ContextBasedTemplateStep, ContextBasedTemplateCount, tostring(template.Name)))
         end
 
-        CycleTemplateStep = CycleTemplateStep + 1
+        ContextBasedTemplateStep = ContextBasedTemplateStep + 1
     end
 
     SPEW("Time taken: " .. GetSystemTimeSeconds() - start)
