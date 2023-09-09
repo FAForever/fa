7d40b47795b4efe330ff564c297a481a9dcedbcd
clyfordv <clyfordv@gmail.com>
2023-09-09 17:36:06 -0400

    working, added to flapjack

--- projectiles/TIFMissileCruise03/TIFMissileCruise03_Script.lua
+++ projectiles/TIFMissileCruise03/TIFMissileCruise03_Script.lua
@@ -3,9 +3,10 @@
 --
 
 local TMissileCruiseProjectile = import("/lua/terranprojectiles.lua").TMissileCruiseProjectile
+local TacticalMissileComponent = import('/lua/sim/DefaultProjectiles.lua').TacticalMissileComponent
 local EffectTemplate = import("/lua/effecttemplates.lua")
 
-TIFMissileCruise03 = ClassProjectile(TMissileCruiseProjectile) {
+TIFMissileCruise03 = ClassProjectile(TMissileCruiseProjectile, TacticalMissileComponent) {
 
     FxTrails = EffectTemplate.TMissileExhaust01,
     FxTrailOffset = -0.85,
@@ -28,51 +29,7 @@ TIFMissileCruise03 = ClassProjectile(TMissileCruiseProjectile) {
         self.MoveThread = self.Trash:Add(ForkThread(self.MovementThread,self))
     end,
 
-    MovementThread = function(self)        
-        self.Distance = self:GetDistanceToTarget()
-        self:SetTurnRate(8)
-        WaitTicks(4)        
-        while not self:BeenDestroyed() do
-            self:SetTurnRateByDist()
-            WaitTicks(2)
-        end
-    end,
 
-    SetTurnRateByDist = function(self)
-        local dist = self:GetDistanceToTarget()
-        if dist > self.Distance then
-        	self:SetTurnRate(75)
-        	WaitTicks(31)
-        	self:SetTurnRate(8)
-        	self.Distance = self:GetDistanceToTarget()
-        end
-        -- Get the nuke as close to 90 deg as possible
-        if dist > 50 then        
-            -- Freeze the turn rate as to prevent steep angles at long distance targets
-            WaitTicks(21)
-            self:SetTurnRate(10)
-        elseif dist > 30 and dist <= 50 then
-						-- Increase check intervals
-						self:SetTurnRate(12)
-						WaitTicks(16)
-            self:SetTurnRate(12)
-        elseif dist > 10 and dist <= 25 then
-						-- Further increase check intervals
-                        WaitTicks(4)
-            self:SetTurnRate(50)
-				elseif dist > 0 and dist <= 10 then
-						-- Further increase check intervals            
-            self:SetTurnRate(100)   
-            KillThread(self.MoveThread)         
-        end
-    end,        
-
-    GetDistanceToTarget = function(self)
-        local tpos = self:GetCurrentTargetPosition()
-        local mpos = self:GetPosition()
-        local dist = VDist2(mpos[1], mpos[3], tpos[1], tpos[3])
-        return dist
-    end,
 }
 TypeClass = TIFMissileCruise03
 
