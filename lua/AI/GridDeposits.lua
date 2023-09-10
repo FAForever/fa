d1420b296ecdad2d229931b186aed0d36661625a
Jip <w.b.wijnia@gmail.com>
2023-09-10 21:04:18 +0200

    Working version with easy debugging

--- lua/AI/GridDeposits.lua
+++ lua/AI/GridDeposits.lua
@@ -15,8 +15,7 @@ function DisableDebugging()
 end
 
 ---@class AIGridDepositsCell : AIGridCell
----@field MassDeposits { Marker: MarkerResource, NavAmphLayer: number }
----@field HydroDeposits { Marker: MarkerResource, NavAmphLayer: number }
+---@field Deposits { Marker: MarkerResource, NavAmphLayer: number, Type: 'Mass' | 'Hydrocarbon', Position: Vector }[]
 
 ---@class AIGridDeposits : AIGrid
 ---@field Cells AIGridDepositsCell[][]
@@ -32,13 +31,12 @@ GridDeposits = Class(Grid) {
         for k = 1, cellCount do
             for l = 1, cellCount do
                 local cell = cells[k][l]
-                cell.MassDeposits = {}
-                cell.HydroDeposits = {}
+                cell.Deposits = {}
             end
         end
 
         --self:Update()
-        --self:DebugUpdate()
+        self:DebugUpdate()
     end,
 
     --- Converts a world position to a cell
@@ -79,7 +77,8 @@ GridDeposits = Class(Grid) {
         end
 
         local cell = self:ToCellFromWorldSpace(position[1], position[3])
-        table.insert(cell.MassDeposits, { Marker = deposit })
+        table.insert(cell.Deposits, { Marker = deposit, Type = 'Mass', Position = position })
+        SPEW("Registered extractor")
     end,
 
     ---@param self AIGridDeposits
@@ -101,19 +100,133 @@ GridDeposits = Class(Grid) {
         end
 
         local cell = self:ToCellFromWorldSpace(position[1], position[3])
-        table.insert(cell.HydroDeposits, { Marker = deposit })
+        table.insert(cell.Deposits, { Marker = deposit, Type = 'Hydrocarbon', Position = position })
+        SPEW("Registered hydrocarbon plant")
     end,
 
+    --- Retrieves all resources that are within a given pathing distance to the provided position
     ---@param self AIGridDeposits
+    ---@param depositType 'Mass' | 'Hydrocarbon'
     ---@param position Vector
     ---@param distance number
-    GetResourcesWithinDistance = function(self, depositType, position, distance)
+    ---@param layer NavLayers
+    GetResourcesWithinDistance = function(self, depositType, position, distance, layer)
+        local resources = { }
+
+        local navLabel = NavUtils.GetLabel(layer, position)
+
+        local gx, gz = self:ToGridSpace(position[1], position[3])
+        local gridDistance = NavUtils.WorldDistanceToGridDistance(distance)
+        for lx = -gridDistance, gridDistance do
+            local x = gx + lx
+            for lz = -gridDistance, gridDistance do
+                local z = gz + lz
+                local cell = self.Cells[x][z]
+
+                -- cell is outside of map
+                if not cell then
+                    continue
+                end
+
+                local deposits = cell.Deposits
+                for k = 1, table.getn(deposits) do
+                    local deposit = deposits[k]
+
+                    -- wrong type of resource deposit
+                    if not deposit.Type == depositType then
+                        continue
+                    end
+
+                    local label = NavUtils.GetLabel(layer, deposit.Position)
+
+                    -- wrong type of label, we can't possibly compute a distance
+                    if navLabel != label then
+                        continue
+                    end
+
+                    local path, count, pathDistance = NavUtils.PathTo(layer, position, deposit.Position)
+
+                    -- apparently we can't reach this marker
+                    if not pathDistance then
+                        continue
+                    end
+
+                    if pathDistance <= distance then
+                        table.insert(resources, deposit.Marker)
+                    end
+                end
+            end
+        end
+
+        return resources
     end,
 
+    --- Retrieves all resources that are within a given radius, ignoring all other factors
     ---@param self AIGridDeposits
+    ---@param depositType 'Mass' | 'Hydrocarbon'
     ---@param position Vector
     ---@param distance number
     GetResourcesWithinRadius = function(self, depositType, position, distance)
+        local resources = { }
+
+        local gx, gz = self:ToGridSpace(position[1], position[3])
+        local gridDistance = NavUtils.WorldDistanceToGridDistance(distance)
+        for lx = -gridDistance, gridDistance do
+            local x = gx + lx
+            for lz = -gridDistance, gridDistance do
+                local z = gz + lz
+                local cell = self.Cells[x][z]
+
+                -- cell is outside of map
+                if not cell then
+                    continue
+                end
+
+                local deposits = cell.Deposits
+                for k = 1, table.getn(deposits) do
+                    local deposit = deposits[k]
+
+                    -- wrong type of resource deposit
+                    if not deposit.Type == depositType then
+                        continue
+                    end
+
+                    -- compute squared distance
+                    local dx = position[1] - deposit.Position[1]
+                    local dz = position[3] - deposit.Position[3]
+                    local distanceToMarker =dx * dx + dz * dz
+
+                    if distanceToMarker <= (distance * distance) then
+                        table.insert(deposit.Marker, resources)
+                    end
+                end
+            end
+        end
+
+        return resources
+    end,
+
+    ---------------------------------------------------------------------------
+    --#region Debugging
+
+    ---@param self AIGridDeposits
+    DebugUpdateThread = function(self)
+        while true do
+
+            local position = GetMouseWorldPos()
+            local resources = self:GetResourcesWithinDistance('Mass' , position, 50, 'Amphibious')
+
+            for k, resource in resources do
+                DrawLinePop(resource.Position, position, 'ffffff')
+            end
+
+            WaitTicks(1)
+        end
+    end,
+
+    ---@param self AIGridDeposits
+    DebugUpdate = function(self)
+        ForkThread(self.DebugUpdateThread, self)
     end,
 }
 
