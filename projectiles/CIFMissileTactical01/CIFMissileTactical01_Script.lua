f0507148c3be3dedc935dae9d38f8588d01418e8
clyfordv <clyfordv@gmail.com>
2023-09-11 17:23:29 -0400

    MML (not Aeon)

--- projectiles/CIFMissileTactical01/CIFMissileTactical01_Script.lua
+++ projectiles/CIFMissileTactical01/CIFMissileTactical01_Script.lua
@@ -3,56 +3,22 @@
 -- Splits into child projectile if it takes enough damage.
 
 local CLOATacticalMissileProjectile = import("/lua/cybranprojectiles.lua").CLOATacticalMissileProjectile
+local TacticalMissileComponent = import('/lua/sim/DefaultProjectiles.lua').TacticalMissileComponent
 
-CIFMissileTactical01 = ClassProjectile(CLOATacticalMissileProjectile) {
+CIFMissileTactical01 = ClassProjectile(CLOATacticalMissileProjectile, TacticalMissileComponent) {
     NumChildMissiles = 3,
+    
+    LaunchTicks = 2,
+    LaunchTurnRate = 6,
+    HeightDistanceFactor = 5,
+    MinHeight = 2,
+    FinalBoostAngle = 0,
+
     OnCreate = function(self)
         CLOATacticalMissileProjectile.OnCreate(self)
         self:SetCollisionShape('Sphere', 0, 0, 0, 2)
         self.Split = false
         self.MoveThread = self.Trash:Add(ForkThread(self.MovementThread,self))
-    end,
-
-    MovementThread = function(self)
-        self.Distance = self:GetDistanceToTarget()
-        self:SetTurnRate(8)
-        WaitTicks(4)
-        while not self:BeenDestroyed() do
-            self:SetTurnRateByDist()
-            WaitTicks(2)
-        end
-    end,
-
-    SetTurnRateByDist = function(self)
-        local dist = self:GetDistanceToTarget()
-        if dist > self.Distance then
-            self:SetTurnRate(75)
-            WaitTicks(31)
-            self:SetTurnRate(8)
-            self.Distance = self:GetDistanceToTarget()
-        end
-        if dist > 50 then
-            -- Freeze the turn rate as to prevent steep angles at long distance targets
-            WaitTicks(21)
-            self:SetTurnRate(10)
-        elseif dist > 30 and dist <= 50 then
-            self:SetTurnRate(12)
-            WaitTicks(16)
-            self:SetTurnRate(12)
-        elseif dist > 10 and dist <= 30 then
-            WaitTicks(4)
-            self:SetTurnRate(50)
-        elseif dist > 0 and dist <= 10 then
-            self:SetTurnRate(100)
-            KillThread(self.MoveThread)
-        end
-    end,
-
-    GetDistanceToTarget = function(self)
-        local tpos = self:GetCurrentTargetPosition()
-        local mpos = self:GetPosition()
-        local dist = VDist2(mpos[1], mpos[3], tpos[1], tpos[3])
-        return dist
     end,
 
     OnImpact = function(self, targetType, targetEntity)       
--- projectiles/SIFLaanseTacticalMissile01/SIFLaanseTacticalMissile01_Script.lua
+++ projectiles/SIFLaanseTacticalMissile01/SIFLaanseTacticalMissile01_Script.lua
@@ -4,54 +4,20 @@
 -- Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
 ---------------------------------------------------------------------------------------------------
 local SLaanseTacticalMissile = import("/lua/seraphimprojectiles.lua").SLaanseTacticalMissile
+local TacticalMissileComponent = import('/lua/sim/DefaultProjectiles.lua').TacticalMissileComponent
 
-SIFLaanseTacticalMissile01 = ClassProjectile(SLaanseTacticalMissile) {
+SIFLaanseTacticalMissile01 = ClassProjectile(SLaanseTacticalMissile, TacticalMissileComponent) {
+
+    LaunchTicks = 2,
+    LaunchTurnRate = 6,
+    HeightDistanceFactor = 5,
+    MinHeight = 2,
+    FinalBoostAngle = 0,
+    
     OnCreate = function(self)
         SLaanseTacticalMissile.OnCreate(self)
         self:SetCollisionShape('Sphere', 0, 0, 0, 2)
         self.MoveThread = self:ForkThread(self.MovementThread)
-    end,
-
-    MovementThread = function(self)
-        self.Distance = self:GetDistanceToTarget()
-        self:SetTurnRate(8)
-        WaitTicks(4)        
-        while not self:BeenDestroyed() do
-            self:SetTurnRateByDist()
-            WaitTicks(2)
-        end
-    end,
-
-    SetTurnRateByDist = function(self)
-        local dist = self:GetDistanceToTarget()
-        if dist > self.Distance then
-        	self:SetTurnRate(75)
-        	WaitTicks(31)
-        	self:SetTurnRate(8)
-        	self.Distance = self:GetDistanceToTarget()
-        end
-        if dist > 50 then
-            --Freeze the turn rate as to prevent steep angles at long distance targets
-            WaitTicks(21)
-            self:SetTurnRate(10)
-        elseif dist > 30 and dist <= 50 then
-						self:SetTurnRate(12)
-						WaitTicks(16)
-            self:SetTurnRate(12)
-        elseif dist > 10 and dist <= 25 then
-            WaitTicks(4)
-            self:SetTurnRate(50)
-				elseif dist > 0 and dist <= 10 then
-            self:SetTurnRate(100)
-            KillThread(self.MoveThread)
-        end
-    end,        
-
-    GetDistanceToTarget = function(self)
-        local tpos = self:GetCurrentTargetPosition()
-        local mpos = self:GetPosition()
-        local dist = VDist2(mpos[1], mpos[3], tpos[1], tpos[3])
-        return dist
     end,
 }
 TypeClass = SIFLaanseTacticalMissile01
