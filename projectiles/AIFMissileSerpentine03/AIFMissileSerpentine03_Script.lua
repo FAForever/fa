022ac81c75460681d1cce222a33f55d81dce48d3
clyfordv <clyfordv@gmail.com>
2023-09-12 12:55:10 -0400

    aeon

--- projectiles/AIFMissileSerpentine03/AIFMissileSerpentine03_Script.lua
+++ projectiles/AIFMissileSerpentine03/AIFMissileSerpentine03_Script.lua
@@ -1,52 +1,20 @@
 -- Serpentine Missile 03
 
 local AMissileSerpentine02Projectile = import("/lua/aeonprojectiles.lua").AMissileSerpentine02Projectile
+local TacticalMissileComponent = import('/lua/sim/DefaultProjectiles.lua').TacticalMissileComponent
 
-AIFMissileTactical02 = ClassProjectile(AMissileSerpentine02Projectile) {
+AIFMissileTactical02 = ClassProjectile(AMissileSerpentine02Projectile, TacticalMissileComponent) {
+
+    LaunchTicks = 6,
+    LaunchTurnRate = 6,
+    HeightDistanceFactor = 3,
+    MinHeight = 5,
+    FinalBoostAngle = 0,
 
     OnCreate = function(self)
         AMissileSerpentine02Projectile.OnCreate(self)
         self:SetCollisionShape('Sphere', 0, 0, 0, 2.0)
         self.Trash:Add(ForkThread( self.MovementThread,self))
-    end,
-
-    MovementThread = function(self)
-        self:SetTurnRate(3)
-        WaitTicks(21)
-        while not self:BeenDestroyed() do
-            self:SetTurnRateByDist()
-            WaitTicks(1)
-        end
-    end,
-
-    SetTurnRateByDist = function(self)
-        local dist = self:GetDistanceToTarget()
-        --Get the nuke as close to 90 deg as possible
-        if dist > 100 then
-            --Freeze the turn rate as to prevent steep angles at long distance targets
-            WaitTicks(21)
-            self:SetTurnRate(20)
-        elseif dist > 64 and dist <= 107 then
-						-- Increase check intervals
-						self:SetTurnRate(30)
-						WaitTicks(16)
-            self:SetTurnRate(30)
-        elseif dist > 21 and dist <= 53 then
-						-- Further increase check intervals
-            WaitTicks(4)
-            self:SetTurnRate(50)
-				elseif dist > 0 and dist <= 21 then
-						-- Further increase check intervals            
-            self:SetTurnRate(100)
-            KillThread(self.MoveThread)
-        end
-    end,
-
-    GetDistanceToTarget = function(self)
-        local tpos = self:GetCurrentTargetPosition()
-        local mpos = self:GetPosition()
-        local dist = VDist2(mpos[1], mpos[3], tpos[1], tpos[3])
-        return dist
     end,
 }
 TypeClass = AIFMissileTactical02
--- projectiles/AIFMissileTactical02/AIFMissileTactical02_Script.lua
+++ projectiles/AIFMissileTactical02/AIFMissileTactical02_Script.lua
@@ -1,50 +1,19 @@
 -- Aeon Land-Based Tactical Missile
 
 local AMissileSerpentineProjectile = import("/lua/aeonprojectiles.lua").AMissileSerpentineProjectile
-AIFMissileTactical02 = ClassProjectile(AMissileSerpentineProjectile) {
+local TacticalMissileComponent = import('/lua/sim/DefaultProjectiles.lua').TacticalMissileComponent
+AIFMissileTactical02 = ClassProjectile(AMissileSerpentineProjectile, TacticalMissileComponent) {
+
+    LaunchTicks = 6,
+    LaunchTurnRate = 6,
+    HeightDistanceFactor = 5,
+    MinHeight = 5,
+    FinalBoostAngle = 0,
+    
     OnCreate = function(self)
         AMissileSerpentineProjectile.OnCreate(self)
         self:SetCollisionShape('Sphere', 0, 0, 0, 2.0)
         self.Trash:Add(ForkThread( self.MovementThread,self ))
-    end,
-
-    MovementThread = function(self)
-        self:TrackTarget(false)
-        self:SetCollision(true)
-        WaitTicks(21)
-        self:SetTurnRate(5)
-        WaitTicks(6)
-        self:TrackTarget(true)
-        self:SetTurnRate(10)
-        WaitTicks(6)
-        while not self:BeenDestroyed() do
-            self:SetTurnRateByDist()
-            WaitTicks(6)
-        end
-    end,
-
-    SetTurnRateByDist = function(self)
-        local dist = self:GetDistanceToTarget()
-        if dist > 125 and self.MovementTurnLevel < 2 then
-            self:SetTurnRate(30)
-            self.MovementTurnLevel = 1
-        elseif dist > 85 and dist <= 125 and self.MovementTurnLevel < 3 then
-            self:SetTurnRate(40)
-            self.MovementTurnLevel = 2
-        elseif dist > 40 and dist <= 85 and self.MovementTurnLevel < 4 then
-            self:SetTurnRate(50)
-            self.MovementTurnLevel = 3
-        elseif dist < 40 then
-            self:SetTurnRate(85)
-            self.MovementTurnLevel = 4
-        end
-    end,
-
-    GetDistanceToTarget = function(self)
-        local tpos = self:GetCurrentTargetPosition()
-        local mpos = self:GetPosition()
-        local dist = VDist2(mpos[1], mpos[3], tpos[1], tpos[3])
-        return dist
     end,
 }
 TypeClass = AIFMissileTactical02
