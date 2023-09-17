44d0a155bdae9be08338bee891523b464a0f75b3
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2023-09-17 14:41:36 +0200

    Drat version to generate spawns like expansion markers

--- lua/sim/MarkerUtilities/Expansions.lua
+++ lua/sim/MarkerUtilities/Expansions.lua
@@ -27,20 +27,16 @@ local NavUtils = import("/lua/sim/NavUtils.lua")
 local Generated = false
 
 ---@class MarkerExpansion : MarkerData
+---@field Extractors MarkerResource[]
 ---@field RallyPoints MarkerRallyPoint[]
 
 function IsGenerated()
     return Generated
 end
 
-function Generate()
-    if Generated then
-        return
-    end
-
-    Generated = true
-
-    local start = GetSystemTimeSecondsOnlyForProfileUse()
+---@return MarkerExpansion[]
+---@return integer
+local function GenerateExpansionMarkers ()
 
     ---@class ExtractorNode 
     ---@field Identifier number
@@ -96,7 +92,6 @@ function Generate()
             table.insert(structuredExtractorData, node)
             table.insert(cell.Extractors, node)
         end
-
     end
 
     ---------------------------------------------------
@@ -129,15 +124,6 @@ function Generate()
                 end
             end
         end
-
-        -- local numberOfNeighbors = table.getsize(instance.Neighbors)
-        -- local numberOfCandidates = table.getsize(instance.Candidates)
-
-        -- if numberOfNeighbors <= 1 and numberOfCandidates >= 2 then
-        --     for id, neighbor in instance.Candidates do
-        --         instance.Neighbors[id] = neighbor
-        --     end
-        -- end
     end
 
     ---------------------------------------------------
@@ -146,18 +132,12 @@ function Generate()
     -- of those extractors is used as the location of
     -- the marker
 
-    local startLocations, startLocationCount = import("/lua/sim/markerutilities.lua").GetMarkersByType('Start Location')
-
     ---@type Stack
     local stack = Stack()
 
     ---@type MarkerExpansion[]
-    local largeExpansions = { }
-    local largeExpansionCount = 0
-
-    ---@type MarkerExpansion[]
-    local smallExpansions = { }
-    local smallExpansionsCount = 0
+    local expansions = { }
+    local expansionCount = 0
 
     for k = 1, table.getn(structuredExtractorData) do
         stack:Clear()
@@ -190,10 +170,56 @@ function Generate()
         center[3] = center[3] / numberOfExtractors
         center[2] = GetSurfaceHeight(center[1], center[3])
 
+
+
+        ---@type MarkerExpansion
+        local expansionMarker = {
+            position = center,
+            Position = center,
+            Extractors = extractors,
+            RallyPoints = { },
+            NavLabel = NavUtils.GetLabel('Land', center),
+        }
+
+        expansions[expansionCount + 1] = expansionMarker
+        expansionCount = expansionCount + 1
+    end
+
+    return expansions, expansionCount
+
+end
+
+---@param expansions MarkerExpansion[]
+---@param expansionCount integer
+---@return MarkerExpansion[]
+---@return integer
+local function AssimilateExpansionMarkers(expansions, expansionCount)
+
+    local startLocations, startLocationCount = import("/lua/sim/markerutilities.lua").GetMarkersByType('Start Location')
+    local mapSize = math.max(ScenarioInfo.size[1], ScenarioInfo.size[2])
+    local threshold = 30 + 0.02 * mapSize
+
+    ---------------------------------------------------------------------------
+    -- prepare the start locations
+
+    for k = 1, startLocationCount do
+        local startLocation = startLocations[k]
+        startLocation.Extractors = { }
+        startLocation.RallyPoints = { }
+    end
+
+    ---------------------------------------------------------------------------
+    -- assimilate expansions
+
+    local head = 1
+    for k = 1, expansionCount do
+        local expansion = expansions[k]
+        local center = expansion.Position
+
         -- find nearest spawn location
         local nearestStartLocation
         local nearestStartLocationDistance
-        for k = 1, startLocationCount do 
+        for k = 1, startLocationCount do
             local startLocation = startLocations[k]
             local dx = startLocation.position[1] - center[1]
             local dz = startLocation.position[3] - center[3]
@@ -209,36 +235,88 @@ function Generate()
             end
         end
 
-        -- skip those that are too close to a spawn location
-        if nearestStartLocationDistance > 40 then
-            ---@type MarkerExpansion
-            local expansionMarker = {
-                position = center,
-                Extractors = extractors,
-                Hydrocarbons = { },
-                RallyPoints = { },
-                NavLabel = NavUtils.GetLabel('Land', center),
-            }
-
-            if numberOfExtractors > 3 then
-                expansionMarker.size = 20
-                expansionMarker.type = 'Large Expansion Area'
-                largeExpansions[string.format("Large Expansion Area %d", largeExpansionCount + 1)] = expansionMarker
-                largeExpansionCount = largeExpansionCount + 1
-            elseif numberOfExtractors > 1 then
-                expansionMarker.size = 10
-                expansionMarker.type = 'Expansion Area'
-                smallExpansions[string.format("Expansion Area %d", smallExpansionsCount + 1)] = expansionMarker
-                smallExpansionsCount = smallExpansionsCount + 1
+        -- assimilate it into the spawn location
+        if math.sqrt(nearestStartLocationDistance) < threshold then
+            local extractors = nearestStartLocation.Extractors
+            for k, extractor in expansion.Extractors do
+                table.insert(extractors, extractor)
             end
+        else
+            expansions[head] = expansion
+            head = head + 1
         end
     end
 
-    ---------------------------------------------------
-    -- And at last, populate the information   
-    -- neighboring extractors that we found. The center
-    -- of those extractors is used as the location of
-    -- the marker
+    ---------------------------------------------------------------------------
+    -- clean up remaining expansions
+
+    for k = head, expansionCount do
+        expansions[k] = nil
+    end
+
+    return expansions, head - 1
+end
+
+function Generate()
+    if Generated then
+        return
+    end
+
+    Generated = true
+
+    local start = GetSystemTimeSecondsOnlyForProfileUse()
+
+    ---------------------------------------------------------------------------
+    -- Generate the expansions and assimilate them into spawns
+
+    local expansions, expansionCount = GenerateExpansionMarkers()
+    local remainingExpansions, remainingExpansionCount = AssimilateExpansionMarkers(expansions, expansionCount)
+
+    ---------------------------------------------------------------------------
+    -- Divide expansions into small and large expansions
+
+    local smallExpansions = { }
+    local smallExpansionsHead = 1
+
+    local largeExpansions = { }
+    local largeExpansionsHead = 1
+
+    for k = 1, remainingExpansionCount do
+        local expansion = remainingExpansions[k]
+
+        local extractorCount = table.getn(expansion.Extractors)
+        if extractorCount > 3 then
+            expansion.Type = 'Large Expansion Area'
+            expansion.Name = string.format("Large Expansion Area %d", largeExpansionsHead)
+            expansion.Size = 20
+
+            -- legacy entries
+            expansion.type = expansion.Type
+            expansion.name = expansion.Name
+            expansion.size = expansion.Size
+
+            largeExpansions[largeExpansionsHead] = expansion
+            largeExpansionsHead = largeExpansionsHead + 1
+        elseif extractorCount > 1 then
+
+            expansion.Type = 'Expansion Area'
+            expansion.Name = string.format("Expansion Area %d", largeExpansionsHead)
+            expansion.Size = 10
+
+            -- legacy entries
+            expansion.type = expansion.Type
+            expansion.name = expansion.Name
+            expansion.size = expansion.Size
+
+            smallExpansions[smallExpansionsHead] = expansion
+            smallExpansionsHead = smallExpansionsHead + 1
+        end
+    end
+
+    ---------------------------------------------------------------------------
+    -- And at last, populate the information neighboring extractors that we 
+    -- found. The center of those extractors is used as the location of the 
+    -- marker
 
     import("/lua/sim/markerutilities.lua").OverwriteMarkerByType('Large Expansion Area', largeExpansions)
     import("/lua/sim/markerutilities.lua").OverwriteMarkerByType('Expansion Area', smallExpansions)
