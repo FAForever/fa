48c9dac573495c24ce0a5fc87133e9131798a96b
clyf <clyfordv@gmail.com>
2023-09-17 11:51:49 -0400

    Rework how tactical missiles compute their path (#5433)
    Parameterizes and componentizes how tactical missiles work. As a result it is easier to maintain and more flexible towards future changes.

--- projectiles/CIFMissileTactical02/CIFMissileTactical02_Script.lua
+++ projectiles/CIFMissileTactical02/CIFMissileTactical02_Script.lua
@@ -5,11 +5,18 @@
 -- if it takes enough damage.
 -- 
 local CLOATacticalMissileProjectile = import("/lua/cybranprojectiles.lua").CLOATacticalMissileProjectile
+local TacticalMissileComponent = import('/lua/sim/DefaultProjectiles.lua').TacticalMissileComponent
 
-CIFMissileTactical02 = ClassProjectile(CLOATacticalMissileProjectile) {
+CIFMissileTactical02 = ClassProjectile(CLOATacticalMissileProjectile, TacticalMissileComponent) {
 
     NumChildMissiles = 3,
     FxWaterHitScale = 1.65,
+
+    LaunchTicks = 6,
+    LaunchTurnRate = 6,
+    HeightDistanceFactor = 5,
+    MinHeight = 5,
+    FinalBoostAngle = 0,
 
     OnCreate = function(self)
         CLOATacticalMissileProjectile.OnCreate(self)
@@ -53,45 +60,6 @@ CIFMissileTactical02 = ClassProjectile(CLOATacticalMissileProjectile) {
             end
         end
         CLOATacticalMissileProjectile.OnDamage(self, instigator, amount, vector, damageType)
-    end,
-
-    MovementThread = function(self)
-        self:SetTurnRate(8)
-        WaitTicks(4)
-        while not self:BeenDestroyed() do
-            self:SetTurnRateByDist()
-            WaitTicks(2)
-        end
-    end,
-
-    SetTurnRateByDist = function(self)
-        local dist = self:GetDistanceToTarget()
-        -- Get the nuke as close to 90 deg as possible
-        if dist > 50 then
-            -- Freeze the turn rate as to prevent steep angles at long distance targets
-            WaitTicks(21)
-            self:SetTurnRate(20)
-        elseif dist > 64 and dist <= 107 then
-            -- Increase check intervals
-            self:SetTurnRate(30)
-            WaitTicks(16)
-            self:SetTurnRate(30)
-        elseif dist > 21 and dist <= 64 then
-            -- Further increase check intervals
-            WaitTicks(4)
-            self:SetTurnRate(50)
-        elseif dist > 0 and dist <= 21 then
-            -- Further increase check intervals            
-            self:SetTurnRate(100)
-            KillThread(self.MoveThread)
-        end
-    end,        
-
-    GetDistanceToTarget = function(self)
-        local tpos = self:GetCurrentTargetPosition()
-        local mpos = self:GetPosition()
-        local dist = VDist2(mpos[1], mpos[3], tpos[1], tpos[3])
-        return dist
     end,
 
     OnExitWater = function(self)
