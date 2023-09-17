8dfe7b58a02eca7e0c8a96f7865b5aa6766557b8
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2023-09-17 21:07:34 +0200

    Update game version and add changelog

--- lua/version.lua
+++ lua/version.lua
@@ -1,7 +1,7 @@
 
-local Version = "3767"
----@alias PATCH "3767"
----@alias VERSION "1.5.3767"
+local Version = "3768"
+---@alias PATCH "3768"
+---@alias VERSION "1.5.3768"
 ---@return PATCH
 function GetVersion()
     LOG('Supreme Commander: Forged Alliance version ' .. Version)
