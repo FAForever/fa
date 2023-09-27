738b4e892d8be63e6525acb2dfe267335ec77a58
clyfordv <clyfordv@gmail.com>
2023-09-27 12:27:16 -0400

    license

--- lua/ui/game/syncstrike.lua
+++ lua/ui/game/syncstrike.lua
@@ -1,5 +1,5 @@
 --******************************************************************************************************
---** Copyright (c) 2022  Charles "clyf" Lyford
+--** Copyright (c) 2022  clyf
 --**
 --** Permission is hereby granted, free of charge, to any person obtaining a copy
 --** of this software and associated documentation files (the "Software"), to deal
