36e36ee9f008fd7e8b6727bb16de7ec0e9689cd2
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2023-10-08 18:41:23 +0200

    Introduce a wall overspill mechanic (#5514)

--- lua/sim/DefaultUnits/WallStructureUnit.lua
+++ lua/sim/DefaultUnits/WallStructureUnit.lua
@@ -1,5 +1,36 @@
 
 local StructureUnit = import("/lua/sim/defaultunits/structureunit.lua").StructureUnit
 
+-- upvalue scope for performance
+local EntityCategoryContains = EntityCategoryContains
+
+local StructureUnitOnDamage = StructureUnit.OnDamage
+
+local CategoriesWall = categories.WALL
+
 ---@class WallStructureUnit : StructureUnit
-WallStructureUnit = ClassUnit(StructureUnit) { }
+WallStructureUnit = ClassUnit(StructureUnit) {
+
+    WallOverspillFactor = 0.2,
+
+    ---@param self WallStructureUnit
+    ---@param instigator Unit
+    ---@param amount number
+    ---@param vector Vector
+    ---@param damageType DamageType
+    OnDamage = function(self, instigator, amount, vector, damageType)
+        StructureUnitOnDamage(self, instigator, amount, vector, damageType)
+
+        if damageType != 'WallOverspill' then
+            local adjacentUnits = self.AdjacentUnits
+            if adjacentUnits then
+                local wallOverspillFactor = self.WallOverspillFactor
+                for id, adjacentUnit in adjacentUnits do
+                    if EntityCategoryContains(CategoriesWall, adjacentUnit) then
+                        adjacentUnit:OnDamage(instigator, wallOverspillFactor * amount, vector, 'WallOverspill')
+                    end
+                end
+            end
+        end
+    end,
+}
