083dca7de3795719bd7bb5eaf8736829427e208b
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2023-10-15 09:55:52 +0200

    Add context based templates that trigger on the unit you're trying to build (#5543)

---
+++ lua/ui/game/hotkeys/context-based-templates-data/CapExtractorWithFabs.lua
@@ -0,0 +1,104 @@
+--******************************************************************************************************
+--** Copyright (c) 2023  Willem 'Jip' Wijnia
+--**
+--** Permission is hereby granted, free of charge, to any person obtaining a copy
+--** of this software and associated documentation files (the "Software"), to deal
+--** in the Software without restriction, including without limitation the rights
+--** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--** copies of the Software, and to permit persons to whom the Software is
+--** furnished to do so, subject to the following conditions:
+--**
+--** The above copyright notice and this permission notice shall be included in all
+--** copies or substantial portions of the Software.
+--**
+--** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+--** SOFTWARE.
+--******************************************************************************************************
+
+---@type ContextBasedTemplate
+Template = {
+    Name = 'Storages and fabricators',
+    TriggersOnUnit = categories.MASSEXTRACTION * (categories.TECH2 + categories.TECH3),
+    TemplateSortingOrder = 101,
+    TemplateData = {
+        10,
+        10,
+        {
+            'uab1106',
+            30057,
+            -2,
+            0
+        },
+        {
+            'uab1106',
+            30070,
+            2,
+            0
+        },
+        {
+            'uab1106',
+            30083,
+            0,
+            -2
+        },
+        {
+            'uab1106',
+            30096,
+            0,
+            2
+        },
+        {
+            'uab1104',
+            30109,
+            -4,
+            0
+        },
+        {
+            'uab1104',
+            30134,
+            -2,
+            2
+        },
+        {
+            'uab1104',
+            30158,
+            0,
+            4
+        },
+        {
+            'uab1104',
+            30182,
+            2,
+            2
+        },
+        {
+            'uab1104',
+            30206,
+            4,
+            0
+        },
+        {
+            'uab1104',
+            30231,
+            2,
+            -2
+        },
+        {
+            'uab1104',
+            30255,
+            0,
+            -4
+        },
+        {
+            'uab1104',
+            30279,
+            -2,
+            -2
+        }
+    },
+}
