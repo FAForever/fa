26d6a632e27511418d9a47df16659628c4728d6b
Basilisk3 <126026384+Basilisk3@users.noreply.github.com>
2023-10-25 16:08:38 +0200

    Apply feedback about the Bubble Shield

--- loc/US/strings_db.lua
+++ loc/US/strings_db.lua
@@ -1433,7 +1433,7 @@ Unit_Description_0007="Grants Tech 3 and Experimental schematic access and furth
 Unit_Description_0008="Doubles the main cannon's damage, and increases its range and that of Overcharge.\n\n+100 Main cannon damage\n+8 Main cannon range"
 Unit_Description_0009="Tactical Cruise Missiles replaced by much more expensive, powerful Tactical Nuclear Missiles."
 Unit_Description_0010="Adds Teleportation capability. Requires considerable energy to activate."
-Unit_Description_0011="Creates a Bubble Shield around the ACU, which protects friendly units inside its radius. If present, the shield will transfer a portion of the damage received to intersecting shield bubbles. Requires energy to run.\n\nShield Regen Rate = 60 HP/s\nShield Recharge Time = 90 seconds"
+Unit_Description_0011="Creates a Bubble Shield around the ACU, which protects friendly units inside it. If present, the shield will transfer a portion of the damage received to intersecting shield bubbles. Requires energy to run.\n\nShield Regen Rate = 60 HP/s\nShield Recharge Time = 90 seconds"
 Unit_Description_0012="Increases ACU's resource generation to 15 mass per second and 2520 energy per second."
 Unit_Description_0013="Creates a protective Personal Shield around the ACU. Requires energy to run.\n\nShield Regen Rate = 30 HP/s\nShield Recharge Time = 140 seconds"
 Unit_Description_0014="Creates an Engineering Drone which act as a flying T1 Engineer. Assists the ACU by default, but can be given its own orders.\n\n+5 Buildpower Drone"
