3a97d7929a6f1ed5b1c09f3c3174e9b799e168a4
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2023-10-31 10:36:03 +0100

    Fix a bug with zappers intersecting the wrong missile

--- lua/sim/Projectile.lua
+++ lua/sim/Projectile.lua
@@ -184,7 +184,6 @@ Projectile = ClassProjectile(ProjectileMethods) {
     ---@param other Projectile The projectile we're checking the collision with
     ---@return boolean
     OnCollisionCheck = function(self, other)
-
         -- we can't hit our own
         if self.Army == other.Army then
             return false
@@ -237,7 +236,7 @@ Projectile = ClassProjectile(ProjectileMethods) {
         -- check for projectile types that require a defensive weapon to intercept
         if selfHashedCategories['TACTICAL'] or selfHashedCategories['STRATEGIC'] or selfHashedCategories['TORPEDO'] then
             if firingWeapon.Blueprint.WeaponCategory == 'Defense' then
-                return alliedCheck
+                return firingWeapon:GetCurrentTarget() == self
             else
                 return false
             end
