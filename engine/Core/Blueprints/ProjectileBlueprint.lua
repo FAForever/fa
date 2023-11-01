18e9dfd618d29f1fa1ec86f9b1706129950814e1
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2023-11-01 19:02:07 +0100

    Re-introduce the Seraphim-specific scorch splats

--- engine/Core/Blueprints/ProjectileBlueprint.lua
+++ engine/Core/Blueprints/ProjectileBlueprint.lua
@@ -34,6 +34,8 @@
 ---@field CameraFollowTimeout number
 --- how large is the strategic icon square for the projectile
 ---@field StrategicIconSize number
+--- flag to not use generic scorch splats
+---@field NoGenericScorchSplats? boolean
 
 ---@class ProjectileBlueprintEconomy
 --- energy cost to build this projectile
