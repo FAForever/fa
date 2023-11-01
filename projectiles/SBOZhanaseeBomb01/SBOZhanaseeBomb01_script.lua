f7960acbe15f03cf818c9ac80deaca2ccc73460a
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2023-11-01 19:04:26 +0100

    Re-introduce the Seraphim-specific scorch splats (#5616)

--- projectiles/SBOZhanaseeBomb01/SBOZhanaseeBomb01_script.lua
+++ projectiles/SBOZhanaseeBomb01/SBOZhanaseeBomb01_script.lua
@@ -8,7 +8,12 @@ local SZhanaseeBombProjectile = import("/lua/seraphimprojectiles.lua").SZhanasee
 SBOZhanaseeBombProjectile01 = ClassProjectile(SZhanaseeBombProjectile){
     OnImpact = function(self, targetType, targetEntity)        
 		SZhanaseeBombProjectile.OnImpact(self, targetType, targetEntity) 
-        CreateLightParticle(self, -1, self.Army, 26, 5, 'sparkle_white_add_08', 'ramp_white_24' )
+        local army = self.Army
+        CreateLightParticle(self, -1, army, 26, 5, 'sparkle_white_add_08', 'ramp_white_24' )
+
+        if targetType == 'Terrain' then
+            CreateDecal( self:GetPosition(), Random() * 6.28, 'Scorch_012_albedo', '', 'Albedo', 40, 40, 300, 200, army)
+        end
 
         -- One initial projectile following same directional path as the original
         self:CreateProjectile('/effects/entities/SBOZhanaseeBombEffect01/SBOZhanaseeBombEffect01_proj.bp', 0, 0, 0, 0, 10.0, 0):SetCollision(false):SetVelocity(0,10.0, 0)
