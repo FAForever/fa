6c2b6cb04b130fc5e277b75708bcb3615a272529
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2023-11-04 14:35:31 +0100

    Changelog for game version 3773 (#5637)

--- lua/version.lua
+++ lua/version.lua
@@ -1,7 +1,7 @@
 
-local Version = "3772"
----@alias PATCH "3772"
----@alias VERSION "1.5.3772"
+local Version = "3773"
+---@alias PATCH "3773"
+---@alias VERSION "1.5.3773"
 ---@return PATCH
 function GetVersion()
     LOG('Supreme Commander: Forged Alliance version ' .. Version)
