8e953729307f3021d177dd34ba16ab9f7ec3660c
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2023-11-05 08:47:42 +0100

    Add license and reduce overhead for projectiles pt. 1

--- projectiles/AANDepthCharge01/AANDepthCharge01_script.lua
+++ projectiles/AANDepthCharge01/AANDepthCharge01_script.lua
@@ -1,14 +1,41 @@
+--******************************************************************************************************
+--** Copyright (c) 2023 FAForever
+--**
+--** Permission is hereby granted, free of charge, to any person obtaining a copy
+--** of this software and associated documentation files (the "Software"), to deal
+--** in the Software without restriction, including without limitation the rights
+--** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--** copies of the Software, and to permit persons to whom the Software is
+--** furnished to do so, subject to the following conditions:
+--**
+--** The above copyright notice and this permission notice shall be included in all
+--** copies or substantial portions of the Software.
+--**
+--** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+--** SOFTWARE.
+--******************************************************************************************************
+
 local ADepthChargeProjectile = import("/lua/aeonprojectiles.lua").ADepthChargeProjectile
+local ADepthChargeProjectileOnCreate = ADepthChargeProjectile.OnCreate
+local ADepthChargeProjectileOnEnterWater = ADepthChargeProjectile.OnEnterWater
+local ADepthChargeProjectileOnImpact = ADepthChargeProjectile.OnImpact
+
 local VisionMarkerOpti = import("/lua/sim/vizmarker.lua").VisionMarkerOpti
 
 -- Depth Charge Script
 ---@class AANDepthCharge01 : ADepthChargeProjectile
+---@field HasImpacted boolean
 AANDepthCharge01 = ClassProjectile(ADepthChargeProjectile) {
     CountdownLengthInTicks = 101,
 
     ---@param self AANDepthCharge01
     OnCreate = function(self)
-        ADepthChargeProjectile.OnCreate(self)
+        ADepthChargeProjectileOnCreate(self)
         self.HasImpacted = false
         self.Trash:Add(ForkThread(self.CountdownExplosion, self))
     end,
@@ -23,7 +50,7 @@ AANDepthCharge01 = ClassProjectile(ADepthChargeProjectile) {
 
     ---@param self AANDepthCharge01
     OnEnterWater = function(self)
-        ADepthChargeProjectile.OnEnterWater(self)
+        ADepthChargeProjectileOnEnterWater(self)
         self:SetMaxSpeed(20)
         self:SetVelocity(0)
         self:SetAcceleration(5)
@@ -48,11 +75,11 @@ AANDepthCharge01 = ClassProjectile(ADepthChargeProjectile) {
     ---@param self AANDepthCharge01
     OnImpact = function(self, TargetType, TargetEntity)
         local px, _, pz = self:GetPositionXYZ()
-        local marker = VisionMarkerOpti({Owner = self})
+        local marker = VisionMarkerOpti({ Owner = self })
         marker:UpdatePosition(px, pz)
         marker:UpdateDuration(5)
         marker:UpdateIntel(self.Army, 5, 'Vision', true)
-        ADepthChargeProjectile.OnImpact(self, TargetType, TargetEntity)
+        ADepthChargeProjectileOnImpact(self, TargetType, TargetEntity)
     end,
 }
 TypeClass = AANDepthCharge01
--- projectiles/AANDepthCharge02/AANDepthCharge02_script.lua
+++ projectiles/AANDepthCharge02/AANDepthCharge02_script.lua
@@ -1,20 +1,43 @@
+--******************************************************************************************************
+--** Copyright (c) 2023 FAForever
+--**
+--** Permission is hereby granted, free of charge, to any person obtaining a copy
+--** of this software and associated documentation files (the "Software"), to deal
+--** in the Software without restriction, including without limitation the rights
+--** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--** copies of the Software, and to permit persons to whom the Software is
+--** furnished to do so, subject to the following conditions:
+--**
+--** The above copyright notice and this permission notice shall be included in all
+--** copies or substantial portions of the Software.
+--**
+--** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+--** SOFTWARE.
+--******************************************************************************************************
+
 local ADepthChargeProjectile = import("/lua/aeonprojectiles.lua").ADepthChargeProjectile
+local ADepthChargeProjectileOnImpact = ADepthChargeProjectile.OnImpact
+
 local VisionMarkerOpti = import("/lua/sim/vizmarker.lua").VisionMarkerOpti
 
 -- Depth Charge Script
 ---@class AANDepthCharge02 : ADepthChargeProjectile
 AANDepthCharge02 = ClassProjectile(ADepthChargeProjectile) {
-
     ---@param self AANDepthCharge02
     ---@param TargetType string
     ---@param TargetEntity Prop|Unit
     OnImpact = function(self, TargetType, TargetEntity)
-        local px,_,pz = self:GetPositionXYZ()
-        local marker = VisionMarkerOpti({Owner = self})
-        marker:UpdatePosition(px,pz)
+        local px, _, pz = self:GetPositionXYZ()
+        local marker = VisionMarkerOpti({ Owner = self })
+        marker:UpdatePosition(px, pz)
         marker:UpdateDuration(5)
         marker:UpdateIntel(self.Army, 5, 'Vision', true)
-        ADepthChargeProjectile.OnImpact(self, TargetType, TargetEntity)
+        ADepthChargeProjectileOnImpact(self, TargetType, TargetEntity)
     end,
 }
 TypeClass = AANDepthCharge02
