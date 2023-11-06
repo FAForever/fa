24a8761b8d21a211cd108f1af866d47979ee533d
Basilisk3 <126026384+Basilisk3@users.noreply.github.com>
2023-11-06 21:27:04 +0100

    Add the automatic rotation towards the optimal rollout point for the Seraphim Air Factories (with thanks to Garanas)

--- lua/seraphimunits.lua
+++ lua/seraphimunits.lua
@@ -107,6 +107,15 @@ SAirFactoryUnit = ClassUnit(AirFactoryUnit) {
     end,
 
     RolloffBody = function(self)
+
+        local rollOffPoint = self.RollOffPoint
+        local unitBeingBuilt = self.UnitBeingBuilt
+        if unitBeingBuilt and EntityCategoryContains(categories.ENGINEER, unitBeingBuilt) then
+            local spin, x, y, z = self:CalculateRollOffPoint()
+            unitBeingBuilt:SetRotation(spin)
+            rollOffPoint[1], rollOffPoint[2], rollOffPoint[3] = x, y, z
+        end
+
         self:SetBusy(true)
         local unitBuilding = self.UnitBeingBuilt
 
