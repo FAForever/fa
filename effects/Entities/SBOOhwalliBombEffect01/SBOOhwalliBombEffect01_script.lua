3551bac57559c34ef4b41f1289381b438868e244
Josh <row.josh02@gmail.com>
2023-11-12 16:50:39 +0000

    Introduce and fix code annotations (#5644)

--- effects/Entities/SBOOhwalliBombEffect01/SBOOhwalliBombEffect01_script.lua
+++ effects/Entities/SBOOhwalliBombEffect01/SBOOhwalliBombEffect01_script.lua
@@ -1,14 +1,13 @@
---****************************************************************************
---**
---**  File     :  /data/projectiles/SBOOhwalliBombEffect01/SBOOhwalliBombEffect01_script.lua
---**  Author(s):  Greg Kohne, Gordon Duclos
---**
---**  Summary  :  Ohwalli Strategic Bomb effect script, non-damaging
---**
---**  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
---****************************************************************************
+------------------------------------------------------------------------------
+-- File     :  /data/projectiles/SBOOhwalliBombEffect01/SBOOhwalliBombEffect01_script.lua
+-- Author(s):  Greg Kohne, Gordon Duclos
+-- Summary  :  Ohwalli Strategic Bomb effect script, non-damaging
+-- Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
+------------------------------------------------------------------------------
 local EffectTemplate = import("/lua/effecttemplates.lua")
 
+--- Ohwalli Strategic Bomb effect script, non-damaging
+---@class SBOOhwalliBombEffect01 : EmitterProjectile
 SBOOhwalliBombEffect01 = Class(import("/lua/sim/defaultprojectiles.lua").EmitterProjectile) {
 	FxTrails = EffectTemplate.SOhwalliBombPlumeFxTrails01,
 }
--- effects/Entities/UEFBuild/UEFBuild01_script.lua
+++ effects/Entities/UEFBuild/UEFBuild01_script.lua
@@ -1,3 +1,4 @@
-
+--- Dummy projectile for UEF build effect
+-- @class UEFBuild01 : DummyProjectile
 UEFBuild01 = ClassDummyProjectile(import("/lua/sim/projectile.lua").DummyProjectile) { }
 TypeClass = UEFBuild01
