be7658e2e614f957164d3564ae650f0a40ebacea
MrRowey <row.josh02@gmail.com>
2023-11-15 15:34:57 +0000

    Update cybranweapons.lua

--- lua/cybranweapons.lua
+++ lua/cybranweapons.lua
@@ -6,6 +6,17 @@
 ------------------------------------------------------------------------------
 
 --- Weapon Files ---
+CAAAutocannon = import('/lua/sim/weapons/cybran/CAAAutocannon.lua').CAAAutocannon
+CAANanoDartWeapon = import('/lua/sim/weapons/cybran/CAANanoDartWeapon.lua').CAANanoDartWeapon
+CAABurstCloudFlakArtilleryWeapon = import('/lua/sim/weapons/cybran/CAABurstCloudFlakArtilleryWeapon.lua').CAABurstCloudFlakArtilleryWeapon
+CAAMissileNaniteWeapon = import('/lua/sim/weapons/cybran/CAAMissileNaniteWeapon.lua').CAAMissileNaniteWeapon
+CAMEMPMissileWeapon = import('/lua/sim/weapons/cybran/CAMEMPMissileWeapon.lua').CAMEMPMissileWeapon
+CAMZapperWeapon = import('/lua/sim/weapons/cybran/CAMZapperWeapon.lua').CAMZapperWeapon
+CAMZapperWeapon02 = import('/lua/sim/weapons/cybran/CAMZapperWeapon02.lua').CAMZapperWeapon02
+CAMZapperWeapon03 = import('/lua/sim/weapons/cybran/CAMZapperWeapon03.lua').CAMZapperWeapon03
+CANTorpedoLauncherWeapon = import('/lua/sim/weapons/cybran/CANTorpedoLauncherWeapon.lua').CANTorpedoLauncherWeapon
+CANNaniteTorpedoWeapon = import('/lua/sim/weapons/cybran/CANNaniteTorpedoWeapon.lua').CANNaniteTorpedoWeapon
+CCannonMolecularWeapon = import('/lua/sim/weapons/cybran/CCannonMolecularWeapon.lua').CCannonMolecularWeapon
 CDFBrackmanCrabHackPegLauncherWeapon = import('/lua/sim/weapons/cybran/CDFBrackmanCrabHackPegLauncherWeapon.lua').CDFBrackmanCrabHackPegLauncherWeapon
 CDFParticleCannonWeapon = import('/lua/sim/weapons/cybran/CDFParticleCannonWeapon.lua').CDFParticleCannonWeapon
 CDFProtonCannonWeapon = import('/lua/sim/weapons/cybran/CDFProtonCannonWeapon.lua').CDFProtonCannonWeapon
@@ -15,23 +26,19 @@ CDFHeavyMicrowaveLaserGeneratorCom = import('/lua/sim/weapons/cybran/CDFHeavyMic
 CDFEMP = import('/lua/sim/weapons/cybran/CDFEMP.lua').CDFEMP
 CDFElectronBolterWeapon = import('/lua/sim/weapons/cybran/CDFElectronBolterWeapon.lua').CDFElectronBolterWeapon
 CDFHeavyElectronBolterWeapon = import('/lua/sim/weapons/cybran/CDFHeavyElectronBolterWeapon.lua').CDFHeavyElectronBolterWeapon
-CIFSmartCharge = import('/lua/sim/weapons/cybran/CIFSmartCharge.lua').CIFSmartCharge
-CANTorpedoLauncherWeapon = import('/lua/sim/weapons/cybran/CANTorpedoLauncherWeapon.lua').CANTorpedoLauncherWeapon
-CANNaniteTorpedoWeapon = import('/lua/sim/weapons/cybran/CANNaniteTorpedoWeapon.lua').CANNaniteTorpedoWeapon
 CDFMissileMesonWeapon = import('/lua/sim/weapons/cybran/CDFMissileMesonWeapon.lua').CDFMissileMesonWeapon
 CDFRocketIridiumWeapon = import('/lua/sim/weapons/cybran/CDFRocketIridiumWeapon.lua').CDFRocketIridiumWeapon
 CDFRocketIridiumWeapon02 = import('/lua/sim/weapons/cybran/CDFRocketIridiumWeapon02.lua').CDFRocketIridiumWeapon02
-CIFMissileCorsairWeapon = import('/lua/sim/weapons/cybran/CIFMissileCorsairWeapon.lua').CIFMissileCorsairWeapon
 CDFLaserPulseLightWeapon = import('/lua/sim/weapons/cybran/CDFLaserPulseLightWeapon.lua').CDFLaserPulseLightWeapon
 CDFLaserHeavyWeapon = import('/lua/sim/weapons/cybran/CDFLaserHeavyWeapon.lua').CDFLaserHeavyWeapon
 CDFLaserHeavyWeapon02 = import('/lua/sim/weapons/cybran/CDFLaserHeavyWeapon02.lua').CDFLaserHeavyWeapon02
 CDFLaserDisintegratorWeapon01 = import('/lua/sim/weapons/cybran/CDFLaserDisintegratorWeapon01.lua').CDFLaserDisintegratorWeapon01
 CDFLaserDisintegratorWeapon02 = import('/lua/sim/weapons/cybran/CDFLaserDisintegratorWeapon02.lua').CDFLaserDisintegratorWeapon02
 CDFHeavyDisintegratorWeapon = import('/lua/sim/weapons/cybran/CDFHeavyDisintegratorWeapon.lua').CDFHeavyDisintegratorWeapon
-CAAAutocannon = import('/lua/sim/weapons/cybran/CAAAutocannon.lua').CAAAutocannon
-CAANanoDartWeapon = import('/lua/sim/weapons/cybran/CAANanoDartWeapon.lua').CAANanoDartWeapon
-CAABurstCloudFlakArtilleryWeapon = import('/lua/sim/weapons/cybran/CAABurstCloudFlakArtilleryWeapon.lua').CAABurstCloudFlakArtilleryWeapon
-CAAMissileNaniteWeapon = import('/lua/sim/weapons/cybran/CAAMissileNaniteWeapon.lua').CAAMissileNaniteWeapon
+CDFOverchargeWeapon = import('/lua/sim/weapons/cybran/CDFOverchargeWeapon.lua').CDFOverchargeWeapon
+CEMPAutoCannon = import('/lua/sim/weapons/cybran/CEMPAutoCannon.lua').CEMPAutoCannon
+CIFSmartCharge = import('/lua/sim/weapons/cybran/CIFSmartCharge.lua').CIFSmartCharge
+CIFMissileCorsairWeapon = import('/lua/sim/weapons/cybran/CIFMissileCorsairWeapon.lua').CIFMissileCorsairWeapon
 CIFGrenadeWeapon = import('/lua/sim/weapons/cybran/CIFGrenadeWeapon.lua').CIFGrenadeWeapon
 CIFArtilleryWeapon = import('/lua/sim/weapons/cybran/CIFArtilleryWeapon.lua').CIFArtilleryWeapon
 CIFMissileStrategicWeapon = import('/lua/sim/weapons/cybran/CIFMissileStrategicWeapon.lua').CIFMissileStrategicWeapon
@@ -39,15 +46,8 @@ CIFMissileLoaTacticalWeapon = import('/lua/sim/weapons/cybran/CIFMissileLoaTacti
 CIFBombNeutronWeapon = import('/lua/sim/weapons/cybran/CIFBombNeutronWeapon.lua').CIFBombNeutronWeapon
 CIFNaniteTorpedoWeapon = import('/lua/sim/weapons/cybran/CIFNaniteTorpedoWeapon.lua').CIFNaniteTorpedoWeapon
 CIFMissileLoaWeapon = import('/lua/sim/weapons/cybran/CIFMissileLoaWeapon.lua').CIFMissileLoaWeapon
-CAMEMPMissileWeapon = import('/lua/sim/weapons/cybran/CAMEMPMissileWeapon.lua').CAMEMPMissileWeapon
-CAMZapperWeapon = import('/lua/sim/weapons/cybran/CAMZapperWeapon.lua').CAMZapperWeapon
-CAMZapperWeapon02 = import('/lua/sim/weapons/cybran/CAMZapperWeapon02.lua').CAMZapperWeapon02
-CAMZapperWeapon03 = import('/lua/sim/weapons/cybran/CAMZapperWeapon03.lua').CAMZapperWeapon03
-CCannonMolecularWeapon = import('/lua/sim/weapons/cybran/CCannonMolecularWeapon.lua').CCannonMolecularWeapon
-CEMPAutoCannon = import('/lua/sim/weapons/cybran/CEMPAutoCannon.lua').CEMPAutoCannon
 CKrilTorpedoLauncherWeapon = import('/lua/sim/weapons/cybran/CKrilTorpedoLauncherWeapon.lua').CKrilTorpedoLauncherWeapon
 CMobileKamikazeBombWeapon = import('/lua/sim/weapons/cybran/CMobileKamikazeBombWeapon.lua').CMobileKamikazeBombWeapon
-CDFOverchargeWeapon = import('/lua/sim/weapons/cybran/CDFOverchargeWeapon.lua').CDFOverchargeWeapon
 
 -- kept for mod backwards compatibility
 local WeaponFile = import("/lua/sim/defaultweapons.lua")
