91f73f4cbaa624d0877b2e6e79046843df6f4e53
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2023-12-19 09:16:00 -0800

    Greatly improve charge effects of the Yolona Oss (#5758)

--- units/XSB2401/XSB2401_script.lua
+++ units/XSB2401/XSB2401_script.lua
@@ -15,6 +15,7 @@ XSB2401 = ClassUnit(SStructureUnit) {
     Weapons = {
         ExperimentalNuke = ClassWeapon(SIFExperimentalStrategicMissile) {
             OnWeaponFired = function(self)
+                self.unit:DestroyReloadEmitters()
                 self.unit.Trash:Add(ForkThread(self.unit.HideMissile,self.unit))
             end,
 
@@ -22,73 +23,143 @@ XSB2401 = ClassUnit(SStructureUnit) {
                 self.unit.Trash:Add(ForkThread(self.unit.ChargeNukeSound,self.unit))
                 SIFExperimentalStrategicMissile.PlayFxWeaponUnpackSequence(self)
             end,
+
+            PlayFxRackSalvoReloadSequence = function(self)
+                self.unit.PlayReloadEffects(self)
+                SIFExperimentalStrategicMissile.PlayFxRackSalvoReloadSequence(self)
+            end,
         },
         DeathWeapon = ClassWeapon(DeathNukeWeapon) {},
     },
 
+    ---@param self XSB2401
+    ---@param builder Unit
+    ---@param layer string
     StartBeingBuiltEffects = function(self, builder, layer)
         SStructureUnit.StartBeingBuiltEffects(self, builder, layer)
-        self.Trash:Add(ForkThread( CreateSeraphimExperimentalBuildBaseThread, builder, self.OnBeingBuiltEffectsBag, 2, self ))
+        self.Trash:Add(ForkThread( CreateSeraphimExperimentalBuildBaseThread, self, builder, self.OnBeingBuiltEffectsBag, 2 ))
     end,
 
+    ---@param self XSB2401
+    ---@param builder Unit
+    ---@param layer string
     OnStopBeingBuilt = function(self, builder, layer)
         SStructureUnit.OnStopBeingBuilt(self, builder, layer)
 
         local bp = self.Blueprint
-        self.Trash:Add(CreateAnimator(self):PlayAnim(bp.Display.AnimationOpen))
         local missileBone = bp.Display.MissileBone
         if missileBone then
             if not self.MissileSlider then
                 self.MissileSlider = CreateSlider(self, missileBone)
                 self.Trash:Add(self.MissileSlider)
+                self.MissileSlider:SetSpeed(400)
             end
         end
+        -- Convey to the player how the launcher reloads by showing the effect right when it is built
+        -- Also creates the initial pulsing glow for when the launcher is reloaded and missile is built
+        self.PlayReloadEffects(self:GetWeapon(1))
     end,
 
+    ---@param self XSB2401
+    ---@param weapon SIFExperimentalStrategicMissile
     OnSiloBuildStart = function(self, weapon)
-        self.Trash:Add(ForkThread(self.RaiseMissile,self))
+        self.Trash:Add(ForkThread(self.RaiseMissileThread,self))
         self.Trash:Add(ForkThread(self.WatchBuild,self))
         self.MissileBuilt = false
         self:PlayBuildEffects()
         SStructureUnit.OnSiloBuildStart(self, weapon)
     end,
 
+    --- @param self XSB2401
+    --- @param weapon SIFExperimentalStrategicMissile
     OnSiloBuildEnd = function(self, weapon)
         self.MissileBuilt = true
-        self:StopBuildEffects()
         SStructureUnit.OnSiloBuildEnd(self,weapon)
     end,
 
+    --- @param self XSB2401
     PlayBuildEffects = function(self)
-        if not self.MissileEffect then
-            self.MissileEffect = {}
-        end
-
-        local effectBones = self.Blueprint.General.BuildBones.BuildEffectBones
-        for bidx, bone in effectBones do
-            for k, v in EffectTemplate.SJammerCrystalAmbient do
-                if not self.MissileEffect[bidx] then
-                    self.MissileEffect[bidx] = {}
-                end
-                self.MissileEffect[bidx][k] = CreateAttachedEmitter(self,bone, self.Army, v)
-            end
-        end
         self:PlayUnitSound('Construct')
         self:PlayUnitAmbientSound('ConstructLoop')
     end,
 
+    --- @param self XSB2401
     StopBuildEffects = function(self)
-        local effectBones = self.Blueprint.General.BuildBones.BuildEffectBones
-        for bidx, bone in effectBones do
-            for k, v in EffectTemplate.SJammerCrystalAmbient do
-                self.MissileEffect[bidx][k]:Destroy()
-            end
-        end
         self:StopUnitAmbientSound('ConstructLoop')
         self:PlayUnitSound('ConstructStop')
     end,
 
-    RaiseMissile = function(self)
+    --- Creates an emitter indicating the stage of the reload process.
+    ---@param self XSB2401
+    ---@param bidx number Index of the bone to use: 1, 2, 3 = "NO1", "NO2", "NO3"
+    CreateReloadEmitter = function(self, bidx)
+        if not self.MissileEffect then
+            self.MissileEffect = {}
+        end
+        local scales = {0.8, 0.7, 0.45}
+        local effectBones = self.Blueprint.General.BuildBones.BuildEffectBones
+        for k, v in EffectTemplate.SJammerCrystalAmbient do
+            if not self.MissileEffect[bidx] then
+                self.MissileEffect[bidx] = {}
+            end
+            self.MissileEffect[bidx][k] = CreateAttachedEmitter(self, effectBones[bidx], self.Army, v):OffsetEmitter(0, -0.2 * scales[bidx], -scales[bidx]):ScaleEmitter(scales[bidx])
+        end
+    end,
+
+    --- @param self XSB2401
+    DestroyReloadEmitters = function(self)
+        local effectBones = self.Blueprint.General.BuildBones.BuildEffectBones
+        for bidx, bone in self.MissileEffect do
+            for _, effect in self.MissileEffect[bidx] do
+                effect:Destroy()
+            end
+        end
+    end,
+
+    -- Keep this function here instead of in the weapon so OnStopBeingBuilt can call it.
+    --- @param weapon SIFExperimentalStrategicMissile
+    PlayReloadEffects = function(weapon)
+        local bp = weapon.Blueprint
+        local muzzleBones = bp.RackBones[weapon.CurrentRackSalvoNumber].MuzzleBones
+        local unit = weapon.unit
+        local army = weapon.Army
+        local scale = weapon.FxRackChargeMuzzleFlashScale
+
+        -- Create the upwards lines and coalescing orbs effects
+        for _, effect in EffectTemplate.SIFExperimentalStrategicLauncherReload01 do
+            for _, muzzle in muzzleBones do
+                CreateAttachedEmitter(unit, muzzle, army, effect):ScaleEmitter(scale)
+            end
+        end
+
+        -- Create the reload stage effects: 3 effects in the middle column and a blue glow when the missile is built and launcher is reloaded
+        local reloadTime = bp.RackSalvoReloadTime
+        weapon.Trash:Add(ForkThread(function()
+            local stage = 0
+            while stage < 3 do
+                WaitSeconds(reloadTime/3)
+                stage = stage + 1
+                unit:CreateReloadEmitter(stage)
+            end
+            while not unit.MissileBuilt do
+                WaitTicks(1)
+            end
+            local template = EffectTemplate.SIFExperimentalStrategicLauncherLoaded01
+            for i, effect in template do
+                if not unit.MissileEffect[stage + 1] then
+                    unit.MissileEffect[stage + 1] = {}
+                end
+                for k, muzzle in muzzleBones do
+                    unit.MissileEffect[stage + 1][k] = CreateAttachedEmitter(unit, muzzle, army, effect):ScaleEmitter(scale)
+                end
+            end
+            unit:StopBuildEffects()
+        end))
+    end,
+
+    --- Raises the missile depending on completion progress.
+    --- @param self XSB2401
+    RaiseMissileThread = function(self)
         self.NotCancelled = true
         WaitTicks(6)
 
@@ -96,28 +167,38 @@ XSB2401 = ClassUnit(SStructureUnit) {
         if missileBone and self.NotCancelled then
             self:ShowBone(missileBone, true)
             if self.MissileSlider then
+                local progress = 0
+                repeat
+                    if not self.NotCancelled then return end
+                    progress = self:GetWorkProgress()
+                    self.MissileSlider:SetGoal(0, 0, 115 * progress)
+                    WaitTicks(1)
+                    -- Stopping at 100% progress is impossible; the work progress bar hides and resets to 0 when work is finished
+                until progress >= 0.99
                 self.MissileSlider:SetGoal(0, 0, 115)
-                self.MissileSlider:SetSpeed(1.93)
             end
         end
     end,
 
+    --- @param self XSB2401
     HideMissile = function(self)
         WaitTicks(2)
         self:RetractMissile()
     end,
 
+    --- @param self XSB2401
     RetractMissile = function(self)
         local missileBone = self.Blueprint.Display.MissileBone
         if missileBone then
             self:HideBone(missileBone, true)
             if self.MissileSlider then
-                self.MissileSlider:SetSpeed(400)
                 self.MissileSlider:SetGoal(0,0,0)
             end
         end
     end,
 
+    --- Hides the missile if building is cancelled.
+    --- @param self XSB2401
     WatchBuild = function(self)
         while true do
             if not self:IsUnitState('SiloBuildingAmmo') and not self.MissileBuilt then
@@ -130,6 +211,7 @@ XSB2401 = ClassUnit(SStructureUnit) {
         end
     end,
 
+    --- @param self XSB2401
     ChargeNukeSound = function(self)
         WaitTicks(16)
         self:PlayUnitAmbientSound('NukeCharge')
