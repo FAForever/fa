affc611742c1e0e34580b51f1377c4c2e1901d7c
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2023-12-31 20:21:37 -0800

    Annotate Aeon missiles

--- projectiles/AIFMissileSerpentine01/AIFMissileSerpentine01_script.lua
+++ projectiles/AIFMissileSerpentine01/AIFMissileSerpentine01_script.lua
@@ -1,6 +1,6 @@
 local AMissileSerpentineProjectile = import("/lua/aeonprojectiles.lua").AMissileSerpentineProjectile
 
--- Aeon Serpentine Missile
+--- Used by ual0111 (T2 Mobile Missile Launcher)
 ---@class AIFMissileSerpentine01: AMissileSerpentineProjectile
 AIFMissileSerpentine01 = ClassProjectile(AMissileSerpentineProjectile) {
 
--- projectiles/AIFMissileSerpentine02/AIFMissileSerpentine02_script.lua
+++ projectiles/AIFMissileSerpentine02/AIFMissileSerpentine02_script.lua
@@ -1,6 +1,6 @@
 local AMissileSerpentineProjectile = import("/lua/aeonprojectiles.lua").AMissileSerpentineProjectile
 
---- Aeon Serpentine Missile
+--- Used by UAS0304 (T3 Stategic Missile Submarine)
 ---@class AIFMissileSerpentine02 : AMissileSerpentineProjectile
 AIFMissileSerpentine02 = ClassProjectile(AMissileSerpentineProjectile) {
     FxWaterHitScale = 1.65,
