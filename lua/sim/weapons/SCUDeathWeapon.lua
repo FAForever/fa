ea4f3507ee55306f0e56bbf327b4b029361f28f0
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2024-01-05 21:57:38 +0100

    Create separate classes for all base weapons (#5803)

--- lua/sim/weapons/SCUDeathWeapon.lua
+++ lua/sim/weapons/SCUDeathWeapon.lua
@@ -20,26 +20,18 @@
 --** SOFTWARE.
 --**********************************************************************************
 
-DefaultProjectileWeapon = import("/lua/sim/weapons/DefaultProjectileWeapon.lua").DefaultProjectileWeapon
-KamikazeWeapon = import("/lua/sim/weapons/KamikazeWeapon.lua").KamikazeWeapon
-BareBonesWeapon = import("/lua/sim/weapons/BareBonesWeapon.lua").BareBonesWeapon
-OverchargeWeapon = import("/lua/sim/weapons/OverchargeWeapon.lua").OverchargeWeapon
-DefaultBeamWeapon = import("/lua/sim/weapons/DefaultBeamWeapon.lua").DefaultBeamWeapon
-DeathNukeWeapon = import("/lua/sim/weapons/DeathNukeWeapon.lua").DeathNukeWeapon
-SCUDeathWeapon = import("/lua/sim/weapons/SCUDeathWeapon.lua").SCUDeathWeapon
+local BareBonesWeapon = import("/lua/sim/weapons/BareBonesWeapon.lua").BareBonesWeapon
 
--- kept for mod backwards compatibility
-local Weapon = import("/lua/sim/weapon.lua").Weapon
-local XZDist = import("/lua/utilities.lua").XZDistanceTwoVectors
+---@class SCUDeathWeapon : BareBonesWeapon
+SCUDeathWeapon = ClassWeapon(BareBonesWeapon) {
+    ---@param self SCUDeathWeapon
+    OnFire = function(self)
+    end,
 
-local EntityMethods = moho.entity_methods
-local EntityGetPosition = EntityMethods.GetPosition
-local EntityGetPositionXYZ = EntityMethods.GetPositionXYZ
-
-local UnitMethods = moho.unit_methods
-local UnitGetVelocity = UnitMethods.GetVelocity
-local UnitGetTargetEntity = UnitMethods.GetTargetEntity
-
-local MathClamp = math.clamp
-local GetSurfaceHeight = GetSurfaceHeight
-local VDist2 = VDist2
+    ---@param self SCUDeathWeapon
+    Fire = function(self)
+        local bp = self.Blueprint
+        local proj = self.unit:CreateProjectile(bp.ProjectileId, 0, 0, 0, nil, nil, nil):SetCollision(false)
+        proj:PassDamageData(self:GetDamageTable())
+    end,
+}
