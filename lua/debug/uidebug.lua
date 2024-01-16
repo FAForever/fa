5323fe2f60af807a717987e769c68fd0bc0a4c1b
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-01-16 19:50:54 +0100

    Add standard deviation

--- lua/debug/uidebug.lua
+++ lua/debug/uidebug.lua
@@ -1,7 +1,37 @@
+--******************************************************************************************************
+--** Copyright (c) 2024  FAForever
+--**
+--** Permission is hereby granted, free of charge, to any person obtaining a copy
+--** of this software and associated documentation files (the "Software"), to deal
+--** in the Software without restriction, including without limitation the rights
+--** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--** copies of the Software, and to permit persons to whom the Software is
+--** furnished to do so, subject to the following conditions:
+--**
+--** The above copyright notice and this permission notice shall be included in all
+--** copies or substantial portions of the Software.
+--**
+--** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+--** SOFTWARE.
+--******************************************************************************************************
+
 local UIUtil = import("/lua/ui/uiutil.lua")
 local LayoutHelpers = import("/lua/maui/layouthelpers.lua")
+local Statistics = import("/lua/shared/statistics.lua")
 
 local fps = false
+
+local framerateCurrent = 1
+local framerateMax = 30
+local framerates = {}
+for k = 1, framerateMax do
+    framerates[k] = 0
+end
 
 function ShowFPS()
     if fps then
@@ -9,7 +39,7 @@ function ShowFPS()
         fps = false
         return
     end
-    
+
     fps = UIUtil.CreateText(GetFrame(0), '', 12)
     fps:SetColor('ffffff')
     fps:SetFont(UIUtil.fixedFont, 12)
@@ -17,24 +47,36 @@ function ShowFPS()
     fps:DisableHitTest()
     fps.Depth:Set(GetFrame(0):GetTopmostDepth() + 1)
     LayoutHelpers.AtRightTopIn(fps, GetFrame(0), 425, 0)
-    
+
     fps:SetNeedsFrameUpdate(true)
     fps.OnFrame = function(self, deltatime)
-        if not self.timer or self.timer > 0.4 then
-            for key, val in __EngineStats.Children do
-                if val.Name == 'Frame' then
-                    for childKey, childVal in val.Children do
-                        if childVal.Name == 'FPS' then
-                            self:SetText(string.format("FPS: %.0f", childVal.Value))
-                            break
+        for key, val in __EngineStats.Children do
+            if val.Name == 'Frame' then
+                for childKey, childVal in val.Children do
+                    if childVal.Name == 'FPS' then
+
+                        framerates[framerateCurrent] = childVal.Value
+                        framerateCurrent = framerateCurrent + 1
+                        if framerateCurrent > framerateMax then
+                            framerateCurrent = 1
                         end
+
+                        local mean = Statistics.Mean(framerates, framerateMax)
+                        local std = Statistics.Deviation(framerates, framerateMax, mean)
+
+                        self:SetText(string.format("FPS: %03.0f (σ %0.03f)", mean, std))
+                        break
                     end
-                    break
                 end
+                break
             end
-            self.timer = 0
-        else
-            self.timer = self.timer + deltatime
         end
+    end
+end
+
+--- Called by the module manager when this module is dirty due to a disk change
+function __moduleinfo.OnDirty()
+    if fps then
+        fps:Destroy()
     end
 end
--- lua/shared/statistics.lua
+++ lua/shared/statistics.lua
@@ -1,15 +1,37 @@
+--******************************************************************************************************
+--** Copyright (c) 2024  FAForever
+--**
+--** Permission is hereby granted, free of charge, to any person obtaining a copy
+--** of this software and associated documentation files (the "Software"), to deal
+--** in the Software without restriction, including without limitation the rights
+--** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--** copies of the Software, and to permit persons to whom the Software is
+--** furnished to do so, subject to the following conditions:
+--**
+--** The above copyright notice and this permission notice shall be included in all
+--** copies or substantial portions of the Software.
+--**
+--** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+--** SOFTWARE.
+--******************************************************************************************************
 
 local TableGetn = table.getn
 
 --- Computes the sum of an array of values
--- @param t Table to compute values over, only array elements are used.
--- @param n Number of elements, defaults to table.getn(t)
+---@param t number[] Array to compute values over, only array elements are used.
+---@param n number Count of elements, defaults to table.getn(t)
+---@return number
 function Sum(t, n)
     -- allow for optionals
     n = n or TableGetn(t)
 
     local sum = 0
-    for k = 1, n do 
+    for k = 1, n do
         sum = sum + t[k]
     end
 
@@ -17,28 +39,28 @@ function Sum(t, n)
 end
 
 --- Computes the mean of an array of values
--- @param t Table to compute values over, only array elements are used.
--- @param n Number of elements, defaults to table.getn(t)
+---@param t number[] Array to compute values over, only array elements are used.
+---@param n number Count of elements, defaults to table.getn(t)
+---@return number
 function Mean(t, n)
-
     -- allow for optionals
     n = n or TableGetn(t)
 
     -- compute mean
     local mean = 0
-    for k = 1, n do 
+    for k = 1, n do
         mean = mean + t[k]
     end
 
     return mean * (1 / n)
 end
 
---- Computes the deviation of an array of values
--- @param t Table to compute values over, only array elements are used.
--- @param n Number of elements, defaults to table.getn(t)
--- @param m Mean of table, defaults to Mean(t, n)
+--- Computes the standard deviation of an array of values
+---@param t number[] Array to compute values over, only array elements are used.
+---@param n number Count of elements, defaults to table.getn(t)
+---@param m number Mean of table, defaults to Mean(t, n)
+---@return number
 function Deviation(t, n, m)
-
     -- allow for optionals
     n = n or TableGetn(t)
     m = m or Mean(t, n)
@@ -46,7 +68,7 @@ function Deviation(t, n, m)
     -- compute deviation
     local i = 0
     local deviation = 0
-    for k = 1, n do 
+    for k = 1, n do
         i = t[k] - m
         deviation = deviation + i * i
     end
