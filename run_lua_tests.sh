b0b48d1c9b291306f295bc76ac7c5836c60397e7
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-01-19 19:36:09 +0100

    Fix test file

--- run_lua_tests.sh
+++ run_lua_tests.sh
@@ -4,7 +4,6 @@
 # Assumes that FAF lua is installed as `lua`
 
 had_error=0
-tests_complete=0
 
 run_test() {
   file="$1"
@@ -22,16 +21,11 @@ run_test() {
 
 # make sure the test files run in the tests directory
 pushd tests >/dev/null
-while read file; do
-  # exclude the unit tester module
-  if [ "$file" != "./lust.lua" ]; then
-    run_test "$file"
-    ((tests_complete++))
-  fi
-done < <(find . -name '*.lua')
-popd >/dev/null
 
-echo "Ran $tests_complete tests"
+run_test "test_colors.lua"
+run_test "test_utils.lua"
+
+popd >/dev/null
 
 if [[ $had_error != 0 ]]; then
   echo "Tests returned errors."
