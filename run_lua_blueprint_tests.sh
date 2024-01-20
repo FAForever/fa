f315d03f03a6d2c737681ee464a5a646554d999a
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2024-01-20 09:33:55 +0100

    Fix issues with intel ranges (#5836)
    Introduces a test suite so that we can no longer have these type of issues related to intel in the future.

---
+++ run_lua_blueprint_tests.sh
@@ -0,0 +1,69 @@
+# sanity checks on the unit blueprint files
+
+directory="./units"
+output_file="./tests/blueprints/generated-unit-blueprint-list.lua"
+
+# Check if the directory exists
+if [ ! -d "$directory" ]; then
+    echo "Error: Directory $directory not found."
+    exit 1
+fi
+
+# Create an associative array to store file paths
+declare -A lua_files
+
+# Iterate over Lua files in the directory
+for file in "$directory"/*/*.bp; do
+    if [ -f "$file" ]; then
+
+        # Add the file to the Lua table
+        filename=$(basename "$file")
+        lua_files["$filename"]=$file
+    fi
+done
+
+# Generate Lua table
+lua_table="Files = {\n"
+
+for key in "${!lua_files[@]}"; do
+    lua_table="$lua_table \"${lua_files[$key]}\",\n"
+done
+
+lua_table="$lua_table}\n"
+
+# Write Lua table to disk
+echo -e "$lua_table" >"$output_file"
+
+echo "Lua table written to $output_file"
+
+# Run the tests
+
+had_error=0
+tests_complete=0
+
+run_test() {
+    file="$1"
+
+    output="$(lua "$file" 2>&1 |
+        sed "s:/dev/fd/[0-9]\+:$file:g")"
+
+    if [[ $output != "" ]]; then
+        echo "$output" >/dev/fd/2
+        if [[ "$output" == *"FAIL"* || "$output" == *"stack traceback"* ]]; then
+            had_error=1
+        fi
+    fi
+}
+
+pushd tests >/dev/null
+run_test "./unit.spec.lua"
+popd >/dev/null
+
+if [[ $had_error != 0 ]]; then
+    echo "Tests returned errors."
+    exit $had_error
+else
+    echo "Tests OK."
+fi
+
+echo "${lua--help}"
--- run_lua_tests.sh
+++ run_lua_tests.sh
@@ -4,17 +4,16 @@
 # Assumes that FAF lua is installed as `lua`
 
 had_error=0
-tests_complete=0
 
 run_test() {
   file="$1"
 
-  output="$(lua "$file" 2>&1 \
-    | sed "s:/dev/fd/[0-9]\+:$file:g")"
+  output="$(lua "$file" 2>&1 |
+    sed "s:/dev/fd/[0-9]\+:$file:g")"
 
   if [[ $output != "" ]]; then
-    echo "$output" > /dev/fd/2
-    if [[ "$output" == *"FAIL"* ]]; then
+    echo "$output" >/dev/fd/2
+    if [[ "$output" == *"FAIL"* || "$output" == *"stack traceback"* ]]; then
       had_error=1
     fi
   fi
@@ -22,16 +21,11 @@ run_test() {
 
 # make sure the test files run in the tests directory
 pushd tests >/dev/null
-while read file; do
-  # exclude the unit tester module
-  if [ "$file" != "./lust.lua" ]; then
-    run_test "$file"
-    ((tests_complete ++))
-  fi
-done < <(find . -name '*.lua')
-popd >/dev/null
 
-echo "Ran $tests_complete tests"
+run_test "test_colors.lua"
+run_test "test_utils.lua"
+
+popd >/dev/null
 
 if [[ $had_error != 0 ]]; then
   echo "Tests returned errors."
