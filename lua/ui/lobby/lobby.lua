11868f2fe20d8b7494a2f91dfa05d521b2fe40c7
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-02-04 10:05:14 +0100

    Remove unneccesary calls to 'SavePreferences'

--- lua/ui/lobby/lobby.lua
+++ lua/ui/lobby/lobby.lua
@@ -2417,7 +2417,6 @@ local function UpdateGame()
                 function()
                     -- store in preferences so that we can retrieve it during blueprint loading
                     SetPreference('PreGameData', preGameData)
-                    -- SavePreferences()
                 end
             )
 
@@ -6613,7 +6612,6 @@ end
 -- Write the given list of preset profiles to persistent storage.
 function SavePresetsList(list)
     Prefs.SetToCurrentProfile("LobbyPresets", list)
-    -- SavePreferences()
 end
 
 --- Delegate to UIUtil's CreateInputDialog, adding the ridiculus chatEdit hack.
