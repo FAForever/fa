8f23262d0eadb684b1e5e578f766a50d94909873
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-02-09 21:01:42 +0100

    Gradually stop spinning the rotators when the unit is killed

--- units/UES0103/UES0103_script.lua
+++ units/UES0103/UES0103_script.lua
@@ -10,28 +10,43 @@
 local TSeaUnit = import("/lua/terranunits.lua").TSeaUnit
 local TAALinkedRailgun = import("/lua/terranweapons.lua").TAALinkedRailgun
 local TDFGaussCannonWeapon = import("/lua/terranweapons.lua").TDFGaussCannonWeapon
-local Entity = import("/lua/sim/entity.lua").Entity
 
 ---@class UES0103 : TSeaUnit
+---@field Spinner01 moho.RotateManipulator
+---@field Spinner02 moho.RotateManipulator
+---@field Spinner03 moho.RotateManipulator
 UES0103 = ClassUnit(TSeaUnit) {
     Weapons = {
         MainGun = ClassWeapon(TDFGaussCannonWeapon) {},
         AAGun = ClassWeapon(TAALinkedRailgun) {},
     },
 
-    OnStopBeingBuilt = function(self,builder,layer)
-        TSeaUnit.OnStopBeingBuilt(self,builder,layer)
-        self.Trash:Add(CreateRotator(self, 'Spinner01', 'y', nil, 360, 0, 180))
-        self.Trash:Add(CreateRotator(self, 'Spinner02', 'y', nil, 90, 0, 180))
-        self.Trash:Add(CreateRotator(self, 'Spinner03', 'y', nil, -180, 0, -180))
-        self.RadarEnt = Entity {}
-        self.Trash:Add(self.RadarEnt)
-        local bp = self:GetBlueprint()
-        self.RadarEnt:InitIntel(self.Army, 'Radar', bp.Intel.RadarRadius or 75)
-        self.RadarEnt:EnableIntel('Radar')
-        self.RadarEnt:InitIntel(self.Army, 'Sonar', bp.Intel.SonarRadius or 75)
-        self.RadarEnt:EnableIntel('Sonar')
-        self.RadarEnt:AttachBoneTo(-1, self, 0)
+    ---@param self UES0103
+    ---@param builder Unit
+    ---@param layer Layer
+    OnStopBeingBuilt = function(self, builder, layer)
+        TSeaUnit.OnStopBeingBuilt(self, builder, layer)
+        local trash = self.Trash
+        self.Spinner01 = trash:Add(CreateRotator(self, 'Spinner01', 'y', nil, 360, 0, 180))
+        self.Spinner02 = trash:Add(CreateRotator(self, 'Spinner02', 'y', nil, 90, 0, 180))
+        self.Spinner03 = trash:Add(CreateRotator(self, 'Spinner03', 'y', nil, -180, 0, -180))
+    end,
+
+    ---@param self UES0103
+    ---@param instigator Unit
+    ---@param type DamageType
+    ---@param overkillRatio number
+    OnKilled = function(self, instigator, type, overkillRatio)
+        TSeaUnit.OnKilled(self, instigator, type, overkillRatio)
+
+        if self:GetFractionComplete() >= 1.0 then
+            self.Spinner01:SetTargetSpeed(0)
+            self.Spinner01:SetAccel(-40)
+            self.Spinner02:SetTargetSpeed(0)
+            self.Spinner02:SetAccel(-10)
+            self.Spinner03:SetAccel(20)
+            self.Spinner03:SetTargetSpeed(0)
+        end
     end,
 }
 
