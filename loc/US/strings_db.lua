d7f0ec5591d8ef1d377439f4f39bc4b730ed4ede
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2024-02-10 23:59:05 -0800

    Update Chrono description

--- loc/US/strings_db.lua
+++ loc/US/strings_db.lua
@@ -1749,7 +1749,7 @@ Unit_Description_s204="Constructs Tech 3 Naval units. Buildable for a much cheap
 -- AEON -- Armored Commander Unit/Upgrades
 Unit_Description_0305="The Armored Command Unit (ACU) is a combination of barracks and command center. Contains all the blueprints necessary to build a basic army from scratch. Upgradeable with combat enhancements, advanced engineering suites, resource allocation system, and teleportation."
 Unit_Description_0156="Grants Tech 2 schematic access and increases the ACU's build speed and maximum health.\n\n+32 Buildpower\n+2000 Health\n+10 Regen"
-Unit_Description_0157="Creates a Quantum Stasis Field around the ACU. Immobilizes enemy units within the ACU's main cannon range.\n\n+3000 Health"
+Unit_Description_0157="Creates a Quantum Stasis Field around the ACU. Immobilizes enemy units within the ACU's main cannon range. Multiple Quantum Stasis Fields interfere with each other, having a limited effect.\n\n+3000 Health"
 Unit_Description_0158="Increases the range of the ACU's main cannon and that of Overcharge.\n\n+8 Main cannon range"
 Unit_Description_0159="Grants the ACU a long range omni Sensor and increased optical range.\n\n+50 Vision Radius\n+54 Omni Radius"
 Unit_Description_0160="Grants Tech 3 and Experimental schematic access and further increases the ACU's build speed and maximum health.\n\n+58 Buildpower\n+1000 Health\n+10 Regen"
