27f5d31f9aefa1ff15da660ea597699b833e9b50
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2024-02-17 18:12:45 +0100

    Refactor Seraphim torpedo's and improve aesthetics and performance (#5911)

--- projectiles/SANHeavyCavitationTorpedo04/SANHeavyCavitationTorpedo04_script.lua
+++ projectiles/SANHeavyCavitationTorpedo04/SANHeavyCavitationTorpedo04_script.lua
@@ -1,23 +1,37 @@
-﻿-- File     :  /data/projectiles/SANHeavyCavitationTorpedo04/SANHeavyCavitationTorpedo04_script.lua
--- Author(s):  Gordon Duclos
--- Summary  :  Heavy Cavitation Torpedo Projectile script, XSA0204
---  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
-----------------------------------------------------------------------------------------------------
+﻿--******************************************************************************************************
+--** Copyright (c) 2023 FAForever
+--**
+--** Permission is hereby granted, free of charge, to any person obtaining a copy
+--** of this software and associated documentation files (the "Software"), to deal
+--** in the Software without restriction, including without limitation the rights
+--** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--** copies of the Software, and to permit persons to whom the Software is
+--** furnished to do so, subject to the following conditions:
+--**
+--** The above copyright notice and this permission notice shall be included in all
+--** copies or substantial portions of the Software.
+--**
+--** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+--** SOFTWARE.
+--******************************************************************************************************
+
 local SHeavyCavitationTorpedo = import("/lua/seraphimprojectiles.lua").SHeavyCavitationTorpedo
+
 local EffectTemplate = import("/lua/effecttemplates.lua")
 
 --- Heavy Cavitation Torpedo Projectile script, XSA0204
 ---@class SANHeavyCavitationTorpedo04 : SHeavyCavitationTorpedo
 SANHeavyCavitationTorpedo04 = ClassProjectile(SHeavyCavitationTorpedo) {
 
-    ---@param self SANHeavyCavitationTorpedo04
-    OnCreate = function(self)
-        SHeavyCavitationTorpedo.OnCreate(self)
-        self:SetCollisionShape('Sphere', 0, 0, 0, 0.5)
-        self.Trash:Add(ForkThread(self.PauseUntilTrack, self))
-        CreateEmitterOnEntity(self, self.Army, EffectTemplate.SHeavyCavitationTorpedoFxTrails)
-    end,
+    FxTrails = { EffectTemplate.SHeavyCavitationTorpedoFxTrails },
+    FxEnterWater = EffectTemplate.WaterSplash01,
 
+    ---@deprecated
     ---@param self SANHeavyCavitationTorpedo04
     PauseUntilTrack = function(self)
         WaitTicks(2)
