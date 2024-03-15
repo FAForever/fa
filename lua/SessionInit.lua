70c9c85caea7f5233aafc5ec6fa3e65242d336a8
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-03-15 11:44:11 +0100

    Add safe guards in case fields are missing

--- lua/SessionInit.lua
+++ lua/SessionInit.lua
@@ -9,13 +9,13 @@
 doscript '/lua/userInit.lua'
 
 -- Add UI-only mods to the list of mods to use
-for i,m in ipairs(import("/lua/mods.lua").GetUiMods()) do
+for i, m in ipairs(import("/lua/mods.lua").GetUiMods()) do
     table.insert(__active_mods, m)
 end
 
 LOG('Active mods in session:')
 for _, mod in __active_mods do
-    LOG(string.format('\t"%-30s v%02d (%-37s by %s', mod.name..'"', mod.version, mod.uid..')', mod.author))
+    LOG(string.format('\t"%-30s v%02d (%-37s by %s', tostring(mod.name) .. '"', tostring(mod.version), tostring(mod.uid) .. ')', tostring(mod.author)))
 end
 
 doscript '/lua/UserSync.lua'
