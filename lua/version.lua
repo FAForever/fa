eec032237ca4d38b4acb4d515bf922cc20b37830
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-03-17 20:51:00 +0100

    Setup for game version 3803

--- lua/version.lua
+++ lua/version.lua
@@ -1,7 +1,7 @@
 
-local Version = "3802"
----@alias PATCH "3802"
----@alias VERSION "1.5.3802"
+local Version = "3803"
+---@alias PATCH "3803"
+---@alias VERSION "1.5.3803"
 ---@return PATCH
 function GetVersion()
     LOG('Supreme Commander: Forged Alliance version ' .. Version)
