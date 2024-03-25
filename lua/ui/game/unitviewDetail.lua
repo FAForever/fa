31d2339ba59c741934d17d8416024924df92e7c5
Basilisk3 <126026384+Basilisk3@users.noreply.github.com>
2024-03-25 20:07:31 +0100

    UnitviewDetail: No longer display 'Range' and 'Reload' values for the 'Teleport in' weapon

--- lua/ui/game/unitviewDetail.lua
+++ lua/ui/game/unitviewDetail.lua
@@ -647,6 +647,9 @@ function WrapAndPlaceText(bp, builder, descID, control)
                             if CycleProjs > 1 then
                                  weaponDetails2 = string.format(LOC('<LOC uvd_0015>Damage: %.8g x%d, Splash: %.3g')..', '..LOC('<LOC uvd_Range>')..', '..LOC('<LOC uvd_Reload>'),
                                     Damage, CycleProjs, info.DamageRadius, info.MinRadius, info.MaxRadius, CycleTime)
+                            elseif info.WeaponCategory == "Teleport" then
+                                weaponDetails2 = string.format(LOC('<LOC uvd_0010>Damage: %.7g, Splash: %.3g'),
+                                Damage, info.DamageRadius)
                             else
                                 weaponDetails2 = string.format(LOC('<LOC uvd_0010>Damage: %.7g, Splash: %.3g')..', '..LOC('<LOC uvd_Range>')..', '..LOC('<LOC uvd_Reload>'),
                                     Damage, info.DamageRadius, info.MinRadius, info.MaxRadius, CycleTime)
