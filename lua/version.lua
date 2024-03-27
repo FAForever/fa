6a51569c7756de5b531a0db081ee5952b25be4fe
Basilisk3 <126026384+Basilisk3@users.noreply.github.com>
2024-03-27 02:42:09 +0100

    Setup for game version 3806 (#6020)
    Co-authored-by: lL1l1 <82986251+lL1l1@users.noreply.github.com>

--- lua/version.lua
+++ lua/version.lua
@@ -1,7 +1,7 @@
 
-local Version = "3805"
----@alias PATCH "3805"
----@alias VERSION "1.5.3805"
+local Version = "3806"
+---@alias PATCH "3806"
+---@alias VERSION "1.5.3806"
 ---@return PATCH
 function GetVersion()
     LOG('Supreme Commander: Forged Alliance version ' .. Version)
