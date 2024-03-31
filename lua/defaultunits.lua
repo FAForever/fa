06db1c788270f488a9a1b9124811142d1285d1a5
Pokute <pickledpokute@gmail.com>
2024-03-31 23:09:38 +0300

    Remove unused perf upvalues.

--- lua/defaultunits.lua
+++ lua/defaultunits.lua
@@ -88,9 +88,4 @@ local RolloffPositionTable = { 0, 0, 0 }
 -- allows us to skip ai-specific functionality
 local GameHasAIs = ScenarioInfo.GameHasAIs
 
--- compute once and store as upvalue for performance
-local StructureUnitRotateTowardsEnemiesLand = categories.STRUCTURE + categories.LAND + categories.NAVAL
-local StructureUnitRotateTowardsEnemiesArtillery = categories.ARTILLERY * (categories.TECH2 + categories.TECH3 + categories.EXPERIMENTAL)
-local StructureUnitOnStartBeingBuiltRotateBuildings = categories.STRUCTURE * (categories.DIRECTFIRE + categories.INDIRECTFIRE) * (categories.DEFENSE + (categories.ARTILLERY - (categories.TECH3 + categories.EXPERIMENTAL)))
-
 --#endregion
