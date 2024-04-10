18026288a75639fad7cf7f9ca2b88cb3092a553c
clyf <clyfordv@gmail.com>
2024-04-10 14:13:28 -0700

    optional Unit params

--- lua/sim/Unit.lua
+++ lua/sim/Unit.lua
@@ -113,7 +113,7 @@ local cUnitGetBuildRate = cUnit.GetBuildRate
 ---@field Brain AIBrain
 ---@field buildBots? Unit[]
 ---@field Blueprint UnitBlueprint
----@field UnitName string
+---@field UnitName? string
 ---@field BuildEffectsBag TrashBag
 ---@field BuildArmManipulator? moho.BuilderArmManipulator
 ---@field Trash TrashBag
@@ -139,9 +139,9 @@ local cUnitGetBuildRate = cUnit.GetBuildRate
 ---@field SiloProjectile? ProjectileBlueprint
 ---@field ReclaimTimeMultiplier? number
 ---@field CaptureTimeMultiplier? number
----@field BaseName string
----@field CDRData table
----@field PlatoonData table
+---@field BaseName? string
+---@field CDRData? table
+---@field PlatoonData? table
 Unit = ClassUnit(moho.unit_methods, IntelComponent, VeterancyComponent) {
 
     IsUnit = true,
