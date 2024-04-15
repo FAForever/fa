285820e059ce317ab252e7168f3c794d4bb282d7
clyf <clyfordv@gmail.com>
2024-04-15 08:15:22 -0400

    AIBrain CustomUnits field

--- lua/aibrain.lua
+++ lua/aibrain.lua
@@ -551,6 +551,7 @@ local CategoriesDummyUnit = categories.DUMMYUNIT
 ---@field UnitBuiltTriggerList table
 ---@field PingCallbackList { CallbackFunction: fun(pingData: any), PingType: string }[]
 ---@field BrainType 'Human' | 'AI'
+---@field CustomUnits { [string]: EntityId[] }
 AIBrain = Class(AIBrainHQComponent, AIBrainStatisticsComponent, AIBrainJammerComponent, AIBrainEnergyComponent,
     moho.aibrain_methods) {
 
