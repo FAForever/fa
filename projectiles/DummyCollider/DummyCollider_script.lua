d4808b49b7f358f483aca7be0547a3c5c551a453
clyf <clyfordv@gmail.com>
2024-04-22 19:45:41 -0400

    dummy collider fix

--- projectiles/DummyCollider/DummyCollider_script.lua
+++ projectiles/DummyCollider/DummyCollider_script.lua
@@ -1,6 +1,14 @@
 ---@class DummyCollider : DummyProjectile
 DummyCollider = ClassDummyProjectile(import("/lua/sim/projectile.lua").DummyProjectile) {
 
+    OnCreate = function(self)
+        self:SetVizToFocusPlayer('Never') -- Set to 'Always' to see a nice box
+        self:SetVizToAllies('Never')
+        self:SetVizToNeutrals('Never')
+        self:SetVizToEnemies('Never')
+        self:SetCollision(true)
+    end,
+
     OnCollisionCheck = function(self, other)
         return self.Parent:OnCollisionCheck(other)
     end,
