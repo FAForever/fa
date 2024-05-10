fdf7c459c1a8ea6548dc875905e7a6faea77f52f
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2024-05-09 23:20:23 -0700

    Annotate DefaultProjWep's ReloadState OnLostTarget

--- lua/sim/weapons/DefaultProjectileWeapon.lua
+++ lua/sim/weapons/DefaultProjectileWeapon.lua
@@ -1192,6 +1192,7 @@ DefaultProjectileWeapon = ClassWeapon(Weapon) {
         end,
 
         OnLostTarget = function(self)
+            -- Override default OnLostTarget to prevent bypassing reload time by switching to idle state immediately
         end,
     },
 
