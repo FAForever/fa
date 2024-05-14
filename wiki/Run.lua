ad8db8c4f204ae589da04074ad40ea5265a4bc47
relent0r <aaronpaulwarner@gmail.com>
2024-05-15 09:27:16 +1200

    try dump the args table

--- wiki/Run.lua
+++ wiki/Run.lua
@@ -34,6 +34,10 @@ end
 
 local args = parse_args(arg)
 
+print("Contents of the args table:")
+for k, v in pairs(args) do
+    print(k, v)
+end
 
 print("Output Directory: " .. tostring(args["OutputDirectory"]))
 print("Wiki Generator Directory: " .. tostring(args["WikiGeneratorDirectory"]))
