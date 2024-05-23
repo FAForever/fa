92313edd98f2c65b918621bb736bf125647e244a
clyf <clyfordv@gmail.com>
2024-05-23 17:16:47 -0400

    persistent shoulder drone guard orders

--- lua/sim/units/uef/TConstructionPodUnit.lua
+++ lua/sim/units/uef/TConstructionPodUnit.lua
@@ -1,8 +1,16 @@
 local TConstructionUnit = import("/lua/terranunits.lua").TConstructionUnit
 
 ---@class TConstructionPodUnit : TConstructionUnit
+---@field guardDummy Unit
 TConstructionPodUnit = ClassUnit(TConstructionUnit) {
     Parent = nil,
+
+    OnCreate = function(self)
+        TConstructionUnit.OnCreate(self)
+        self.guardDummy = CreateUnitHPR('ZXA0003', self:GetArmy(), 0,0,0,0,0,0)
+        self.guardDummy:AttachTo(self, -1)
+        self.Trash:Add(self.guardDummy)
+    end,
 
     OnScriptBitSet = function(self, bit)
         TConstructionUnit.OnScriptBitSet(self, bit)
@@ -16,6 +24,20 @@ TConstructionPodUnit = ClassUnit(TConstructionUnit) {
         if bit == 1 then
             self.rebuildDrone = false
         end
+    end,
+
+    OnAttachedToTransport = function(self, transport, bone)
+        local guards = self:GetGuards()
+        IssueClearCommands(guards)
+        IssueGuard(guards, self.guardDummy)
+        TConstructionUnit.OnAttachedToTransport(self, transport, bone)
+    end,
+
+    OnDetachedFromTransport = function(self, transport, bone)
+        TConstructionUnit.OnDetachedFromTransport(self, transport, bone)
+        local guards = self.guardDummy:GetGuards()
+        IssueClearCommands(guards)
+        IssueGuard(guards, self)
     end,
 
     SetParent = function(self, parent, podName)
