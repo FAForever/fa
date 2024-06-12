a39d6d9c366715e80a5eab9c3d2550feb2df16b5
Josh <row.josh02@gmail.com>
2024-06-12 13:57:29 +0100

    minor tweeks

--- lua/seraphimprojectiles.lua
+++ lua/seraphimprojectiles.lua
@@ -5,54 +5,63 @@
 --  Copyright © 2007 Gas Powered Games, Inc.  All rights reserved.
 ------------------------------------------------------------
 
---- Projectile Files
-SIFHuAntiNuke = import('/lua/sim/projectiles/seraphim/SIFHuAntiNuke.lua').SIFHuAntiNuke
-SIFKhuAntiNukeTendril = import('/lua/sim/projectiles/seraphim/SIFKhuAntiNukeTendril.lua').SIFKhuAntiNukeTendril
-SIFKhuAntiNukeSmallTendril = import('/lua/sim/projectiles/seraphim/SIFKhuAntiNukeSmallTendril.lua').SIFKhuAntiNukeSmallTendril
+SAAHotheFlareProjectile = import('/lua/sim/projectiles/seraphim/SAAHotheFlareProjectile.lua').SAAHotheFlareProjectile
+SAireauBolter = import('/lua/sim/projectiles/seraphim/SAireauBolter.lua').SAireauBolter
+SAnaitTorpedo = import('/lua/sim/projectiles/seraphim/SAnaitTorpedo.lua').SAnaitTorpedo
+SAnjelluTorpedoDefenseProjectile = import('/lua/sim/projectiles/seraphim/SAnjelluTorpedoDefenseProjectile.lua').SAnjelluTorpedoDefenseProjectile
+
 SBaseTempProjectile = import('/lua/sim/projectiles/seraphim/SBaseTempProjectile.lua').SBaseTempProjectile
 SChronatronCannon = import('/lua/sim/projectiles/seraphim/SChronatronCannon.lua').SChronatronCannon
 SChronatronCannonOverCharge = import('/lua/sim/projectiles/seraphim/SChronatronCannonOverCharge.lua').SChronatronCannonOverCharge
-SLightChronatronCannon = import('/lua/sim/projectiles/seraphim/SLightChronatronCannon.lua').SLightChronatronCannon
-SLightChronatronCannonOverCharge = import('/lua/sim/projectiles/seraphim/SLightChronatronCannonOverCharge.lua').SLightChronatronCannonOverCharge
-SPhasicAutogun = import('/lua/sim/projectiles/seraphim/SPhasicAutogun.lua').SPhasicAutogun
-SHeavyPhasicAutogun = import('/lua/sim/projectiles/seraphim/SHeavyPhasicAutogun.lua').SHeavyPhasicAutogun
-SHeavyPhasicAutogun02 = import('/lua/sim/projectiles/seraphim/SHeavyPhasicAutogun02.lua').SHeavyPhasicAutogun02
-SOhCannon = import('/lua/sim/projectiles/seraphim/SOhCannon.lua').SOhCannon
-SOhCannon02 = import('/lua/sim/projectiles/seraphim/SOhCannon02.lua').SOhCannon02
-SShriekerAutoCannon = import('/lua/sim/projectiles/seraphim/SShriekerAutoCannon.lua').SShriekerAutoCannon
-SAireauBolter = import('/lua/sim/projectiles/seraphim/SAireauBolter.lua').SAireauBolter
-STauCannon = import('/lua/sim/projectiles/seraphim/STauCannon.lua').STauCannon
-SHeavyQuarnonCannon = import('/lua/sim/projectiles/seraphim/SHeavyQuarnonCannon.lua').SHeavyQuarnonCannon
-SLaanseTacticalMissile = import('/lua/sim/projectiles/seraphim/SLaanseTacticalMissile.lua').SLaanseTacticalMissile
-SZthuthaamArtilleryShell = import('/lua/sim/projectiles/seraphim/SZthuthaamArtilleryShell.lua').SZthuthaamArtilleryShell
-SSuthanusArtilleryShell = import('/lua/sim/projectiles/seraphim/SSuthanusArtilleryShell.lua').SSuthanusArtilleryShell
-SSuthanusMobileArtilleryShell = import('/lua/sim/projectiles/seraphim/SSuthanusMobileArtilleryShell.lua').SSuthanusMobileArtilleryShell
-SThunthoArtilleryShell = import('/lua/sim/projectiles/seraphim/SThunthoArtilleryShell.lua').SThunthoArtilleryShell
-SThunthoArtilleryShell2 = import('/lua/sim/projectiles/seraphim/SThunthoArtilleryShell2.lua').SThunthoArtilleryShell2
-SShleoAACannon = import('/lua/sim/projectiles/seraphim/SShleoAACannon.lua').SShleoAACannon
-SOlarisAAArtillery = import('/lua/sim/projectiles/seraphim/SOlarisAAArtillery.lua').SOlarisAAArtillery
-SLosaareAAAutoCannon = import('/lua/sim/projectiles/seraphim/SLosaareAAAutoCannon.lua').SLosaareAAAutoCannon
-SLosaareAAAutoCannon02 = import('/lua/sim/projectiles/seraphim/SLosaareAAAutoCannon02.lua').SLosaareAAAutoCannon02
-SOtheTacticalBomb = import('/lua/sim/projectiles/seraphim/SOtheTacticalBomb.lua').SOtheTacticalBomb
-SAnaitTorpedo = import('/lua/sim/projectiles/seraphim/SAnaitTorpedo.lua').SAnaitTorpedo
-SHeavyCavitationTorpedo = import('/lua/sim/projectiles/seraphim/SHeavyCavitationTorpedo.lua').SHeavyCavitationTorpedo
-SUallCavitationTorpedo = import('/lua/sim/projectiles/seraphim/SUallCavitationTorpedo.lua').SUallCavitationTorpedo
-SIFInainoStrategicMissile = import('/lua/sim/projectiles/seraphim/SIFInainoStrategicMissile.lua').SIFInainoStrategicMissile
-SExperimentalStrategicMissile = import('/lua/sim/projectiles/seraphim/SExperimentalStrategicMissile.lua').SExperimentalStrategicMissile
-SIMAntiMissile01 = import('/lua/sim/projectiles/seraphim/SIMAntiMissile01.lua').SIMAntiMissile01
-SExperimentalStrategicBomb = import('/lua/sim/projectiles/seraphim/SExperimentalStrategicBomb.lua').SExperimentalStrategicBomb
-SIFNukeWaveTendril = import('/lua/sim/projectiles/seraphim/SIFNukeWaveTendril.lua').SIFNukeWaveTendril
-SIFNukeSpiralTendril = import('/lua/sim/projectiles/seraphim/SIFNukeSpiralTendril.lua').SIFNukeSpiralTendril
-SEnergyLaser = import('/lua/sim/projectiles/seraphim/SEnergyLaser.lua').SEnergyLaser
-SZhanaseeBombProjectile = import('/lua/sim/projectiles/seraphim/SZhanaseeBombProjectile.lua').SZhanaseeBombProjectile
-SAAHotheFlareProjectile = import('/lua/sim/projectiles/seraphim/SAAHotheFlareProjectile.lua').SAAHotheFlareProjectile
-SOhwalliStrategicBombProjectile = import('/lua/sim/projectiles/seraphim/SOhwalliStrategicBombProjectile.lua').SOhwalliStrategicBombProjectile
-SAnjelluTorpedoDefenseProjectile = import('/lua/sim/projectiles/seraphim/SAnjelluTorpedoDefenseProjectile.lua').SAnjelluTorpedoDefenseProjectile
-SDFSniperShotNormal = import('/lua/sim/projectiles/seraphim/SDFSniperShotNormal.lua').SDFSniperShotNormal
-SDFSniperShot = import('/lua/sim/projectiles/seraphim/SDFSniperShot.lua').SDFSniperShot
+SDFAireauProjectile = import('/lua/sim/projectiles/seraphim/SDFAireauProjectile.lua').SDFAireauProjectile
+
 SDFExperimentalPhasonProjectile = import('/lua/sim/projectiles/seraphim/SDFExperimentalPhasonProjectile.lua').SDFExperimentalPhasonProjectile
 SDFSinnuntheWeaponProjectile = import('/lua/sim/projectiles/seraphim/SDFSinnuntheWeaponProjectile.lua').SDFSinnuntheWeaponProjectile
-SDFAireauProjectile = import('/lua/sim/projectiles/seraphim/SDFAireauProjectile.lua').SDFAireauProjectile
+SDFSniperShot = import('/lua/sim/projectiles/seraphim/SDFSniperShot.lua').SDFSniperShot
+SDFSniperShotNormal = import('/lua/sim/projectiles/seraphim/SDFSniperShotNormal.lua').SDFSniperShotNormal
+
+SEnergyLaser = import('/lua/sim/projectiles/seraphim/SEnergyLaser.lua').SEnergyLaser
+SExperimentalStrategicBomb = import('/lua/sim/projectiles/seraphim/SExperimentalStrategicBomb.lua').SExperimentalStrategicBomb
+SExperimentalStrategicMissile = import('/lua/sim/projectiles/seraphim/SExperimentalStrategicMissile.lua').SExperimentalStrategicMissile
+SHeavyCavitationTorpedo = import('/lua/sim/projectiles/seraphim/SHeavyCavitationTorpedo.lua').SHeavyCavitationTorpedo
+
+SHeavyPhasicAutogun = import('/lua/sim/projectiles/seraphim/SHeavyPhasicAutogun.lua').SHeavyPhasicAutogun
+SHeavyPhasicAutogun02 = import('/lua/sim/projectiles/seraphim/SHeavyPhasicAutogun02.lua').SHeavyPhasicAutogun02
+SHeavyQuarnonCannon = import('/lua/sim/projectiles/seraphim/SHeavyQuarnonCannon.lua').SHeavyQuarnonCannon
+SIFHuAntiNuke = import('/lua/sim/projectiles/seraphim/SIFHuAntiNuke.lua').SIFHuAntiNuke
+
+SIFInainoStrategicMissile = import('/lua/sim/projectiles/seraphim/SIFInainoStrategicMissile.lua').SIFInainoStrategicMissile
+SIFKhuAntiNukeSmallTendril = import('/lua/sim/projectiles/seraphim/SIFKhuAntiNukeSmallTendril.lua').SIFKhuAntiNukeSmallTendril
+SIFKhuAntiNukeTendril = import('/lua/sim/projectiles/seraphim/SIFKhuAntiNukeTendril.lua').SIFKhuAntiNukeTendril
+SIFNukeSpiralTendril = import('/lua/sim/projectiles/seraphim/SIFNukeSpiralTendril.lua').SIFNukeSpiralTendril
+
+SIFNukeWaveTendril = import('/lua/sim/projectiles/seraphim/SIFNukeWaveTendril.lua').SIFNukeWaveTendril
+SIMAntiMissile01 = import('/lua/sim/projectiles/seraphim/SIMAntiMissile01.lua').SIMAntiMissile01
+SLaanseTacticalMissile = import('/lua/sim/projectiles/seraphim/SLaanseTacticalMissile.lua').SLaanseTacticalMissile
+SLightChronatronCannon = import('/lua/sim/projectiles/seraphim/SLightChronatronCannon.lua').SLightChronatronCannon
+SLightChronatronCannonOverCharge = import('/lua/sim/projectiles/seraphim/SLightChronatronCannonOverCharge.lua').SLightChronatronCannonOverCharge
+
+SLosaareAAAutoCannon = import('/lua/sim/projectiles/seraphim/SLosaareAAAutoCannon.lua').SLosaareAAAutoCannon
+SLosaareAAAutoCannon02 = import('/lua/sim/projectiles/seraphim/SLosaareAAAutoCannon02.lua').SLosaareAAAutoCannon02
+SOhCannon = import('/lua/sim/projectiles/seraphim/SOhCannon.lua').SOhCannon
+SOhCannon02 = import('/lua/sim/projectiles/seraphim/SOhCannon02.lua').SOhCannon02
+SOhwalliStrategicBombProjectile = import('/lua/sim/projectiles/seraphim/SOhwalliStrategicBombProjectile.lua').SOhwalliStrategicBombProjectile
+
+SOlarisAAArtillery = import('/lua/sim/projectiles/seraphim/SOlarisAAArtillery.lua').SOlarisAAArtillery
+SOtheTacticalBomb = import('/lua/sim/projectiles/seraphim/SOtheTacticalBomb.lua').SOtheTacticalBomb
+SPhasicAutogun = import('/lua/sim/projectiles/seraphim/SPhasicAutogun.lua').SPhasicAutogun
+SShleoAACannon = import('/lua/sim/projectiles/seraphim/SShleoAACannon.lua').SShleoAACannon
+
+SShriekerAutoCannon = import('/lua/sim/projectiles/seraphim/SShriekerAutoCannon.lua').SShriekerAutoCannon
+SSuthanusArtilleryShell = import('/lua/sim/projectiles/seraphim/SSuthanusArtilleryShell.lua').SSuthanusArtilleryShell
+SSuthanusMobileArtilleryShell = import('/lua/sim/projectiles/seraphim/SSuthanusMobileArtilleryShell.lua').SSuthanusMobileArtilleryShell
+STauCannon = import('/lua/sim/projectiles/seraphim/STauCannon.lua').STauCannon
+
+SThunthoArtilleryShell = import('/lua/sim/projectiles/seraphim/SThunthoArtilleryShell.lua').SThunthoArtilleryShell
+SThunthoArtilleryShell2 = import('/lua/sim/projectiles/seraphim/SThunthoArtilleryShell2.lua').SThunthoArtilleryShell2
+SUallCavitationTorpedo = import('/lua/sim/projectiles/seraphim/SUallCavitationTorpedo.lua').SUallCavitationTorpedo
+SZhanaseeBombProjectile = import('/lua/sim/projectiles/seraphim/SZhanaseeBombProjectile.lua').SZhanaseeBombProjectile
+SZthuthaamArtilleryShell = import('/lua/sim/projectiles/seraphim/SZthuthaamArtilleryShell.lua').SZthuthaamArtilleryShell
 
 -- kept for mod backwards compatibility
 local DefaultProjectileFile = import("/lua/sim/defaultprojectiles.lua")
