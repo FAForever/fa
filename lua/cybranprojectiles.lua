30fdabe7998c3ff66cb26ba8551dbb3f6fb33627
Josh <row.josh02@gmail.com>
2024-06-12 13:31:36 +0100

    minor chanages

--- lua/cybranprojectiles.lua
+++ lua/cybranprojectiles.lua
@@ -5,54 +5,65 @@
 --  Copyright © 2005 Gas Powered Games, Inc.  All rights reserved.
 ------------------------------------------------------------
 
---  CYBRAN PROJECILES SCRIPTS
-CDFBrackmanHackPegProjectile01 = import('/lua/sim/projectiles/cybran/CDFBrackmanHackPegProjectile01.lua').CDFBrackmanHackPegProjectile01
-CDFBrackmanHackPegProjectile02 = import('/lua/sim/projectiles/cybran/CDFBrackmanHackPegProjectile02.lua').CDFBrackmanHackPegProjectile02
-CIFProtonBombProjectile = import('/lua/sim/projectiles/cybran/CIFProtonBombProjectile.lua').CIFProtonBombProjectile
-CDFProtonCannonProjectile = import('/lua/sim/projectiles/cybran/CDFProtonCannonProjectile.lua').CDFProtonCannonProjectile
-CDFHvyProtonCannonProjectile = import('/lua/sim/projectiles/cybran/CDFHvyProtonCannonProjectile.lua').CDFHvyProtonCannonProjectile
+--  CYBRAN PROJECTILES SCRIPTS
 CAADissidentProjectile = import('/lua/sim/projectiles/cybran/CAADissidentProjectile.lua').CAADissidentProjectile
 CAAElectronBurstCloudProjectile = import('/lua/sim/projectiles/cybran/CAAElectronBurstCloudProjectile.lua').CAAElectronBurstCloudProjectile
 CAAMissileNaniteProjectile = import('/lua/sim/projectiles/cybran/CAAMissileNaniteProjectile.lua').CAAMissileNaniteProjectile
 CAAMissileNaniteProjectile03 = import('/lua/sim/projectiles/cybran/CAAMissileNaniteProjectile03.lua').CAAMissileNaniteProjectile03
+
 CAANanoDartProjectile = import('/lua/sim/projectiles/cybran/CAANanoDartProjectile.lua').CAANanoDartProjectile
 CAANanoDartProjectile02 = import('/lua/sim/projectiles/cybran/CAANanoDartProjectile02.lua').CAANanoDartProjectile02
 CAANanoDartProjectile03 = import('/lua/sim/projectiles/cybran/CAANanoDartProjectile03.lua').CAANanoDartProjectile03
+
 CArtilleryProjectile = import('/lua/sim/projectiles/cybran/CArtilleryProjectile.lua').CArtilleryProjectile
 CArtilleryProtonProjectile = import('/lua/sim/projectiles/cybran/CArtilleryProtonProjectile.lua').CArtilleryProtonProjectile
 CBeamProjectile = import('/lua/sim/projectiles/cybran/CBeamProjectile.lua').CBeamProjectile
 CBombProjectile = import('/lua/sim/projectiles/cybran/CBombProjectile.lua').CBombProjectile
+
 CCannonSeaProjectile = import('/lua/sim/projectiles/cybran/CCannonSeaProjectile.lua').CCannonSeaProjectile
 CCannonTankProjectile = import('/lua/sim/projectiles/cybran/CCannonTankProjectile.lua').CCannonTankProjectile
+CCorsairRocketProjectile = import('/lua/sim/projectiles/cybran/CCorsairRocketProjectile.lua').CCorsairRocketProjectile
+CDepthChargeProjectile = import('/lua/sim/projectiles/cybran/CDepthChargeProjectile.lua').CDepthChargeProjectile
+
+CDFBrackmanHackPegProjectile01 = import('/lua/sim/projectiles/cybran/CDFBrackmanHackPegProjectile01.lua').CDFBrackmanHackPegProjectile01
+CDFBrackmanHackPegProjectile02 = import('/lua/sim/projectiles/cybran/CDFBrackmanHackPegProjectile02.lua').CDFBrackmanHackPegProjectile02
+CDFHvyProtonCannonProjectile = import('/lua/sim/projectiles/cybran/CDFHvyProtonCannonProjectile.lua').CDFHvyProtonCannonProjectile
+CDFProtonCannonProjectile = import('/lua/sim/projectiles/cybran/CDFProtonCannonProjectile.lua').CDFProtonCannonProjectile
+
 CDFTrackerProjectile = import('/lua/sim/projectiles/cybran/CDFTrackerProjectile.lua').CDFTrackerProjectile
 CDisintegratorLaserProjectile = import('/lua/sim/projectiles/cybran/CDisintegratorLaserProjectile.lua').CDisintegratorLaserProjectile
 CDisintegratorLaserProjectile02 = import('/lua/sim/projectiles/cybran/CDisintegratorLaserProjectile02.lua').CDisintegratorLaserProjectile02
 CElectronBolterProjectile = import('/lua/sim/projectiles/cybran/CElectronBolterProjectile.lua').CElectronBolterProjectile
-CHeavyElectronBolterProjectile = import('/lua/sim/projectiles/cybran/CHeavyElectronBolterProjectile.lua').CHeavyElectronBolterProjectile
+
 CEMPFluxWarheadProjectile = import('/lua/sim/projectiles/cybran/CEMPFluxWarheadProjectile.lua').CEMPFluxWarheadProjectile
 CFlameThrowerProjectile = import('/lua/sim/projectiles/cybran/CFlameThrowerProjectile.lua').CFlameThrowerProjectile
-CIFMolecularResonanceShell = import('/lua/sim/projectiles/cybran/CIFMolecularResonanceShell.lua').CIFMolecularResonanceShell
-CIridiumRocketProjectile = import('/lua/sim/projectiles/cybran/CIridiumRocketProjectile.lua').CIridiumRocketProjectile
-CCorsairRocketProjectile = import('/lua/sim/projectiles/cybran/CCorsairRocketProjectile.lua').CCorsairRocketProjectile
-CLaserLaserProjectile = import('/lua/sim/projectiles/cybran/CLaserLaserProjectile.lua').CLaserLaserProjectile
+CHeavyDisintegratorPulseLaser = import('/lua/sim/projectiles/cybran/CHeavyDisintegratorPulseLaser.lua').CHeavyDisintegratorPulseLaser
+CHeavyElectronBolterProjectile = import('/lua/sim/projectiles/cybran/CHeavyElectronBolterProjectile.lua').CHeavyElectronBolterProjectile
+
 CHeavyLaserProjectile = import('/lua/sim/projectiles/cybran/CHeavyLaserProjectile.lua').CHeavyLaserProjectile
 CHeavyLaserProjectile2 = import('/lua/sim/projectiles/cybran/CHeavyLaserProjectile2.lua').CHeavyLaserProjectile2
-CMolecularCannonProjectile = import('/lua/sim/projectiles/cybran/CMolecularCannonProjectile.lua').CMolecularCannonProjectile
+CIFMolecularResonanceShell = import('/lua/sim/projectiles/cybran/CIFMolecularResonanceShell.lua').CIFMolecularResonanceShell
+CIFProtonBombProjectile = import('/lua/sim/projectiles/cybran/CIFProtonBombProjectile.lua').CIFProtonBombProjectile
+
+CIridiumRocketProjectile = import('/lua/sim/projectiles/cybran/CIridiumRocketProjectile.lua').CIridiumRocketProjectile
+CKrilTorpedo = import('/lua/sim/projectiles/cybran/CKrilTorpedo.lua').CKrilTorpedo
+CLaserLaserProjectile = import('/lua/sim/projectiles/cybran/CLaserLaserProjectile.lua').CLaserLaserProjectile
+CLOATacticalChildMissileProjectile = import('/lua/sim/projectiles/cybran/CLOATacticalChildMissileProjectile.lua').CLOATacticalChildMissileProjectile
+
+CLOATacticalMissileProjectile = import('/lua/sim/projectiles/cybran/CLOATacticalMissileProjectile.lua').CLOATacticalMissileProjectile
 CMissileAAProjectile = import('/lua/sim/projectiles/cybran/CMissileAAProjectile.lua').CMissileAAProjectile
+CMolecularCannonProjectile = import('/lua/sim/projectiles/cybran/CMolecularCannonProjectile.lua').CMolecularCannonProjectile
 CNeutronClusterBombChildProjectile = import('/lua/sim/projectiles/cybran/CNeutronClusterBombChildProjectile.lua').CNeutronClusterBombChildProjectile
+
 CNeutronClusterBombProjectile = import('/lua/sim/projectiles/cybran/CNeutronClusterBombProjectile.lua').CNeutronClusterBombProjectile
 CParticleCannonProjectile = import('/lua/sim/projectiles/cybran/CParticleCannonProjectile.lua').CParticleCannonProjectile
 CRailGunProjectile = import('/lua/sim/projectiles/cybran/CRailGunProjectile.lua').CRailGunProjectile
 CRocketProjectile = import('/lua/sim/projectiles/cybran/CRocketProjectile.lua').CRocketProjectile
-CLOATacticalMissileProjectile = import('/lua/sim/projectiles/cybran/CLOATacticalMissileProjectile.lua').CLOATacticalMissileProjectile
-CLOATacticalChildMissileProjectile = import('/lua/sim/projectiles/cybran/CLOATacticalChildMissileProjectile.lua').CLOATacticalChildMissileProjectile
+
 CShellAAAutoCannonProjectile = import('/lua/sim/projectiles/cybran/CShellAAAutoCannonProjectile.lua').CShellAAAutoCannonProjectile
 CShellRiotProjectile = import('/lua/sim/projectiles/cybran/CShellRiotProjectile.lua').CShellRiotProjectile
 CTorpedoShipProjectile = import('/lua/sim/projectiles/cybran/CTorpedoShipProjectile.lua').CTorpedoShipProjectile
 CTorpedoSubProjectile = import('/lua/sim/projectiles/cybran/CTorpedoSubProjectile.lua').CTorpedoSubProjectile
-CDepthChargeProjectile = import('/lua/sim/projectiles/cybran/CDepthChargeProjectile.lua').CDepthChargeProjectile
-CHeavyDisintegratorPulseLaser = import('/lua/sim/projectiles/cybran/CHeavyDisintegratorPulseLaser.lua').CHeavyDisintegratorPulseLaser
-CKrilTorpedo = import('/lua/sim/projectiles/cybran/CKrilTorpedo.lua').CKrilTorpedo
 
 
 -- kept for mod backwards compatibility
