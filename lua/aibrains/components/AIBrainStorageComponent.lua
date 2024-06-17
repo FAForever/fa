25f7854847a1c17f7880b9285a211e92dbc5aa7f
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2024-06-17 15:12:02 +0200

    Implement the blinking lights at a unit level (#6265)

--- lua/aibrains/components/AIBrainStorageComponent.lua
+++ lua/aibrains/components/AIBrainStorageComponent.lua
@@ -102,9 +102,9 @@ AIBrainStorageComponent = ClassSimple {
 
         ---@type AIBrainMassStorageState
         local state = 'EconFullMassStore'
-        if ratio == 0 then
+        if ratio < 0.10 then
             state = 'EconLowMassStore'
-        elseif ratio < 1.0 then
+        elseif ratio < 0.9 then
             state = 'EconMidMassStore'
         end
 
@@ -143,9 +143,9 @@ AIBrainStorageComponent = ClassSimple {
 
         ---@type AIBrainEnergyStorageState
         local state = 'EconFullEnergyStore'
-        if ratio == 0 then
+        if ratio < 0.10 then
             state = 'EconLowEnergyStore'
-        elseif ratio < 1.0 then
+        elseif ratio < 0.9 then
             state = 'EconMidEnergyStore'
         end
 
