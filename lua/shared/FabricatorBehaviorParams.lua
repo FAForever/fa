e45bd0c039b92cb4235eda6c333624d0f1a97116
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2024-06-20 17:02:08 -0700

    Add comment

--- lua/shared/FabricatorBehaviorParams.lua
+++ lua/shared/FabricatorBehaviorParams.lua
@@ -1,6 +1,6 @@
 
 DisableRatio = 0.8
-DisableStorage = 105000
+DisableStorage = 105000 -- 100000 energy needed for max overcharge damage, also add 5k as a buffer
 
 EnableRatio = 0.9
 EnableTrend = 100
