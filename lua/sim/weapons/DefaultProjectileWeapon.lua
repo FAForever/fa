9bb0917d2f355ee191b139df26b1e7b6b3a3017d
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2024-06-21 21:16:22 -0700

    Do full rack reload when losing target while firing
    Prevents bypassing reloads

--- lua/sim/weapons/DefaultProjectileWeapon.lua
+++ lua/sim/weapons/DefaultProjectileWeapon.lua
@@ -1145,8 +1145,23 @@ DefaultProjectileWeapon = ClassWeapon(Weapon) {
             else
                 Weapon.OnLostTarget(self)
 
-                if self.Blueprint.WeaponUnpacks then
-                    ChangeState(self, self.WeaponPackingState)
+                -- Deal with the rack firing sequence
+                if self.CurrentRackSalvoNumber > self.NumRackBones then
+                    self.CurrentRackSalvoNumber = 1
+
+                    if bp.RackSalvoReloadTime > 0 then
+                        ChangeState(self, self.RackSalvoReloadState)
+                    elseif bp.WeaponUnpacks then
+                        ChangeState(self, self.WeaponPackingState)
+                    else
+                        ChangeState(self, self.IdleState)
+                    end
+                else
+                    if bp.WeaponUnpacks then
+                        ChangeState(self, self.WeaponPackingState)
+                    else
+                        ChangeState(self, self.IdleState)
+                    end
                 end
             end
         end,
