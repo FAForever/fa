3c3323beac9112a86e1dcd1e6f01353759cdcf9a
Basilisk3 <126026384+Basilisk3@users.noreply.github.com>
2024-06-27 13:35:59 +0200

    Add a check for `isStructure` because structures can have the 'land' category

--- lua/system/blueprints-units.lua
+++ lua/system/blueprints-units.lua
@@ -545,9 +545,9 @@ local function PostProcessUnit(unit)
         unit.Interface.HelpText = unit.Description or "" --[[@as string]]
     end
 
-    -- Define a specific UnitWeight for all units. Not done in the blueprints to keep mod compatibility.
+    -- Define a specific UnitWeight for all land units. Not done in the blueprints to keep mod compatibility.
 	-- Experimentals have a weight of 1 because transports gained 1 speed and some survival maps load experimentals into transports.
-    if not unit.General.UnitWeight then    
+    if not unit.General.UnitWeight and not isStructure then    
         if isLand and isTech1 then
             unit.General.UnitWeight = 0.15
         elseif isLand and isTech2 then
