aac3ae78de277c16092e9740290cbac5adfc3773
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2024-08-03 23:23:36 +0200

    Game version 3811 (#6388)

--- lua/version.lua
+++ lua/version.lua
@@ -1,7 +1,7 @@
 
-local Version = "3810"
----@alias PATCH "3810"
----@alias VERSION "1.5.3810"
+local Version = "3811"
+---@alias PATCH "3811"
+---@alias VERSION "1.5.3811"
 ---@return PATCH
 function GetVersion()
     LOG('Supreme Commander: Forged Alliance version ' .. Version)
