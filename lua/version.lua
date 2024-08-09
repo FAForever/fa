7baad99082bc585df32bc42ef5f9a9ad64d42b48
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2024-08-09 11:15:32 +0200

    Add information about the game type and commit hash to the game (#6393)
    ## Description of the proposed changes
    
    Embed the hash and game type into the game by processing the files just
    before we deploy them. For now we can log this information to make it
    easier to understand for the developerat what commit a replay was. In
    the future we can use the same data to inform the host that a new game
    version is available and that he should restart.
    
    ## Testing done on the proposed changes
    
    I ran the bash scripts manually by copying them into a bash interpreter.
    
    ## Additional context
    
    Users always experience a (short) duration of desyncs when a release
    happens. And in general, users are unaware of the exact version that
    resides on the FAF Develop and FAF Beta Balance game types

--- lua/version.lua
+++ lua/version.lua
@@ -1,9 +1,51 @@
+--******************************************************************************************************
+--** Copyright (c) 2024 FAForever
+--**
+--** Permission is hereby granted, free of charge, to any person obtaining a copy
+--** of this software and associated documentation files (the "Software"), to deal
+--** in the Software without restriction, including without limitation the rights
+--** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--** copies of the Software, and to permit persons to whom the Software is
+--** furnished to do so, subject to the following conditions:
+--**
+--** The above copyright notice and this permission notice shall be included in all
+--** copies or substantial portions of the Software.
+--**
+--** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+--** SOFTWARE.
+--******************************************************************************************************
+
+---------------------------------------------------------------------------
+--#region Workflow automation
+
+-- The following fields are overwritten when a deployment happens. See also:
+-- - https://github.com/FAForever/fa/blob/develop/.github/workflows/deploy-faf.yaml
+-- - https://github.com/FAForever/fa/blob/develop/.github/workflows/deploy-fafbeta.yaml
+-- - https://github.com/FAForever/fa/blob/develop/.github/workflows/deploy-fafdevelop.yaml
+
+local GameType = "unknown"
+
+local Commit = "unknown"
+
+--#endregion
 
 local Version = "3811"
 ---@alias PATCH "3811"
 ---@alias VERSION "1.5.3811"
----@return PATCH
+---@return PATCH    # Game release
 function GetVersion()
-    LOG('Supreme Commander: Forged Alliance version ' .. Version)
+    LOG(string.format('Supreme Commander: Forged Alliance Lua version %s at %s (%s)', Version, GameType, Commit))
     return Version
+end
+
+---@return PATCH
+---@return string # game type
+---@return string # commit hash
+function GetVersionData()
+    return Version, GameType, Commit
 end
