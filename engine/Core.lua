1a0395e457cb8d44f003898db4983fa187e114b2
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2024-09-08 19:56:59 -0700

    Reorder description of EulerToQuaternion

--- engine/Core.lua
+++ engine/Core.lua
@@ -151,9 +151,9 @@ end
 function EulerToQuaternion(roll, pitch, yaw)
 end
 -- {
+--     sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2), -- x
 --     cos(roll/2) * sin(pitch/2) * cos(yaw/2) + sin(roll/2) * cos(pitch/2) * sin(yaw/2), -- y
 --     cos(roll/2) * cos(pitch/2) * sin(yaw/2) - sin(roll/2) * sin(pitch/2) * cos(yaw/2), -- z
---     sin(roll/2) * cos(pitch/2) * cos(yaw/2) - cos(roll/2) * sin(pitch/2) * sin(yaw/2), -- x
 --     cos(roll/2) * cos(pitch/2) * cos(yaw/2) + sin(roll/2) * sin(pitch/2) * sin(yaw/2)  -- w
 -- }
 
