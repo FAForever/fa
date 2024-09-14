FAForever/fa

commit e9d17d4b3f645dfa155d5f2a174e0dc7b11b483c

Fix issues surrounding the Ahwassa (#5503)

lua/sim/Unit.lua
@@ -3223,6 +3223,11 @@ Unit = ClassUnit(moho.unit_methods, IntelComponent, VeterancyComponent) {
         -- Store latest layer for performance, preventing .Layer engine calls.
         self.Layer = new 
 
+        if old != 'None' then
+            self:DestroyMovementEffects()
+            self:CreateMovementEffects(self.MovementEffectsBag, nil)
+        end
+
         -- Bail out early if dead. The engine calls this function AFTER entity:Destroy() has killed
         -- the C object. Any functions down this line which expect a live C object (self:CreateAnimator())
         -- for example, will throw an error.
@@ -3346,7 +3351,6 @@ Unit = ClassUnit(moho.unit_methods, IntelComponent, VeterancyComponent) {
         local layer = self.Layer
         if (new == 'Up' and old == 'Bottom') or (new == 'Down' and old == 'Top') then
             self:DestroyIdleEffects()
-
             if new == 'Up' and layer == 'Sub' then
                 self:PlayUnitSound('SurfaceStart')
             end

effects/emitters/contrail_delayed_mist_01_emit.bp
@@ -10,7 +10,7 @@ EmitterBlueprint {
     AlignRotation = false,
     AlignToBone = false,
     Flat = false,
-    LODCutoff = 128.00,
+    LODCutoff = 190.00,
     EmitIfVisible = true,
     CatchupEmit = false,
     CreateIfVisible = false,
@@ -45,13 +45,13 @@ EmitterBlueprint {
     EmitRateCurve = {
         XRange = 1.00,
         Keys = {
-            { x=0.50,y=2.50,z=1 },
+            { x=0.50,y=2.00,z=1 },
         },
     },
     LifetimeCurve = {
         XRange = 1.00,
         Keys = {
-            { x=0.48,y=20.00,z=5.00 },
+            { x=0.48,y=14.00,z=5.00 },
         },
     },
     VelocityCurve = {

effects/emitters/contrail_delayed_mist_02_emit.bp
@@ -10,7 +10,7 @@ EmitterBlueprint {
 	AlignRotation = false,
 	AlignToBone = false,
 	Flat = false,
-	LODCutoff = 0.00,
+	LODCutoff = 190.00,
 	EmitIfVisible = true,
 	CatchupEmit = false,
 	CreateIfVisible = false,

units/XSA0402/XSA0402_unit.bp
@@ -118,13 +118,13 @@ UnitBlueprint{
                 {
                     LODCutoff = 1500,
                     LookupName = "/textures/environment/Falloff_seraphim_lookup.dds",
+                    SecondaryName = "/units/uaa0310/uaa0310_secondary.dds",
                     ShaderName = "Seraphim",
-                    AlbedoName = "xsa0402_lod1_albedo.dds",
-                    SpecularName = "xsa0402_lod1_specteam.dds",
                 },
             },
         },
         MovementEffects = {
+
             Air = {
                 Contrails = {
                     Bones = { "Tail_Extent" },

commit a8c847f8da278d8504a950807da282df4a7c7e2c

Fix movement effects being recreated before footfall
Caused by: https://github.com/FAForever/fa/pull/5503

lua/sim/Unit.lua
@@ -3312,11 +3312,6 @@ Unit = ClassUnit(moho.unit_methods, IntelComponent, VeterancyComponent, DebugUni
         -- Store latest layer for performance, preventing .Layer engine calls.
         self.Layer = new 
 
-        if old != 'None' then
-            self:DestroyMovementEffects()
-            self:CreateMovementEffects(self.MovementEffectsBag, nil)
-        end
-
         -- Bail out early if dead. The engine calls this function AFTER entity:Destroy() has killed
         -- the C object. Any functions down this line which expect a live C object (self:CreateAnimator())
         -- for example, will throw an error.
@@ -3360,6 +3355,12 @@ Unit = ClassUnit(moho.unit_methods, IntelComponent, VeterancyComponent, DebugUni
             self.Footfalls = self:CreateFootFallManipulators(movementEffects[new].Footfall)
         end
         self:CreateLayerChangeEffects(new, old)
+        
+        -- re-create movement effects for units with different effects per layer
+        if old != 'None' then
+            self:DestroyMovementEffects()
+            self:CreateMovementEffects(self.MovementEffectsBag, nil)
+        end
 
         -- Trigger the re-worded stuff that used to be inherited, no longer because of the engine bug above.
         if self.LayerChangeTrigger then