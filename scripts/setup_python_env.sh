4eff7f9766b0524ed6763738336d01769109a46e
Zjonn <zjonnn@gmail.com>
2024-09-16 20:46:20 +0200

    Check if user has at least python3.8 version

--- scripts/setup_python_env.sh
+++ scripts/setup_python_env.sh
@@ -11,11 +11,20 @@ SCRIPT_PATH=$(realpath "${BASH_SOURCE[0]}")
 SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
 VENV_PATH="${SCRIPT_DIR}/${VENV_NAME}"
 
+# Check if the user has at least python3.8 (It will reach end-of-life at the end of 2024)
+REQUIRED_VERSION="3.8"
+INSTALLED_VERSION=$(python3 --version 2>&1 | awk '{print $2}')
+
+if [ "$(printf '%s\n' "$REQUIRED_VERSION" "$INSTALLED_VERSION" | sort -V | head -n 1)" != "$REQUIRED_VERSION" ]; then
+    echo "At least python${REQUIRED_VERSION} is required, you have python${INSTALLED_VERSION}"
+    exit 1
+fi
+
 # Check if the script is being sourced
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
     echo "Error: This script must be sourced, not executed."
     echo "Please run: source ${BASH_SOURCE[0]}"
-    exit 1
+    exit 2
 fi
 
 # Create venv if it doesn't exist
