c8d1b9eae2f71fdfdb7e5e4137dd2349eab6db6d
Basilisk3 <126026384+Basilisk3@users.noreply.github.com>
2024-09-22 11:52:49 +0200

    Fix formatting

--- lua/version.lua
+++ lua/version.lua
@@ -28,9 +28,9 @@
 -- - https://github.com/FAForever/fa/blob/develop/.github/workflows/deploy-fafbeta.yaml
 -- - https://github.com/FAForever/fa/blob/develop/.github/workflows/deploy-fafdevelop.yaml
 
-local GameType = 'unknown'    -- The use of `'` instead of `"` is **intentional**
+local GameType = 'unknown'  -- The use of `'` instead of `"` is **intentional**
 
-local Commit = 'unknown'      -- The use of `'` instead of `"` is **intentional**
+local Commit = 'unknown'    -- The use of `'` instead of `"` is **intentional**
 
 --#endregion
 
