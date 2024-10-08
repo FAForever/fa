daadb8546f29407c5009b72c18c64b65ec7864d2
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-10-08 14:37:18 +0200

    Add example files that are to be generated

---
+++ lua/ui/lobby/changelog/generated/dummy.lua
@@ -0,0 +1,9 @@
+
+Changelog = {
+    version = 3639,
+    description = {
+        "# XYZ (Month Day, Year)",
+        "",
+        "This is a template changelog and should be replaced by a real changelog upon deploying.",
+    }
+}
