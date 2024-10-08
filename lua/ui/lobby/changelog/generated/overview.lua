034fb23ddc391c8fbdea9c1a2edae4fd74925221
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-10-08 14:37:18 +0200

    Add example files that are to be generated

---
+++ lua/ui/lobby/changelog/generated/overview.lua
@@ -0,0 +1,10 @@
+
+---@type UIChangelogOverview
+Overview = {
+    Changelogs = {
+        {
+            URL = "http://faforever.github.io/fa/",
+            Path = "/lua/ui/lobby/changelog/generated/dummy.lua" 
+        }
+    }
+}
