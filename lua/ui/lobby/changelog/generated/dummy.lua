f4b2384e1325e7f91146bd229030f78137b198a1
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-10-08 14:55:17 +0200

    Add dummy changelogs

--- lua/ui/lobby/changelog/generated/dummy.lua
+++
@@ -1,9 +0,0 @@
-
-Changelog = {
-    version = 3639,
-    description = {
-        "# XYZ (Month Day, Year)",
-        "",
-        "This is a template changelog and should be replaced by a real changelog upon deploying.",
-    }
-}
--- lua/ui/lobby/changelog/generated/overview.lua
+++ lua/ui/lobby/changelog/generated/overview.lua
@@ -3,8 +3,16 @@
 Overview = {
     Changelogs = {
         {
+            Version = 3000,
+            Name = "Developers patch",
             URL = "http://faforever.github.io/fa/",
-            Path = "/lua/ui/lobby/changelog/generated/dummy.lua" 
+            Path = "/lua/ui/lobby/changelog/generated/3000-dummy.lua" 
+        },
+        {
+            Version = 3001,
+            Name = "Balance patch",
+            URL = "http://faforever.github.io/fa/",
+            Path = "/lua/ui/lobby/changelog/generated/3001-dummy.lua" 
         }
     }
 }
