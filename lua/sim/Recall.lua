81a2588cc63a50c8e08f5ce6fa52c035fc0025dd
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2024-11-04 11:58:07 -0800

    Properly sync defeated army's ability to recall
    hides the "request recall" button in the UI after defeat

--- lua/sim/Recall.lua
+++ lua/sim/Recall.lua
@@ -111,10 +111,10 @@ end
 ---@return CannotRecallReason
 ---@return number? cooldown no timeout/cooldown if absent
 function ArmyRecallRequestCooldown(army)
-    if army == -1 then
+    local brain = GetArmyBrain(army)
+    if army == -1 or brain:IsDefeated() then
         return "observer"
     end
-    local brain = GetArmyBrain(army)
     if ScenarioInfo.RecallDisabled then
         return "scenario"
     end
