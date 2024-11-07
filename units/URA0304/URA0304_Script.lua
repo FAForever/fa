2935c3592c15fa7371e4fd092772cb5ccd606a02
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2024-11-07 03:33:36 -0800

    Debug setup

--- units/URA0304/URA0304_Script.lua
+++ units/URA0304/URA0304_Script.lua
@@ -12,7 +12,35 @@ local CAAAutocannon = import("/lua/cybranweapons.lua").CAAAutocannon
 ---@class URA0304 : CAirUnit
 URA0304 = ClassUnit(CAirUnit) {
     Weapons = {
-        Bomb = ClassWeapon(CIFBombNeutronWeapon) {},
+        ---@class Bomb : CIFBombNeutronWeapon
+        Bomb = ClassWeapon(CIFBombNeutronWeapon) {
+            LastDrop = 0,
+            Drops = 0,
+            SumDropInterval = 0,
+
+            ---@param self Bomb
+            CreateProjectileAtMuzzle = function(self, muzzle)
+                if self.LastDrop ~= 0 then
+                    local dt = GetGameTick() - self.LastDrop
+                    self.Drops = self.Drops + 1
+                    self.SumDropInterval = self.SumDropInterval + dt
+                    local ux,uy,uz = self.unit:GetVelocity()
+                    local spd = math.sqrt(ux*ux+uy*uy+uz*uz) * 10 -- convert from ogrids/tick to ogrids/s
+                        LOG(('Ticks between bomb drops %d (avg over %d: %.1f); Unit speed ogrids/s: %.1f'):format(
+                        dt, self.Drops, self.SumDropInterval / self.Drops, spd
+                    ))
+                end
+                self.LastDrop = GetGameTick()
+                CIFBombNeutronWeapon.CreateProjectileAtMuzzle(self, muzzle)
+            end,
+
+            OnLostTarget = function(self)
+                self.LastDrop = 0
+                self.Drops = 0
+                self.SumDropInterval = 0
+                CIFBombNeutronWeapon.OnLostTarget(self)
+            end,
+        },
         AAGun1 = ClassWeapon(CAAAutocannon) {},
         AAGun2 = ClassWeapon(CAAAutocannon) {},
     },
