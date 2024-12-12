508885dfcedf97b7fd5e36d7c42f21ce3f522b7e
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2024-12-12 01:07:39 -0800

    Annotate `AddBlinkyBox` (#6577)

--- engine/User.lua
+++ engine/User.lua
@@ -124,8 +124,8 @@
 --- | 'NUMPAD_DECIMAL'
 --- | 'NUMPAD_DIVIDE'
 
---- No clue what this does
----@param entityId number
+--- Repeatedly the selection box of the unit to the hovered-over state to create a blinking effect
+---@param entityId EntityId
 ---@param onTime number
 ---@param offTime number
 ---@param totalTime number
