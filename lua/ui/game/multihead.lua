b21373c5129b54ee1ba4b852d95c6a8297460bf3
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-12-26 08:04:34 +0100

    Add license and clean up imports

--- lua/ui/game/multihead.lua
+++ lua/ui/game/multihead.lua
@@ -1,15 +1,28 @@
---*****************************************************************************
---* File: lua/modules/ui/game/gamemain.lua
---* Author: Chris Blackwell
---* Summary: Entry point for the in game UI
---*
---* Copyright � 2005 Gas Powered Games, Inc.  All rights reserved.
---*****************************************************************************
+--******************************************************************************************************
+--** Copyright (c) 2022  Willem 'Jip' Wijnia
+--**
+--** Permission is hereby granted, free of charge, to any person obtaining a copy
+--** of this software and associated documentation files (the "Software"), to deal
+--** in the Software without restriction, including without limitation the rights
+--** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--** copies of the Software, and to permit persons to whom the Software is
+--** furnished to do so, subject to the following conditions:
+--**
+--** The above copyright notice and this permission notice shall be included in all
+--** copies or substantial portions of the Software.
+--**
+--** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+--** SOFTWARE.
+--******************************************************************************************************
 
 local UIUtil = import("/lua/ui/uiutil.lua")
 local LayoutHelpers = import("/lua/maui/layouthelpers.lua")
 local Group = import("/lua/maui/group.lua").Group
-local GameCommon = import("/lua/ui/game/gamecommon.lua")
 local Bitmap = import("/lua/maui/bitmap.lua").Bitmap
 
 ---@type WorldView | false
@@ -55,3 +68,7 @@ function CreateSecondView()
     LayoutHelpers.FillParent(view, secondHeadGroup)
 
 end
+
+-- backwards compatibility for mods
+
+local GameCommon = import("/lua/ui/game/gamecommon.lua")
