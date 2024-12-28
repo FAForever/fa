2dfed0238d08657ced1939fd23b57c2d1eab6d87
Aaron Warner <aaronpaulwarner@gmail.com>
2024-12-28 16:24:31 +1300

    Update documentation to remove any confusion for.

--- engine/Sim/CPlatoon.lua
+++ engine/Sim/CPlatoon.lua
@@ -53,11 +53,11 @@ end
 
 --- TODO.
 -- Example: local formIt = poolPlatoon:CanFormPlatoon(template, personality:GetPlatoonSize(), self.Location, radius)
----@param template table
----@param size number
----@param location Vector
----@param radius number
-function CPlatoon:CanFormPlatoon(template, size, location, radius)
+---@param template table The template table for the faction, see platoontemplates for more details.
+---@param count number The number of platoons that should be formed.
+---@param location Vector The position vector to search for units from.
+---@param radius number The radius to search for units.
+function CPlatoon:CanFormPlatoon(template, count, location, radius)
 end
 
 --- Destroys the platoon including all its units.
@@ -121,8 +121,12 @@ end
 
 --- TODO.
 -- Example: local hndl = poolPlatoon:FormPlatoon(template, personality:GetPlatoonSize(), self.Location, radius)
+---@param template table The template table for the faction, see platoontemplates for more details.
+---@param count number The number of platoons that should be formed.
+---@param position Vector The position vector to search for units from.
+---@param radius number The radius to search for units.
 -- @return Formed platoon
-function CPlatoon:FormPlatoon()
+function CPlatoon:FormPlatoon(template, count, position, radius)
 end
 
 --- TODO.
