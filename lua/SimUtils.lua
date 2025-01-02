0651ab83b6d80b707b2d29c0c77ffb078dfde0bd
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-01-01 17:26:19 -0800

    debug logging
    Use with replay 23962051 to see drones not being cleaned up

--- lua/SimUtils.lua
+++ lua/SimUtils.lua
@@ -532,11 +532,14 @@ end
 ---@param trackers RebuildTracker[]
 ---@param army Army
 function TryRebuildUnits(trackers, army)
+    LOG(repr(trackers, {depth = 2}), army, debug.traceback())
     local rebuilders = {}
     for k, tracker in ipairs(trackers) do
         if tracker.Success then
+            LOG('skipping rebuilder', k)
             continue
         end
+        LOG('creating rebuilder', k)
         -- create invisible drone which belongs to allied army. BuildRange = 10000
         local rebuilder = CreateUnitHPR('ZXA0001', army, 5, 20, 5, 0, 0, 0)
         rebuilder.TargetBuildTime = tracker.TargetBuildTime
@@ -544,6 +547,8 @@ function TryRebuildUnits(trackers, army)
 
         IssueBuildMobile({ rebuilder }, tracker.UnitPos, tracker.UnitBlueprintID, {})
     end
+
+    LOG('supposedly Created rebuilders')
 
     WaitTicks(3) -- wait some ticks (3 is minimum), IssueBuildMobile() is not instant
 
@@ -553,7 +558,11 @@ function TryRebuildUnits(trackers, army)
         rebuilder:SetConsumptionPerSecondEnergy(0)
     end
 
+    LOG('supposedly rebuilding')
+
     WaitTicks(1)
+
+    LOG('supposedly rebuilt')
 
     for k, rebuilder in ipairs(rebuilders) do
         local tracker = trackers[k]
@@ -562,7 +571,11 @@ function TryRebuildUnits(trackers, army)
         if newUnit and math.abs(progressDif) < 0.001 then
             newUnit:SetHealth(newUnit, tracker.UnitHealth)
             tracker.Success = true
+            LOG('succeeded tracker', k)
+        else
+            LOG('failed tracker', k, 'newUnit', newUnit, 'progressDiff', math.abs(progressDif))
         end
+        LOG('rebuilder destroyed', k)
         rebuilder:Destroy()
     end
 end
