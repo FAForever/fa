965c29e68521dbc1d25cfd58df208ec17246c2ee
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-01-10 03:42:10 -0800

    Implement a fixed version of the rotator script

--- units/UAB4201/UAB4201_script.lua
+++ units/UAB4201/UAB4201_script.lua
@@ -18,7 +18,27 @@ local TrashBagAdd = TrashBag.Add
 ---@class UAB4201 : AStructureUnit
 UAB4201 = ClassUnit(AStructureUnit) {
     Weapons = {
-        AntiMissile = ClassWeapon(AAMWillOWisp) {},
+        ---@class UAB4201_AntiMissile: AAMWillOWisp
+        ---@field RotatorManipulator moho.RotateManipulator
+        ---@field RotatorManipulatorCounter number
+        AntiMissile = ClassWeapon(AAMWillOWisp) {
+            RotatorManipulatorCounter = 1,
+
+            ---@param self UAB4201_AntiMissile
+            OnCreate = function(self)
+                AAMWillOWisp.OnCreate(self)
+                self.RotatorManipulator = TrashBagAdd(self.Trash, CreateRotator(self.unit, 'Dome', 'z', 0, 40, nil, nil))
+            end,
+
+            --- Rotate the dome 45 degrees when the volcano fires.
+            ---@param self UAB4201_AntiMissile
+            PlayFxWeaponUnpackSequence = function(self)
+                local rotations = self.RotatorManipulatorCounter
+                self.RotatorManipulator:SetGoal(45 * rotations)
+                self.RotatorManipulatorCounter = rotations < 8 and rotations + 1 or 0
+                AAMWillOWisp.PlayFxWeaponUnpackSequence(self)
+            end,
+        },
     },
 }
 
