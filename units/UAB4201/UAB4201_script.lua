394ba4cf415b00a4728435a1c394eb5a01b69bf2
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-01-10 03:41:51 -0800

    Remove volcano's rotator script

--- units/UAB4201/UAB4201_script.lua
+++ units/UAB4201/UAB4201_script.lua
@@ -18,39 +18,7 @@ local TrashBagAdd = TrashBag.Add
 ---@class UAB4201 : AStructureUnit
 UAB4201 = ClassUnit(AStructureUnit) {
     Weapons = {
-        ---@class UAB4201_AntiMissile: AAMWillOWisp
-        ---@field RotatorManipulator moho.RotateManipulator
-        ---@field RotatorManipulatorCounter number
-        AntiMissile = ClassWeapon(AAMWillOWisp) {
-            --- Rotate the dome 45 degrees when the volcano fires. Non-functional due to the volcano not having a rack recoild distance.
-            ---@param self UAB4201_AntiMissile
-            ---@param rackList Bone[]
-            PlayRackRecoil = function(self, rackList)
-                AAMWillOWisp.PlayRackRecoil(self, rackList)
-
-                local unit = self.unit
-                local rotatorManipulator = self.RotatorManipulator
-
-                --CreateRotator(unit, bone, axis, [goal], [speed], [accel], [goalspeed])
-                if not rotatorManipulator then
-                    rotatorManipulator = CreateRotator(unit, 'Dome', 'z', 20, 40, 40, 40)
-                    rotatorManipulator:SetGoal(45)
-                    self.RotatorManipulatorCounter = 1
-                else
-                    self.RotatorManipulatorCounter = self.RotatorManipulatorCounter + 1
-                    rotatorManipulator:SetGoal(45 * self.RotatorManipulatorCounter)
-                end
-                TrashBagAdd(unit.Trash, self.RotatorManipulator)
-            end,
-
-            PlayRackRecoilReturn = function(self, rackList)
-                AAMWillOWisp.PlayRackRecoilReturn(self, rackList)
-                if self.RotatorManipulatorCounter == 8 then
-                    self.RotatorManipulator:Destroy()
-                    self.RotatorManipulator = nil
-                end
-            end,
-        },
+        AntiMissile = ClassWeapon(AAMWillOWisp) {},
     },
 }
 
