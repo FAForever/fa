4b7b0d85cffea133fd3a41ce9627dad23714a9b4
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-01-11 22:46:50 -0800

    Make Aeon TMD launch flare to target height

--- lua/sim/weapons/aeon/AAMWillOWisp.lua
+++ lua/sim/weapons/aeon/AAMWillOWisp.lua
@@ -22,8 +22,49 @@
 
 local DefaultProjectileWeapon = import("/lua/sim/defaultweapons.lua").DefaultProjectileWeapon
 local EffectTemplate = import("/lua/effecttemplates.lua")
+local EntityMethods = moho.entity_methods
+local EntityGetPositionXYZ = EntityMethods.GetPositionXYZ
+local EntityGetBoneDirection = EntityMethods.GetBoneDirection
+local GetDistanceBetweenTwoPoints2 = import('/lua/utilities.lua').GetDistanceBetweenTwoPoints2
+
+---@param proj Projectile
+---@param t number
+local function floatProjectile(proj, t)
+    WaitSeconds(t)
+    proj:SetVelocity(0, 0, 0)
+    proj:SetBallisticAcceleration(-4.9)
+end
 
 ---@class AAMWillOWisp : DefaultProjectileWeapon
 AAMWillOWisp = ClassWeapon(DefaultProjectileWeapon) {
     FxMuzzleFlash = EffectTemplate.AAntiMissileFlareFlash,
+
+    --- Launch the flare to the target's height
+    ---@param self AAMWillOWisp
+    ---@param muzzle Bone
+    ---@return Projectile
+    CreateProjectileAtMuzzle = function(self, muzzle)
+        local proj = DefaultProjectileWeapon.CreateProjectileAtMuzzle(self, muzzle)
+
+        local target = self:GetCurrentTarget()
+        if target then
+            local targetX, targetY, targetZ = EntityGetPositionXYZ(target)
+            local targetDX, targetDY, targetDZ = target:GetVelocity()
+
+            local unit = self.unit
+            local posX, posY, posZ = EntityGetPositionXYZ(unit, muzzle)
+            
+            local arriveTime = (GetDistanceBetweenTwoPoints2(targetX, targetZ, posX, posZ) - 10) / (math.sqrt(targetDX * targetDX + targetDZ * targetDZ) * 10)
+
+            local dy = math.max(targetY - posY + targetDY * arriveTime + 9, 17)
+
+            local vy0 = dy / arriveTime * 2
+            local g = -vy0 / arriveTime
+            proj:SetVelocity(0, vy0, 0)
+            proj:SetBallisticAcceleration(g)
+            ForkThread(floatProjectile, proj, arriveTime)
+        end
+
+        return proj
+    end,
 }
