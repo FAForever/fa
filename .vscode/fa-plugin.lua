f4b0e8cddcd57ae62f7f5495264e2d81e8d5c536
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-03-01 05:29:59 -0800

    Add hook file intellisense support

--- .vscode/fa-plugin.lua
+++ .vscode/fa-plugin.lua
@@ -1,3 +1,26 @@
+-- repository to use for hook file intellisense. Same as the one in the dev init file.
+local initPath = 'C:/ProgramData/FAForever/bin/init_local_development.lua'
+
+local initFile, err = io.open(initPath, 'r')
+local locationOfRepository
+if initFile then
+    for line in initFile:lines() do
+        local start, finish, repoPath = string.find(line, 'locationOfRepository = [\'"]([%w:/]+)[\'"]')
+        if repoPath then
+            locationOfRepository = repoPath
+            print('[FA Plugin] Using repository path: ' .. repoPath)
+            break
+        end
+    end
+    if not locationOfRepository then
+        print('[FA Plugin] Could not find repository path in init file:' .. initPath)
+    end
+else
+    print('[FA plugin] Could not open init file: ' .. tostring(initPath)
+        .. '\n[FA plugin] Error message: ' .. tostring(err)
+    )
+end
+
 ---@class diff
 ---@field start  integer # The number of bytes at the beginning of the replacement
 ---@field finish integer # The number of bytes at the end of the replacement
@@ -34,6 +57,21 @@ function OnSetText(uri, text)
             finish = finish - 1,
             text = '{}',
         }
+    end
+
+    if locationOfRepository then
+        -- prepend the content of hooked files just like the game would
+        local first, last, hookDir = uri:find('/hook(/[%w/]+.lua)')
+        if hookDir then
+            local repoFile = io.open(locationOfRepository .. hookDir)
+            if repoFile then
+                diffs[#diffs+1] = {
+                    start = 1,
+                    finish = 1,
+                    text = repoFile:read("*a")
+                }
+            end
+        end
     end
 
     return diffs
