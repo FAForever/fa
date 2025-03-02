c651bb301058e05206efd9bb5a31d760a823d123
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-03-02 13:19:56 -0800

    Upvalue globals in plugin

--- .vscode/fa-plugin.lua
+++ .vscode/fa-plugin.lua
@@ -1,11 +1,17 @@
+local StringFind = string.find
+local StringGmatch = string.gmatch
+local StringMatch = string.match
+
+local IoOpen = io.open
+
 -- repository to use for hook file intellisense. Same as the one in the dev init file.
 local initPath = 'C:/ProgramData/FAForever/bin/init_local_development.lua'
 
-local initFile, err = io.open(initPath, 'r')
+local initFile, err = IoOpen(initPath, 'r')
 local locationOfRepository
 if initFile then
     for line in initFile:lines() do
-        local start, finish, repoPath = string.find(line, 'locationOfRepository = [\'"]([%w:/]+)[\'"]')
+        local start, finish, repoPath = StringFind(line, 'locationOfRepository = [\'"]([%w:/]+)[\'"]')
         if repoPath then
             locationOfRepository = repoPath
             print('[FA Plugin] Using repository path: ' .. repoPath)
@@ -33,7 +39,7 @@ function OnSetText(uri, text)
     local diffs = {}
     -- Change `#` (valid Supcom Lua comment) to `--` (valid in language server's lua)
     -- get first line
-    local pos = text:match('^%s*()#')
+    local pos = StringMatch(text, '^%s*()#')
     if pos ~= nil then
         diffs[#diffs + 1] = {
             start = pos,
@@ -42,7 +48,7 @@ function OnSetText(uri, text)
         }
     end
     -- get any lines after that
-    for pos in text:gmatch '\r\n%s*()#' do
+    for pos in StringGmatch(text, '\r\n%s*()#') do
         diffs[#diffs + 1] = {
             start = pos,
             finish = pos,
@@ -51,7 +57,7 @@ function OnSetText(uri, text)
     end
     -- Change `{&1 &1}` (valid Supcom Lua) to `{}` (valid in language server's lua)
     -- It's changed inside comments too, but lua regex doesn't make that easy to fix.
-    for start, finish in (text):gmatch('(){&%d+ &%d+}()') do
+    for start, finish in StringGmatch(text, '(){&%d+ &%d+}()') do
         diffs[#diffs + 1] = {
             start = start,
             finish = finish - 1,
@@ -61,9 +67,9 @@ function OnSetText(uri, text)
 
     if locationOfRepository then
         -- prepend the content of hooked files just like the game would
-        local first, last, hookDir = uri:find('/hook(/[%w/]+.lua)')
+        local first, last, hookDir = StringFind(uri, '/hook(/[%w/]+.lua)')
         if hookDir then
-            local repoFile = io.open(locationOfRepository .. hookDir)
+            local repoFile = IoOpen(locationOfRepository .. hookDir)
             if repoFile then
                 diffs[#diffs+1] = {
                     start = 1,
