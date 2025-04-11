76b5791909252e4a1c450c959cd5f0592d94acbd
Basilisk3 <126026384+Basilisk3@users.noreply.github.com>
2025-04-11 12:35:09 +0200

    Update the changes

--- lua/system/blueprints-units.lua
+++ lua/system/blueprints-units.lua
@@ -328,7 +328,7 @@ local function PostProcessUnit(unit)
         end
     end
 
-    -- Check if a unit's WaterVisionRadius is larger than its SonarRadius
+    -- Check if a unit's SonarRadius is lower than both its WaterVisionRadius and its RadarRadius
     if unit.Intel.WaterVisionRadius and unit.Intel.SonarRadius and unit.Intel.RadarRadius then
         if (unit.Intel.WaterVisionRadius > unit.Intel.SonarRadius) and (unit.Intel.RadarRadius > unit.Intel.SonarRadius) then
             WARN(string.format("SonarRadius of unit ( %s ), is lower than both its WaterVisionRadius and its RadarRadius, it needs to be higher to be relevant!"
