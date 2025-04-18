9a3608534fc9a2cc41d77bc66832d42ff18b2c71
Dmitry Kovalev <kov.dmm@gmail.com>
2025-04-18 07:14:06 +0500

    update patch version

--- lua/version.lua
+++ lua/version.lua
@@ -1,19 +1,12 @@
 local GameType = "GAF"
-
 local Commit = "GAF Balance"
+local Version = "1.5"
 
-local Version = "2"
----@alias PATCH "2"
----@alias VERSION "2"
----@return PATCH
 function GetVersion()
     LOG(string.format('Supreme Commander: Forged Alliance Lua version %s at %s (%s)', Version, GameType, Commit))
     return Version
 end
 
----@return PATCH
----@return string # game type
----@return string # commit hash
 function GetVersionData()
     return Version, GameType, Commit
 end
