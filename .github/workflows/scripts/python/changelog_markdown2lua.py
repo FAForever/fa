735a0b51d4b9b5201f90f08c9e87746e6f7f9c46
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2025-05-01 14:22:51 +0200

    Fix a missmatch with the formatting of a string in the conversion script

--- .github/workflows/scripts/python/changelog_markdown2lua.py
+++ .github/workflows/scripts/python/changelog_markdown2lua.py
@@ -17,9 +17,9 @@ MAX_LINE_LENGTH = 150
 
 LUA_DESC_LINE = '        "{line}"'
 LUA_FILE = """Changelog = {{
-    version = {version},
+    version = {Version},
     description = {{
-{description}
+{Description}
     }}
 }}
 """
