75d2c88691db14e3502831661ccb597ee25bf583
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-05-07 23:42:14 -0700

    Fix hidden selection restarting command mode
    Instead of restarting it, it should be set to ignore selection so that command mode doesn't end when the selection changes

--- lua/ui/game/selection.lua
+++ lua/ui/game/selection.lua
@@ -46,13 +46,15 @@ end
 
 function Hidden(callback)
     local CM = import("/lua/ui/game/commandmode.lua")
-    local current_command = CM.GetCommandMode()
-    local old_selection = GetSelectedUnits() or {}
 
+    local old_selection = GetSelectedUnits() or {}
     hidden_select = true
+    CM.SetIgnoreSelection(true)
+
     callback()
+
     SelectUnits(old_selection)
-    CM.StartCommandMode(current_command[1], current_command[2])
+    CM.SetIgnoreSelection(false)
     hidden_select = false
 end
 
