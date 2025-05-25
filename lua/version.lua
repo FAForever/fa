543847c3b8641a7831244f34d6f5bb0e7f9e3e85
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2025-05-25 11:03:29 +0200

    Update version in Version.lua

--- lua/version.lua
+++ lua/version.lua
@@ -34,9 +34,9 @@ local Commit = 'unknown'    -- The use of `'` instead of `"` is **intentional**
 
 --#endregion
 
-local Version = "3822"
----@alias PATCH "3822"
----@alias VERSION "1.5.3822"
+local Version = "3823"
+---@alias PATCH "3823"
+---@alias VERSION "1.5.3823"
 ---@return PATCH    # Game release
 function GetVersion()
     LOG(string.format('Supreme Commander: Forged Alliance Lua version %s at %s (%s)', Version, GameType, Commit))
