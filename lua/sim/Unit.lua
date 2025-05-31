34c7bcbe251d549c61300624be2bb8501b70e2d3
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-05-31 01:50:53 -0700

    Improve annotations related to Buffs #6837
    Annotate unknown fields of unit class used in buffs
    Fix Unit.Buffs table annotation
    
    add `OnApplyBuff` field to BuffBlueprint
    
    Fix "undefined global" warnings, add regions
    
    Fix incorrect returns
    
    Fix undefined `Count` field in buff apply/remove
    
    Fix OnBuffRemove function param
    
    Annotate buff effect functions
    
    Fix BuffRegenFieldCalculate initialBool param type
    
    Remove instigator from setstunned call

--- lua/sim/Unit.lua
+++ lua/sim/Unit.lua
@@ -135,7 +135,7 @@ local cUnitGetBuildRate = cUnit.GetBuildRate
 ---@field UnitId UnitId
 ---@field EntityId EntityId
 ---@field EventCallbacks table<string, function[]>
----@field Buffs {Affects: table<BuffEffectName, BlueprintBuff.Effect>, buffTable: table<string, table>}
+---@field Buffs { Affects: table<BuffAffectName, table<BuffName, BlueprintBuffAffectState> >, BuffTable: table<string, table> }
 ---@field EngineFlags? table<string, any>
 ---@field TerrainType TerrainType
 ---@field EngineCommandCap? table<string, boolean>
@@ -167,6 +167,9 @@ local cUnitGetBuildRate = cUnit.GetBuildRate
 ---@field TargetUpgradeBuildTime? number # Keeps track of upgrades *during* unit transfer
 ---@field TargetFractionComplete? number # When an unbuilt unit is rebuilt during unit transfer, this overrides what fraction it is rebuilt to.
 ---@field isFinishedUnit? boolean # set to true in `OnStopBeingBuilt`
+---@field ImmuneToStun? boolean
+---@field Anims? Animator[] # Animators that get stopped when a unit is stunned. Not used in FAF.
+---@field IsBeingTransferred? boolean
 Unit = ClassUnit(moho.unit_methods, IntelComponent, VeterancyComponent, DebugUnitComponent) {
 
     IsUnit = true,
