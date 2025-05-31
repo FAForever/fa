a1b013d94d5402ffaa831feb63c35050aa739611
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2025-05-31 21:05:28 +0200

    Revert "Move the check for any remaining brains to the end"
    This reverts commit eb85819310f453d7db216dc63c5f2db76fb9cdc7.

--- lua/sim/VictoryCondition/UnitCondition.lua
+++ lua/sim/VictoryCondition/UnitCondition.lua
@@ -56,6 +56,17 @@ UnitCondition = Class(AbstractVictoryCondition) {
             end
         end
 
+        -- no remaining players, just end the game
+        if table.empty(aliveBrains) then
+            if self.EnabledSpewing then
+                SPEW("All players are defeated, game will end")
+            end
+
+            self:EndGame()
+            return
+        end
+
+
         -- process all defeated brains
         for k = 1, TableGetn(defeatedBrains) do
             local defeatedBrain = defeatedBrains[k]
@@ -86,16 +97,6 @@ UnitCondition = Class(AbstractVictoryCondition) {
             for k = 1, TableGetn(aliveBrains) do
                 local aliveBrain = aliveBrains[k]
                 self:VictoryForArmy(aliveBrain)
-            end
-
-            self:EndGame()
-            return
-        end
-
-        -- no remaining players, just end the game
-        if table.empty(aliveBrains) then
-            if self.EnabledSpewing then
-                SPEW("All players are defeated, game will end")
             end
 
             self:EndGame()
