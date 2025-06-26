70c54879b1259c2590a81996a22717e942516cf3
FAForever Machine User <github@faforever.com>
2025-06-26 21:16:26 +0000

    Post-process deployment

--- lua/version.lua
+++ lua/version.lua
@@ -28,9 +28,9 @@
 -- - https://github.com/FAForever/fa/blob/develop/.github/workflows/deploy-fafbeta.yaml
 -- - https://github.com/FAForever/fa/blob/develop/.github/workflows/deploy-fafdevelop.yaml
 
-local GameType = 'unknown'  -- The use of `'` instead of `"` is **intentional**
+local GameType = "FAF Develop"  -- The use of `'` instead of `"` is **intentional**
 
-local Commit = 'unknown'    -- The use of `'` instead of `"` is **intentional**
+local Commit = "1bd0223b69b2f9af22d3ebd89471e72bb11108cc"    -- The use of `'` instead of `"` is **intentional**
 
 --#endregion
 
