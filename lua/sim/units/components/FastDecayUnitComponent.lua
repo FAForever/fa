372a006f3314bcdf7a254e7f2cb0219c68cac01d
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-07-16 18:43:01 -0700

    Refactor variables

--- lua/sim/units/components/FastDecayUnitComponent.lua
+++ lua/sim/units/components/FastDecayUnitComponent.lua
@@ -7,29 +7,29 @@ local TrashAdd = TrashBag.Add
 FastDecayComponent = ClassSimple {
     --- Call this at the end of your OnCreate function.
     ---@param self FastDecayComponent
-    ---@param decayPercentPerTick? number # %HP lost per tick when unit is decaying quickly. Defaults to wall decay rate.
-    ---@param decayCompletionThreshold? number # Below this % completion, units decay quickly. Defaults to 200 resources invested.
-    StartFastDecayThread = function(self, decayPercentPerTick, decayCompletionThreshold)
-        TrashAdd(self.Trash, ForkThread(self.FastDecayThread, self, decayPercentPerTick, decayCompletionThreshold))
+    ---@param decayPerTick? number # HP fraction lost per tick when unit is decaying quickly. Defaults to wall decay rate.
+    ---@param completionThreshold? number # Below this fraction completion, units decay quickly. Defaults to 200 resources invested.
+    StartFastDecayThread = function(self, decayPerTick, completionThreshold)
+        TrashAdd(self.Trash, ForkThread(self.FastDecayThread, self, decayPerTick, completionThreshold))
     end,
 
     --- While the unit is at low completion and not actively being built, rapidly reduces its HP.
     --- Starting construction on the unit restores decayed HP.
     ---@param self FastDecayComponent
-    ---@param decayPercentPerTick? number # %HP lost per tick when unit is decaying quickly.
-    ---@param decayCompletionThreshold? number # Below this % completion, units decay quickly.
-    FastDecayThread = function(self, decayPercentPerTick, decayCompletionThreshold)
+    ---@param decayPerTick? number # HP fraction lost per tick when unit is decaying quickly.
+    ---@param completionThreshold? number # Below this fraction completion, units decay quickly.
+    FastDecayThread = function(self, decayPerTick, completionThreshold)
         local maxHealth = self:GetMaxHealth()
         local bpEcon = self.Blueprint.Economy
         local highestEcoStat = math.max(bpEcon.BuildCostEnergy, bpEcon.BuildCostMass, bpEcon.BuildTime)
 
         -- Defaults to decaying as fast as walls naturally decay (their highest cost is 20 energy)
-        local healthPercentDecayPerTick = decayPercentPerTick or (0.1 / 20)
+        decayPerTick = decayPerTick or (0.1 / 20)
         -- Compensate for engine's decay of 0.1 resource/tick of the highest cost resource
         local naturalPercentDecayPerTick = 0.1 / highestEcoStat
-        local decayPerTick = -maxHealth * (healthPercentDecayPerTick - naturalPercentDecayPerTick)
+        decayPerTick = -maxHealth * (decayPerTick - naturalPercentDecayPerTick)
         -- Defaults to a limit of 200 resources invested
-        local completionThreshold = decayCompletionThreshold or (200 / highestEcoStat)
+        completionThreshold = completionThreshold or (200 / highestEcoStat)
 
         if decayPerTick < 0 then
             local decayedHp = 0
