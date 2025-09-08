25d7a11bfd5f3f25ca2a535727002499e1c3d099
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-09-07 18:51:33 -0700

    Use generics for table.find

--- lua/system/utils.lua
+++ lua/system/utils.lua
@@ -106,9 +106,10 @@ end
 
 --- Returns the key for `val` if it is in table `t`.
 --- Otherwise, returns nil.
----@param t? table
----@param val any
----@return any keyOrNil
+---@generic K, V
+---@param t? table<K, V>
+---@param val V
+---@return K? keyOrNil
 function table.find(t,val)
     if not t then return end -- prevents looping over nil table
     for k,v in t do
