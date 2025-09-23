b390264cdca3d000bbd1eae3b5b09463f7d408e7
Dhomie <medic.guy12@gmail.com>
2025-09-23 16:32:48 +0200

    Undo unintended DebugComponent.lua changes

--- lua/shared/components/DebugComponent.lua
+++ lua/shared/components/DebugComponent.lua
@@ -35,7 +35,7 @@ DebugComponent = ClassSimple {
     EnabledLogging = true,
     EnabledWarnings = true,
     EnabledErrors = true,
-    EnabledDrawing = false,
+    EnabledDrawing = true,
 
     --#endregion
 }
