49f5f23a79421a0d7eecb0ca70866c33565ae4f8
(Jip) Willem Wijnia <w.b.wijnia@gmail.com>
2025-10-11 11:43:26 +0200

    Add lobby option to toggle overflow (#6938)

--- lua/ui/lobby/autolobby/AutolobbyController.lua
+++ lua/ui/lobby/autolobby/AutolobbyController.lua
@@ -212,6 +212,7 @@ AutolobbyCommunications = Class(MohoLobbyMethods, AutolobbyServerCommunicationsC
             DisconnectionDelay02 = '90',
             DisconnectShare = 'SameAsShare',
             DisconnectShareCommanders = 'Explode',
+            TeamShareOverflow = "enabled",
 
             -- yep, great
             Ranked = true,
