b07168577e256ba5611a83a609b79dfd0c5cc63f
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2025-11-07 09:21:33 -0800

    Add comments explaining cloak mesh

--- units/XRL0302/XRL0302_Script.lua
+++ units/XRL0302/XRL0302_Script.lua
@@ -175,8 +175,10 @@ XRL0302 = ClassUnit(CWalkingLandUnit) {
         self:SetMaintenanceConsumptionActive()
     end,
 
+    --- Sets cloak mesh on a delay because `Unit.StopBeingBuiltEffects` sets the normal mesh regardless of cloak
     ---@param self XRL0302
     HideUnit = function(self)
+        -- Wait until `StopBeingBuiltEffects` is done
         WaitTicks(1)
         self:SetMesh(self.Blueprint.Display.CloakMeshBlueprint, true)
     end,
