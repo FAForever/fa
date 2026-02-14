c853c998282eccbf76ac8e4a5d2f766bc9474a75
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2026-02-14 11:25:11 -0800

    Fix manual overcharge not working

--- lua/sim/weapons/DefaultProjectileWeapon.lua
+++ lua/sim/weapons/DefaultProjectileWeapon.lua
@@ -853,7 +853,7 @@ DefaultProjectileWeapon = ClassWeapon(Weapon) {
             -- they need a RackSalvoReloadTime that's 1/RateOfFire set to avoid firing twice on the first shot
             local unit = self.unit
             local bp = self.Blueprint
-            if bp.ManualFire and not bp.OverchargeWeapon then
+            if bp.ManualFire and not bp.OverChargeWeapon then
                 unit:SetBusy(true)
             else
                 unit:SetBusy(false)
