f1a7ba7e8755478eb70a081d09c53cb8ed1721f1
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2026-06-22 14:34:51 -0700

    Implement ForceAffinity client option

--- init.lua
+++ init.lua
@@ -43,32 +43,37 @@ if SetProcessPriority and GetProcessAffinityMask and SetProcessAffinityMask then
         LOG("Process - Failed to adjust process priority, this may impact your framerate")
     end
 
-    -- affinity values acts like a bit mask, we retrieve the mask and shift it if we think there are sufficient computing units
-    local success, processAffinityMask, systemAffinityMask = GetProcessAffinityMask();
-    if success then
-        -- system has 24 (logical) computing units or more, skip the first two computing units and all cores beyond the first 24. We need 
-        -- to do this because of floating point imprecision - we simply can't deduct a few digits to prevent using the first two cores
-        if systemAffinityMask >= 16777215 then
-            processAffinityMask = 16777212 -- 2 ^ 24 - 3 - 1
-
-        -- system has 6 (logical) computing units or more, skip first two computing units
-        elseif (systemAffinityMask >= 63) then
-            processAffinityMask = systemAffinityMask - 3 -- (2 ^ 6 - 1) - 3
-        end
-
-        -- update the afinity mask
-        if processAffinityMask != systemAffinityMask then
-            local success = SetProcessAffinityMask(processAffinityMask);
-            if success then
-                LOG("Process - affinity set to: " .. tostring(processAffinityMask))
+    local forceAffinity = ForceAffinity == "true"
+    if forceAffinity then
+        -- affinity values acts like a bit mask, we retrieve the mask and shift it if we think there are sufficient computing units
+        local success, processAffinityMask, systemAffinityMask = GetProcessAffinityMask();
+        if success then
+            -- system has 24 (logical) computing units or more, skip the first two computing units and all cores beyond the first 24. We need 
+            -- to do this because of floating point imprecision - we simply can't deduct a few digits to prevent using the first two cores
+            if systemAffinityMask >= 16777215 then
+                processAffinityMask = 16777212 -- 2 ^ 24 - 3 - 1
+    
+            -- system has 6 (logical) computing units or more, skip first two computing units
+            elseif (systemAffinityMask >= 63) then
+                processAffinityMask = systemAffinityMask - 3 -- (2 ^ 6 - 1) - 3
+            end
+    
+            -- update the afinity mask
+            if processAffinityMask != systemAffinityMask then
+                local success = SetProcessAffinityMask(processAffinityMask);
+                if success then
+                    LOG("Process - affinity set to: " .. tostring(processAffinityMask))
+                else
+                    LOG("Process - Failed to adjust the process affinity, this may impact your framerate")
+                end
             else
-                LOG("Process - Failed to adjust the process affinity, this may impact your framerate")
+                LOG("Process - Failed to update the process affinity, this may impact your framerate")
             end
         else
-            LOG("Process - Failed to update the process affinity, this may impact your framerate")
+            LOG("Process - Failed to retrieve the process affinity, this may impact your framerate")
         end
     else
-        LOG("Process - Failed to retrieve the process affinity, this may impact your framerate")
+        LOG("Process - Process affinity adjustment is disabled in client settings, this may impact your framerate")
     end
 else
     LOG("Process - Failed to find process priority and affinity related functions, this may impact your framerate")
