8b5f32bd8312e0fe54de700dd2efc12fd03a714c
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2026-08-03 00:27:47 -0700

    calculate army indices properly

--- lua/ui/lobby/autolobby/AutolobbyController.lua
+++ lua/ui/lobby/autolobby/AutolobbyController.lua
@@ -624,11 +624,19 @@ AutolobbyCommunications = Class(MohoLobbyMethods, AutolobbyServerCommunicationsC
         -- put the player where it belongs
         self.PlayerOptions[playerOptions.StartSpot] = playerOptions
 
-        -- send all player options to the server, this is necessary to do immediately (for the host) so that the server knows who is (not) connected. 
-        for slot, otherPlayerOptions in self.PlayerOptions do
+        -- Army numbers need to be calculated: they are numbered incrementally in slot order.
+        local slots = {}
+        for slotIndex, _ in pairs(self.PlayerOptions) do
+            table.insert(slots, slotIndex)
+        end
+        table.sort(slots)
+
+        -- send player options to the server
+        for armyIndex, slotIndex in ipairs(slots) do
+            local otherPlayerOptions = self.PlayerOptions[slotIndex]
             local ownerId = otherPlayerOptions.OwnerID
             self:SendPlayerOptionToServer(ownerId, 'Team', otherPlayerOptions.Team)
-            self:SendPlayerOptionToServer(ownerId, 'Army', otherPlayerOptions.StartSpot)
+            self:SendPlayerOptionToServer(ownerId, 'Army', armyIndex)
             self:SendPlayerOptionToServer(ownerId, 'StartSpot', otherPlayerOptions.StartSpot)
             self:SendPlayerOptionToServer(ownerId, 'Faction', otherPlayerOptions.Faction)
         end
