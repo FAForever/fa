952358d3dafd0ef48353599bdc52f4288c6553d3
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2026-08-09 15:24:05 -0700

    Add FilePicker overload

--- lua/ui/controls/filepicker.lua
+++ lua/ui/controls/filepicker.lua
@@ -142,6 +142,7 @@ function IsFilenameInvalid(filename)
 end
 
 ---@class FilePicker : Group
+---@overload fun(parent: Control, fileType: SpecialFileType, onlyShowMine: any, selectAction: any, debugName: any): FilePicker
 FilePicker = ClassUI(Group) {
     ---@param self FilePicker
     ---@param parent Control
