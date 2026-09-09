478a26c4e903f63aa3eb9f85b5e552ee980b2622
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2026-09-09 00:55:41 -0700

    Test setup for using scaled econ overlay

--- lua/ui/game/econoverlayparams.lua
+++ lua/ui/game/econoverlayparams.lua
@@ -1,12 +1,26 @@
+local Prefs = import('/lua/user/prefs.lua')
+local scale = 2
+local scaleSuffix = {
+    [0.8] = '_0.8x',
+    [1] = '',
+    [1.25] = '_1.25x',
+    [1.5] = '_1.5x',
+    [1.75] = '_1.75x',
+    [2] = '_2.0x',
+}
+local suffix = scaleSuffix[scale or 1] or ''
+local function ScaleNumber(number)
+    return math.floor(number * scale)
+end
 
 EconOverlayParams = {
     positiveColor = "FF00D000",
     negativeColor = "red",
-    leftTexture = import("/lua/ui/uiutil.lua").UIFile("/game/economic-overlay/econ_bmp_l.dds"),
-    midTexture = import("/lua/ui/uiutil.lua").UIFile("/game/economic-overlay/econ_bmp_m.dds"),
-    rightTexture = import("/lua/ui/uiutil.lua").UIFile("/game/economic-overlay/econ_bmp_r.dds"),
+    leftTexture = import("/lua/ui/uiutil.lua").UIFile("/game/economic-overlay/econ_bmp_l" .. suffix .. ".dds"),
+    midTexture = import("/lua/ui/uiutil.lua").UIFile("/game/economic-overlay/econ_bmp_m" .. suffix .. ".dds"),
+    rightTexture = import("/lua/ui/uiutil.lua").UIFile("/game/economic-overlay/econ_bmp_r" .. suffix .. ".dds"),
     fontName = "Ariel",
-    fontSize = 9,
-    energyTopOffset = 13.0,
-    massTopOffset = 1.0,
+    fontSize = ScaleNumber(9),
+    energyTopOffset = ScaleNumber(13.0),
+    massTopOffset = ScaleNumber(1.0),
 }
