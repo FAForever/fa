ca379799fb83eeb53d84d5b3e3d851c1c3e6e025
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2026-09-14 12:25:09 -0700

    Note engine call of GetConstructEconomyModel

--- lua/game.lua
+++ lua/game.lua
@@ -41,6 +41,8 @@ VeteranDefault = {
 ---
 --- In order to keep backwards compatibility, there is a new option in the blueprint economy section.
 --- if DifferentialUpgradeCostCalculation is set to true, the base upgrade cost will be subtracted
+--- 
+--- Called by the engine when rendering command graph meshes for some reason.
 ---@param builder Unit
 ---@param targetData UnitBlueprintEconomy
 ---@param upgradeBaseData UnitBlueprintEconomy
