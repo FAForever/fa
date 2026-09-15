1d176277722753fe132c490dee222ef5e5e24c97
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2026-09-15 06:27:19 -0700

    Update CAiNavigatorImpl.lua

--- engine/Sim/CAiNavigatorImpl.lua
+++ engine/Sim/CAiNavigatorImpl.lua
@@ -11,7 +11,7 @@ end
 function CNavigator:AtGoal()
 end
 
----Broadcast event to resume any listening task that is currently suspended
+--- Broadcast event to resume any listening task that is currently suspended
 function CNavigator:BroadcastResumeTaskEvent()
 end
 
@@ -23,12 +23,12 @@ end
 function CNavigator:FollowingLeader()
 end
 
----This returns the current navigator target position for the unit
+--- This returns the current navigator target position for the unit
 ---@return Vector?
 function CNavigator:GetCurrentTargetPos()
 end
 
----This returns the current goal position of our navigator
+--- This returns the current goal position of our navigator
 ---@return Vector #Its not verified if nil can be returned here.
 function CNavigator:GetGoalPos()
 end
@@ -49,18 +49,18 @@ end
 function CNavigator:IsIgnorningFormation()
 end
 
----Set the navigator's destination as another unit (chase/follow)
+--- Set the navigator's destination as another unit (chase/follow)
 ---@param target Unit
 function CNavigator:SetDestUnit(target)
 end
 
----Set the navigator's destination as a particular position
+--- Set the navigator's destination as a particular position
 ---@param goal Vector
 function CNavigator:SetGoal(goal)
 end
 
----Set flag in navigator so the unit will know whether to stop at final goal:or speed through it.
----This would be set to True during a patrol or a series:of waypoints in a complex path.
+--- Set flag in navigator so the unit will know whether to stop at final goal or speed through it.
+--- This would be set to `true` during a patrol or a series of waypoints in a complex path.
 ---@param flag boolean
 function CNavigator:SetSpeedThroughGoal(flag)
 end
