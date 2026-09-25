ced9d8519d7937fba81e7d9e466de50208b8d335
lL1l1 <82986251+lL1l1@users.noreply.github.com>
2026-09-24 19:30:52 -0700

    Update table loop benchmarks

--- lua/benchmarks/table-loops.lua
+++ lua/benchmarks/table-loops.lua
@@ -1,14 +1,25 @@
-
--- ForGetn:         259 ms
--- ForPairs:        372 ms
--- Foreach:         370 ms
--- ForiPairs:       405 ms
--- WhileGetNCached: 293 ms
--- WhileGetn:       8140 ms
-
--- Whenever table.insert is used for a table then you can use the cheaper
--- looping structure. However, when typical hashes are involved this cheaper
--- version will not reach all the elements of the table.
+-- |-------------------------------|---------|--------|---------|
+-- | Name                          | Mean ms | 3x Dev | Hashes? |
+-- |-------------------------------|---------|--------|---------|
+-- | "For table.getn upvalued"     |   8.02  | 0.22   |    N    |
+-- | "For table.getn"              |   8.06  | 0.36   |    N    |
+-- | "While cache table.getn"      |   8.43  | 0.26   |    N    |
+-- | "For ipairs upvalued"         | 146.    | 4.0    |    N    |
+-- | "For ipairs"                  | 146.    | 2.3    |    N    |
+-- | "While table.getn"            | 162.    | 7.1    |    N    |
+-- |-------------------------------|---------|--------|---------|
+-- | "For each with next upvalued" | 231.    | 4.6    |    Y    |
+-- | "For each"                    | 232.    | 5.1    |    Y    |
+-- | "For next upvalued"           | 232.    | 5.8    |    Y    |
+-- | "For next"                    | 233.    | 4.2    |    Y    |
+-- | "For pairs upvalued"          | 238.    | 5.2    |    Y    |
+-- | "For pairs"                   | 238.    | 4.5    |    Y    |
+-- |-------------------------------|---------|--------|---------|
+--
+-- Conclusions: 
+-- - Use `for i = 1, TableGetN({})` for arrays 
+--   - Don't use `table.insert` when traversing arrays
+-- - Use `for k, v in {}` for tables
 
 ModuleName = "Table Loops"
 BenchmarkData = {
@@ -18,6 +29,12 @@ BenchmarkData = {
     Foreach = "For each",
     WhileGetn = "While table.getn",
     WhileGetnCached = "While cache table.getn",
+    ForGetnUpval = "For table.getn upvalued",
+    ForPairsUpval = "For pairs upvalued",
+    ForNext = "For next",
+    ForNextUpval = "For next upvalued",
+    ForEachUpval = "For each with next upvalued",
+    ForiPairsUpval = "For ipairs upvalued",
 }
 
 -- prepare data
@@ -39,11 +56,38 @@ function ForGetn(loop)
     local final = timer()
     return final - start
 end
+function ForGetnUpval(loop)
+    local timer = GetSystemTimeSecondsOnlyForProfileUse
+    local start = timer()
+
+    local TableGetN = table.getn
+    for _ = 1, loop do
+        for k = 1, TableGetN(data) do
+        end
+    end
+
+    local final = timer()
+    return final - start
+end
 
 function ForPairs(loop)
     local timer = GetSystemTimeSecondsOnlyForProfileUse
     local start = timer()
 
+    for _ = 1, loop do
+        for k, v in pairs(data) do
+        end
+    end
+
+    local final = timer()
+    return final - start
+end
+
+function ForPairsUpval(loop)
+    local timer = GetSystemTimeSecondsOnlyForProfileUse
+    local start = timer()
+
+    local pairs = pairs
     for _ = 1, loop do
         for k, v in pairs(data) do
         end
@@ -66,12 +110,72 @@ function ForiPairs(loop)
     return final - start
 end
 
+function ForiPairsUpval(loop)
+    local timer = GetSystemTimeSecondsOnlyForProfileUse
+    local start = timer()
+
+    local ipairs = ipairs
+    for _ = 1, loop do
+        for k, v in ipairs(data) do
+        end
+    end
+
+    local final = timer()
+    return final - start
+end
+
 function Foreach(loop)
     local timer = GetSystemTimeSecondsOnlyForProfileUse
     local start = timer()
 
+    -- engine imports global `next`
     for _ = 1, loop do
         for k, v in data do
+        end
+    end
+
+    local final = GetSystemTimeSecondsOnlyForProfileUse()
+
+    return final - start
+end
+
+function ForEachUpval(loop)
+    local timer = GetSystemTimeSecondsOnlyForProfileUse
+    local start = timer()
+
+    -- engine imports global `next`
+    local next = next
+    for _ = 1, loop do
+        for k, v in data do
+        end
+    end
+
+    local final = GetSystemTimeSecondsOnlyForProfileUse()
+
+    return final - start
+end
+
+function ForNext(loop)
+    local timer = GetSystemTimeSecondsOnlyForProfileUse
+    local start = timer()
+
+    for _ = 1, loop do
+        for k, v in next, data do
+        end
+    end
+
+    local final = GetSystemTimeSecondsOnlyForProfileUse()
+
+    return final - start
+end
+
+function ForNextUpval(loop)
+    local timer = GetSystemTimeSecondsOnlyForProfileUse
+    local start = timer()
+
+    local next = next
+    for _ = 1, loop do
+        for k, v in next, data do
         end
     end
 
